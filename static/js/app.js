bc1901832983b5b03aed55a4fa2f73950d724d9a
Mike Morici <Michael.Morici@cfpb.gov>
2014-05-30 18:31:14 -0400

    Moves chosen init to app.js.

---
+++ static/js/app.js
@@ -0,0 +1,9 @@
+/* ==========================================================================
+   Initialize Chosen.js
+   ========================================================================== */
+
+$(".chosen-select").chosen({
+    width: '100%',
+    disable_search_threshold: 10,
+    no_results_text: "Oops, nothing found!"
+});
