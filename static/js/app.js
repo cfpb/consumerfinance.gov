55cf950eceeae87727f4574254ed771984990d2b
Mike Morici <Michael.Morici@cfpb.gov>
2014-05-30 14:58:45 -0400

    Remove old demo js.

--- static/js/app.js
+++
@@ -1,9 +0,0 @@
-// Count all features included in the test page.
-$('.feature-list').append(
-  '<li class="feature-list_item">' +
-  '<h3 class="feature-list_header">jQuery</h3>' +
-  '<p>jQuery counts a total of ' +
-  '<strong>' + ( $('.feature-list li').size() + 1 ) + '</strong> ' +
-  'features.</p>' +
-  '</li>'
-);
