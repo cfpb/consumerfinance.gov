43a465791cb88f2c47e6a380132c74055d9e9cbc
Mike Morici <Michael.Morici@cfpb.gov>
2014-06-02 19:07:43 -0400

    Adds script to reset chosen.js elements with rese button.

--- static/js/app.js
+++ static/js/app.js
@@ -6,3 +6,11 @@ $(".chosen-select").chosen({
     width: '100%',
     no_results_text: "Oops, nothing found!"
 });
+
+// Reset buttons sould also reset Chosen.js elements
+$('input[type="reset"]').on('click', function() {
+    var $this = $(this),
+        $form = $this.parents('form'),
+        $chosenSelects = $form.find('.chosen-select');
+        $chosenSelects.val('').trigger("chosen:updated");
+});
