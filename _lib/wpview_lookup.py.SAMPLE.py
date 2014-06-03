e11250d4b23097b2ec7b1e81e50a970e8a6f6b2f
Mike Morici <Michael.Morici@cfpb.gov>
2014-06-03 15:31:52 -0400

    Adds pop stories.

--- _lib/wpview_lookup.py.SAMPLE.py
+++ _lib/wpview_lookup.py.SAMPLE.py
@@ -6,15 +6,25 @@ import requests
 
 class WPView(object):
     def __init__(self, data):
-        self.data = data
+        self.data = data['post']
 
     @property
     def hero(self):
-        hero_id = self.data['post']['custom_fields']['related_hero'][0]
+        hero_id = self.data['custom_fields']['related_hero'][0]
         hero_url = "http://your.wordpress.domain/api/get_post/?post_id=" + hero_id + "&post_type=cfpb_hero"
         response = requests.get(hero_url)
         hero_data = json.loads(response.text)
         return hero_data['post']
+
+    @property
+    def related_links(self):
+        custom_fields = self.data['custom_fields']
+        related =[]
+        for x in xrange(0,4):
+            key = 'related_link_%s' % x
+            if key in custom_fields:
+                related.append(custom_fields[key])
+        return related
 
 def do_lookup():
     request = flask.request    
