3ab3a8456ede8cbc0d9ec9f40515df1cb9ed0797
Mike Morici <Michael.Morici@cfpb.gov>
2014-06-16 12:59:55 -0400

    Fixes formatting for getQueryVariable function.

--- static/js/app.js
+++ static/js/app.js
@@ -88,11 +88,13 @@ function getQueryVariable(variable /*, queryString*/)
         query = window.location.search.substring(1);
     }
 
-    vars = query.split("&");
+    vars = query.split('&');
 
-    for (var i=0;i<vars.length;i++) {
-        var pair = vars[i].split("=");
-        if(pair[0] == variable){return pair[1];}
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (pair[0] == variable) {
+            return pair[1];
+        }
     }
 
     return(false);
