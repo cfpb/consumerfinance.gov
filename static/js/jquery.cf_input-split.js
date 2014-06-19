fff97de9a2118aa114a5e618b8b1e7e73c894c80
Mike Morici <Michael.Morici@cfpb.gov>
2014-06-19 15:40:44 -0400

    Allows for .cf_inputSplit to be called on an input or an inputs ancestor.

--- static/js/jquery.cf_input-split.js
+++ static/js/jquery.cf_input-split.js
@@ -3,14 +3,21 @@
     $.fn.cf_inputSplit = function(replacement, inputs, delimeter) {
         return $(this).each(function() {
             var inputSplit = {
-                $singleInput: $(this),
+                $oldHTML: $(this),
                 $replacementHTML: $(replacement),
+                $oldInput: $(),
                 replacementInputs: [],
                 delimeter: delimeter,
+                formElementSelectors: 'input, select, textarea',
 
                 init: function() {
+                    // This plugin can be called on an input or an ancestor of
+                    // an input. This means we need to do a little work to
+                    // figure out what the original form input is.
+                    this.$oldInput = this.getFormElements(this.$oldHTML);
+
                     // Hide the old input and show the new ones
-                    this.$singleInput.hide();
+                    this.$oldInput.hide();
                     this.$replacementHTML.show();
 
                     for (var i = 0; i < inputs.length; i++) {
@@ -18,10 +25,6 @@
                         // it in a new array.
                         var $input = $(inputs[i]);
                         this.replacementInputs[i] = $input;
-
-                        // Remove the name attributes from each replacement input
-                        // to prevent their values from affecting the query string.
-                        $input.removeAttr('name');
 
                         // 3. Set up an event listener so we can update the value
                         // of the original input when the new inputs change.
@@ -34,7 +37,7 @@
                 },
 
                 updateOldInputVal: function() {
-                    this.$singleInput.val(this.getVals());
+                    this.$oldInput.val(this.getVals());
                 },
 
                 getVals: function() {
@@ -54,6 +57,16 @@
                         return vals.join(this.delimeter);
                     } else {
                         return vals[0];
+                    }
+                },
+
+                getFormElements: function($html) {
+                    // If $html is a form element then return it.
+                    // If it's not then return any form elements inside of it.
+                    if ($html.is(this.formElementSelectors)) {
+                        return $html;
+                    } else {
+                        return $html.find(this.formElementSelectors);
                     }
                 }
             };
