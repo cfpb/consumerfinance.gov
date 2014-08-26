490cecf6c09afc8775c7b8bdffbe1e812105b1a9
Mike Morici <Michael.Morici@cfpb.gov>
2014-08-26 16:05:49 -0400

    Fixes indexing of empty views.

--- _lib/wordpress_view_processor.py
+++ _lib/wordpress_view_processor.py
@@ -33,8 +33,9 @@ def process_view(post):
     custom_fields = post['custom_fields']
 
     # limit popular posts to five items
-    popular_posts = [slug for slug in custom_fields['popular_posts'][:5]]
-    post['popular_posts'] = popular_posts
+    if 'popular_posts' in custom_fields:
+        popular_posts = [slug for slug in custom_fields['popular_posts'][:5]]
+        post['popular_posts'] = popular_posts
 
     # convert related links into a proper list
     related =[]
@@ -45,14 +46,15 @@ def process_view(post):
     post['related_links'] = related
 
     # append the hero information
-    hero_id = post['custom_fields']['related_hero'][0]
-    hero_url = os.path.expandvars("$WORDPRESS/hero/" + hero_id + "/?json=1")
-    response = requests.get(hero_url)
-    hero_data = json.loads(response.text)
-    hero_data = hero_data['post']
-    related_post_url = os.path.expandvars("$WORDPRESS/api/get_post/?post_slug=" + hero_data['custom_fields']['related_post'][0])
-    related_post_response = requests.get(related_post_url)
-    hero_data['related_post'] = process_post(json.loads(related_post_response.text)['post'])
-    post['hero'] = hero_data
+    if 'related_hero' in custom_fields and custom_fields['related_hero'][0] != '':
+        hero_id = custom_fields['related_hero'][0]
+        hero_url = os.path.expandvars("$WORDPRESS/hero/" + hero_id + "/?json=1")
+        response = requests.get(hero_url)
+        hero_data = json.loads(response.text)
+        hero_data = hero_data['post']
+        related_post_url = os.path.expandvars("$WORDPRESS/api/get_post/?post_slug=" + hero_data['custom_fields']['related_post'][0])
+        related_post_response = requests.get(related_post_url)
+        hero_data['related_post'] = process_post(json.loads(related_post_response.text)['post'])
+        post['hero'] = hero_data
 
     return post
