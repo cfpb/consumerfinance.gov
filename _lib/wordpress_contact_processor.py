557b7bddd0251a169fc51f10fe70e3f4187ff0bb
Mike Morici <Michael.Morici@cfpb.gov>
2014-09-05 17:00:13 -0400

    Fixes contacts after wp json changed.

--- _lib/wordpress_contact_processor.py
+++ _lib/wordpress_contact_processor.py
@@ -28,49 +28,34 @@ def documents(name, url, **kwargs):
     for post in posts_at_url(url):
         yield process_contact(post)
 
+def convert_custom_field(post_type, attribute, index=0, new_attribute=None):
+    
+    if attribute in post_type['custom_fields']:
+        if new_attribute is None:
+            post_type[attribute] = post_type['custom_fields'][attribute][index]
+        else:
+            post_type[new_attribute] = post_type['custom_fields'][attribute][index]
+
 def process_contact(contact):
     del contact['comments']
     contact['_id'] = contact['slug']
-    custom_fields = contact['custom_fields']
-    if 'email' in custom_fields:
-        if custom_fields['email'][0]:
-            contact['email'] = custom_fields['email'][0]
-        if custom_fields['email'][1]:
-            contact['email_desc'] = custom_fields['email'][1]
-    if 'email_2' in custom_fields:
-        if custom_fields['email_2'][0]:
-            contact['email_2'] = custom_fields['email_2'][0]
-        if custom_fields['email_2'][1]:
-            contact['email_2_desc'] = custom_fields['email_2'][1]
-    if 'phone' in custom_fields:
-        if custom_fields['phone'][0]:
-            contact['phone'] = custom_fields['phone'][0]
-        if custom_fields['phone'][1]:
-            contact['phone_desc'] = custom_fields['phone'][1]
-    if 'phone_2' in custom_fields:
-        if custom_fields['phone_2'][0]:
-            contact['phone_2'] = custom_fields['phone_2'][0]
-        if custom_fields['phone_2'][1]:
-            contact['phone_2_desc'] = custom_fields['phone_2'][1]
-    if 'sitewide_desc' in custom_fields:
-        contact['sitewide_desc'] = custom_fields['sitewide_desc'][0]
-    if 'fax' in custom_fields and custom_fields['fax'][0]:
-        contact['fax'] = custom_fields['fax'][0]
-    if 'street' in custom_fields and custom_fields['street']:
-        contact['street'] = custom_fields['street'][0]
-    if 'street_2' in custom_fields and custom_fields['street_2']:
-        contact['street_2'] = custom_fields['street_2'][0]
-    if 'city' in custom_fields and custom_fields['city']:
-        contact['city'] = custom_fields['city'][0]
-    if 'state' in custom_fields and custom_fields['state']:
-        contact['state'] = custom_fields['state'][0]
-    if 'zip_code' in custom_fields and custom_fields['zip_code']:
-        contact['zip_code'] = custom_fields['zip_code'][0]
-    if 'addr_desc' in custom_fields and custom_fields['addr_desc']:
-        contact['addr_desc'] = custom_fields['addr_desc'][0]
-    if 'web' in custom_fields:
-        if custom_fields['web'][0]:
-            contact['web'] = custom_fields['web'][0]
-        if custom_fields['web'][1]:
-            contact['web_desc'] = custom_fields['web'][1]
+    convert_custom_field(contact, 'email_addr')
+    convert_custom_field(contact, 'email_desc')
+    convert_custom_field(contact, 'email_2_addr')
+    convert_custom_field(contact, 'email_2_desc')
+    convert_custom_field(contact, 'phone_num')
+    convert_custom_field(contact, 'phone_desc')
+    convert_custom_field(contact, 'phone_2_num')
+    convert_custom_field(contact, 'phone_2_desc')
+    convert_custom_field(contact, 'fax_num')
+    convert_custom_field(contact, 'fax_desc')
+    convert_custom_field(contact, 'sitewide_desc')
+    convert_custom_field(contact, 'attn')
+    convert_custom_field(contact, 'street')
+    convert_custom_field(contact, 'city')
+    convert_custom_field(contact, 'state')
+    convert_custom_field(contact, 'zip_code')
+    convert_custom_field(contact, 'addr_desc')
+    convert_custom_field(contact, 'web_0', index=0, new_attribute='web_addr')
+    convert_custom_field(contact, 'web_0', index=1, new_attribute='web_desc')
     return contact
