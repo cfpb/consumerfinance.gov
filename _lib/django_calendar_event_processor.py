6af1b1328cb095f91a9d150a9d6f765918624ed8
Mike Morici <Michael.Morici@cfpb.gov>
2014-10-17 18:40:36 -0400

    Brings in sheer_blog calendar example.

---
+++ _lib/django_calendar_event_processor.py
@@ -0,0 +1,29 @@
+import sys
+import json
+import os.path
+import requests
+
+def posts_at_url(url):
+    
+    current_page = 1
+    max_page = sys.maxint
+    count = 20000
+
+    while current_page <= max_page:
+
+        url = os.path.expandvars(url)
+        resp = requests.get(url, params={'page':current_page, 'count': str(count)})
+        results = json.loads(resp.content) 
+        current_page += 1
+        max_page = int(results['count']) / count + 1
+        for p in results['results']:
+            yield p
+     
+def documents(name, url, **kwargs):
+    
+    for event in posts_at_url(url):
+        yield process_event(event)
+
+def process_event(event):
+    event['_id'] = event['id']
+    return event
