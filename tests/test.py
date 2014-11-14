bd936b45967af85b2670c5d6faac9e142a7ee73f
Dan Ford <dpford@gmail.com>
2014-11-14 16:49:04 -0500

    added watchroom fixture

--- tests/test.py
+++ tests/test.py
@@ -26,7 +26,7 @@ class NewsroomTestCase(LiveServerTestCase):
         config = {'debug': True, 
                   'index': 'cfgov_test', 
                   'elasticsearch': [{'host': 'localhost', 'port': 9200}], 
-                  'location': os.path.join(os.getcwd(), '..')}
+                  'location': os.getcwd()}
         application = app_with_config(config)
         application.config['TESTING'] = True
         application.config['LIVESERVER_PORT'] = 7000
@@ -34,55 +34,54 @@ class NewsroomTestCase(LiveServerTestCase):
 
     def setUp(self):
         es = Elasticsearch()
-        print "inner mapping here", IndicesClient(es).get_mapping(index='cfgov_test')
         self.driver = webdriver.Chrome()
         self.driver.get('http://localhost:7000/newsroom/')
-        # self.filter_dropdown_button = self.driver.find_element_by_xpath('//button[contains(text(), "Filter posts")]')
+        self.filter_dropdown_button = self.driver.find_element_by_xpath('//button[contains(text(), "Filter posts")]')
         click_filter_posts(self)
 
     def test_filter_display_button(self):
         filter_options = self.driver.find_element_by_class_name('padded-container')
-        assert filter_options.is_displayed()
 
-    # @attr('checkbox')
-    # def test_filter_checkboxes(self):
-    #     categoryList = ["Op-Ed", "Press Release", "Speech", "Testimony", "Blog"]
-    #     for cat in categoryList:
-    #         # use this after view changes value of checkbox input to 'Blog'
-    #         # checkbox = self.driver.find_element_by_xpath('//label/input[@value=\"'+ cat +'\"]/..')
-    #         checkbox = self.driver.find_element_by_xpath('//label/span[contains(text(), \"'+ cat +'\")]/..')
-    #         checkbox.click()
-    #         assert "is-checked" in checkbox.get_attribute('class')
-    #         checkbox.click()
-    #         assert "is-checkedFocus" in checkbox.get_attribute('class')
 
-    # @attr('search')
-    # @attr('category')
-    # def test_filter_category_search(self):
-    #     categoryList = ["Op-Ed", "Press Release", "Speech", "Testimony"] # No Blog posts yet so add in when data is there
-    #     for cat in categoryList:
-    #         click_filter_posts(self)
-    #         # checkbox = self.driver.find_element_by_xpath('//label/span[contains(text(), \"'+ cat +'\")]/..')
-    #         checkbox = WebDriverWait(self.driver, 10).until(
-    #             ec.visibility_of_element_located((By.XPATH, '//label/span[contains(text(), \"'+ cat +'\")]/..'))
-    #         )
-    #         checkbox.click()
-    #         filter_results_button = WebDriverWait(self.driver, 10).until(
-    #             ec.visibility_of_element_located((By.XPATH, '//input[@value="Apply filters"]'))
-    #         )
-    #         filter_results_button.click()
-    #         cat = coerce_category_for_dom(cat)
-    #         cat_type_elem = WebDriverWait(self.driver, 10).until(
-    #             ec.visibility_of_element_located((By.XPATH, '//a[@href="?filter_category='+ cat +'"]'))
-    #         )
-    #         assert cat_type_elem
-    #         click_filter_posts(self)
-    #         cat = coerce_category_for_dom(cat)
-    #         # checkbox = self.driver.find_element_by_xpath('//label/span[contains(text(), \"'+ cat +'\")]/..')
-    #         checkbox = WebDriverWait(self.driver, 10).until(
-    #             ec.visibility_of_element_located((By.XPATH, '//label/span[contains(text(), \"'+ cat +'\")]/..'))
-    #         )
-    #         checkbox.click()
+    @attr('checkbox')
+    def test_filter_checkboxes(self):
+        categoryList = ["Op-Ed", "Press Release", "Speech", "Testimony", "Blog"]
+        for cat in categoryList:
+            # use this after view changes value of checkbox input to 'Blog'
+            # checkbox = self.driver.find_element_by_xpath('//label/input[@value=\"'+ cat +'\"]/..')
+            checkbox = self.driver.find_element_by_xpath('//label/span[contains(text(), \"'+ cat +'\")]/..')
+            checkbox.click()
+            assert "is-checked" in checkbox.get_attribute('class')
+            checkbox.click()
+            assert "is-checkedFocus" in checkbox.get_attribute('class')
+
+    @attr('search')
+    @attr('category')
+    def test_filter_category_search(self):
+        categoryList = ["Op-Ed", "Press Release", "Speech", "Testimony"] # No Blog posts yet so add in when data is there
+        for cat in categoryList:
+            click_filter_posts(self)
+            # checkbox = self.driver.find_element_by_xpath('//label/span[contains(text(), \"'+ cat +'\")]/..')
+            checkbox = WebDriverWait(self.driver, 10).until(
+                ec.visibility_of_element_located((By.XPATH, '//label/span[contains(text(), \"'+ cat +'\")]/..'))
+            )
+            checkbox.click()
+            filter_results_button = WebDriverWait(self.driver, 10).until(
+                ec.visibility_of_element_located((By.XPATH, '//input[@value="Apply filters"]'))
+            )
+            filter_results_button.click()
+            cat = coerce_category_for_dom(cat)
+            cat_type_elem = WebDriverWait(self.driver, 10).until(
+                ec.visibility_of_element_located((By.XPATH, '//a[@href="?filter_category='+ cat +'"]'))
+            )
+            assert cat_type_elem
+            click_filter_posts(self)
+            cat = coerce_category_for_dom(cat)
+            # checkbox = self.driver.find_element_by_xpath('//label/span[contains(text(), \"'+ cat +'\")]/..')
+            checkbox = WebDriverWait(self.driver, 10).until(
+                ec.visibility_of_element_located((By.XPATH, '//label/span[contains(text(), \"'+ cat +'\")]/..'))
+            )
+            checkbox.click()
 
     def tearDown(self):
         self.driver.close()
