8d87ba860ae312f6bdb2d9a40998c62c36e0d4fc
Dan Ford <dpford@gmail.com>
2014-11-17 14:13:32 -0500

    refactored the mapping creation and document indexing

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,80 +1,45 @@
 import os
 import json
-import logging
-import dateutil.parser
 
 from elasticsearch import Elasticsearch
-from elasticsearch.client import IndicesClient
 
 index_name = "cfgov_test"
 root = os.getcwd()
 
-
 def setup_package():
-    logging.basicConfig()
-    logger = logging.getLogger("")
-    logger.setLevel(logging.DEBUG)
-    tracer = logging.getLogger('elasticsearch.trace')
-    tracer.setLevel(logging.DEBUG)
-    tracer.addHandler(logging.FileHandler('elasticsearch-py.txt'))
     es = Elasticsearch()
     if es.indices.exists(index_name):
         es.indices.delete(index_name)
     es.indices.create(index=index_name)
 
-    posts_mapping = { "properties" :
-          {
-            "title" : {"type" : "string", "store" : "yes"},
-            "text" : {"type" : "string", "store" : "yes"},
-            "date" : {"type" : "date", "store": "yes"},
-            "category" : {"type":"string", "index": "not_analyzed"},
-            "author" : {"type":"string", "index":"not_analyzed"},
-            "tags" : {"type":"string", "store": "yes", "index":"not_analyzed"},
-            "excerpt" : {"type":"string", "store": "yes"},
-            "custom_fields": {
-                    "properties": {
-                        "display_in_newsroom": {"type":"string", "index":"not_analyzed"}
-                    }
-            }
-          }
-        }
-
     # Create the mappings
-    es.indices.put_mapping(index=index_name,
-                           doc_type="newsroom",
-                           body={"newsroom": posts_mapping,
-                                 "watchroom": posts_mapping})
-
-    newsroom_json = open(os.path.join(root, 'tests/fixtures/newsroom.json'))
-    newsroom = json.load(newsroom_json)
-
-    view_json = open(os.path.join(root, 'tests/fixtures/views.json'))
-    view = json.load(view_json)
-
-    watchroom_json = open(os.path.join(root, 'tests/fixtures/watchroom.json'))
-    watchroom = json.load(watchroom_json)
+    create_mapping('newsroom', '_settings/posts_mappings.json')
+    create_mapping('watchroom', '_settings/posts_mappings.json')
 
     # Index the documents
-    for document in newsroom:
-        es.create(index=index_name,
-                  doc_type="newsroom",
-                  id=document['_id'],
-                  body=document)
-
-    for document in view:
-        es.create(index=index_name,
-                  doc_type="views",
-                  id=document['_id'],
-                  body=document)
-
-    for document in watchroom:
-        es.create(index=index_name,
-                  doc_type="newsroom",
-                  id=document['_id'],
-                  body=document)
-
-
+    index_documents('newsroom', 'tests/fixtures/newsroom.json')
+    index_documents('views', 'tests/fixtures/views.json')
+    index_documents('watchroom', 'tests/fixtures/watchroom.json')
 
 def teardown_package():
     es = Elasticsearch()
     es.indices.delete(index_name)
+
+def create_mapping(doc_type, mapping_json_path):
+    es = Elasticsearch()
+    mapping_json = open(os.path.join(root, mapping_json_path))
+    mapping = json.load(mapping_json)
+    es.indices.put_mapping(index=index_name,
+                           doc_type=doc_type,
+                           body={doc_type: mapping})
+
+def index_documents(doc_type, json_path):
+    es = Elasticsearch()
+    json_file = open(os.path.join(root, json_path))
+    documents = json.load(json_file)
+
+    for document in documents:
+        es.create(index=index_name,
+                  doc_type=doc_type,
+                  id=document['_id'],
+                  body=document)
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,12 +1,3 @@
-# import nose
-
-# from selenose.cases import SeleniumTestCase
-# from nose.plugins.attrib import attr
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as ec
-
-
 def click_filter_posts(test):
     test.filter_dropdown_button = test.driver.find_element_by_xpath('//button[contains(text(), "Filter posts")]')
     if test.filter_dropdown_button.get_attribute('aria-pressed') == 'false':
