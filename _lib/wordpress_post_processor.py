9aef21a3c4ba98506bf339f717d7559cf014c0b2
Steven Bellamy <steven.bellamy@cfpb.gov>
2015-03-20 09:22:38 -0400

    Fixes issue caused by New Author taxonomy

--- _lib/wordpress_post_processor.py
+++ _lib/wordpress_post_processor.py
@@ -49,7 +49,7 @@ def process_post(post, newsroom = False):
         post['links'] = links
     else:
         post['tags'] = [tag['title'] for tag in post['taxonomy_fj_tag']]
-        post['author'] = [author['title'] for author in post['taxonomy_author']]
+        post['author'] = [author['title'] for author in post['taxonomy_fj_author']]
     if newsroom and post['type'] == 'post':
         post['category'][0] = "Blog"
     author_template = Template("$first_name $last_name")
