61549dc914018c59b35387edcaf17dd7a22fff39
Kimberly Munoz <kimberly.munoz@cfpb.gov>
2015-05-07 12:06:22 -0400

    updated gitignore

--- _tests/unit_tests/footer-button-spec.js
+++ _tests/unit_tests/footer-button-spec.js
@@ -1,23 +1,33 @@
-var chai = require('chai');
+'use strict';
+var chai = require( 'chai' );
 var expect = chai.expect;
-var jsdom = require('mocha-jsdom');
+var jsdom = require( 'mocha-jsdom' );
 
-describe('Footer Button Testing', function() {
-    'use strict';
+describe( 'The Footer Return to Top Button', function() {
 
-    var $, footerButton;
+  var $, footerButton;
 
-    jsdom();
+  jsdom();
 
-    before(function (){
-        $ = require('jquery');
-        footerButton = require('../../src/static/js/modules/footer-button.js');
-    });
+  before( function() {
+    $ = require( 'jquery' );
+    footerButton = require( '../../src/static/js/modules/footer-button.js' );
+  } );
 
-    beforeEach(function(){
-    });
+  beforeEach( function() {
+    // Adding a simplified version of the thing we want to test.
+    $( 'body' ).html( $( '<a class="js-return-to-top" href="#">' +
+                         'Back to top' +
+                       '</a>' ) );
+  } );
 
-  it('is a test', function(){
-    expect(footerButton).to.be.ok;
-  });
-});
+  it( 'should exist', function() {
+    expect( footerButton ).to.be.ok;
+    expect( $( '.js-return-to-top' ) ).to.be.ok;
+  } );
+
+  it( 'should return to top of the page when clicked', function() {
+    $( '.js-return-to-top' ).click();
+  } );
+
+} );
