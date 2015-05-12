01c203d50e895183e42eb5e7b7c502acef156911
Kimberly Munoz <kimberly.munoz@cfpb.gov>
2015-05-12 16:57:16 -0400

    Adding tests

--- _tests/unit_tests/modules/footer-button-spec.js
+++ _tests/unit_tests/modules/footer-button-spec.js
@@ -5,14 +5,14 @@ var expect = chai.expect;
 var jsdom = require( 'mocha-jsdom' );
 
 describe( 'The Footer Return-to-Top Button', function() {
-
-  var $;
+  var $, footerButton, sandbox;
 
   jsdom();
 
   before( function() {
     $ = require( 'jquery' );
-
+    footerButton = require( '../../../src/static/js/modules/footer-button.js' );
+    sandbox = sinon.sandbox.create();
   } );
 
   beforeEach( function() {
@@ -22,20 +22,39 @@ describe( 'The Footer Return-to-Top Button', function() {
                             'href="http://www.google.com/">' +
                             'Back to top' +
                           '</a>' ) );
-    require( '../../../src/static/js/modules/footer-button.js' ).init();
+  } );
+
+  afterEach( function() {
+    sandbox.restore();
+  } );
+
+  it( 'should not trigger an animate before an init', function() {
+    $( 'body' ).scrollTop( 500 );
+    var AnimateSpy = sandbox.spy( $.prototype, 'animate' );
+
+    $( '.js-return-to-top' ).trigger( 'click' );
+    expect( AnimateSpy.called ).to.not.be.ok;
   } );
 
   it( 'should trigger animate function when clicked', function() {
+    footerButton.init();
     $( 'body' ).scrollTop( 500 );
-    var animateSpy = sinon.spy( $.prototype, 'animate' );
+    var animateSpy = sandbox.spy( $.prototype, 'animate' );
 
     $( '.js-return-to-top' ).trigger( 'click' );
-    expect( animateSpy ).called;
+    expect( animateSpy.called ).to.be.ok;
   } );
 
   it( 'should not navigate away from the page', function() {
     $( '.js-return-to-top' ).trigger( 'click' );
     expect( window.location.href ).to.not.equal( 'http://www.google.com/' );
+  } );
+
+  it( 'should remove handlers when item is removed', function() {
+    var animateSpy = sinon.spy( $.prototype, 'animate' );
+    $( '.js-return-to-top' ).remove();
+    $( '.js-return-to-top' ).trigger( 'click' );
+    expect( animateSpy.called ).to.not.be.ok;
   } );
 
 } );
