e1a2c58fd36d0548d8bdd19dbde8a4a1ca1dffe6
James Wilson <Jimmy.Wilson@cfpb.gov>
2015-05-21 13:23:31 -0400

    Initialize scripts in the correct order
    - modified page specific scripts with init wrapper to ensure scripts are initialized in the correct order
    - ensured jQuery is required first to avoid undefined errors

--- src/static/js/routes/the-bureau/bureau-structure/index.js
+++ src/static/js/routes/the-bureau/bureau-structure/index.js
@@ -11,35 +11,39 @@ require( 'slick' );
 var ContentSlider = require( '../../../modules/content-slider' );
 var BreakpointHandler = require( '../../../modules/breakpoint-handler' ).BreakpointHandler;
 
-if ( !$( 'html' ).hasClass( 'lt-ie9' ) ) {
-  var settings = {
-    breakpoint: 599,
-    type:       'max',
-    enter:      _createSlider,
-    leave:      _destroySlider
-  };
+function init() {
+  if ( !$( 'html' ).hasClass( 'lt-ie9' ) ) {
+    var settings = {
+      breakpoint: 599,
+      type:       'max',
+      enter:      _createSlider,
+      leave:      _destroySlider
+    };
 
-  var handler = new BreakpointHandler( settings );
+    var handler = new BreakpointHandler( settings );
+  }
+
+  var _slider;
+  function _createSlider() {
+    // Close any open expandables.
+    $( '.org-chart' ).find( '.expandable__expanded .expandable_target' ).click();
+    // Hide org chart branches.
+    $( '.org-chart_branches' ).hide();
+    // Show content slider & mobile nav links.
+    $( '#content-slider, .content-hide' ).show();
+    // Initialize slider.
+    _slider = new ContentSlider( '#content-slider', 1 );
+  }
+
+  function _destroySlider() {
+    // Destroy slider.
+    if ( _slider ) _slider.destroy();
+    _slider = null;
+    // Hide content slider & mobile nav links.
+    $( '#content-slider, .content-hide' ).hide();
+    // Show org chart branches.
+    $( '.org-chart_branches' ).show();
+  }
 }
 
-var _slider;
-function _createSlider() {
-  // Close any open expandables.
-  $( '.org-chart' ).find( '.expandable__expanded .expandable_target' ).click();
-  // Hide org chart branches.
-  $( '.org-chart_branches' ).hide();
-  // Show content slider & mobile nav links.
-  $( '#content-slider, .content-hide' ).show();
-  // Initialize slider.
-  _slider = new ContentSlider( '#content-slider', 1 );
-}
-
-function _destroySlider() {
-  // Destroy slider.
-  if ( _slider ) _slider.destroy();
-  _slider = null;
-  // Hide content slider & mobile nav links.
-  $( '#content-slider, .content-hide' ).hide();
-  // Show org chart branches.
-  $( '.org-chart_branches' ).show();
-}
+module.exports = { init: init };
