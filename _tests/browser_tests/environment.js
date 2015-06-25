09fcbe86dfd8d87e063377667081962991549e5f
James Wilson <Jimmy.Wilson@cfpb.gov>
2015-06-25 17:46:19 -0400

    Replaced existing browser tests with new Protractor tests
    - Initialized with simple `contact-us` and `the-bureau` tests to get started
    - Organized into suites to allow testing for one-off instances (typically browser size but could also be non-js tests)
    - Utilizes Page Objects for easily sharing pages among multiple tests
    - Added instructions to help new users

---
+++ _tests/browser_tests/environment.js
@@ -0,0 +1,20 @@
+// Common configuration files with defaults plus overrides from environment vars
+var webServerDefaultPort = 7000;
+
+module.exports = {
+  // The address of a running selenium server.
+  seleniumAddress:
+    (process.env.SELENIUM_URL || 'http://localhost:4444/wd/hub'),
+
+  // Default http port to host the web server
+  webServerDefaultPort: webServerDefaultPort,
+
+  // Protractor interactive tests
+  interactiveTestPort: 6969,
+
+  // A base URL for your application under test.
+  baseUrl:
+    'http://' + (process.env.HTTP_HOST || 'localhost') +
+          ':' + (process.env.HTTP_PORT || webServerDefaultPort)
+
+};
