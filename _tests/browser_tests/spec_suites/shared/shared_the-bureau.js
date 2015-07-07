1e8da9e89826c62426ed3d712db2308749db6c98
Kimberly Munoz <kimberly.munoz@cfpb.gov>
2015-07-07 09:25:42 -0400

    Adding Tests for Students-and-graduates

--- _tests/browser_tests/spec_suites/shared/shared_the-bureau.js
+++ _tests/browser_tests/spec_suites/shared/shared_the-bureau.js
@@ -1,6 +1,6 @@
-var TheBureauPage = require( '../../page_objects/page_the-bureau.js' );
+var careers_studentsAndGraduates = require( '../../page_objects/page_the-bureau.js' );
 
-describe( 'Beta The Bureau Page', function() {
+describe( 'Beta Careers/Students and Graduates Page', function() {
   var page;
 
   beforeEach( function() {
@@ -9,7 +9,7 @@ describe( 'Beta The Bureau Page', function() {
   } );
 
   it( 'should properly load in a browser', function() {
-    expect( page.pageTitle() ).toBe( 'The Bureau' );
+    expect( page.pageTitle() ).toBe( 'Students and Graduates' );
   } );
 
   it( 'should include 3 bureau missions, Educate, Enforce, Empower', function() {
