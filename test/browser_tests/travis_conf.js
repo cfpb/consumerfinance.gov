1443788eac90d7c77c41efd08c9a4f5c5133e0fa
Kimberly Munoz <kimberly.munoz@cfpb.gov>
2015-07-28 14:19:41 -0400

    Travis_conf.js

---
+++ test/browser_tests/travis_conf.js
@@ -0,0 +1,15 @@
+'use strict';
+
+exports.config = {
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY,
+
+  capabilities: {
+    'browserName': 'chrome',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'name': 'cfgov-refresh test'
+  },
+
+  specs: ['spec_suites/shared/*.js']
+};
