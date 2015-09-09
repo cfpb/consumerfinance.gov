b5a61fb8b527fcad9e2f505a1af61772567bc8cd
Emmanuel Apau <emmanuel.apau@cfpb.gov>
2015-09-09 16:02:33 -0400

    typo

--- cfgov/cfgov/wsgi.py
+++ cfgov/cfgov/wsgi.py
@@ -1,7 +1,7 @@
 """
 WSGI config for cfgov project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+It exposes the WSGI callable as a module-level variable named `application`.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
