4b3dd3d5ba6541cad43a29346c0d9b904511a97e
James Wilson <jimmynotjim@me.com>
2015-09-21 17:12:07 -0400

    Added testing for web-storage-proxy.js
    - Refactored code to be less complex and allow for easier testing
    - Updated code to utilize storage object instead of boolean to allow for storage options outside the window object
    - Added tests to increase coverage to 100% across the board
    - Skipping tests of internal private functions

--- src/static/js/modules/util/web-storage-proxy.js
+++ src/static/js/modules/util/web-storage-proxy.js
@@ -3,98 +3,109 @@
 
    An interface for interacting with web storage
    (local storage and session storage).
-   A final parameter of true on all methods specifies to use session storage
-   (non-persistant storage). A final parameter of false on all methods
-   specifies to use local storage (persistent storage).
-   E.g. webStorageProxy.setItem("name", "Anselm", true)
-   for session storage or webStorageProxy.getItem("name")
-   for local storage (note omitted 'true' value).
    Note: values stored in local storage are not accessible from session storage
    and vice versa. They both work on different objects within the browser.
-   ========================================================================== */
+   ========================================================================= */
 
 'use strict';
 
-// Default session type.
-// true = sessionStorage (non-persistent).
-// false = localStorage (persistent).
-var _sessionOnly = false;
+// Default storage type.
+var _storage = window.sessionStorage;
 
 /**
  * Set an item value specified by the key in web storage.
  * @param {string} key The key for the value.
  * @param {string} value The value to store.
- * @param {boolean} sessionOnly (Optional)
- *   Use non-persistent storage (true) or persistent storage (false).
+ * @param {object} storage (Optional)
+ *   Use non-persistent storage (sessionStorage)
+ *   or persistent storage (localStorage).
  * @returns {string} The value set in web storage.
  */
-function setItem( key, value, sessionOnly ) {
-  var storage = _sessionOrLocal( sessionOnly );
+function setItem( key, value, storage ) {
+  storage = _getStorageType( storage );
   storage.setItem( key, value );
+
   return value;
 }
 
 /**
  * Get an item value specified by the key in web storage.
  * @param {string} key The key for the value.
- * @param {boolean} sessionOnly (Optional)
- *   Use non-persistent storage (true) or persistent storage (false).
+ * @param {object} storage (Optional)
+ *   Use non-persistent storage (sessionStorage)
+ *   or persistent storage (localStorage).
  * @returns {string} The value set in web storage.
  */
-function getItem( key, sessionOnly ) {
-  var storage = _sessionOrLocal( sessionOnly );
+function getItem( key, storage ) {
+  storage = _getStorageType( storage );
+
   return storage.getItem( key );
 }
 
 /**
  * Remove an item specified by the key.
  * @param {string} key The key for the value.
- * @param {boolean} sessionOnly (Optional)
- *   Use non-persistent storage (true) or persistent storage (false).
+ * @param {object} storage (Optional)
+ *   Use non-persistent storage (sessionStorage)
+ *   or persistent storage (localStorage).
  * @returns {boolean} Returns true if the item existed and it was
  *   removed. Returns false if the item didn't exist to begin with.
  */
-function removeItem( key, sessionOnly ) {
-  var storage = _sessionOrLocal( sessionOnly );
+function removeItem( key, storage ) {
+  storage = _getStorageType( storage );
   var returnVal = true;
-  if ( !storage.getItem( key ) ) returnVal = false;
-  if ( returnVal ) storage.removeItem( key );
+
+  if ( !storage.getItem( key ) ) {
+    returnVal = false;
+  }
+
+  if ( returnVal ) {
+    storage.removeItem( key );
+  }
 
   return returnVal;
 }
 
 /**
- * Internal function for setting default session type.
- * @param {boolean} sessionOnly
- *   Use non-persistent storage (true) or persistent storage (false).
- * @throws {Error} If parameter isn't a boolean.
+ * Set the default session type.
+ * @param {object} storage
+ *   Use non-persistent storage (sessionStorage)
+ *   or persistent storage (localStorage).
+ * @throws {Error} If parameter isn't a object.
  */
-function setSessionType( sessionOnly ) {
-  if ( typeof sessionOnly !== 'boolean' ) {
-    throw new Error( 'Setting requires a boolean value.' );
+function setStorage( storage ) {
+  if ( typeof storage !== 'object' ) {
+    throw new Error( 'Setting must be an object.' );
   }
-  _sessionOnly = sessionOnly;
+
+  _storage = storage;
 }
 
 /**
  * Internal function for whether to use local or session storage.
- * @param {boolean} sessionOnly
- *   Use non-persistent storage (true) or persistent storage (false).
+ * @param {object} storage
+ *   Use non-persistent storage (sessionStorage)
+ *   or persistent storage (localStorage).
  * @returns {object} A local storage or session storage instance.
  */
-function _sessionOrLocal( sessionOnly ) {
+/* istanbul ignore next */
+function _getStorageType( storage ) {
   // Use default setting if none is provided.
-  if ( typeof sessionOnly === 'undefined' ) sessionOnly = _sessionOnly;
-  var storage;
-  if ( sessionOnly ) storage = sessionStorage;
-  else storage = localStorage;
+  if ( typeof storage !== 'object' ) {
+    if ( typeof _storage === 'undefined' ) {
+      storage = window.sessionStorage;
+    } else {
+      storage = _storage;
+    }
+  }
+
   return storage;
 }
 
 // Expose public methods.
 module.exports = {
-  setItem:        setItem,
-  getItem:        getItem,
-  removeItem:     removeItem,
-  setSessionType: setSessionType
+  setItem:    setItem,
+  getItem:    getItem,
+  removeItem: removeItem,
+  setStorage: setStorage
 };
