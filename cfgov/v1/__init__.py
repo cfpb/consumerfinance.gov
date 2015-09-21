3e30737c98a4a9e890d362ba4cbbb2315163bc29
kurtw <kurtw@users.noreply.github.com>
2015-09-21 09:59:07 -0400

    Remove unused Python 2 support

--- cfgov/v1/__init__.py
+++ cfgov/v1/__init__.py
@@ -1,5 +1,3 @@
-from __future__ import absolute_import  # Python 2 only
-
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.core.urlresolvers import reverse
 from django.template.defaultfilters import slugify
