1b7afd338e902da4f479719e54fd9b194b67b38d
Emmanuel Apau <emmanuel.apau@cfpb.gov>
2015-09-24 14:49:56 -0400

    forced jinj2 templates to default to autoescaping templates

--- cfgov/v1/__init__.py
+++ cfgov/v1/__init__.py
@@ -11,6 +11,7 @@ from compressor.contrib.jinja2ext import CompressorExtension
 def environment(**options):
     options.setdefault('extensions', []).append(CompressorExtension)
     env = Environment(**options)
+    env.autoescape = True
     env.globals.update({
         'static': staticfiles_storage.url,
         'reverse': reverse,
