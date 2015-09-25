5dab5fe03aefa02941af88cf4e8630e852a906c8
Ross Karchner <ross.karchner@cfpb.gov>
2015-09-25 12:46:36 -0400

    v1 environment is now inclusive of sheerlike

--- cfgov/v1/__init__.py
+++ cfgov/v1/__init__.py
@@ -4,12 +4,13 @@ from django.template.defaultfilters import slugify
 from wagtail.wagtailcore.templatetags import wagtailcore_tags
 
 from jinja2 import Environment
+from sheerlike import environment as sheerlike_environment
 from compressor.contrib.jinja2ext import CompressorExtension
 
 
 def environment(**options):
     options.setdefault('extensions', []).append(CompressorExtension)
-    env = Environment(**options)
+    env = sheerlike_environment(**options)
     env.globals.update({
         'static': staticfiles_storage.url,
         'reverse': reverse,
