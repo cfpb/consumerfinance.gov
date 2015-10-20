a5b5c1cd5c39c66486e9a39344ac9db5caa62eec
Emmanuel Apau <emmanuel.apau@cfpb.gov>
2015-10-20 16:32:15 -0400

    reverted unnecessary changes to match flapjack branch

--- cfgov/v1/__init__.py
+++ cfgov/v1/__init__.py
@@ -3,7 +3,7 @@ from django.core.urlresolvers import reverse
 from django.template.defaultfilters import slugify
 from wagtail.wagtailcore.templatetags import wagtailcore_tags
 
-import jinja2
+from jinja2 import Environment
 from sheerlike import environment as sheerlike_environment
 from compressor.contrib.jinja2ext import CompressorExtension
 
@@ -14,9 +14,10 @@ def environment(**options):
     env.autoescape = True
     env.globals.update({
         'static': staticfiles_storage.url,
-        'reverse': reverse
+        'reverse': reverse,
     })
     env.filters.update({
-        'slugify': slugify
+        'slugify': slugify,
+        'richtext': wagtailcore_tags.richtext
     })
     return env
