435131759e23bd8374d0c8124acde675b4785067
kurtw <kurtw@users.noreply.github.com>
2015-10-28 14:24:47 -0400

    DJANGO_SETTINGS_MODULE should be base since local requirements are not installed

--- cfgov/cfgov/wsgi.py
+++ cfgov/cfgov/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cfgov.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cfgov.settings.base')
 
 application = get_wsgi_application()
