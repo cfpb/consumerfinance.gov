0f1a18c65b5752e6b9a9eb9e07c530a54e7562b6
Emmanuel Apau <emmanuel.apau@cfpb.gov>
2015-11-18 10:24:06 -0500

    deleted demo/wagtail templates, added all created molecules, organism, snippts to demo page, refactored code with new atom, updated base requirements to include django_extentions

--- cfgov/v1/models/atoms.py
+++ cfgov/v1/models/atoms.py
@@ -1,6 +1,45 @@
+from django.core.exceptions import ValidationError
+
 from wagtail.wagtailcore import blocks
+from wagtail.wagtailimages.blocks import ImageChooserBlock
+
+
+def isRequired(field_name):
+    return [str(field_name) + ' is required.']
 
 
 class Hyperlink(blocks.StructBlock):
-    label = blocks.CharBlock(max_length=50)
-    href = blocks.CharBlock(default='/')
+    text = blocks.CharBlock(max_length=50)
+    url = blocks.CharBlock(default='/')
+
+
+class ImageBasic(blocks.StructBlock):
+    upload = ImageChooserBlock(required=False)
+    url = blocks.CharBlock(required=False)
+    alt = blocks.CharBlock(required=False)
+
+    def clean(self, data):
+        error_dict = {}
+        try:
+            block_data = super(ImageBasic, self).clean(data)
+        except ValidationError as e:
+            error_dict.update(e.params)
+            block_data = data
+
+        if not block_data['upload'] and not block_data['url'] and not block_data['alt']:
+            img_err = ['Please upload or enter an image path']
+            error_dict.update({'upload': img_err, 'url': img_err, 'alt': isRequired('Image alt')})
+
+        if block_data['upload'] and block_data['url']:
+            img_err = ['Please select one method of image rendering']
+            error_dict.update({
+                'upload': img_err,
+                'url': img_err})
+
+        if block_data['url'] and not block_data['alt']:
+            error_dict.update({'alt': isRequired('Image Alt')})
+
+        if error_dict:
+            raise ValidationError("ImageBasic validation errors", params=error_dict)
+        else:
+            return block_data
--- cfgov/v1/models/molecules.py
+++ cfgov/v1/models/molecules.py
@@ -13,11 +13,7 @@ def isRequired(field_name):
 class HalfWidthLinkBlob(blocks.StructBlock):
     heading = blocks.CharBlock(max_length=100, required=True)
     body = blocks.RichTextBlock(blank=True)
-    links = blocks.ListBlock(blocks.StructBlock([
-        ('text', blocks.CharBlock(required=False)),
-        ('url', blocks.URLBlock(required=False)),
-    ], icon='user', required=False)
-    )
+    links = blocks.ListBlock(atoms.Hyperlink(), required=False)
 
     class Meta:
         icon = 'link'
@@ -27,39 +23,10 @@ class HalfWidthLinkBlob(blocks.StructBlock):
 class ImageText5050(blocks.StructBlock):
     title = blocks.CharBlock(max_length=100, required=True)
     description = blocks.RichTextBlock(blank=True)
-    image = ImageChooserBlock(required=False)
-    image_path = blocks.CharBlock(required=False)
-    image_alt = blocks.CharBlock(required=False)
+    image = atoms.ImageBasic()
     is_widescreen = blocks.BooleanBlock(required=False)
     is_button = blocks.BooleanBlock(required=False)
-    link_url = blocks.URLBlock(required=False)
-    link_text = blocks.CharBlock(max_length=100, required=False)
-
-    def clean(self, data):
-        error_dict = {}
-        try:
-            block_data = super(ImageText5050, self).clean(data)
-        except ValidationError as e:
-            error_dict.update(e.params)
-            block_data = data
-
-        if not block_data['image'] and not block_data['image_path'] and not block_data['image_alt']:
-            img_err = ['Please upload or enter an image path']
-            error_dict.update({'image': img_err, 'image_path': img_err, 'image_alt': isRequired('Image alt')})
-
-        if block_data['image'] and block_data['image_path']:
-            img_err = ['Please select one method of image rendering']
-            error_dict.update({
-                'image': img_err,
-                'image_path': img_err})
-
-        if block_data['image_path'] and not block_data['image_alt']:
-            error_dict.update({'image_alt': isRequired('Image Alt')})
-
-        if error_dict:
-            raise ValidationError("ImageText5050 validation errors", params=error_dict)
-        else:
-            return block_data
+    link = atoms.Hyperlink()
 
     class Meta:
         icon = 'image'
@@ -69,38 +36,9 @@ class ImageText5050(blocks.StructBlock):
 class ImageText2575(blocks.StructBlock):
     heading = blocks.CharBlock(max_length=100, required=True)
     body = blocks.RichTextBlock(required=True)
-    image = ImageChooserBlock(required=False)
-    image_path = blocks.CharBlock(required=False)
-    image_alt = blocks.CharBlock(required=False)
-    link_url = blocks.URLBlock(required=False)
-    link_text = blocks.CharBlock(max_length=100, required=False)
+    image = atoms.ImageBasic()
+    link = atoms.Hyperlink()
     has_rule = blocks.BooleanBlock(required=False)
-
-    def clean(self, data):
-        error_dict = {}
-        try:
-            block_data = super(ImageText2575, self).clean(data)
-        except ValidationError as e:
-            error_dict.update(e.params)
-            block_data = data
-
-        if not block_data['image'] and not block_data['image_path'] and not block_data['image_alt']:
-            img_err = ['Please upload or enter an image path']
-            error_dict.update({'image': img_err, 'image_path': img_err, 'image_alt': isRequired('Image alt')})
-
-        if block_data['image'] and block_data['image_path']:
-            img_err = ['Please select one method of image rendering']
-            error_dict.update({
-                'image': img_err,
-                'image_path': img_err})
-
-        if block_data['image_path'] and not block_data['image_alt']:
-            error_dict.update({'image_alt': isRequired('Image Alt')})
-
-        if error_dict:
-            raise ValidationError("ImageText2575 validation errors", params=error_dict)
-        else:
-            return block_data
 
     class Meta:
         icon = 'image'
@@ -111,8 +49,7 @@ class TextIntroduction(blocks.StructBlock):
     heading = blocks.CharBlock(max_length=100, required=True)
     intro = blocks.CharBlock(max_length=100, required=True)
     body = blocks.RichTextBlock(required=False)
-    link_url = blocks.URLBlock(required=False)
-    link_text = blocks.CharBlock(max_length=100, required=False)
+    link = atoms.Hyperlink()
     has_rule = blocks.BooleanBlock(required=False)
 
     class Meta:
@@ -124,40 +61,11 @@ class Hero(blocks.StructBlock):
     heading = blocks.CharBlock(max_length=100, required=True)
     body = blocks.RichTextBlock(required=False)
 
-    image = ImageChooserBlock(required=False)
-    image_path = blocks.CharBlock(required=False)
-    image_alt = blocks.CharBlock(required=False)
+    image = atoms.ImageBasic()
 
     background_color = blocks.CharBlock(max_length=100, required=False)
-    link_url = blocks.URLBlock(required=False)
-    link_text = blocks.CharBlock(max_length=100, required=False)
+    link = atoms.Hyperlink()
     is_button = blocks.BooleanBlock(required=False)
-
-    def clean(self, data):
-        error_dict = {}
-        try:
-            block_data = super(Hero, self).clean(data)
-        except ValidationError as e:
-            error_dict.update(e.params)
-            block_data = data
-
-        if not block_data['image'] and not block_data['image_path'] and not block_data['image_alt']:
-            img_err = ['Please upload or enter an image path']
-            error_dict.update({'image': img_err, 'image_path': img_err, 'image_alt': isRequired('Image alt')})
-
-        if block_data['image'] and block_data['image_path']:
-            img_err = ['Please select one method of image rendering']
-            error_dict.update({
-                'image': img_err,
-                'image_path': img_err})
-
-        if block_data['image_path'] and not block_data['image_alt']:
-            error_dict.update({'image_alt': isRequired('Image Alt')})
-
-        if error_dict:
-            raise ValidationError("Hero validation errors", params=error_dict)
-        else:
-            return block_data
 
     class Meta:
         icon = 'image'
@@ -200,7 +108,7 @@ class ContactAddress(blocks.StructBlock):
     zip_code = blocks.CharBlock(max_length=15, required=False)
 
     class Meta:
-        template = 'v1/wagtail/molecules/contact-address.html'
+        template = '_includes/molecules/contact-address.html'
         icon = 'mail'
         label = 'Address'
 
@@ -210,7 +118,7 @@ class ContactEmail(blocks.StructBlock):
 
     class Meta:
         icon = 'mail'
-        template = 'v1/wagtail/molecules/contact-email.html'
+        template = '_includes/molecules/contact-email.html'
         label = 'Email'
 
 
@@ -226,5 +134,5 @@ class ContactPhone(blocks.StructBlock):
 
     class Meta:
         icon = 'mail'
-        template = 'v1/wagtail/molecules/contact-phone.html'
+        template = '_includes/molecules/contact-phone.html'
         label = 'Phone'
