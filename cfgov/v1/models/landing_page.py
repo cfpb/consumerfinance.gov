ac40d134228769eb52af565fae45e2d2243edf20
Emmanuel Apau <emmanuel.apau@cfpb.gov>
2015-11-19 15:41:44 -0500

    updated molecules to have group headings

--- cfgov/v1/models/landing_page.py
+++ cfgov/v1/models/landing_page.py
@@ -22,10 +22,22 @@ class LandingPage(CFGOVPage):
         ('text_introduction', molecules.TextIntroduction())
     ], blank=True)
 
-    content = StreamField([
-        ('half_width_link_blob', molecules.HalfWidthLinkBlob()),
-        ('image_text_25_75', molecules.ImageText2575()),
-        ('image_text_50_50', molecules.ImageText5050()),
+    image_text_25_75_group_header = models.CharField(max_length=100, blank=True)
+    image_text_25_75_content = StreamField([
+        ('image_text_25_75', molecules.ImageText2575())
+    ], blank=True)
+
+    image_text_50_50_group_header = models.CharField(max_length=100, blank=True)
+    image_text_50_50_content = StreamField([
+        ('image_text_50_50', molecules.ImageText5050())
+    ], blank=True)
+
+    half_width_link_blob_group_header = models.CharField(max_length=100, blank=True)
+    half_width_link_blob_content = StreamField([
+        ('half_width_link_blob', molecules.HalfWidthLinkBlob())
+    ], blank=True)
+
+    wells = StreamField([
         ('well', organisms.Well())
     ], blank=True)
 
@@ -39,7 +51,19 @@ class LandingPage(CFGOVPage):
     content_panels = CFGOVPage.content_panels + [
         StreamFieldPanel('hero'),
         StreamFieldPanel('introduction'),
-        StreamFieldPanel('content'),
+        MultiFieldPanel([
+            FieldPanel('image_text_25_75_group_header'),
+            StreamFieldPanel('image_text_25_75_content'),
+        ], heading='Image Text 25 75'),
+        MultiFieldPanel([
+            FieldPanel('image_text_50_50_group_header'),
+            StreamFieldPanel('image_text_50_50_content'),
+        ], heading='Image Text 50 50'),
+        MultiFieldPanel([
+            FieldPanel('half_width_link_blob_group_header'),
+            StreamFieldPanel('half_width_link_blob_content'),
+        ], heading='Half Width Link Blob'),
+        StreamFieldPanel('wells'),
         StreamFieldPanel('sidebar'),
     ]
 
