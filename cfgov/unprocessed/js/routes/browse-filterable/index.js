910480e13bcef43e39704682ae81d00e9e0f0e3e
James Wilson <jimmynotjim@me.com>
2015-11-23 15:14:51 -0500

    Prototyping a better multi-select
    - Based on UK.gov checkbox multi-choice pattern
    - Filtering script based on Awesomecomplete
    - jQuery free
    - Needs 508 testing
    - Doesn't support option groups yet
    - There's a tab bug due to a change in visibility hidden handling in webkit
    - Styled default multi-select styles in cf-enhancements

--- cfgov/unprocessed/js/routes/browse-filterable/index.js
+++ cfgov/unprocessed/js/routes/browse-filterable/index.js
@@ -7,6 +7,7 @@
 // List of organisms used.
 var Notification = require( '../../molecules/Notification' );
 var FilterableListControls = require( '../../organisms/FilterableListControls' );
+var Multiselect = require( '../../molecules/Multiselect' );
 
 var notifications = document.querySelectorAll( '.m-notification' );
 var notification;
@@ -14,3 +15,5 @@ for ( var i = 0, len = notifications.length; i < len; i++ ) {
 	notification = new Notification( notifications[i] ).init();
 }
 var filterableListControls = new FilterableListControls( document.body ).init();
+var multiselect = new Multiselect( document.querySelector( 'select[multiple]' ) );
+multiselect.init();
