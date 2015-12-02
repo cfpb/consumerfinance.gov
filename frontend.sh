4be3301f249500a97d52a1b38cfe3f27957917b3
Emmanuel Apau <emmanuel.apau@cfpb.gov>
2015-12-02 11:17:50 -0500

    broke setup script into front/backend scripts

---
+++ frontend.sh
@@ -0,0 +1,99 @@
+#!/bin/sh
+
+# ==========================================================================
+# Setup script for installing project dependencies.
+# NOTE: Run this script while in the project root directory.
+#       It will not run correctly when run from another directory.
+# ==========================================================================
+
+# Set script to exit on any errors.
+set -e
+
+# Initialize project dependency directories.
+init(){
+  ENVVAR_SAMPLE=.env_SAMPLE
+  ENVVAR=.env
+  NODE_DIR=node_modules
+  BOWER_DIR=bower_components
+
+  # Copy sample environment variables, if not already done.
+  if [ ! -f $ENVVAR ]; then
+    echo 'Setting default environment variables...'
+    cp -a $ENVVAR_SAMPLE $ENVVAR
+  fi
+
+  if [ -f .bowerrc ]; then
+    # Get the "directory" line from .bowerrc
+    BOWER_DIR=$(grep "directory" .bowerrc | cut -d '"' -f 4)
+  fi
+
+  echo 'Environment variables:' $ENVVAR
+  echo 'npm components directory:' $NODE_DIR
+  echo 'Bower components directory:' $BOWER_DIR
+}
+
+# Clean project dependencies.
+clean(){
+  # If the node and bower directories already exist,
+  # clear them so we know we're working with a clean
+  # slate of the dependencies listed in package.json
+  # and bower.json.
+  if [ -d $NODE_DIR ] || [ -d $BOWER_DIR ]; then
+    echo 'Removing project dependency directories...'
+    rm -rf $NODE_DIR
+    rm -rf $BOWER_DIR
+  fi
+  echo 'Project dependencies have been removed.'
+}
+
+# Install project dependencies.
+install(){
+  echo 'Installing project dependencies...'
+
+  # Copy globally-installed packages.
+  # Protractor - JavaScript acceptance testing.
+  if [ $(is_installed protractor) = 0 ]; then
+    echo 'Installing Protractor dependencies locally...'
+    npm install protractor
+    ./$NODE_DIR/protractor/bin/webdriver-manager update
+  else
+    echo 'Global Protractor installed. Copying global install locally...'
+    protractor_symlink=$(command -v protractor)
+    protractor_binary=$(readlink $protractor_symlink)
+    protractor_full_path=$(dirname $protractor_symlink)/$(dirname $protractor_binary)/../../protractor
+    if [ ! -d $protractor_full_path/selenium ]; then
+      echo '\033[33;31m ERROR: Please run `webdriver-manager update` and try again!'
+      exit
+    fi
+    mkdir -p ./$NODE_DIR/protractor
+    cp -r $protractor_full_path ./$NODE_DIR/
+    rm -rf ./$NODE_DIR/protractor/node_modules/
+  fi
+
+  npm install
+  bower install --config.interactive=false
+}
+
+# Run tasks to build the project for distribution.
+build(){
+  echo 'Building project...'
+  gulp clean
+  gulp build
+}
+
+# Returns 1 if a global command-line program installed, else 0.
+# For example, echo "node: $(is_installed node)".
+is_installed(){
+  # Set to 1 initially.
+  local return_=1
+
+  # Set to 0 if program is not found.
+  type $1 >/dev/null 2>&1 || { local return_=0; }
+
+  echo "$return_"
+}
+
+init
+clean
+install
+build
