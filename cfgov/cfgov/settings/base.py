0188858526b10ede5057acfd089fe27d5aec4598
Emmanuel Apau <emmanuel.apau@cfpb.gov>
2015-12-04 14:45:59 -0500

    updated helper text for clarity

--- cfgov/cfgov/settings/base.py
+++ cfgov/cfgov/settings/base.py
@@ -264,7 +264,7 @@ PDFREACTOR_LIB = os.environ.get('PDFREACTOR_LIB', '/opt/PDFreactor/wrappers/pyth
 # S3 Configuration
 if os.environ.get('S3_ENABLED', False):
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-    AWS_S3_SECURE_URLS = False  # use http instead of https
+    AWS_S3_SECURE_URLS = False  # True = use https; False = use http
     AWS_QUERYSTRING_AUTH = False  # don't add complex authentication-related query parameters for requests
     AWS_S3_ACCESS_KEY_ID = os.environ.get('AWS_S3_ACCESS_KEY_ID')
     AWS_S3_SECRET_ACCESS_KEY = os.environ.get('AWS_S3_SECRET_ACCESS_KEY')
