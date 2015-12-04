639c09edb29f5361933eb26fb19ae73dca0d7a8b
kurtw <kurtw@users.noreply.github.com>
2015-12-04 10:30:41 -0500

    Add Wagtail Styleguide to local settings

--- cfgov/cfgov/settings/local.py
+++ cfgov/cfgov/settings/local.py
@@ -1,7 +1,7 @@
 from .base import *
 
 DEBUG = True
-INSTALLED_APPS += ('django_extensions',)
+INSTALLED_APPS += ('django_extensions', 'wagtail.contrib.wagtailstyleguide')
 
 DATABASES = {
     'default': {
