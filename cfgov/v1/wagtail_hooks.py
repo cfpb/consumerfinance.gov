eb3143cccb715fe882c074953d09936e07ac2d95
Emmanuel Apau <emmanuel.apau@cfpb.gov>
2015-12-08 14:40:39 -0500

    prototype pages json creation

--- cfgov/v1/wagtail_hooks.py
+++ cfgov/v1/wagtail_hooks.py
@@ -1,5 +1,6 @@
-import os
+import os, json, itertools
 
+from django.conf import settings
 from django.http import Http404
 from django.contrib.auth.models import Permission
 
@@ -28,6 +29,8 @@ def share_the_page(request, page):
     if is_publishing:
         revision.publish()
 
+    update_pages_json(request, page)
+
 
 @hooks.register('before_serve_page')
 def check_request_site(page, request, serve_args, serve_kwargs):
@@ -40,3 +43,37 @@ def check_request_site(page, request, serve_args, serve_kwargs):
 @hooks.register('register_permissions')
 def register_share_permissions():
     return Permission.objects.filter(codename='share_page')
+
+
+def update_pages_json(request, page):
+    page = page.specific
+
+    data = json.load(open(settings.PROJECT_ROOT.child('v1') + '/pages.json'))
+
+    # Base page doesn't have a generic ALL children object so you must create cases for each
+    # repetitive
+
+    elements = []
+
+    if hasattr(page, 'demopage'):
+        chain = itertools.chain(page.organisms.stream_data, page.molecules.stream_data)
+    if hasattr(page, 'landingpage'):
+        chain = itertools.chain(page.hero.stream_data,
+                                page.introduction.stream_data,
+                                page.image_text_25_75_content.stream_data,
+                                page.image_text_50_50_content.stream_data,
+                                page.half_width_link_blob_content.stream_data,
+                                page.wells.stream_data
+                                )
+
+    children = list(chain)
+    for child in children:
+        elements.append(child[0])
+
+    data['page_' + str(page.id)] = {
+        'slug': str(page.slug),
+        'elements': elements
+    }
+
+    with open(settings.PROJECT_ROOT.child('v1') + '/pages.json', 'w') as outfile:
+        json.dump(data, outfile)
