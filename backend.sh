7981a5916219804a76dac09aebb832006d179d5d
Anselm Bradford <Anselm.Bradford@cfpb.gov>
2015-12-16 14:12:00 -0500

    Separates dev and production nom modules

--- backend.sh
+++ backend.sh
@@ -11,6 +11,30 @@ set -e
 
 # Initialize project dependency directories.
 init(){
+  # Set default command-line environment flag, if user didn't supply one.
+  if [ -z "$1" ]; then
+    cli_flag='development'
+  else
+    cli_flag=$1
+  fi
+
+  # Warn if unsupported command-line flag was used.
+  if [ "$cli_flag" != "development" ] &&
+     [ "$cli_flag" != "test" ] &&
+     [ "$cli_flag" != "production" ]; then
+    echo "\033[33;33mWARNING: '$cli_flag' flag not found, reverting to development environment.\033[0m"
+    cli_flag='development'
+  fi
+
+  # Notify of environment that user is in.
+  if [ "$cli_flag" = "development" ]; then
+    echo 'Frontend:\033[33;33m development environment\033[0m.'
+  elif [ "$cli_flag" = "test" ]; then
+    echo 'Frontend:\033[33;1m test environment\033[0m.'
+  elif [ "$cli_flag" = "production" ]; then
+    echo 'Frontend:\033[32;1m production environment\033[0m.'
+  fi
+
   ENVVAR_SAMPLE=.env_SAMPLE
   ENVVAR=.env
 
@@ -28,19 +52,21 @@ init(){
 build(){
   echo 'Installing project dependencies...'
 
+  # Install requirements for Django Server or tox.
+  if [ "$cli_flag" = "development" ]; then
+    pip install -r ./requirements/local.txt
+  elif [ "$cli_flag" = "test" ]; then
+    pip install -r ./requirements/test.txt
+  elif [ "$cli_flag" = "production" ]; then
+    pip install -r ./requirements/base.txt
+  fi
+
   # Update test dependencies.
   # Macro Polo - Jinja template unit testing.
   pip install -r ./test/macro_tests/requirements.txt
 
   # Tox - Django server unit testing.
   pip install tox
-
-  # Django Server
-  if [ -z "$1" ]; then
-    pip install -r ./requirements/local.txt
-  else
-    pip install -r ./requirements/$1.txt
-  fi
 
   dbsetup
 }
@@ -73,6 +99,5 @@ is_installed(){
   echo "$return_"
 }
 
-init
-build $1
-
+init "$1"
+build
