0a4ee527ea002a79b2177354ff5e848f6d827250
Emmanuel Apau <emmanuel.apau@cfpb.gov>
2015-12-22 09:41:54 -0500

    removed restrictions until after first release

--- cfgov/v1/models/molecules.py
+++ cfgov/v1/models/molecules.py
@@ -12,7 +12,7 @@ def isRequired(field_name):
 
 
 class HalfWidthLinkBlob(blocks.StructBlock):
-    heading = blocks.CharBlock(max_length=100, required=True)
+    heading = blocks.CharBlock(max_length=100, required=False)
     body = blocks.RichTextBlock(blank=True)
     links = blocks.ListBlock(atoms.Hyperlink(), required=False)
 
@@ -22,7 +22,7 @@ class HalfWidthLinkBlob(blocks.StructBlock):
 
 
 class ImageText5050(blocks.StructBlock):
-    heading = blocks.CharBlock(max_length=100, required=True)
+    heading = blocks.CharBlock(max_length=100, required=False)
     body = blocks.RichTextBlock(blank=True)
     image = atoms.ImageBasic()
     is_widescreen = blocks.BooleanBlock(required=False, label="Use 16:9 image")
@@ -35,8 +35,8 @@ class ImageText5050(blocks.StructBlock):
 
 
 class ImageText2575(blocks.StructBlock):
-    heading = blocks.CharBlock(max_length=100, required=True)
-    body = blocks.RichTextBlock(required=True)
+    heading = blocks.CharBlock(max_length=100, required=False)
+    body = blocks.RichTextBlock(required=False)
     image = atoms.ImageBasic()
     links = blocks.ListBlock(atoms.Hyperlink(), required=False)
     has_rule = blocks.BooleanBlock(required=False)
@@ -47,7 +47,7 @@ class ImageText2575(blocks.StructBlock):
 
 
 class TextIntroduction(blocks.StructBlock):
-    heading = blocks.CharBlock(max_length=100, required=True)
+    heading = blocks.CharBlock(max_length=100, required=False)
     intro = blocks.RichTextBlock(required=False)
     body = blocks.RichTextBlock(required=False)
     links = blocks.ListBlock(atoms.Hyperlink(required=False), required=False)
@@ -59,7 +59,7 @@ class TextIntroduction(blocks.StructBlock):
 
 
 class Hero(blocks.StructBlock):
-    heading = blocks.CharBlock(max_length=100, required=True)
+    heading = blocks.CharBlock(max_length=100, required=False)
     body = blocks.RichTextBlock(required=False)
 
     image = atoms.ImageBasic()
@@ -74,12 +74,12 @@ class Hero(blocks.StructBlock):
 
 
 class FormFieldWithButton(blocks.StructBlock):
-    btn_text = blocks.CharBlock(max_length=100, required=True)
+    btn_text = blocks.CharBlock(max_length=100, required=False)
 
     required = blocks.BooleanBlock(required=False)
-    id = blocks.CharBlock(max_length=100, required=True)
+    id = blocks.CharBlock(max_length=100, required=False)
     info = blocks.RichTextBlock(required=False, label="Disclaimer")
-    label = blocks.CharBlock(max_length=100, required=True)
+    label = blocks.CharBlock(max_length=100, required=False)
     type = blocks.ChoiceBlock(choices=[
         ('text', 'Text'),
         ('checkbox', 'Checkbox'),
@@ -87,7 +87,7 @@ class FormFieldWithButton(blocks.StructBlock):
         ('number', 'Number'),
         ('url', 'URL'),
         ('radio', 'Radio'),
-    ], required=True)
+    ], required=False)
     placeholder = blocks.CharBlock(max_length=100, required=False)
 
     def clean(self, data):
@@ -113,8 +113,8 @@ class FormFieldWithButton(blocks.StructBlock):
 
 
 class CallToAction(blocks.StructBlock):
-    slug_text = blocks.CharBlock(max_length=100, required=True)
-    paragraph_text = blocks.RichTextBlock(required=True)
+    slug_text = blocks.CharBlock(max_length=100, required=False)
+    paragraph_text = blocks.RichTextBlock(required=False)
     button = atoms.Hyperlink()
 
     class Meta:
@@ -124,11 +124,11 @@ class CallToAction(blocks.StructBlock):
 
 
 class ContactAddress(blocks.StructBlock):
-    label = blocks.CharBlock(max_length=50)
+    label = blocks.CharBlock(max_length=50, required=False)
     title = blocks.CharBlock(max_length=100, required=False)
-    street = blocks.CharBlock(max_length=100)
-    city = blocks.CharBlock(max_length=50)
-    state = blocks.CharBlock(max_length=25)
+    street = blocks.CharBlock(max_length=100, required=False)
+    city = blocks.CharBlock(max_length=50, required=False)
+    state = blocks.CharBlock(max_length=25, required=False)
     zip_code = blocks.CharBlock(max_length=15, required=False)
 
     class Meta:
@@ -163,7 +163,7 @@ class ContactPhone(blocks.StructBlock):
 
 
 class RelatedLinks(blocks.StructBlock):
-    heading = blocks.CharBlock()
+    heading = blocks.CharBlock(required=False)
     links = blocks.ListBlock(atoms.Hyperlink())
 
     class Meta:
