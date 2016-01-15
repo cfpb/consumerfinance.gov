d272a978041233e4be34bcb0df8b7146fec7d9a7
Emmanuel Apau <emmanuel.apau@cfpb.gov>
2016-01-15 11:51:06 -0500

    error checking in the init data migration

--- cfgov/v1/migrations/9999_init_data.py
+++ cfgov/v1/migrations/9999_init_data.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 import os
 
 from django.contrib.auth.hashers import make_password
-from django.contrib.auth.models import User
 from django.db import migrations, models
 from django.conf import settings
 
@@ -15,24 +14,28 @@ from wagtail.wagtailcore.models import Page
 
 def initial_data(apps, schema_editor):
     if settings.DEBUG:
-        admin_user = User(username='admin',
-                          password=make_password(os.environ.get('ADMIN_PW')),
-                          is_superuser=True, is_active=True, is_staff=True)
-        admin_user.save()
+        UserModel = apps.get_model(settings.AUTH_USER_MODEL)
+        if not UserModel.objects.filter(username='admin').exists():
+            admin_user = UserModel(username='admin',
+                              password=make_password(os.environ.get('WAGTAIL_ADMIN_PW')),
+                              is_superuser=True, is_active=True, is_staff=True)
+
+            admin_user.save()
 
         # Renaming Site root name to `V1`
-        site_root = Page.objects.get(id=2)
-        site_root.title = 'V1'
-        site_root.save()
+        if not Page.objects.filter(title='V1').exists():
+            site_root = Page.objects.get(id=2)
+            site_root.title = 'V1'
+            site_root.save()
 
         # Events Landing Page required for event `import-data` command
-        events = EventLandingPage(title='Events', slug='events')
-        site_root.add_child(instance=events)
-        revision = events.save_revision(
-            user=admin_user,
-            submitted_for_moderation=False,
-        )
-        revision.publish()
+        if not EventLandingPage.objects.filter(title='Events').exists():
+            events = EventLandingPage(title='Events', slug='events')
+            site_root.add_child(instance=events)
+            revision = events.save_revision(
+                submitted_for_moderation=False,
+            )
+            revision.publish()
 
 
 class Migration(migrations.Migration):
