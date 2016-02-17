766b210bdb883b822f7e0d8529bca262247cc5ac
Richa <richa.agarwal85@gmail.com>
2016-02-17 08:54:34 -0800

    Add a couple needed things back in

--- cfgov/cfgov/urls.py
+++ cfgov/cfgov/urls.py
@@ -232,6 +232,8 @@ urlpatterns = [
         namespace='transcripts')),
 ]
 
+if settings.DEBUG :     
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  
 
 # Catch remaining URL patterns that did not match a route thus far.
 urlpatterns.append(url(r'', include(wagtail_urls)))
