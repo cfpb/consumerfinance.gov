d97fe291267dd2bd3531b0f2782848f127ac8e9d
Victor Cabral <victor.cabral@cfpb.gov>
2016-02-17 11:17:26 -0500

    basic local develop configuration

--- cfgov/cfgov/settings/local.py
+++ cfgov/cfgov/settings/local.py
@@ -6,9 +6,9 @@ INSTALLED_APPS += ('django_extensions', 'wagtail.contrib.wagtailstyleguide')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('MYSQL_NAME'),
-        'USER': os.environ.get('MYSQL_USER'),
-        'PASSWORD': os.environ.get('MYSQL_PW', ''),
+        'NAME': os.environ.get('MYSQL_NAME', 'cfgov'),
+        'USER': os.environ.get('MYSQL_USER', 'cfgov'),
+        'PASSWORD': os.environ.get('MYSQL_PW', 'cfgov123'),
         'HOST': os.environ.get('MYSQL_HOST', ''),  # empty string == localhost
         'PORT': os.environ.get('MYSQL_PORT', ''),  # empty string == default
     },
