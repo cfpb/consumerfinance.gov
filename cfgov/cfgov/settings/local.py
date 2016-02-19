2c3cfa8eaaf810a5fb900ea19f7945b78fbe8504
Victor Cabral <victor.cabral@cfpb.gov>
2016-02-19 10:51:30 -0500

    total reverse based on newly found environment variables will not be needed

--- cfgov/cfgov/settings/local.py
+++ cfgov/cfgov/settings/local.py
@@ -6,8 +6,8 @@ INSTALLED_APPS += ('django_extensions', 'wagtail.contrib.wagtailstyleguide')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('MYSQL_NAME', 'v1'),
-        'USER': os.environ.get('MYSQL_USER', 'root'),
+        'NAME': os.environ.get('MYSQL_NAME'),
+        'USER': os.environ.get('MYSQL_USER'),
         'PASSWORD': os.environ.get('MYSQL_PW', ''),
         'HOST': os.environ.get('MYSQL_HOST', ''),  # empty string == localhost
         'PORT': os.environ.get('MYSQL_PORT', ''),  # empty string == default
