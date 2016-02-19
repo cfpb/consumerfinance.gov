d2870d28de309ab329c9ef77aac705e39fc8dfff
Victor Cabral <victor.cabral@cfpb.gov>
2016-02-19 10:48:06 -0500

    going back to old version of passwords

--- cfgov/cfgov/settings/local.py
+++ cfgov/cfgov/settings/local.py
@@ -6,9 +6,9 @@ INSTALLED_APPS += ('django_extensions', 'wagtail.contrib.wagtailstyleguide')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('MYSQL_NAME', 'cfgov'),
-        'USER': os.environ.get('MYSQL_USER', 'cfgov'),
-        'PASSWORD': os.environ.get('MYSQL_PW', 'cfgov123'),
+        'NAME': os.environ.get('MYSQL_NAME', 'v1'),
+        'USER': os.environ.get('MYSQL_USER', 'root'),
+        'PASSWORD': os.environ.get('MYSQL_PW', ''),
         'HOST': os.environ.get('MYSQL_HOST', ''),  # empty string == localhost
         'PORT': os.environ.get('MYSQL_PORT', ''),  # empty string == default
     },
