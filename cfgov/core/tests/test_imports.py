4eacdc5c0c2c3e2620d262f5543219081b946917
Richa <richa.agarwal85@gmail.com>
2016-02-25 01:28:39 -0800

    Add tests for getting processors

--- cfgov/core/tests/test_imports.py
+++ cfgov/core/tests/test_imports.py
@@ -1,8 +1,19 @@
 from django.test import TestCase
+from django.conf import settings
 from v1.processors import eventpage, contact
+from core.management.commands import import_data
 import collections
 
 class ImportDataTest(TestCase):
+
+    def test_get_event_processor(self):
+        processor = import_data.Command().get_processor('events', settings.SHEER_PROCESSORS)
+        self.assertEqual(processor.__name__, 'processors.wordpress_event')
+
+    def test_get_contact_processor(self):
+        processor = import_data.Command().get_processor('contact', settings.SHEER_PROCESSORS)
+        self.assertEqual(processor.__name__, 'processors.wordpress_contact')
+
     def test_contact_convert(self):
         imported_data = {u'attachments': [], u'relative_url': u'/contact/your-money-your-goals-toolkit/', u'excerpt': u'<p>Your Money, Your Goals is a toolkit for social service organizations and others to use when working with low- and moderate-income and underserved consumers. The Office of Financial Empowerment can help you get started using YMYG in your work.</p>\n', u'id': 37397, 'web': {'url': u'http://www.consumerfinance.gov/your-money-your-goals/'}, u'author': {u'first_name': u'James', u'last_name': u'Hupp', u'name': u'James Hupp', u'url': u'', u'slug': u'jhupp', u'nickname': u'jhupp', u'id': 4, u'description': u''}, u'content': u'<p>Your Money, Your Goals is a toolkit for social service organizations and others to use when working with low- and moderate-income and underserved consumers. The Office of Financial Empowerment can help you get started using YMYG in your work.</p>\n', u'comment_count': 0, u'categories': [], u'type': u'contact', 'email': [{'addr': u'empowerment@consumerfinance.gov'}], u'status': u'publish', u'parent': 0, u'tags': [], 'phone': [], u'date': u'2015-04-19T23:47:14-0500', u'slug': u'your-money-your-goals-toolkit', u'comment_status': u'closed', u'title_plain': u'Your Money, Your Goals Toolkit', u'url': u'http://www.consumerfinance.gov/contact/your-money-your-goals-toolkit/', u'title': u'Your Money, Your Goals Toolkit', u'modified': u'2015-04-27T10:55:34+0000', u'dek': u'', '_id': u'your-money-your-goals-toolkit', u'order': 0}
         output = contact.DataConverter().convert(imported_data)
