079ea3f0dfdcad06f14132adfb28085f273a5a9f
Richa <richa.agarwal85@gmail.com>
2016-02-26 20:09:30 -0800

    Break convert into lots of smaller functions

--- cfgov/v1/processors/eventpage.py
+++ cfgov/v1/processors/eventpage.py
@@ -4,6 +4,101 @@ from core.management.commands._helpers import PageDataConverter
 
 
 class DataConverter(PageDataConverter):
+    def format_tags(self, doc_tags):
+        tags = ''
+        for tag in doc_tags:
+            if ' ' in tag:
+                tag = '"%s"' % tag
+            tags += tag + ', '
+        if tags:
+            tags = tags[0:-2]
+        return tags
+
+    def format_author(self, author):
+        if not author or not author.get('name'):
+            return u''
+        return '"%s"' % author.get('name')
+
+    def format_venue_name(self, venue):
+        if not venue or not venue.get('name'):
+            return u''
+        return venue['name']
+
+    def format_venue_address_info(self, venue, info):
+        if not venue or not venue.get('address') or not venue['address'].get(info):
+            return u''
+        return venue['address'][info]
+
+    def format_livestream(self, livestream, info):
+        if not livestream or not livestream.get(info):
+            return u''
+        return livestream[info]
+
+    def format_time_period_content(self, doc_tense, tense):
+        # Time period content
+        if not doc_tense or not doc_tense.get('summary'):
+            return u''
+        summary = doc_tense['summary']
+        if 'http://content' in summary:
+            summary = summary.replace('http://content', 'http://www')
+        return summary
+
+    def format_agenda_times(self, doc_agenda, t, index):
+        if not doc_agenda[index] or not doc_agenda[index].get(t[0]):
+            return u''
+        if doc_agenda[index].get(t[0]).get('date'):
+            time = doc_agenda[index][t[0]]['date']
+            dt = dateutil.parser.parse(time)
+            return dt.strftime('%Y-%m-%d %H:%M')
+
+    def get_agenda_dict(self, doc_agenda):
+        # Agenda
+        agenda_dict = {}
+        if not doc_agenda:
+            agenda_dict['agenda_items-count'] = 0
+            return agenda_dict
+        agenda_dict['agenda_items-count'] = len(doc_agenda)
+        for i in range(len(doc_agenda)):
+            for info in (('order', i), ('type', 'item'), ('deleted', u'')):
+                agenda_dict['agenda_items-'+str(i)+'-'+info[0]] = info[1]
+            for t in [('beginning_time', 'start_dt'),('ending_time', 'end_dt')]:
+                agenda_dict['agenda_items-'+str(i)+'-value-' + t[1]] = self.format_agenda_times(doc_agenda, t, i)
+
+            agenda_dict['agenda_items-'+str(i)+'-value-description'] = doc_agenda[i].get('desc', u'')
+            agenda_dict['agenda_items-'+str(i)+'-value-location'] = doc_agenda[i].get('location', u'')
+            agenda_dict['agenda_items-'+str(i)+'-value-speakers-count'] = len(doc_agenda[i].get('speakers', u''))
+            for y in range(len(doc_agenda[i].get('speakers', u''))):
+                speakers_prefix = 'agenda_items-'+str(i)+'-value-speakers-' + str(y)
+                agenda_dict[speakers_prefix+'-value-name'] = doc_agenda[i]['speakers'][y].get('name', u'')
+                agenda_dict[speakers_prefix+'-value-url'] = doc_agenda[i]['speakers'][y].get('url', u'')
+                agenda_dict[speakers_prefix+'-order'] = y
+                agenda_dict[speakers_prefix+'-deleted'] = u''
+        return agenda_dict
+
+    def get_flickr_url(self, archive):
+        if not archive or not archive.get('flickr'):
+            return u''
+        return archive['flickr']
+
+    def get_youtube_url(self, archive):
+        if not archive or not archive.get('youtube'):
+            return u''
+        return archive['youtube']
+
+    def get_times_dict(self, beginning_time, ending_time):
+        # Start and end times
+        times_dict = {}
+        times = [(beginning_time, 'start_dt'),
+                 (ending_time, 'end_dt')]
+        if times and times[0][0]:
+            date = dateutil.parser.parse(times[0][0].get('date'))
+            times_dict['date_published'] = date.strftime('%Y-%m-%d')
+        for t in times:
+            if t[0]:
+                dt = dateutil.parser.parse(t[0].get('date'))
+                times_dict[t[1]] = dt.strftime('%Y-%m-%d %H:%M')
+        return times_dict
+
     def convert(self, doc):
         post_dict = {
             'title':      doc.get('title', u''),
@@ -12,85 +107,20 @@ class DataConverter(PageDataConverter):
         }
         self.add_defaults(post_dict)
 
-        # Start and end times
-        times = [(doc.get('beginning_time', None), 'start_dt'),
-                 (doc.get('ending_time', None), 'end_dt')]
-        if times and times[0][0]:
-            date = dateutil.parser.parse(times[0][0].get('date'))
-            post_dict['date_published'] = date.strftime('%Y-%m-%d')
-        for t in times:
-            if t[0]:
-                dt = dateutil.parser.parse(t[0].get('date'))
-                post_dict[t[1]] = dt.strftime('%Y-%m-%d %H:%M')
+        post_dict['tags'] = self.format_tags(doc.get('tags'))
+        post_dict['authors'] = self.format_author(doc.get('author'))
+        post_dict['venue_name'] = self.format_venue_name(doc.get('venue'))
+        post_dict['flickr_url'] = self.get_flickr_url(doc.get('archive'))
+        post_dict['youtube_url'] = self.get_youtube_url(doc.get('archive'))
+        post_dict.update(self.get_agenda_dict(doc.get('agenda')))
+        post_dict.update(self.get_times_dict(doc.get('beginning_time'), doc.get('ending_time')))
 
-        # Tags and authors
-        tags = ''
-        for tag in doc.get('tags'):
-            if ' ' in tag:
-                tag = '"%s"' % tag
-            tags += tag + ', '
-        if tags:
-            tags = tags[0:-2]
-        post_dict['tags'] = tags
-        post_dict['authors'] = '"%s"' % doc.get('author', {}).get('name', '')
-
-        # Venue
-        if doc.get('venue'):
-            post_dict['venue_name'] = doc['venue'].get('name', u'')
-            if doc['venue'].get('address', u''):
-                for info in ['state', 'city', 'street', 'suite', 'zip']:
-                    post_dict['venue_'+info] = \
-                        doc['venue']['address'].get(info, u'')
-
-        # Live stream
-        if doc.get('live_stream'):
-            for info in ['url', 'availability', 'date']:
-                post_dict['live_stream_'+info] = doc['live_stream'].get(info,
-                                                                        u'')
-
-        # Time period content
+        for info in ['state', 'city', 'street', 'suite', 'zip']:
+            post_dict['venue_'+info] = self.format_venue_address_info(doc.get('venue'), info)
+        for info in ['url', 'availability', 'date']:
+            post_dict['live_stream_'+info] = self.format_livestream(doc.get('live_stream'), info)
         for tense in ['archive', 'live', 'future']:
-            if doc.get(tense):
-                summary = doc[tense].get('summary', u'')
-                if 'http://content' in summary:
-                    summary = summary.replace('http://content', 'http://www')
-                post_dict[tense+'_body'] = summary
-                if tense == 'archive':
-                    post_dict['flickr_url'] = doc['archive'].get('flickr', u'')
-                    post_dict['youtube_url'] = doc['archive'].get('youtube',
-                                                                  u'')
+            post_dict[tense+'_body'] = self.format_time_period_content(doc.get(tense), tense)
+        
 
-        # Agenda
-        post_dict['agenda_items-count'] = len(doc.get('agenda', u''))
-        if doc.get('agenda'):
-            for i in range(len(doc.get('agenda', u''))):
-                for info in (('order', i), ('type', 'item'), ('deleted', u'')):
-                    post_dict['agenda_items-'+str(i)+'-'+info[0]] = info[1]
-                times = [('beginning_time', 'start_dt'),
-                         ('ending_time', 'end_dt')]
-                for t in times:
-                    if doc['agenda'][i].get(t[0], u''):
-                        time = doc['agenda'][i].get(t[0]).get('date', u'')
-                        if time:
-                            dt = dateutil.parser.parse(time)
-                            post_dict['agenda_items-'+str(i)+'-value-' +
-                                      t[1]] = dt.strftime('%Y-%m-%d %H:%M')
-                post_dict['agenda_items-'+str(i)+'-value-description'] = \
-                    doc['agenda'][i].get('desc', u'')
-                post_dict['agenda_items-'+str(i)+'-value-location'] = \
-                    doc['agenda'][i].get('location', u'')
-                post_dict['agenda_items-'+str(i)+'-value-speakers-count'] = \
-                    len(doc['agenda'][i].get('speakers', u''))
-                if doc['agenda'][i].get('speakers'):
-                    for y in range(len(doc['agenda'][i].get('speakers', u''))):
-                        post_dict['agenda_items-'+str(i)+'-value-speakers-' +
-                                  str(y)+'-value-name'] = \
-                            doc['agenda'][i]['speakers'][y].get('name', u'')
-                        post_dict['agenda_items-'+str(i)+'-value-speakers-' +
-                                  str(y)+'-value-url'] = \
-                            doc['agenda'][i]['speakers'][y].get('url', u'')
-                        post_dict['agenda_items-'+str(i)+'-value-speakers-' +
-                                  str(y)+'-order'] = y
-                        post_dict['agenda_items-'+str(i)+'-value-speakers-' +
-                                  str(y)+'-deleted'] = u''
         return post_dict
