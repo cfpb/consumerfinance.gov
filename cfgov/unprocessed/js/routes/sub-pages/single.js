911a72a3d90e4c992ea2ce0087b5bd8f430da8ab
Kimberly Munoz <kimberly.munoz@cfpb.gov>
2016-03-16 13:36:32 -0400

    Removing all forklifted content from Sheer
    The following pages have been added to wagtail and no longer need to be
    hardcoded or added in with sheer. All relavent CSS and tests have also
    been deleted in this PR.
    
    Deleting extra forms on deleted subpages
    
    Removing static_built path
    
    Removing more tests that weren't caught before
    
    Fixed footer tests
    
    Removed unnecessary JS and CHANGELOG fix

--- cfgov/unprocessed/js/routes/sub-pages/single.js
+++
@@ -1,9 +0,0 @@
-/* ==========================================================================
-   Scripts for `/sub-pages/*`.
-   ========================================================================== */
-
-'use strict';
-
-var SecondaryNavigation = require( '../../organisms/SecondaryNavigation' );
-var sidebarDom = document.querySelector( '.content_sidebar' );
-var secondaryNavigation = new SecondaryNavigation( sidebarDom ).init();
