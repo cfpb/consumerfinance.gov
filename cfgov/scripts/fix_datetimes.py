65e6ddca80707a050a8b4f44f8eaf429a2b081d4
Richa <richa.agarwal85@gmail.com>
2016-03-24 22:43:41 -0700

    Update script

--- cfgov/scripts/fix_datetimes.py
+++ cfgov/scripts/fix_datetimes.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 from v1.models.learn_page import EventPage, AgendaItemBlock
+from pytz import timezone
 
 # We have been storing dates selected from the Datepicker as UTC, 
 # without first telling the system the date was in ET at selection.
@@ -11,23 +12,44 @@ def run():
 
     # Update EventPages
     for e in EventPage.objects.all().filter(start_dt__isnull=False):
-        e.start_dt += timedelta(hours=4)
+        previous_dt = e.start_dt
+        e.start_dt = e.start_dt.replace(tzinfo=None)
+        e.start_dt = timezone('US/Eastern').localize(e.start_dt)
+        e.start_dt = e.start_dt.astimezone(timezone('UTC'))
         e.save()
+        print "Updated start_dt for EventPage '%s' from %s to %s " %(e.title, previous_dt, e.start_dt)
 
     for e in EventPage.objects.all().filter(end_dt__isnull=False):
-        e.end_dt += timedelta(hours=4)
+        previous_dt = e.end_dt
+        e.end_dt = e.end_dt.replace(tzinfo=None)
+        e.end_dt = timezone('US/Eastern').localize(e.end_dt)
+        e.end_dt = e.end_dt.astimezone(timezone('UTC'))
         e.save()
+        print "Updated end_dt for EventPage '%s' from %s to %s " %(e.title, previous_dt, e.end_dt)
 
     for e in EventPage.objects.all().filter(live_stream_date__isnull=False):
-        e.live_stream_date += timedelta(hours=4)
+        previous_dt = e.live_stream_date
+        e.live_stream_date = e.live_stream_date.replace(tzinfo=None)
+        e.live_stream_date = timezone('US/Eastern').localize(e.live_stream_date)
+        e.live_stream_date = e.live_stream_date.astimezone(timezone('UTC'))
         e.save()
+        print "Updated live_stream_date for EventPage '%s' from %s to %s " %(e.title, previous_dt, e.live_stream_date)
 
     for e in EventPage.objects.all():
         for a in e.agenda_items:
             if a.value['start_dt']:
-                a.value['start_dt'] += timedelta(hours=4)
+                previous_dt = a.value['start_dt']
+                a.value['start_dt'] = a.value['start_dt'].replace(tzinfo=None)
+                a.value['start_dt'] = timezone('US/Eastern').localize(a.value['start_dt'])
+                a.value['start_dt'] = a.value['start_dt'].astimezone(timezone('UTC'))
+                print "Updated start_dt for Agenda Item '%s' from %s to %s " %(a.value['description'], previous_dt, a.value['start_dt'])
             if a.value['end_dt']:
-                a.value['end_dt'] += timedelta(hours =4)
+                previous_dt = a.value['end_dt']
+                a.value['end_dt'] = a.value['end_dt'].replace(tzinfo=None)
+                a.value['end_dt'] = timezone('US/Eastern').localize(a.value['end_dt'])
+                a.value['end_dt'] = a.value['end_dt'].astimezone(timezone('UTC'))
+                print "Updated end_dt for Agenda Item '%s' from %s to %s " %(a.value['description'], previous_dt, a.value['end_dt'])
         e.save()
+        e.save_revision()
 
 
