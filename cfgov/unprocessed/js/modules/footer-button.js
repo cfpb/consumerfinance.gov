f50c00f08a772cd2c9cbc6871947641f6c1a8175
Anselm Bradford <Anselm.Bradford@cfpb.gov>
2016-03-28 17:20:48 -0400

    Disables GTM in return to top link

--- cfgov/unprocessed/js/modules/footer-button.js
+++ cfgov/unprocessed/js/modules/footer-button.js
@@ -6,16 +6,26 @@
 
 var $ = require( 'jquery' );
 
+var ANIMATION_DURATION = 300;
+
 /**
  * Set up event handler for button to scroll to top of page.
+ * Add event listeners and disable Google Tag Manager tracking
+ * for a link click.
  */
 function init() {
-  var duration = 300;
+  var footerBtnDom = document.querySelector( '.js-return-to-top' );
+  footerBtnDom.addEventListener( 'click', _handleFooterBtnClick );
 
-  $( '.js-return-to-top' ).click( function( event ) {
-    event.preventDefault();
-    $( 'html, body' ).animate( { scrollTop: 0 }, duration );
-  } );
+  // Disable Google Tag Manager tracking on this link.
+  footerBtnDom.setAttribute( 'data-gtm_ignore', 'true' );
+}
+
+function _handleFooterBtnClick( event ) {
+  event.preventDefault();
+
+  // TODO: Investigate using a CSS transition in place of jquery.
+  $( 'html, body' ).animate( { scrollTop: 0 }, ANIMATION_DURATION );
 }
 
 module.exports = { init: init };
