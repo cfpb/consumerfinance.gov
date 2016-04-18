e8eaa5601ab8e70a01f2ab844dc1542eefde12df
James Wilson <jimmynotjim@me.com>
2016-04-18 14:33:19 -0500

    Added empty test specs to create an accurate coverage report
    Before we were testing any and all js files, including config, polyfill, ie-specific, and old jQuery scripts. To better understand our true coverage, I turned off the `includeUntested` flag and created specs for only the files we should be testing. I also included two tests in the Multiselect I had already been working on previously.
    
    Baseline report created by this PR is:
    
    ```
    =============================== Coverage summary ===============================
    Statements   : 55.05% ( 1178/2140 )
    Branches     : 39.77% ( 278/699 )
    Functions    : 44.58% ( 144/323 )
    Lines        : 55.36% ( 1168/2110 )
    ================================================================================
    ```

---
+++ test/unit_tests/modules/simple-table-row-links-spec.js
@@ -0,0 +1,10 @@
+'use strict';
+var BASE_JS_PATH = '../../../cfgov/unprocessed/js/';
+
+var chai = require( 'chai' );
+var expect = chai.expect;
+var simpleTableRowLinks = require( BASE_JS_PATH + 'modules/simple-table-row-links' );
+
+describe( 'simple-table-row-links', function() {
+
+} );
---
+++ test/unit_tests/modules/util/data-set-spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var BASE_JS_PATH = '../../../../cfgov/unprocessed/js/';
+
+var chai = require( 'chai' );
+var expect = chai.expect;
+var dataSet = require( BASE_JS_PATH + 'modules/util/data-set' );
+
+describe( 'data-set', function() {
+
+} );
