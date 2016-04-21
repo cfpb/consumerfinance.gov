4b8996835ea903ef8cee73e044caa016aa47703e
Ross Karchner <ross.karchner@cfpb.gov>
2016-04-21 10:38:48 -0400

    add URL patterns for picard and the admin part on company-signup
    add Task Launcher to the django admin navigation

--- cfgov/cfgov/settings/local.py
+++ cfgov/cfgov/settings/local.py
@@ -11,6 +11,7 @@ DATABASES = {
         'PASSWORD': os.environ.get('MYSQL_PW', ''),
         'HOST': os.environ.get('MYSQL_HOST', ''),  # empty string == localhost
         'PORT': os.environ.get('MYSQL_PORT', ''),  # empty string == default
+        'OPTIONS': {'init_command': 'SET storage_engine=MYISAM', },
     },
 }
 
