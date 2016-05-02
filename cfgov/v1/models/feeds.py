7a64f57583a8771a7636c4cc0d693d9309bd5d9f
Ross Karchner <ross.karchner@cfpb.gov>
2016-05-02 11:45:12 -0400

    More metadata!

--- cfgov/v1/models/feeds.py
+++ cfgov/v1/models/feeds.py
@@ -6,13 +6,19 @@ import pytz
 
 eastern = pytz.timezone('US/Eastern')
 
+
 class FilterableFeed(Feed):
+    item_guid_is_permalink = False
+
     def __init__(self, page, context):
         self.page = page
         self.context = context
 
     def link(self):
         return self.page.full_url
+
+    def author_name(self):
+        return "Consumer Financial Protection Bureau"
 
     def title(self):
         return "%s | Consumer Financial Protection Bureau" % self.page.title
@@ -31,6 +37,20 @@ class FilterableFeed(Feed):
 
     def item_description(self, item):
         return item.preview_description
+
+    def item_categories(self, item):
+        categories = [cat.get_name_display() for cat in item.categories.all()]
+        tags = [tag.name for tag in item.tags.all()]
+        return categories + tags
+
+    def item_author_name(self, item):
+        if hasattr(item, 'authors'):
+            author_names = [a.name for a in item.authors.all()]
+            author_string = ', '.join(author_names)
+            return author_string
+
+    def item_guid(self, item):
+        return "%s<>consumerfinance.gov" % item.page_ptr_id
 
 
 class FilterableFeedPageMixin(object):
