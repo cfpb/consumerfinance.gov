5775d55e7a1995c971235082faa8222ad26de59e
Ross Karchner <ross.karchner@cfpb.gov>
2016-05-04 10:11:12 -0400

    now to actually write some tests

--- cfgov/v1/util/password_policy.py
+++ cfgov/v1/util/password_policy.py
@@ -4,15 +4,12 @@ import re
 from django.conf import settings
 from django.contrib.auth import hashers
 from django.core.exceptions import ObjectDoesNotExist, ValidationError
-from django.contrib.auth import get_user_model
-from django.utils import timezone
-from django.core.exceptions import ValidationError, ObjectDoesNotExist
-from django.conf import settings
+
 
 def validate_password_rule(regex, password,
                            password_field, failure_message='invalid password'):
     if not re.search(regex, password):
-        raise ValidationError({password_field:failure_message})
+        raise ValidationError({password_field: failure_message})
 
 
 def validate_password_all_rules(password, password_field):
@@ -20,22 +17,30 @@ def validate_password_all_rules(password, password_field):
         validate_password_rule(regex, password, password_field, rule_name)
 
 
-def _check_passwords(password, user, password_field):
-    validate_password_all_rules(password, password_field)
+def validate_password_age(user):
     try:
-        if user:
-            current_password_data = user.passwordhistoryitem_set.latest()
+        current_password_data = user.passwordhistoryitem_set.latest()
 
-            if not current_password_data.can_change_password():
-                raise ValidationError("You can not change passwords more"
-                                      "than once in 24 hours")
+        if not current_password_data.can_change_password():
+            raise ValidationError("You can not change passwords more"
+                                  " than once in 24 hours")
     except ObjectDoesNotExist:
         pass
 
-    if user:
+
+def validate_password_history(user, password):
         queryset = user.passwordhistoryitem_set.order_by('-created')[:10]
 
-        checker =functools.partial(hashers.check_password, password)
+        checker = functools.partial(hashers.check_password, password)
 
-        if any([checker(pass_hist.encrypted_password) for pass_hist in queryset]):
-            raise ValidationError("You may not re-use any of your last 10 passwords")
+        if any([checker(pass_hist.encrypted_password)
+                for pass_hist in queryset]):
+            raise ValidationError("You may not re-use any of your last 10"
+                                  " passwords")
+
+
+def _check_passwords(password, user, password_field):
+    if user:
+        validate_password_age(user)
+        validate_password_history(user, password)
+    validate_password_all_rules(password, password_field)
