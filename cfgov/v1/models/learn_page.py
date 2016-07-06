80c7f8c04e2b73cc94f235d54ec4f87388eaa894
Richa Agarwal <richa.agarwal85@gmail.com>
2016-07-06 03:36:39 -0700

    Upgrade wagtail and squash migrations

--- cfgov/v1/models/learn_page.py
+++ cfgov/v1/models/learn_page.py
@@ -40,7 +40,6 @@ class AbstractFilterPage(CFGOVPage):
     # General content tab panels
     content_panels = CFGOVPage.content_panels + [
         StreamFieldPanel('header'),
-        StreamFieldPanel('content'),
     ]
 
     # Configuration tab panels
