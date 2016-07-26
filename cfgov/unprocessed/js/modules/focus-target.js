mycibox/cfgov-refresh

Refactoring focus target and removing jQuery

cfgov/unprocessed/js/modules/focus-target.js
@@ -10,15 +10,18 @@
 
 'use strict';
 
-var $ = require( 'jquery' );
-
+var createBehavior = require( './util/behavior' ).create;
 /**
  * Parse links to handle webkit bug with keyboard focus.
  */
 function init() {
-  $( 'a[href^="#"]' ).click( function() {
-    var anchor = $( this ).attr( 'href' );
-    $( anchor ).attr( 'tabindex', -1 ).focus();
+  createBehavior( 'a[href^="#"]', 'click', function createBehavior() {
+    var anchorSelector = this.getAttribute( 'href' );
+    var anchorElement = document.querySelector( anchorSelector );
+    if ( anchorElement ) {
+      anchorElement.setAttribute( 'tabindex', -1 );
+      anchorElement.focus();
+    }
   } );
 }
 

cfpb/consumerfinance.gov

cfgov/unprocessed/js/modules/focus-target.js
@@ -10,15 +10,19 @@
 
 'use strict';
 
-var $ = require( 'jquery' );
-
+var attachBehavior = require( './util/behavior' ).attach;
 /**
  * Parse links to handle webkit bug with keyboard focus.
  */
 function init() {
-  $( 'a[href^="#"]' ).click( function() {
-    var anchor = $( this ).attr( 'href' );
-    $( anchor ).attr( 'tabindex', -1 ).focus();
+
+  attachBehavior( 'a[href^="#"]', 'click', function behavior() {
+    var anchorSelector = this.getAttribute( 'href' );
+    var anchorElement = document.querySelector( anchorSelector );
+    if ( anchorElement ) {
+      anchorElement.setAttribute( 'tabindex', -1 );
+      anchorElement.focus();
+    }
   } );
 }
 