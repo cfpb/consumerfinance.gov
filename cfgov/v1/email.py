f2e2889743c4c559a2ef9971e365664aa7a455ba
Andy Chosak <chosak@users.noreply.github.com>
2016-07-29 14:21:57 -0400

    fix send_password_reset_email tests

--- cfgov/v1/email.py
+++ cfgov/v1/email.py
@@ -1,16 +1,16 @@
-from wagtail.wagtailadmin.forms import PasswordResetForm
 from django.contrib.auth.models import User
-from django.http import HttpRequest
+from wagtail.wagtailadmin.forms import PasswordResetForm
 
 
-def send_password_reset_email(email):
+def send_password_reset_email(request, email):
     form = PasswordResetForm({'email': email})
     if not form.is_valid():
         raise User.DoesNotExist(email)
 
     template_base = 'wagtailadmin/account/password_reset/'
     form.save(
-        HttpRequest(),
+        request=request,
         subject_template_name=template_base + 'email_subject.txt',
-        email_template_name=template_base + 'email.txt'
+        email_template_name=template_base + 'email.txt',
+        use_https=request.is_secure()
     )
--- cfgov/v1/tests/test_email.py
+++ cfgov/v1/tests/test_email.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.core import mail
+from django.http import HttpRequest
 from django.test import TestCase
 
 from v1.email import send_password_reset_email
@@ -12,18 +13,27 @@ class SendEmailTestCase(TestCase):
         self.user.set_password('password')
         self.user.save()
 
+        self.request = HttpRequest()
+        self.request.META['SERVER_NAME'] = 'testhost'
+        self.request.META['SERVER_PORT'] = 1234
+
     def test_send_password_reset_email(self):
-        send_password_reset_email(self.email)
+        send_password_reset_email(self.request, self.email)
         self.assertEqual(len(mail.outbox), 1)
 
         message = mail.outbox[0]
         self.assertEqual(message.to, [self.email])
         self.assertEqual(message.from_email, 'webmaster@localhost')
         self.assertEqual(message.subject, 'Password reset')
+        self.assertIn(
+            'http://{SERVER_NAME}:{SERVER_PORT}'.format(**self.request.META),
+            message.message().as_string()
+        )
 
     def test_send_password_reset_email_no_user(self):
         self.assertRaises(
             User.DoesNotExist,
             send_password_reset_email,
+            self.request,
             'invalid_email@example.com'
         )
