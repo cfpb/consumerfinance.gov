517a818cc6930dd2f774962b48a430014debccb0
Chris Contolini <contolini@users.noreply.github.com>
2016-07-29 13:31:20 -0400

    Nyan locally but not in CI environments

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -20,7 +20,9 @@ function testUnitScripts( cb ) {
     .pipe( plugins.istanbul.hookRequire() )
     .on( 'finish', function() {
       gulp.src( configTest.tests + '/unit_tests/**/*.js' )
-        .pipe( plugins.mocha() )
+        .pipe( plugins.mocha( {
+          reporter: process.env.CONTINUOUS_INTEGRATION ? 'spec' : 'nyan'
+        } ) )
         .pipe( plugins.istanbul.writeReports( {
           dir: configTest.tests + '/unit_test_coverage'
         } ) )
