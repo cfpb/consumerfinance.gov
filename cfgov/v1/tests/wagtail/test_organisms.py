2e06f1c58340fe20473667183219a6bcd999090c
Richa Agarwal <richa.agarwal85@gmail.com>
2016-08-03 14:31:06 -0700

    Rewrite well test

--- cfgov/v1/tests/wagtail/test_organisms.py
+++ cfgov/v1/tests/wagtail/test_organisms.py
@@ -1,24 +1,26 @@
-# import os
-# from django.test import TestCase
-# from django.test import Client
+from django.test import TestCase
+from django.test import Client
 
-# from django.conf import settings
 
-# from scripts import _atomic_helpers as atomic
-# from v1.models.landing_page import LandingPage
-# from v1.models.sublanding_page import SublandingPage
-# from v1.models.learn_page import EventPage, LearnPage, DocumentDetailPage
-# from v1.models.browse_page import BrowsePage
-# from v1.models.browse_filterable_page import BrowseFilterablePage, EventArchivePage, NewsroomLandingPage
-# from v1.models.sublanding_filterable_page import SublandingFilterablePage, ActivityLogPage
-# from v1.models.newsroom_page import NewsroomPage, LegacyNewsroomPage
-# from v1.models.blog_page import BlogPage, LegacyBlogPage
-# from v1.models.snippets import Contact
-# from wagtail.wagtailcore.blocks import StreamValue
-# from helpers import publish_page
+from scripts import _atomic_helpers as atomic
+from v1.models.landing_page import LandingPage
+from wagtail.wagtailcore.blocks import StreamValue
+from helpers import publish_page
 
-# c = Client()
+c = Client()
 
-# class OrganismsTestCase(TestCase):
-
-# 	
+class OrganismsTestCase(TestCase):
+	def test_well(self):
+		"""Well content correctly displays on a Landing Page"""
+		landing_page = LandingPage(
+				title='Landing Page', 
+				slug='landing', 
+		)
+		landing_page.content = StreamValue(
+			landing_page.content.stream_block, 
+			[atomic.well], 
+			True
+		)
+		publish_page(child=landing_page)
+		response = c.get('/landing/')
+		self.assertContains(response, 'this is well content')
