b781546f672caf3e4ac3e1db840734b495c5c9b9
Richa Agarwal <richa.agarwal85@gmail.com>
2016-08-03 14:30:43 -0700

    Some cleanup

--- cfgov/v1/tests/wagtail/test_molecules.py
+++ cfgov/v1/tests/wagtail/test_molecules.py
@@ -1,19 +1,13 @@
-import os
 from django.test import TestCase
 from django.test import Client
-
-from django.conf import settings
 
 from scripts import _atomic_helpers as atomic
 from v1.models.landing_page import LandingPage
 from v1.models.sublanding_page import SublandingPage
-from v1.models.learn_page import EventPage, LearnPage, DocumentDetailPage
+from v1.models.learn_page import LearnPage, DocumentDetailPage
 from v1.models.browse_page import BrowsePage
-from v1.models.browse_filterable_page import BrowseFilterablePage, EventArchivePage, NewsroomLandingPage
-from v1.models.sublanding_filterable_page import SublandingFilterablePage, ActivityLogPage
-from v1.models.newsroom_page import NewsroomPage, LegacyNewsroomPage
-from v1.models.blog_page import BlogPage, LegacyBlogPage
-from v1.models.snippets import Contact
+from v1.models.browse_filterable_page import BrowseFilterablePage
+from v1.models.sublanding_filterable_page import SublandingFilterablePage
 from wagtail.wagtailcore.blocks import StreamValue
 from helpers import publish_page
 
