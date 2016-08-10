7430e99fe5bcd59249df7a46b5cadbed2e3ff005
Andy Chosak <chosak@users.noreply.github.com>
2016-08-10 00:01:47 -0400

    better mysql-only table collation migration

--- cfgov/jobmanager/migrations/0002_auto_20160809_1619.py
+++ cfgov/jobmanager/migrations/0002_auto_20160809_1619.py
@@ -4,25 +4,26 @@ from __future__ import unicode_literals
 from django.db import migrations, models
 
 
-def alter_jobmanager_tables(apps, schema_editor):
-    migrations.RunSQL([
-        'ALTER TABLE jobmanager_job '
-        'DEFAULT CHARACTER SET utf8 COLLATE utf8_bin'
-    ])
-
-    if 'mysql' == schema_editor.connection.vendor:
-        migrations.RunSQL([
-            'ALTER TABLE jobmanager_job '
-            'CONVERT TO CHARACTER SET utf8 COLLATE utf8_bin'
-        ])
+class MySQLOnlyRunSQL(migrations.RunSQL):
+    def _run_sql(self, schema_editor, sqls):
+        if schema_editor.connection.vendor.startswith('mysql'):
+            super(MySQLOnlyRunSQL, self)._run_sql(schema_editor, sqls)
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('jobmanager', '0001_initial'),
     ]
 
     operations = [
-        migrations.RunPython(alter_jobmanager_tables),
+        MySQLOnlyRunSQL([
+            (
+                'ALTER TABLE jobmanager_job '
+                'DEFAULT CHARACTER SET utf8 COLLATE utf8_bin'
+            ),
+            (
+                'ALTER TABLE jobmanager_job '
+                'CONVERT TO CHARACTER SET utf8 COLLATE utf8_bin'
+            )
+        ]),
     ]
