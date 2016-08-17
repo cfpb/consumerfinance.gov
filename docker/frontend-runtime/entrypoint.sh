5f60b3a42a27e331609a2928dd8d964fca7b2df4
Ross Karchner <ross@karchner.com>
2016-08-17 15:19:00 -0400

    should have been in previous commit

--- docker/frontend-runtime/entrypoint.sh
+++ docker/frontend-runtime/entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-# run npm install if node_modules doesn exist, or if it is older than
-# packages.json
-if [ ! -d node_modules ] ||  [ "packages.json" -nt node_modules ];
-	then
-	npm install
-fi
+echo "overwriting your node_modules with the versions built into this docker image"
+/bin/cp -rf ../node_modules ./node_modules
+
 gulp build
 gulp watch
