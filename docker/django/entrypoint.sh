f6082ca2e22022637c75b754a898ed641a2d5344
Ross Karchner <ross.karchner@cfpb.gov>
2016-08-22 16:11:22 -0400

    Beginning mysql support

--- docker/django/entrypoint.sh
+++ docker/django/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-scl enable python27 "django-admin migrate --fake-inital --noinput"
+scl enable python27 "django-admin migrate --fake-initial --noinput"
 scl enable python27 "django-admin runserver"
