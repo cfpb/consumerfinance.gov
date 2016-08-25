6453c898e99ddbb0788996acc22b936e83df7bd0
Steven Bellamy <steven.bellamy@cfpb.gov>
2016-08-25 12:45:55 -0400

    Adding documentation to js and macros

--- cfgov/templates/wagtailadmin/js/table-block.js
+++ cfgov/templates/wagtailadmin/js/table-block.js
@@ -1,6 +1,15 @@
 
 'use strict';
 
+/*
+ *  TableBlock HansonTable bridge
+ *
+ *  Code copied from
+ *  https://github.com/torchbox/wagtail/blob/master/wagtail/contrib/table_block/static/table_block/js/table.js
+ *
+ *  Modifications were made to add new form fields to the TableBlock in Wagtail admin.
+ */
+
 function initAtomicTable(id, tableOptions) {
     var containerId = id + '-handsontable-container';
     var tableHeaderCheckboxId = id + '-handsontable-header';
