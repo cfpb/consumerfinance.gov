6e60ee6d3e2a899ddfcfc2b1c9ce58b5a320a0a3
Ross Karchner <ross.karchner@cfpb.gov>
2016-08-26 13:57:27 -0400

    add pdfreactor, phpmyadmin, elasticsearch, and fakes3!

--- docker/django/entrypoint.sh
+++ docker/django/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+mysqladmin -h $MYSQL_HOST create v1 || true
 scl enable python27 "django-admin migrate --fake-initial --noinput"
-scl enable python27 "django-admin runserver"
+scl enable python27 "django-admin runserver 0.0.0.0:8000"
