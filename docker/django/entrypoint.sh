3529f88886a5aa7fa440d69a6dc0ccec1eff76ad
Ross Karchner <ross.karchner@cfpb.gov>
2016-08-29 11:20:41 -0400

    allow django entrypoint to read PDFReactor license from a text file

--- docker/django/entrypoint.sh
+++ docker/django/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+if [ -f /src/pdfreactor_key.txt ]; then
+    export PDFREACTOR_LICENSE=`cat /src/pdfreactor_key.txt`
+fi
 mysqladmin -h $MYSQL_HOST create v1 || true
 scl enable python27 "django-admin migrate --fake-initial --noinput"
 scl enable python27 "django-admin runserver 0.0.0.0:8000"
