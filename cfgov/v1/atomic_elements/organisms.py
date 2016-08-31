fff5e64b335e18950cd5cb6f1694edc8737093d2
Richa Agarwal <richa.agarwal85@gmail.com>
2016-08-31 13:00:59 -0700

    Revert #2295

--- cfgov/v1/atomic_elements/organisms.py
+++ cfgov/v1/atomic_elements/organisms.py
@@ -139,9 +139,9 @@ class Table(blocks.StructBlock):
     ]))
 
     class Meta:
-        icon = None
+        icon = 'form'
         template = '_includes/organisms/table.html'
-        label = ' '
+        label = 'Table'
 
 
 class ModelBlock(blocks.StructBlock):
@@ -299,8 +299,7 @@ class FullWidthText(blocks.StreamBlock):
     quote = molecules.Quote()
     cta = molecules.CallToAction()
     related_links = molecules.RelatedLinks()
-    table = Table(editable=False)
-    table_block = TableBlock(table_options={'renderer':'html'})
+    table = Table()
 
     class Meta:
         icon = 'edit'
