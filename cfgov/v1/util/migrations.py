143ec154f1fea278777d59c7aa8cd414bf7239ea
Andy Chosak <chosak@users.noreply.github.com>
2016-09-07 14:01:20 -0400

    new migration to convert existing jobs to JobListingPages

--- cfgov/v1/util/migrations.py
+++ cfgov/v1/util/migrations.py
@@ -1,5 +1,6 @@
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import transaction
+from treebeard.mp_tree import MP_Node
 
 
 def get_page(apps, slug):
@@ -7,21 +8,9 @@ def get_page(apps, slug):
     return base_page_cls.objects.get(slug=slug)
 
 
-def make_path_string(base_path='', child_number=0):
-    if child_number > 14:
-        # This function uses hex formatting as a shortcut so only supports
-        # up to 15 children (1-F), as Wagtail (and Treebeard) appear to skip
-        # 0 and use '0001' as the first path. Larger number of children use
-        # non-hex letters (such as '000G') and more complex formatting will
-        # be needed here for those cases.
-        raise ValueError('unsupported format')
-
-    return base_path + '000{:x}'.format(child_number + 1).upper()
-
-
 @transaction.atomic
 def get_or_create_page(apps, page_cls_app, page_cls_name, title, slug,
-                       parent_page, live=False, shared=False):
+                       parent_page, live=False, shared=False, **kwargs):
     try:
         return get_page(apps, slug)
     except ObjectDoesNotExist:
@@ -34,14 +23,19 @@ def get_or_create_page(apps, page_cls_app, page_cls_name, title, slug,
 
     parent_page = get_page(apps, slug=parent_page.slug)
 
+    depth = parent_page.depth + 1
+    path = MP_Node._get_path(parent_page.path, depth, parent_page.numchild + 1)
+
+    print(page_cls)
     page = page_cls.objects.create(
         title=title,
         slug=slug,
-        depth=parent_page.depth + 1,
-        path=make_path_string(parent_page.path, parent_page.numchild),
+        depth=depth,
+        path=path,
         content_type=page_content_type,
         live=live,
-        shared=shared
+        shared=shared,
+        **kwargs
     )
 
     parent_page.numchild += 1
