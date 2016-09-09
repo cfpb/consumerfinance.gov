9023cee7af3560c7f32178153138966efd1eb4e6
Andy Chosak <chosak@users.noreply.github.com>
2016-09-09 08:56:28 -0400

    pep8

--- cfgov/flags/utils.py
+++ cfgov/flags/utils.py
@@ -1,14 +1,14 @@
-import itertools
-
 from .models import Flag, FlagState
+
 
 def missing_flag_states_for_site(site, exclude_keys):
     return (FlagState(site=site, flag=f, enabled=f.enabled_by_default)
             for f in Flag.objects.exclude(key__in=exclude_keys))
 
+
 def init_missing_flag_states_for_site(site):
     saved_states = site.flagstate_set.all()
     saved_keys = [ss.flag_id for ss in saved_states]
-    missing_states = missing_flag_states_for_site(site,saved_keys)
-    
+    missing_states = missing_flag_states_for_site(site, saved_keys)
+
     return [state.save() for state in missing_states]
