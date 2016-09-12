b20c9c479b4e380065e6a089cf38d2b9cf621e12
Andy Chosak <chosak@users.noreply.github.com>
2016-09-12 17:26:47 -0400

    fixing broken test

--- cfgov/flags/tests/test_utils.py
+++ cfgov/flags/tests/test_utils.py
@@ -8,19 +8,29 @@ from flags.utils import init_missing_flag_states_for_site
 class InitMissingFlagStatesTestCase(TestCase):
     def setUp(self):
         self.site = Site.objects.get(is_default_site=True)
-        Flag.objects.create(key='FOO')
-        Flag.objects.create(key='BAR')
+        self.flag_names = ('FOO', 'BAR')
+        for flag_name in self.flag_names:
+            Flag.objects.create(key=flag_name)
+
+    def get_test_flag_states(self):
+        return FlagState.objects.filter(flag_id__in=self.flag_names)
 
     def test_none_existing(self):
-        self.assertFalse(FlagState.objects.exists())
+        states = self.get_test_flag_states()
+        self.assertFalse(states.exists())
+
         init_missing_flag_states_for_site(self.site)
-        self.assertEqual(FlagState.objects.count(), 2)
+
+        states = self.get_test_flag_states()
+        self.assertEqual(states.count(), 2)
         self.assertFalse(FlagState.objects.get(flag_id='FOO').enabled)
         self.assertFalse(FlagState.objects.get(flag_id='BAR').enabled)
 
     def test_some_existing(self):
         FlagState.objects.create(flag_id='FOO', enabled=True, site=self.site)
         init_missing_flag_states_for_site(self.site)
-        self.assertEqual(FlagState.objects.count(), 2)
+
+        states = self.get_test_flag_states()
+        self.assertEqual(states.count(), 2)
         self.assertTrue(FlagState.objects.get(flag_id='FOO').enabled)
         self.assertFalse(FlagState.objects.get(flag_id='BAR').enabled)
