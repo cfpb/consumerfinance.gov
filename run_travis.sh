ee3e6d095bba911f661bbe60b11eeb422a8c53b2
higs4281 <higs4281@gmail.com>
2016-09-22 02:31:37 -0400

    Add run_travis script and matrix vars
    The produces separate parallel builds for front and back ends and should
    fix the coverage badge, but it doesn't produce coverage report for js.

---
+++ run_travis.sh
@@ -0,0 +1,8 @@
+echo "running $RUNTEST"
+if [ "$RUNTEST" == "frontend" ]; then
+    gulp lint
+    gulp "test:unit"
+    gulp "test:coveralls"
+elif [ "$RUNTEST" == "backend" ]; then
+    tox -e travis
+fi
