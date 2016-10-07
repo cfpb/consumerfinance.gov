e39cf1464120c46ae30ad87af8ff75861c244f5c
Will Barton <william.barton@cfpb.gov>
2016-10-07 11:54:29 -0400

    Add utility functions to migrate stream fields and tests

---
+++ cfgov/v1/tests/test_util_migrations.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import mock
+from unittest import TestCase
+
+from v1.util.migrations import (
+    migrate_page_types_and_fields,
+    migrate_stream_field,
+)
+
+
+class MigrationsUtilTestCase(TestCase):
+
+    @mock.patch('v1.util.migrations.get_page_stream_data')
+    @mock.patch('v1.util.migrations.set_page_stream_data')
+    def test_migrate_stream_field(self, mock_set_page_stream_data,
+            mock_get_page_stream_data):
+        mock_get_page_stream_data.return_value = \
+                [{'type': 'my_type', 'value': {'afield': 'old data'}}]
+
+        # Mock Wagtail page
+        page = mock.Mock()
+
+        # Mock the field mapper migration function. We'll inspect the
+        # call to this and ensure the return value makes it to
+        # set_page_stream_data.
+        mapper = mock.Mock()
+        mapper.return_value = {'afield': 'new data'}
+
+        migrate_stream_field(page, 'my_field', 'my_type', mapper)
+
+        # The mapper should be called with the page and "old" stream
+        # field data
+        mapper.assert_called_with(page, {'afield': 'old data'})
+
+        # set_page_stream_data should be called with the "new" stream
+        # field data
+        mock_set_page_stream_data.assert_called_with(page, 'my_field',
+                [{'type': 'my_type', 'value': {'afield': 'new data'}}])
+
+    @mock.patch('v1.util.migrations.get_page_stream_data')
+    @mock.patch('v1.util.migrations.set_page_stream_data')
+    def test_migrate_stream_field_not_migrated(self,
+            mock_set_page_stream_data, mock_get_page_stream_data):
+        mock_get_page_stream_data.return_value = \
+                [{'type': 'my_type', 'value': {'afield': 'old data'}}]
+
+        mapper = mock.Mock()
+
+        migrate_stream_field(mock.Mock(), 'my_field', 'other_type', mapper)
+
+        # The mapper should not be called
+        mapper.assert_not_called()
+
+        # set_page_stream_data should not be called
+        mock_set_page_stream_data.assert_not_called()
+
+    @mock.patch('v1.util.migrations.imported_apps.get_model')
+    @mock.patch('v1.util.migrations.migrate_stream_field')
+    def test_migrate_page_types_and_fields(self,
+            mock_migrate_stream_field, mock_get_model):
+        mapper = mock.Mock()
+
+        # Set up a mock page model that return a single mock page
+        mock_page = mock.Mock()
+        mock_model = mock.Mock()
+        mock_model.objects.all.return_value = [mock_page]
+        mock_get_model.return_value = mock_model
+
+        page_types_and_fields = [
+            ('SomePage', 'my_field', 'my_type'),
+        ]
+        migrate_page_types_and_fields([], page_types_and_fields, mapper)
+
+        mock_migrate_stream_field.assert_called_with(mock_page,
+                'my_field', 'my_type', mapper)
--- cfgov/v1/util/migrations.py
+++ cfgov/v1/util/migrations.py
@@ -2,6 +2,10 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.db import transaction
 from treebeard.mp_tree import MP_Node
 
+from django.apps import apps as imported_apps
+from v1.tests.wagtail_pages.helpers import (
+    get_page_stream_data, set_page_stream_data)
+
 
 def get_page(apps, slug):
     base_page_cls = apps.get_model('wagtailcore', 'Page')
@@ -57,3 +61,30 @@ def get_or_create_page(apps, page_cls_app, page_cls_name, title, slug,
     parent_page.save()
 
     return page
+
+
+def migrate_stream_field(page, field_name, field_type, mapper):
+    """ Migrate a stream field of the name and type belonging to
+        the page using the mapper function """
+    old_stream_data = get_page_stream_data(page, field_name)
+    new_stream_data = []
+
+    migrated = False
+    for field in old_stream_data:
+        if field_type == field['type']:
+            field['value'] = mapper(page, field['value'])
+            migrated = True
+
+        new_stream_data.append(field)
+
+    if migrated:
+        print('migrated page {}'.format(page.slug))
+        set_page_stream_data(page, field_name, new_stream_data)
+
+
+@transaction.atomic
+def migrate_page_types_and_fields(apps, page_types_and_fields, mapper):
+    for page_type, field_name, field_type in page_types_and_fields:
+        page_cls = imported_apps.get_model('v1', page_type)
+        for page in page_cls.objects.all():
+            migrate_stream_field(page, field_name, field_type, mapper)
