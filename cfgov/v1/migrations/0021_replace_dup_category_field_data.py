848c85ff9f091e9110e42f65de0232278a356d28
Will Barton <william.barton@cfpb.gov>
2016-10-18 23:34:39 -0400

    Renumber dup category migrations

--- cfgov/v1/migrations/0021_replace_dup_category_field_data.py
+++ cfgov/v1/migrations/0021_replace_dup_category_field_data.py
@@ -74,7 +74,7 @@ def backwards(apps, schema_editor):
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('v1', '0020_full-width-text-anchor'),
+        ('v1', '0020_replace_dup_category_field'),
     ]
 
     operations = [
