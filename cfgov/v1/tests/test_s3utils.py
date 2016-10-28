7529fe5b80a540179efc9eb637da6ac4f8265597
Andy Chosak <chosak@users.noreply.github.com>
2016-10-28 10:04:19 -0400

    fixing broken tests for secure s3 urls

--- cfgov/v1/tests/test_s3utils.py
+++ cfgov/v1/tests/test_s3utils.py
@@ -1,29 +1,27 @@
-from django.test import TestCase
+from django.conf import settings
+from django.test import TestCase, override_settings
+
 from wagtail.wagtailimages.models import get_image_model
 from wagtail.wagtailimages.tests.utils import get_test_image_file
 
 
 class S3UtilsTestCase(TestCase):
+    @override_settings(
+        AWS_QUERYSTRING_AUTH=False,
+        AWS_S3_ACCESS_KEY_ID='test',
+        AWS_S3_CALLING_FORMAT='boto.s3.connection.OrdinaryCallingFormat',
+        AWS_S3_SECRET_ACCESS_KEY='test',
+        AWS_S3_SECURE_URLS=True,
+        AWS_STORAGE_BUCKET_NAME='test_s3_bucket',
+        DEFAULT_FILE_STORAGE='v1.s3utils.MediaRootS3BotoStorage'
+    )
     def test_s3_files_use_secure_urls(self):
-        test_bucket = 'bucket.test'
-        filename = 'test.png'
-
-        image_file = get_test_image_file(filename=filename)
+        image_file = get_test_image_file(filename='test.png')
 
         Image = get_image_model()
         image = Image(file=image_file)
 
-        with self.settings(
-            AWS_QUERYSTRING_AUTH=False,
-            AWS_S3_ACCESS_KEY_ID='test',
-            AWS_S3_SECRET_ACCESS_KEY='test',
-            AWS_S3_SECURE_URLS=True,
-            AWS_STORAGE_BUCKET_NAME=test_bucket,
-            DEFAULT_FILE_STORAGE='v1.s3utils.MediaRootS3BotoStorage',
-        ):
-            expected_url = 'https://{}.s3.amazonaws.com/f/{}'.format(
-                test_bucket,
-                filename
-            )
-
-            self.assertEqual(image.file.url, expected_url)
+        self.assertEqual(
+            image.file.url,
+            'https://s3.amazonaws.com/test_s3_bucket/f/test.png'
+        )
