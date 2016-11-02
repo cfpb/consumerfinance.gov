43330d1df1694687106b13cdf30e6464c29e7071
Jerry Vinokurov <yury.vinokurov@cfpb.gov>
2016-11-02 09:42:42 -0400

    refactored get_appropriate_page_version into CFGOVPage

--- cfgov/v1/models/base.py
+++ cfgov/v1/models/base.py
@@ -212,12 +212,27 @@ class CFGOVPage(Page):
         return {search_type: queryset for search_type, queryset in
                 related.items() if queryset}
 
+    def get_appropriate_page_version(self, hostname):
+        # If we're on the production site, make sure the version of the page
+        # displayed is the latest version that has `live` set to True for
+        # the live site or `shared` set to True for the staging site.
+        staging_hostname = os.environ.get('DJANGO_STAGING_HOSTNAME')
+        revisions = self.revisions.all().order_by('-created_at')
+        for revision in revisions:
+            page_version = json.loads(revision.content_json)
+            if hostname != staging_hostname:
+                if page_version['live']:
+                    return revision.as_page_object()
+            else:
+                if page_version['shared']:
+                    return revision.as_page_object()
+
     def get_breadcrumbs(self, request):
         ancestors = self.get_ancestors()
         home_page_children = request.site.root_page.get_children()
         for i, ancestor in enumerate(ancestors):
             if ancestor in home_page_children:
-                return [util.get_appropriate_page_version(request, ancestor) for ancestor in ancestors[i+1:]]
+                return [ancestor.get_appropriate_page_version(request.site.hostname) for ancestor in ancestors[i+1:]]
         return []
 
     def get_appropriate_descendants(self, hostname, inclusive=True):
@@ -352,7 +367,7 @@ class CFGOVPage(Page):
 
         else:
             # Request is for this very page.
-            page = util.get_appropriate_page_version(request, self)
+            page = self.get_appropriate_page_version(request.site.hostname)
             if page:
                 return RouteResult(page)
             raise Http404
--- cfgov/v1/tests/test_util.py
+++ cfgov/v1/tests/test_util.py
@@ -1,9 +1,13 @@
 import mock
+import json
 
 from django.test import TestCase
 from django.test.client import RequestFactory
 
 from ..util import util
+from v1.tests.wagtail_pages import helpers
+from v1.models import CFGOVPage
+from wagtail.wagtailcore.models import PageRevision
 
 
 class TestUtilFunctions(TestCase):
@@ -19,57 +23,60 @@ class TestUtilFunctions(TestCase):
         ]
         self.page.revisions.all().order_by.return_value = self.page_versions
 
-    @mock.patch('json.loads')
-    def test_production_returns_first_live_page(self, mock_json_loads):
-        self.request.site.hostname = 'localhost'
-        mock_json_loads.side_effect = [page.content_json for page in self.page_versions]
-        util.get_appropriate_page_version(self.request, self.page)
-        assert not self.page_versions[0].as_page_object.called
-        assert not self.page_versions[1].as_page_object.called
-        assert self.page_versions[2].as_page_object.called
-        assert not self.page_versions[3].as_page_object.called
+        self.new_page = CFGOVPage(title='a cfgov page')
+        self.new_page.live = False
+        self.new_page.shared = False
 
-    @mock.patch('json.loads')
-    def test_shared_returns_first_shared_page(self, mock_json_loads):
+        helpers.save_new_page(self.new_page)
+        content_json = json.loads(self.new_page.to_json())
+        # create the various page revisions
+        # revision 1
+        content_json['title'] = 'revision 1'
+        content_json['live'] = True
+        content_json['shared'] = True
+        self.revision_1 = self.new_page.revisions.create(content_json=json.dumps(content_json))
+
+        # rev 2
+        content_json['title'] = 'revision 2'
+        content_json['live'] = True
+        content_json['shared'] = False
+        self.revision_2 = self.new_page.revisions.create(content_json=json.dumps(content_json))
+
+        # rev 3
+        content_json['title'] = 'revision 3'
+        content_json['live'] = False
+        content_json['shared'] = True
+        self.revision_3 = self.new_page.revisions.create(content_json=json.dumps(content_json))
+
+        # rev 4
+        content_json['title'] = 'revision 4'
+        content_json['live'] = False
+        content_json['shared'] = False
+        self.revision_4 = self.new_page.revisions.create(content_json=json.dumps(content_json))
+
+    def test_production_returns_first_live_page(self):
+        self.request.site.hostname = 'localhost'
+        version = self.new_page.get_appropriate_page_version(self.request.site.hostname)
+        self.assertEqual(version.title, 'revision 2')
+
+    def test_shared_returns_first_shared_page(self):
         self.request.site.hostname = 'content.localhost'
-        mock_json_loads.side_effect = [page.content_json for page in self.page_versions]
-        util.get_appropriate_page_version(self.request, self.page)
-        assert not self.page_versions[0].as_page_object.called
-        assert self.page_versions[1].as_page_object.called
-        assert not self.page_versions[2].as_page_object.called
-        assert not self.page_versions[3].as_page_object.called
+        version = self.new_page.get_appropriate_page_version(self.request.site.hostname)
+        self.assertEqual(version.title, 'revision 3')
 
-    @mock.patch('json.loads')
-    def test_shared_returns_None_for_only_draft_versions(self, mock_json_loads):
-        self.page_versions = [
-            mock.Mock(**{'content_json': {'live': False, 'shared': False}}),
-            mock.Mock(**{'content_json': {'live': False, 'shared': False}}),
-            mock.Mock(**{'content_json': {'live': False, 'shared': False}}),
-            mock.Mock(**{'content_json': {'live': False, 'shared': False}}),
-        ]
-        mock_json_loads.side_effect = [page.content_json for page in self.page_versions]
-        util.get_appropriate_page_version(self.request, self.page)
-        assert not self.page_versions[0].as_page_object.called
-        assert not self.page_versions[1].as_page_object.called
-        assert not self.page_versions[2].as_page_object.called
-        assert not self.page_versions[3].as_page_object.called
+    def test_shared_returns_None_for_only_draft_versions(self):
+        self.revision_1.delete()
+        self.revision_2.delete()
+        self.revision_3.delete()
 
+        version = self.new_page.get_appropriate_page_version(self.request.site.hostname)
+        self.assertIsNone(version)
 
-    @mock.patch('json.loads')
-    def test_shared_returns_None_if_page_not_live_when_on_production(self, mock_json_loads):
-        self.request.site.hostname = 'localhost'
-        self.page_versions = [
-            mock.Mock(**{'content_json': {'live': False, 'shared': True}}),
-            mock.Mock(**{'content_json': {'live': False, 'shared': True}}),
-            mock.Mock(**{'content_json': {'live': False, 'shared': True}}),
-            mock.Mock(**{'content_json': {'live': False, 'shared': True}}),
-        ]
-        mock_json_loads.side_effect = [page.content_json for page in self.page_versions]
-        util.get_appropriate_page_version(self.request, self.page)
-        assert not self.page_versions[0].as_page_object.called
-        assert not self.page_versions[1].as_page_object.called
-        assert not self.page_versions[2].as_page_object.called
-        assert not self.page_versions[3].as_page_object.called
+    def test_shared_returns_None_if_page_not_live_when_on_production(self):
+        self.revision_1.delete()
+        self.revision_2.delete()
+        version = self.new_page.get_appropriate_page_version(self.request.site.hostname)
+        self.assertIsNone(version)
 
     @mock.patch('__builtin__.isinstance')
     @mock.patch('__builtin__.vars')
