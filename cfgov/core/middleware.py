10e345f03ebba5ca5789db714bb9f4a6270bdec9
Ross M Karchner <ross@karchner.com>
2016-11-03 17:31:26 -0400

    Upstream is not downstream

--- cfgov/core/middleware.py
+++ cfgov/core/middleware.py
@@ -1,4 +1,4 @@
-class UpstreamCacheControlMiddleware(object):
+class DownstreamCacheControlMiddleware(object):
     def process_response(self, request, response):
         if 'CSRF_COOKIE_USED' in request.META:
             response['Edge-Control'] = 'no-store'
