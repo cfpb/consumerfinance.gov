9c62d40fb4b47dd9e6553a32c88e9a03011d621d
Scott Cranfill <scott.cranfill@cfpb.gov>
2016-12-07 12:06:44 -0500

    Add Story to page_types list

--- cfgov/v1/util/ref.py
+++ cfgov/v1/util/ref.py
@@ -28,15 +28,16 @@ page_types = [
     ('activity-log', 'Activity Log'),
     ('amicus-brief', 'Amicus Brief'),
     ('blog', 'Blog'),
-    ('enforcement', 'Enforcement action'),
+    ('enforcement', 'Enforcement Action'),
     ('final-rule', 'Final Rule'),
     ('foia-freq-req-record', 'FOIA Frequently Requested Record'),
     ('impl-resource', 'Implementation Resource'),
+    ('leadership-calendar', 'Leadership Calendar'),
     ('newsroom', 'Newsroom'),
     ('notice-opportunity-comment', 'Notice and Opportunity for Comment'),
     ('research-reports', 'Research Report'),
-    ('rule-under-dev', 'Rule under development'),
-    ('leadership-calendar', 'Leadership Calendar'),
+    ('rule-under-dev', 'Rule Under Development'),
+    ('story', 'Story'),
 ]
 
 fcm_types = [
