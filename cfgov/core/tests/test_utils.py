6c1cb747f16eba2faf9589328ae39ff36f98d634
Ross Karchner <ross.karchner@cfpb.gov>
2016-12-09 08:45:18 -0500

    remove some merge noise

--- cfgov/core/tests/test_utils.py
+++ cfgov/core/tests/test_utils.py
@@ -1,20 +1,6 @@
-<<<<<<< HEAD
-from django.template.loader import get_template
-from django.test import TestCase, RequestFactory
-from django.core.signing import Signer
-
-from unittest import expectedFailure
-
-from core.utils import (extract_answers_from_request,
-                        hash_for_script,
-                        add_js_hash_to_request,
-                        sign_url,
-                        append_query_args_to_url)
-=======
 from django.test import TestCase
 
 from core.utils import extract_answers_from_request
->>>>>>> 662dd2d30714e2af32351d96a5058aa700d28e37
 
 
 class FakeRequest(object):
@@ -23,68 +9,6 @@ class FakeRequest(object):
         self.POST = params
 
 
-<<<<<<< HEAD
-class JavascriptHashTest(TestCase):
-    script = "alert('hello javascript');"
-    expected_sha = "'sha256-Ft3A+V6Yme/8r0/K/uC38Zj0VJ98VnbTD3MLupPTNuc='"
-
-    def test_hash_generation(self):
-        hash = hash_for_script(self.script)
-        self.assertEqual(hash, self.expected_sha)
-
-    def test_adding_sha_to_request(self):
-        factory = RequestFactory()
-        request = factory.get('/')
-        add_js_hash_to_request(request, self.script)
-        self.assertIn(self.expected_sha, request.script_hashes)
-
-
-class CSPTemplateTagsTest(TestCase):
-    factory = RequestFactory()
-    expected_sha = "'sha256-SypW8zp5ZrylvHKnRaTv4V9wLQxdi9hBpjLLWAzN4Xw='"
-
-    def render_and_check_sha(self, template):
-        request = self.factory.get('/')
-        template.render({}, request=request)
-        self.assertIn(self.expected_sha, request.script_hashes)
-
-    # this test is non-viable until we factor out the need for SheerLikeContext
-    @expectedFailure
-    def test_jinja2_tag(self):
-        template = get_template('test-fixture/csp-jinja2.html',
-                                using='wagtail-env')
-
-        self.render_and_check_sha(template)
-
-    def test_django_tag(self):
-        template = get_template('test-fixture/csp-django.html')
-
-        self.render_and_check_sha(template)
-
-
-class URLBuildingTest(TestCase):
-    def test_append_query_args(self):
-        query_args = {'foo': 'bar'}
-        base_url = 'http://google.com/'
-        result = append_query_args_to_url(base_url, query_args)
-        self.assertEqual(result, 'http://google.com/?foo=bar')
-
-
-class URLSigningTest(TestCase):
-    def test_signing_url(self):
-        key = 'testkey'
-        url, signature = sign_url('http://google.com', secret=key)
-        # this is the default format for signing results in Django
-        recombined = "{0}:{1}".format(url, signature)
-
-        signer = Signer(key)
-        unsigned_url = signer.unsign(recombined)
-
-        self.assertEqual(url, unsigned_url)
-
-
-=======
->>>>>>> 662dd2d30714e2af32351d96a5058aa700d28e37
 class ExtractAnswersTest(TestCase):
 
     def test_no_answers_to_extract(self):
--- cfgov/core/utils.py
+++ cfgov/core/utils.py
@@ -7,23 +7,6 @@ from django.core.signing import Signer
 from django.core.urlresolvers import reverse
 
 
-
-
-<<<<<<< HEAD
-def hash_for_script(js):
-    hasher = hashlib.sha256()
-    hasher.update(js.encode('utf-8'))
-    encoded = base64.b64encode(hasher.digest())
-    return "'sha256-{encoded}'".format(encoded=encoded)
-
-
-def add_js_hash_to_request(request, js):
-        if not hasattr(request, 'script_hashes'):
-            request.script_hashes = []
-        hash = hash_for_script(js)
-        request.script_hashes.append(hash)
-
-
 def append_query_args_to_url(base_url, args_dict):
     return "{0}?{1}".format(base_url, urlencode(args_dict))
 
@@ -51,8 +34,6 @@ def unsigned_redirect(url):
     return ('{0}?{1}'.format(reverse('external-site'), urlencode(query_args)))
 
 
-=======
->>>>>>> 662dd2d30714e2af32351d96a5058aa700d28e37
 def extract_answers_from_request(request):
     answers = [(param.split('_')[1], value) for param, value in
                request.POST.items() if param.startswith('questionid')]
