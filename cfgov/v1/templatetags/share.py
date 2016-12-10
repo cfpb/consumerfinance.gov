ad0b9f0586707c582afd9c9c3eaba8c3b77a21cd
Richa Agarwal <richa.agarwal85@gmail.com>
2016-12-09 17:19:16 -0800

    Refactor and sort activity snippets

--- cfgov/v1/templatetags/share.py
+++ cfgov/v1/templatetags/share.py
@@ -1,10 +1,7 @@
-import os
-from urlparse import urlparse
-
 from django import template
-from wagtail.wagtailcore.models import Page
 
 from v1.models import CFGOVUserPagePermissionsProxy
+from wagtail.wagtailcore.models import Page
 
 register = template.Library()
 
@@ -17,21 +14,6 @@ def is_shared(page):
             return True
         else:
             return False
-
-
-@register.assignment_tag(takes_context=True)
-def get_page_state_url(context, page):
-    page = Page.objects.get(id=page.id).specific
-    if not page.live and not page.shared:
-        return None
-    url = page.url
-    if url is None:  # If page is not aligned to a site root return None
-        return None
-    page_hostname = urlparse(url).hostname
-    staging_hostname = os.environ.get('DJANGO_STAGING_HOSTNAME')
-    if not page.live and page.shared and staging_hostname != page_hostname:
-        url = url.replace(page_hostname, staging_hostname)
-    return url
 
 
 @register.assignment_tag(takes_context=True)
---
+++ cfgov/v1/util/categories.py
@@ -0,0 +1,25 @@
+from v1.util import ref 
+
+import logging
+logger = logging.getLogger(__name__)
+
+def clean_categories(selected_categories):
+    """ This is a (hopefully) temporary solution for dealing w/ the fact
+    that we show Blog and Reports as options for filtering, but
+    aren't categories themselves. Rather, they consist of sub-categories,
+    but since we aren't showing those sub-categories to the end user,
+    selecting the parent category is equivalent to all of them
+    getting checked. The mapping of Blog and Reports to their
+    respective categories exists in cfgov/v1/util/ref.py
+    """
+    if not selected_categories:
+        return None
+    subcategories_dict = dict(ref.categories)
+    for unicorn in ['blog', 'newsroom', 'research-reports']:
+        if unicorn in selected_categories:
+            if unicorn == 'research-reports':
+                unicorn = 'Research Report'
+            for category in subcategories_dict[unicorn.title()]:
+                selected_categories.append(category[0].lower())
+    logger.info('Filtering by categories {}'.format(selected_categories))
+    return selected_categories
