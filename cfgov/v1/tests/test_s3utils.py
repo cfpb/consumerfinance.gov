a40a90dba8b659754101edd2598cd9641dda9263
Andy Chosak <chosak@users.noreply.github.com>
2016-12-12 10:00:12 -0500

    unit test for s3 storage

--- cfgov/v1/tests/test_s3utils.py
+++ cfgov/v1/tests/test_s3utils.py
@@ -1,4 +1,9 @@
+import boto
+
+from django.core.files.base import ContentFile
+from django.core.files.storage import get_storage_class
 from django.test import TestCase, override_settings
+from moto import mock_s3
 
 from wagtail.wagtailimages.models import get_image_model
 from wagtail.wagtailimages.tests.utils import get_test_image_file
@@ -30,3 +35,27 @@ class S3UtilsTestCase(TestCase):
             from v1.s3utils import MediaRootS3BotoStorage
             storage = MediaRootS3BotoStorage()
             self.assertEqual(storage.location, 'test')
+
+
+@override_settings(
+    AWS_QUERYSTRING_AUTH=False,
+    AWS_S3_CALLING_FORMAT='boto.s3.connection.OrdinaryCallingFormat',
+    AWS_S3_ROOT='root',
+    AWS_S3_SECURE_URLS=True,
+    AWS_STORAGE_BUCKET_NAME='test_s3_bucket',
+    DEFAULT_FILE_STORAGE='v1.s3utils.MediaRootS3BotoStorage'
+)
+@mock_s3
+class TestS3StorageLocation(TestCase):
+    def setUp(self):
+        self.s3 = boto.connect_s3()
+        self.s3.create_bucket('test_s3_bucket')
+
+    def test_storage_writes_to_s3(self):
+        f = ContentFile('content')
+        Storage = get_storage_class()
+        filename = Storage().save('content.txt', f)
+
+        bucket = self.s3.get_bucket('test_s3_bucket')
+        key = bucket.get_key('root/content.txt')
+        self.assertEqual(key.get_contents_as_string(), 'content')
