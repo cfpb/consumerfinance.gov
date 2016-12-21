844b312d198aa4f7c78e43c2f6614a86809835b8
Andy Chosak <chosak@users.noreply.github.com>
2016-12-21 13:39:55 -0500

    stash

--- cfgov/v1/management/commands/validate_page_html.py
+++ cfgov/v1/management/commands/validate_page_html.py
@@ -1,8 +1,52 @@
+from __future__ import print_function, unicode_literals
+
 from django.core.management.base import BaseCommand
+from django.utils.functional import cached_property
 from wagtail.wagtailcore.blocks import RawHTMLBlock
+from wagtail.wagtailcore.models import Site
 
 from sheerlike.external_links import convert_http_image_links
 from v1.models import LegacyBlogPage
+from v1.s3utils import http_s3_url_prefix
+from v1.tests.wagtail_pages.helpers import save_page
+
+
+class PageValidator(object):
+    def __init__(self, http_image_prefixes):
+        self.http_image_prefixes = http_image_prefixes
+
+    def validate_page(self, page, fix=False):
+        valid = True
+
+        for block in page.content:
+            if not isinstance(block.block, RawHTMLBlock):
+                continue
+
+            try:
+                corrected = self.correct_html(block.value)
+            except Exception:
+                print('failed page validation', page.full_url)
+                raise
+
+            if corrected != block.value:
+                valid = False
+                block.value = corrected
+
+        if not valid:
+            print('detected invalid page', page.full_url)
+
+            if fix:
+                print('saving updated page', page.full_url)
+                revision = save_page(page)
+
+                if page.live:
+                    revision.publish()
+
+    def correct_html(self, html):
+        return convert_http_image_links(
+            html,
+            convert_url_prefixes=self.http_image_prefixes
+        )
 
 
 class Command(BaseCommand):
@@ -15,18 +59,35 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('-f', '--fix', action='store_true',
                             help='correct page HTML, if possible')
+        parser.add_argument('http_image_prefix', nargs='+',
+                            help='HTTP image URL prefix to correct')
 
     def handle(self, *args, **options):
+        fix = options['fix']
+        http_image_prefixes = options['http_image_prefix']
+
+        validator = PageValidator(http_image_prefixes)
+
         for page_type in self.page_types_with_raw_html:
             for page in page_type.objects.all():
-                self.handle_page(page)
+                validator.validate_page(page, fix=fix)
 
-    def handle_page(self, page):
+    def handle_page(self, page, fix=False):
         for block in page.content:
             if not isinstance(block.block, RawHTMLBlock):
                 continue
 
-            corrected = convert_http_image_links(block.value)
+    @cached_property
+    def http_image_prefixes(self):
+        """Convert these HTTP image URLs to MEDIA_URL ones."""
+        default_site = Site.objects.get(is_default_site=True)
 
-            if corrected != block.value:
-                print('made a change')
+        return [
+            # HTTP links directly to an S3 bucket.
+            # e.g. http://bucket.name/image.png
+            http_s3_url_prefix(),
+
+            # HTTP links to locally hosted Wordpress uploads.
+            # e.g. http://www.mysite.com/uploads/image.png
+            default_site.root_url + '/wp-content/uploads/',
+        ]
--- cfgov/v1/tests/test_s3utils.py
+++ cfgov/v1/tests/test_s3utils.py
@@ -4,21 +4,30 @@ from django.core.files.base import ContentFile
 from django.core.files.storage import get_storage_class
 from django.test import TestCase, override_settings
 from moto import mock_s3
-
-from wagtail.wagtailimages.models import get_image_model
 from wagtail.wagtailimages.tests.utils import get_test_image_file
+from wagtail.wagtailimages.models import get_image_model
+
+from v1.s3utils import (
+    MediaRootS3BotoStorage, http_s3_url_prefix, https_s3_url_prefix
+)
 
 
+@mock_s3
+@override_settings(
+    AWS_QUERYSTRING_AUTH=False,
+    AWS_S3_ACCESS_KEY_ID='test',
+    AWS_S3_CALLING_FORMAT='boto.s3.connection.OrdinaryCallingFormat',
+    AWS_S3_ROOT='root',
+    AWS_S3_SECRET_ACCESS_KEY='test',
+    AWS_S3_SECURE_URLS=True,
+    AWS_STORAGE_BUCKET_NAME='test_s3_bucket',
+    DEFAULT_FILE_STORAGE='v1.s3utils.MediaRootS3BotoStorage'
+)
 class S3UtilsTestCase(TestCase):
-    @override_settings(
-        AWS_QUERYSTRING_AUTH=False,
-        AWS_S3_ACCESS_KEY_ID='test',
-        AWS_S3_CALLING_FORMAT='boto.s3.connection.OrdinaryCallingFormat',
-        AWS_S3_SECRET_ACCESS_KEY='test',
-        AWS_S3_SECURE_URLS=True,
-        AWS_STORAGE_BUCKET_NAME='test_s3_bucket',
-        DEFAULT_FILE_STORAGE='v1.s3utils.MediaRootS3BotoStorage'
-    )
+    def setUp(self):
+        self.s3 = boto.connect_s3()
+        self.s3.create_bucket('test_s3_bucket')
+
     def test_s3_files_use_secure_urls(self):
         image_file = get_test_image_file(filename='test.png')
 
@@ -27,29 +36,12 @@ class S3UtilsTestCase(TestCase):
 
         self.assertEqual(
             image.file.url,
-            'https://s3.amazonaws.com/test_s3_bucket/f/test.png'
+            'https://s3.amazonaws.com/test_s3_bucket/root/test.png'
         )
 
     def test_storage_location_uses_settings(self):
-        with override_settings(AWS_S3_ROOT='test'):
-            from v1.s3utils import MediaRootS3BotoStorage
-            storage = MediaRootS3BotoStorage()
-            self.assertEqual(storage.location, 'test')
-
-
-@override_settings(
-    AWS_QUERYSTRING_AUTH=False,
-    AWS_S3_CALLING_FORMAT='boto.s3.connection.OrdinaryCallingFormat',
-    AWS_S3_ROOT='root',
-    AWS_S3_SECURE_URLS=True,
-    AWS_STORAGE_BUCKET_NAME='test_s3_bucket',
-    DEFAULT_FILE_STORAGE='v1.s3utils.MediaRootS3BotoStorage'
-)
-@mock_s3
-class TestS3StorageLocation(TestCase):
-    def setUp(self):
-        self.s3 = boto.connect_s3()
-        self.s3.create_bucket('test_s3_bucket')
+        storage = MediaRootS3BotoStorage()
+        self.assertEqual(storage.location, 'root')
 
     def test_storage_writes_to_s3(self):
         f = ContentFile('content')
@@ -59,3 +51,29 @@ class TestS3StorageLocation(TestCase):
         bucket = self.s3.get_bucket('test_s3_bucket')
         key = bucket.get_key('root/content.txt')
         self.assertEqual(key.get_contents_as_string(), 'content')
+
+
+class S3UrlPrefixTest(TestCase):
+    def test_http_s3_url_prefix(self):
+        with self.settings(AWS_STORAGE_BUCKET_NAME='foo.bucket'):
+            self.assertEqual(
+                http_s3_url_prefix(),
+                'http://foo.bucket/'
+            )
+
+    def test_http_s3_url_prefix_no_setting(self):
+        with self.settings(AWS_STORAGE_BUCKET_NAME=None):
+            with self.assertRaises(RuntimeError):
+                http_s3_url_prefix()
+
+    def test_https_s3_url_prefix(self):
+        with self.settings(AWS_STORAGE_BUCKET_NAME='foo.bucket'):
+            self.assertEqual(
+                https_s3_url_prefix(),
+                'https://s3.amazonaws.com/foo.bucket/'
+            )
+
+    def test_https_s3_url_prefix_no_setting(self):
+        with self.settings(AWS_STORAGE_BUCKET_NAME=None):
+            with self.assertRaises(RuntimeError):
+                https_s3_url_prefix()
