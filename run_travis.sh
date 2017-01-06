088fb787d30b04fa54de593bfebc97de3ccbf9b2
Will Barton <william.barton@cfpb.gov>
2017-01-05 23:37:12 -0500

    Do we need to do these things for both front and backend tests?

--- run_travis.sh
+++ run_travis.sh
@@ -5,9 +5,17 @@ set -e
 
 echo "running $RUNTEST tests"
 if [ "$RUNTEST" == "frontend" ]; then
+    export CXX=clang++
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+    nvm install 5.5.0
+    nvm use 5.5.0
+    npm install -g gulp
+    chmod +x ./frontend.sh
+    # ./frontend.sh test
     gulp "test:unit"
     gulp "test:coveralls"
 elif [ "$RUNTEST" == "backend" ]; then
+    pip install -r requirements/travis.txt
     tox -e travis
     coveralls
 fi
