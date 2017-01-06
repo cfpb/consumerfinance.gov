88a11067a017c756ceb83ef0e7a30d5a647180ec
Will Barton <william.barton@cfpb.gov>
2017-01-05 23:43:37 -0500

    So we need gulp.

--- run_travis.sh
+++ run_travis.sh
@@ -5,13 +5,11 @@ set -e
 
 echo "running $RUNTEST tests"
 if [ "$RUNTEST" == "frontend" ]; then
-    npm install -g gulp
     chmod +x ./frontend.sh
-    # ./frontend.sh test
+    ./frontend.sh test
     gulp "test:unit"
     gulp "test:coveralls"
 elif [ "$RUNTEST" == "backend" ]; then
-    pip install -r requirements/travis.txt
     tox -e travis
     coveralls
 fi
