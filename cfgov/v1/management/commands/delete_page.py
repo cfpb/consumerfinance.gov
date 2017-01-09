a25fd3e61196ff94e696e18021ae21aa2a0e6ea8
Richa Agarwal <richa.agarwal85@gmail.com>
2017-01-09 11:40:41 -0800

    Add ID as an option

--- cfgov/v1/management/commands/delete_page.py
+++ cfgov/v1/management/commands/delete_page.py
@@ -12,13 +12,29 @@ class Command(BaseCommand):
             nargs='+',
             help='The slug for the page you wish to delete'
         )
+        parser.add_argument(
+            '--id',
+            nargs='+',
+            help='The ID for the page you wish to delete'
+        )
 
     def handle(self, *args, **options):
-        slug = options['slug'][0]
+        page_to_delete = None
+        if options['slug']:
+            slug = options['slug'][0]
+            try:
+                page_to_delete = CFGOVPage.objects.get(slug=slug)
+            except Exception as e:
+                self.stderr.write(str(e))
+                raise CommandError('Slug `{}` did not work'.format(slug))
+        elif options['id']:
+            page_id = options['id'][0]
+            page_to_delete = CFGOVPage.objects.get(id=page_id)
+        else:
+            raise CommandError('Must supply a --slug or an --id')
         try:
-            page_to_delete = CFGOVPage.objects.get(slug=slug)
             page_to_delete.unpublish()
             page_to_delete.delete()
         except Exception as e:
             self.stderr.write(str(e))
-            raise CommandError('failed to delete page {}'.format(slug))
+            raise CommandError('Failed to delete page {}'.format(page_to_delete))
