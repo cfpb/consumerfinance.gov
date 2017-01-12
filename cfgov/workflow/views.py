54cb790048414fdfc19ad5a871f3af8dc34c0cd0
Ross Karchner <ross.karchner@cfpb.gov>
2017-01-12 09:14:38 -0500

    never commited utils.py and had to recreate it

--- cfgov/workflow/views.py
+++ cfgov/workflow/views.py
@@ -6,14 +6,16 @@ from django.core.urlresolvers import reverse
 from wagtail.wagtailcore.models import Page, Site
 
 from workflow.preflight import preflight_check
+from workflow.utils import first_site_for_page
 
 
 def migrate(request, page_pk, destination_site_pk):
     page = get_object_or_404(Page, pk=page_pk)
     destination_site = get_object_or_404(Site, pk=destination_site_pk)
 
-    current_site_root = request.site.root_page
+    current_site_root = first_site_for_page(page).root_page
     new_root = destination_site.root_page
+    #import pdb;pdb.set_trace()
     relative_path = page.url_path[len(current_site_root.url_path):]
     new_path = os.path.join(new_root.url_path, relative_path)
 
