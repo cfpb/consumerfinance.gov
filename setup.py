d4ddbe534ce123f34e1dd78d0bd963e8d490ad89
Ross Karchner <ross.karchner@cfpb.gov>
2017-01-17 13:37:23 -0500

    first crack at a setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read_file(filename):
+    """Read a file into a string"""
+    path = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(path, filename)
+    try:
+        return open(filepath).read()
+    except IOError:
+        return ''
+
+
+setup(
+    name='cfgov',
+    version_format='{tag}.dev{commitcount}+{gitsha}',
+    author='CFPB',
+    author_email='tech@cfpb.gov',
+    maintainer='cfpb',
+    maintainer_email='tech@cfpb.gov',
+    # maybe controversial:  exclude tests from the build wheel?
+    packages= find_packages(where='./cfgov/', exclude=['*.tests', '*.tests.*']),
+    package_dir={'':'cfgov'},
+    include_package_data=True,
+    description=u'django project powering consumerfinance.gov',
+    classifiers=[
+        'Topic :: Internet :: WWW/HTTP',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Framework :: Django',
+    ],
+    long_description=read_file('README.md'),
+    zip_safe=False,
+    setup_requires=['cfgov-setup==1.2','setuptools-git-version'],
+    frontend_build_script= 'frontend.sh',
+)
