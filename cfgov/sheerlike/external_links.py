542c74c35daa26ed58c3a0501b104b4f88fd0804
Ross Karchner <ross.karchner@cfpb.gov>
2017-01-24 16:37:56 -0500

    fix the tests

--- cfgov/sheerlike/external_links.py
+++ cfgov/sheerlike/external_links.py
@@ -2,7 +2,7 @@ import warnings
 
 from django.conf import settings
 
-from v1 import parse_links
+from v1.template_environment import parse_links
 from v1.page_validation import convert_http_image_links
 from v1.s3utils import http_s3_url_prefix, https_s3_url_prefix
 
