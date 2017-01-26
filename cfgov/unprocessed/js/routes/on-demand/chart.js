2acdb1e38d3b3c6c86390d6bf30e7eddad33f7c9
Chris Contolini <contolini@users.noreply.github.com>
2017-01-26 15:12:54 -0800

    Chart Block (#2688)
    CHART BLOCK!

---
+++ cfgov/unprocessed/js/routes/on-demand/chart.js
@@ -0,0 +1,8 @@
+/* ==========================================================================
+   Scripts for Line Chart molecule.
+   ========================================================================== */
+
+'use strict';
+
+// See https://github.com/cfpb/cfpb-chart-builder
+require( 'cfpb-chart-builder' );
