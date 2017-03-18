eae899185651192e47782dd5f8f3a7229622c218
higs4281 <higs4281@gmail.com>
2017-03-17 21:09:53 -0400

    Add a redirect_id field to allow editors to redirect AnswerPages

--- cfgov/ask_cfpb/models/pages.py
+++ cfgov/ask_cfpb/models/pages.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, unicode_literals
 
 from django.utils import timezone
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 from wagtail.wagtailadmin.edit_handlers import (
     FieldPanel,
@@ -33,9 +34,14 @@ class AnswerPage(CFGOVPage):
         null=True,
         related_name='answer_pages',
         on_delete=models.PROTECT)
+    redirect_id = models.IntegerField(
+        blank=True,
+        null=True,
+        help_text="Enter an Answer ID to redirect this page to")
 
     content_panels = CFGOVPage.content_panels + [
         FieldPanel('answer_base', Answer),
+        FieldPanel('redirect_id')
     ]
     search_fields = Page.search_fields + [
         index.SearchField('question'),
@@ -56,3 +62,23 @@ class AnswerPage(CFGOVPage):
             return '{}: {}'.format(self.answer_base.id, self.title)
         else:
             return self.title
+
+    @property
+    def status_string(self):
+        if self.redirect_id:
+            if not self.live:
+                return _("redirected but not live")
+            else:
+                return _("redirected")
+        if not self.live:
+            if self.expired:
+                return _("expired")
+            elif self.approved_schedule:
+                return _("scheduled")
+            else:
+                return _("draft")
+        else:
+            if self.has_unpublished_changes:
+                return _("live + draft")
+            else:
+                return _("live")
--- cfgov/ask_cfpb/views.py
+++ cfgov/ask_cfpb/views.py
@@ -6,6 +6,12 @@ from .models.pages import AnswerPage
 def view_answer(request, slug, language, answer_id):
     answer_page = get_object_or_404(
         AnswerPage, language=language, answer_base__id=answer_id)
+    if answer_page.redirect_id:
+        new_page = get_object_or_404(
+            AnswerPage,
+            language=language,
+            answer_base__id=answer_page.redirect_id)
+        return redirect(new_page.url)
     if "{}-{}-{}".format(slug, language, answer_id) != answer_page.slug:
         return redirect(answer_page.url)
     else:
