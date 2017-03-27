f4cb0a53935533a7c75422d38581c6fb3a9d8d9d
Virginia Czosek <Virginia.Czosek@cfpb.gov>
2017-03-27 13:59:20 -0700

    Add condition in html editor & tips plugins so their buttons are only added in ask cfpb admin pages

--- cfgov/v1/static/js/admin/ask_cfpb_tips.js
+++ cfgov/v1/static/js/admin/ask_cfpb_tips.js
@@ -1,4 +1,4 @@
- 
+
 (function () {
   (function (jQuery) {
     return jQuery.widget("IKS.answermodule", {
@@ -9,42 +9,44 @@
         buttonCssClass: null
       },
       populateToolbar: function (toolbar) {
-        var buttonset,
-        _this = this;
-        buttonset = jQuery("<span class=\"" + this.widgetName + "\"></span>");
-        var buttonElement;
-        buttonElement = jQuery('<span></span>');
-        buttonElement.hallobutton({
-          uuid: _this.options.uuid,
-          editable: _this.options.editable,
-          label: "Ask CFPB answer module",
-          icon: 'icon-edit',
-          command: null
-        });
-
-        buttonset.append(buttonElement);
-        
-        buttonElement.on('click', function (event) {
-          var sel = window.getSelection();
-          var range = sel.getRangeAt(0).cloneRange();
-          var contents = range.cloneContents();
-          var elem = document.createElement("aside");
-          elem.className = "answer-module";
-          elem.appendChild(contents);
-          range.deleteContents();
-          range.insertNode(elem);
-
-          $(range.commonAncestorContainer).find('*').each(function() {
-            if ($(this).is(':empty') || $(this).text() === '') {
-              $(this).remove();
-            }
+        if (window.location.href.indexOf('ask_cfpb') > -1) {
+          var buttonset,
+          _this = this;
+          buttonset = jQuery("<span class=\"" + this.widgetName + "\"></span>");
+          var buttonElement;
+          buttonElement = jQuery('<span></span>');
+          buttonElement.hallobutton({
+            uuid: _this.options.uuid,
+            editable: _this.options.editable,
+            label: "Ask CFPB answer module",
+            icon: 'icon-edit',
+            command: null
           });
 
-          return _this.options.editable.element.trigger('change');
-        });
-        
-        buttonset.hallobuttonset();
-        return toolbar.append(buttonset);
+          buttonset.append(buttonElement);
+          
+          buttonElement.on('click', function (event) {
+            var sel = window.getSelection();
+            var range = sel.getRangeAt(0).cloneRange();
+            var contents = range.cloneContents();
+            var elem = document.createElement("aside");
+            elem.className = "answer-module";
+            elem.appendChild(contents);
+            range.deleteContents();
+            range.insertNode(elem);
+
+            $(range.commonAncestorContainer).find('*').each(function() {
+              if ($(this).is(':empty') || $(this).text() === '') {
+                $(this).remove();
+              }
+            });
+
+            return _this.options.editable.element.trigger('change');
+          });
+          
+          buttonset.hallobuttonset();
+          return toolbar.append(buttonset);
+        }
       }
     });
 })(jQuery);
--- cfgov/v1/static/js/admin/html_editor.js
+++ cfgov/v1/static/js/admin/html_editor.js
@@ -6,47 +6,73 @@
         editable: null
       },
       populateToolbar: function(toolbar) {
-        var button, widget;
+        if (window.location.href.indexOf('ask_cfpb') > -1) {
+          var button, widget;
 
-        var getEnclosing = function(tag) {
-          var node;
-          node = widget.options.editable.getSelection().commonAncestorContainer;
-          return $(node).parents(tag).get(0);
-        };
+          var getEnclosing = function(tag) {
+            var node;
+            node = widget.options.editable.getSelection().commonAncestorContainer;
+            return $(node).parents(tag).get(0);
+          };
 
-        widget = this;
+          widget = this;
 
-        button = $('<span></span>');
-        button.hallobutton({
-          uuid: this.options.uuid,
-          editable: this.options.editable,
-          label: 'Edit HTML',
-          icon: 'icon-code',
-          command: null
-        });
-
-        toolbar.append(button);
-
-        button.on('click', function(event) {
-          $('body > .modal').remove();
-          var container = $('<div class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">\n    <div class="modal-dialog">\n        <div class="modal-content"\
->\n            <button type="button" class="close icon text-replace icon-cross" data-dismiss="modal" aria-hidden="true">&times;</button>\n            <div class="modal-body"><hea\
-der class="nice-padding   hasform"><div class="row"><div class="left"><div class="col"><h1><i class="fa fa-file-code-o"></i>Edit HTML Code</h1></div></header><div class="modal-bo\
-dy-body" style="padding:15px;"></div></div>\n        </div><!-- /.modal-content -->\n    </div><!-- /.modal-dialog -->\n</div>');
-
-          // add container to body and hide it, so content can be added to it before display                                                                             
-          $('body').append(container);
-          container.modal('hide');
-          var modalBody = container.find('.modal-body-body');
-          modalBody.html('<textarea style="height: 400px; border: 1px solid black" id="wagtail-edit-html-content">'+widget.options.editable.element.html()+'</textarea><button i\
-d="wagtail-edit-html-save" type="button" class="button" style="margin-top:15px;">Save</button>');
-          $("#wagtail-edit-html-save").on("click", function() {
-              widget.options.editable.setContents($("#wagtail-edit-html-content").val());
-              widget.options.editable.setModified();
-              container.modal('hide');
+          button = $('<span></span>');
+          button.hallobutton({
+            uuid: this.options.uuid,
+            editable: this.options.editable,
+            label: 'Edit HTML',
+            icon: 'icon-code',
+            command: null
           });
-          container.modal('show');
-        });
+
+          toolbar.append(button);
+
+          button.on('click', function(event) {
+            $('body > .modal').remove();
+            var contents = '<div class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">' + 
+                              '<div class="modal-dialog">' +
+                                '<div class="modal-content">'+
+                                  '<button type="button" class="close icon text-replace icon-cross" '+
+                                           'data-dismiss="modal" aria-hidden="true">' +
+                                      '&times;' + 
+                                  '</button>' +         
+                                  '<div class="modal-body">' +
+                                    '<header class="nice-padding hasform">' +
+                                      '<div class="row">' +
+                                        '<div class="left">' +
+                                          '<div class="col">' + 
+                                            '<h1><i class="fa fa-file-code-o"></i>Edit HTML Code</h1>' +
+                                          '</div>' +
+                                        '</div>' +
+                                      '</div>' +
+                                    '</header>' +
+                                    '<div class="modal-body-body" style="padding:15px;"></div>' +
+                                  '</div> <!-- /.modal-body -->' +         
+                                '</div><!-- /.modal-content -->' +    
+                              '</div><!-- /.modal-dialog -->' +
+                            '</div>';
+            var container = $(contents);
+
+            // add container to body and hide it, so content can be added to it before display                                                                             
+            $('body').append(container);
+            container.modal('hide');
+            var modalBody = container.find('.modal-body-body');
+            var bodyContents =  '<textarea style="height: 400px; border: 1px solid black" id="wagtail-edit-html-content">' +
+                                  widget.options.editable.element.html() +
+                                '</textarea>' +
+                                '<button id="wagtail-edit-html-save" type="button" class="button" style="margin-top:15px;">' +
+                                  'Save' + 
+                                '</button>';
+            modalBody.html(bodyContents);
+            $("#wagtail-edit-html-save").on("click", function() {
+                widget.options.editable.setContents($("#wagtail-edit-html-content").val());
+                widget.options.editable.setModified();
+                container.modal('hide');
+            });
+            container.modal('show');
+          });
+        }
       }
     });
   })(jQuery);
