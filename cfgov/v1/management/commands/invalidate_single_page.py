72c136d54864cab68d8ed01000049b1d7b487301
Richa Agarwal <richa.agarwal85@gmail.com>
2017-04-20 12:37:09 -0700

    Make url argument required

--- cfgov/v1/management/commands/invalidate_single_page.py
+++ cfgov/v1/management/commands/invalidate_single_page.py
@@ -9,15 +9,10 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument(
             '--url',
-            nargs='?',
+            required=True,
             help='The full URL for the page you wish to invalidate'
         )
 
     def handle(self, *args, **options):
         page_url = options['url']
-        if page_url:
-            purge_url_from_cache(page_url)
-        else:
-            raise CommandError('Must supply a page URL')
-
         purge_url_from_cache(page_url)
