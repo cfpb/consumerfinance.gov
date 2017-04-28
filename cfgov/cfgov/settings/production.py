44c4b34fc1e99265c5eb195a45e1f3676b01833e
Will Barton <willbarton@users.noreply.github.com>
2017-04-27 23:02:28 -0400

    Add a default of file logging handler filename

--- cfgov/cfgov/settings/production.py
+++ cfgov/cfgov/settings/production.py
@@ -18,7 +18,7 @@ LOGGING = {
         'disk': {
             'level': 'INFO',
             'class': 'logging.handlers.RotatingFileHandler',
-            'filename': os.getenv('CFGOV_DJANGO_LOG'),
+            'filename': os.getenv('CFGOV_DJANGO_LOG', '/dev/null'),
             'maxBytes': 1024*1024*10,  # max 10 MB per file
             'backupCount': 5,  # keep 5 files around
         },
