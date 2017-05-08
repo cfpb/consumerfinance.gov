0458bb5fb56837e43d16499d1fef64358b4a1500
Will Barton <william.barton@cfpb.gov>
2017-05-08 10:00:13 -0400

    Test for unicode email addresses and usernames

--- cfgov/core/tests/test_management_inactive_users.py
+++ cfgov/core/tests/test_management_inactive_users.py
@@ -1,4 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from StringIO import StringIO
+
 from datetime import timedelta
 
 import mock
@@ -13,7 +17,7 @@ class InactiveUsersTestCase(TestCase):
 
     def setUp(self):
         User = get_user_model()
-        
+
         # This user is clearly inactive at 90 days
         self.user_1 = User.objects.create(username='user_1',
                                           email='user_1@example.com')
@@ -27,51 +31,57 @@ class InactiveUsersTestCase(TestCase):
         self.user_2.save()
 
         # This user is not inactive because it's been 89 days
-        self.user_3 = User.objects.create(username='user_3',
-                                          email='user_3@example.com')
+        self.user_3 = User.objects.create(username='üser_3',
+                                          email='üser_3@example.com')
         self.user_3.last_login = timezone.now() - timedelta(days=89)
         self.user_3.save()
+
+        self.stdout = StringIO()
+
+    def get_stdout(self):
+        return self.stdout.getvalue().decode('utf-8')
 
     def test_get_inactive_users(self):
         """ Test that two users are listed for the default 90 period
         including one that last logged in 90 days ago. """
-        out = StringIO()
-        call_command('inactive_users', stdout=out)
-        self.assertIn("user_1", out.getvalue())
-        self.assertIn("user_2", out.getvalue())
-        self.assertNotIn("user_3", out.getvalue())
+        call_command('inactive_users', stdout=self.stdout)
+        self.assertIn("user_1", self.get_stdout())
+        self.assertIn("user_2", self.get_stdout())
+        self.assertNotIn("üser_3", self.get_stdout())
 
     def test_get_inactive_users_87_days(self):
         """ Test that all users are listed for a custom 87 day period """
         out = StringIO()
-        call_command('inactive_users', period=87, stdout=out)
-        self.assertIn("user_1", out.getvalue())
-        self.assertIn("user_2", out.getvalue())
-        self.assertIn("user_3", out.getvalue())
+        call_command('inactive_users', period=87, stdout=self.stdout)
+        self.assertIn("user_1", self.get_stdout())
+        self.assertIn("user_2", self.get_stdout())
+        self.assertIn("üser_3", self.get_stdout())
 
     def test_get_inactive_users_92_days(self):
         """ Test that no users are listed for a custom 92 day period """
         out = StringIO()
-        call_command('inactive_users', period=92, stdout=out)
-        self.assertNotIn("user_1", out.getvalue())
-        self.assertNotIn("user_2", out.getvalue())
-        self.assertNotIn("user_3", out.getvalue())
+        call_command('inactive_users', period=92, stdout=self.stdout)
+        self.assertNotIn("user_1", self.get_stdout())
+        self.assertNotIn("user_2", self.get_stdout())
+        self.assertNotIn("üser_3", self.get_stdout())
 
     @mock.patch('core.management.commands.inactive_users.mail.EmailMessage')
     def test_sends_email(self, mock_EmailMessage):
         """ Test that mail.EmailMessage is called with the appropriate
         list of users """
         out = StringIO()
-        call_command('inactive_users', emails=['test@example.com'], stdout=out)
-        mock_EmailMessage.assert_called_once()
+        call_command('inactive_users',
+                     emails=['test@example.com'],
+                     stdout=self.stdout)
+        mock_EmailMessage.return_value.send.assert_called_once()
         message = mock_EmailMessage.call_args[0][1]
         self.assertIn("user_1", message)
         self.assertIn("user_2", message)
-        self.assertNotIn("user_3", message)
+        self.assertNotIn("üser_3", message)
 
-        # User list is not printed to stdout but a message about sending the
+        # User list is printed to stdout and a message about sending the
         # email is
-        self.assertNotIn("user_1", out.getvalue())
-        self.assertNotIn("user_2", out.getvalue())
-        self.assertNotIn("user_3", out.getvalue())
-        self.assertIn("test@example.com", out.getvalue())
+        self.assertIn("user_1", self.get_stdout())
+        self.assertIn("user_2", self.get_stdout())
+        self.assertNotIn("üser_3", self.get_stdout())
+        self.assertIn("test@example.com", self.get_stdout() )
