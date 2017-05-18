167d57ae69dc10111c01314812aff78f0fd83954
Scott Cranfill <scott.cranfill@cfpb.gov>
2017-05-18 13:41:29 -0400

    Modify image_alt_value to accept renditions, too
    Per PR feedback from @sebworks

--- cfgov/v1/jinja2tags.py
+++ cfgov/v1/jinja2tags.py
@@ -1,14 +1,23 @@
 from jinja2.ext import Extension
 
+from v1.models import CFGOVRendition
+
 
 def image_alt_value(image):
-    """Given an ImageBasic block as `image`, return the appropriate alt text.
+    """Given an ImageBasic block or a CFGOVImage rendition as `image`,
+    return the appropriate alt text.
 
+    Return the CFGOVImage rendition's alt field, if present.
     Returns the alt field from the block, if it is set.
     Otherwise, returns the alt field from the CFGOVImage object, if it is set.
     Otherwise, returns an empty string.
     """
 
+    # Check to see if the passed value is a CFGOVRendition
+    if isinstance(image, CFGOVRendition):
+        return image.alt
+
+    # Otherwise, if it is a block
     if image:
         block_alt = image.get('alt')
         upload = image.get('upload')
