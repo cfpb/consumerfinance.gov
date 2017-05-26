b9ed9faa2626231cdd806b92ae997f6d3c1648d4
Andy Chosak <chosak@users.noreply.github.com>
2017-05-26 11:41:08 -0400

    don't run in headless mode
    Headless mode seems to be causing problems with clicking on certain
    elements in the Wagtail UI. Trying to run non-headless, but still using
    Xvfb as a frame buffer on Travis.

--- run_travis.sh
+++ run_travis.sh
@@ -16,6 +16,5 @@ elif [ "$RUNTEST" == "acceptance" ]; then
     sh -e /etc/init.d/xvfb start &
     sleep 3
 
-    export HEADLESS_CHROME_BINARY=/usr/bin/google-chrome-beta
     gulp test:acceptance
 fi
