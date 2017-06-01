401e6f33ba2d675addd68d25d0edd2a42093ff35
Ross Karchner <ross.karchner@cfpb.gov>
2017-06-01 08:37:11 -0400

    adds a management command for converting all tables to innodb

---
+++ cfgov/core/management/commands/convert_to_innodb.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from django.db import connections
+
+
+class Command(BaseCommand):
+
+    def handle(self, database="default", *args, **options):
+
+        cursor = connections[database].cursor()
+
+        cursor.execute("SHOW TABLE STATUS")
+
+        for row in cursor.fetchall():
+            if row[1] != "InnoDB":
+                print "Converting %s" % row[0],
+                print cursor.execute("ALTER TABLE %s ENGINE=INNODB" % row[0])
