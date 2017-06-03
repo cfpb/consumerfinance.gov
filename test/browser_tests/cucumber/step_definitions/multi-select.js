0ee5b9d9adf4693b2ad83efb202f308546c1645a
Steven Bellamy <steven.bellamy@cfpb.gov>
2017-06-03 11:02:40 -0400

    Tests Refactoring and minor cleanup

--- test/browser_tests/cucumber/step_definitions/multi-select.js
+++ test/browser_tests/cucumber/step_definitions/multi-select.js
@@ -3,10 +3,9 @@
 
 var multiSelect = require( '../../shared_objects/multi-select.js' );
 var { defineSupportCode } = require( 'cucumber' );
-var { expect } = require( 'chai' );
 
 
-defineSupportCode( function( { Then, When } ) {
+defineSupportCode( function( { When } ) {
   When( 'interacting with search input', function( ) {
 
     return multiSelect.multiSelectSearch.click();
