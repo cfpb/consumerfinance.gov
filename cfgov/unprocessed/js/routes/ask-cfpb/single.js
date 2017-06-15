db59404342585e8cf688c1b88ba70d9d741b8caf
Virginia Czosek <Virginia.Czosek@cfpb.gov>
2017-06-15 11:10:27 -0700

    Send virtual pageview event on Ask answer pages

--- cfgov/unprocessed/js/routes/ask-cfpb/single.js
+++ cfgov/unprocessed/js/routes/ask-cfpb/single.js
@@ -1,13 +1,29 @@
 require( '../on-demand/ask-autocomplete' );
+var Analytics = require( '../../modules/Analytics' );
 
 var Expandable = require( '../../organisms/Expandable' );
 var getBreakpointState = require( '../../modules/util/breakpoint-state' ).get;
 
 var readMoreContainer = document.querySelector( '.o-expandable__read-more' );
 if ( readMoreContainer && getBreakpointState().isBpXS ) {
-	var readMoreExpandable = new Expandable( readMoreContainer ).init();
-	readMoreExpandable.addEventListener( 'expandEnd', function () {
-		readMoreExpandable.destroy();
-		readMoreContainer.querySelector( '.o-expandable_content' ).style.height = '';
-	} );
+  var readMoreExpandable = new Expandable( readMoreContainer ).init();
+  readMoreExpandable.addEventListener( 'expandEnd', function () {
+    readMoreExpandable.destroy();
+    readMoreContainer.querySelector( '.o-expandable_content' ).style.height = '';
+  } );
+}
+
+if ( window.answerID && window.categorySlug ) {
+  function sendEvent() {
+    var eventData = Analytics.getDataLayerOptions( '/askcfpb/' + window.answerID + '/',
+      document.title, 'Virtual Pageview' );
+    eventData.category = window.categorySlug;
+    Analytics.sendEvent( eventData );
+  }
+  
+  if ( Analytics.tagManagerIsLoaded ) {
+    sendEvent();
+  } else {
+    Analytics.addEventListener( 'gtmLoaded', sendEvent );
+  }
 }
--- cfgov/unprocessed/js/routes/es/obtener-respuestas/single.js
+++ cfgov/unprocessed/js/routes/es/obtener-respuestas/single.js
@@ -1,3 +1,20 @@
 require( '../../on-demand/feedback-form' );
 require( '../../on-demand/ask-autocomplete' );
+var Analytics = require( '../../../modules/Analytics' );
 
+if ( window.answerID && window.categorySlug ) {
+  function sendEvent() {
+    var eventData = Analytics.getDataLayerOptions( 
+      '/es/obtener-respuestas/c/'+ window.categorySlug + '/' + window.answerID + '/',
+      document.title, 
+      'Virtual Pageview' );
+    eventData.category = window.categorySlug;
+    Analytics.sendEvent( eventData );
+  }
+  
+  if ( Analytics.tagManagerIsLoaded ) {
+    sendEvent();
+  } else {
+    Analytics.addEventListener( 'gtmLoaded', sendEvent );
+  }
+}
