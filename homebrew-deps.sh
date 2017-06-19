a91665d5d6a135b8b2f0f770d66752d74a63ec6f
Ross Karchner <ross.karchner@cfpb.gov>
2017-06-19 13:14:50 -0400

    add a script for install homebrew dependencies

---
+++ homebrew-deps.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# Runtime dependencies, and things expected by Pillow and Wand
+# https://github.com/python-pillow/Pillow/blob/master/docs/installation.rst#building-on-macos
+# http://docs.wand-py.org/en/0.4.1/guide/install.html#install-imagemagick-on-mac
+
+brew install docker-compose docker-machine docker imagemagick 
