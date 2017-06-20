37fd156da65ce88f4a9836c020ce48bb8584f90e
Ross Karchner <ross.karchner@cfpb.gov>
2017-06-20 10:25:00 -0400

    minor tweaks

--- arewefastyet.sh
+++ arewefastyet.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
-source /usr/local/homebrew/bin/virtualenvwrapper.sh
 
+if [ -z "$VIRTUAL_ENV" ]; then
+    source /usr/local/homebrew/bin/virtualenvwrapper.sh
+    # reset the world
+    docker-machine rm cfgov -y
+
+    rmvirtualenv cfgov-refresh
+    rm -rf ~/Library/Caches/pip
+    rm -rf node_modules
+    rm -rf .env
+
+    # run our standard setup
+    source load-env.sh
+    source setup.sh
+    sleep 10
+    source refresh-data.sh
+else
+  echo you must 'deactivate' your virtualenv before running this
+  exit 1
+fi
+
+source /usr/local/homebrew/bin/virtualenvwrapper.sh
 # reset the world
 docker-machine rm cfgov -y
-deactivate cfgov-refresh
+
 rmvirtualenv cfgov-refresh
 rm -rf ~/Library/Caches/pip
 rm -rf node_modules
