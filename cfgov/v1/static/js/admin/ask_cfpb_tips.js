00b38b26c35c2120fa19e867dad534e35d086b4e
Virginia Czosek <Virginia.Czosek@cfpb.gov>
2017-06-21 17:07:44 -0700

    Make it possible to remove as well as apply Ask answer tip styling by clicking tip button

--- cfgov/v1/static/js/admin/ask_cfpb_tips.js
+++ cfgov/v1/static/js/admin/ask_cfpb_tips.js
@@ -11,6 +11,7 @@
       populateToolbar: function (toolbar) {
         if (window.location.href.indexOf('ask_cfpb') > -1) {
           var buttonset,
+          baseClass = "answer-module",
           _this = this;
           buttonset = jQuery("<span class=\"" + this.widgetName + "\"></span>");
           var buttonElement;
@@ -27,19 +28,59 @@
           
           buttonElement.on('click', function (event) {
             var sel = window.getSelection();
-            var range = sel.getRangeAt(0).cloneRange();
-            var contents = range.cloneContents();
-            var elem = document.createElement("aside");
-            elem.className = "answer-module";
-            elem.appendChild(contents);
-            range.deleteContents();
-            range.insertNode(elem);
-
-            $(range.commonAncestorContainer).find('*').each(function() {
-              if ($(this).is(':empty') || $(this).text() === '') {
-                $(this).remove();
+            var node = sel.baseNode;
+            var parent = node.parentElement;
+            // Walk up the parents of the selected node up to 
+            // the rich text input element. If an element with
+            // the tip class is found, remove the containing
+            // tip element and return.
+            while ( parent ) {
+              if ( parent.classList.contains( baseClass ) ) {
+                var docFrag = document.createDocumentFragment();
+                while (parent.firstChild) {
+                    var child = parent.removeChild(parent.firstChild);
+                    docFrag.appendChild(child);
+                }
+                parent.parentNode.replaceChild(docFrag, parent);
+                return;
+              } else if ( parent.classList.contains('richtext') ) {
+                parent = null;
+              } else {
+                parent = parent.parentNode;
               }
-            });
+            }
+
+            // Otherwise, check that the selection is not empty.
+            // If it has contents, clone them, insert them in a
+            // new tip element, and replace the selection with
+            // the new element.
+            var range = sel.getRangeAt(0).cloneRange();
+
+            if (range.endOffset > range.startOffset) {
+              var contents = range.cloneContents();
+              var elem = document.createElement("aside");
+              elem.className = baseClass;
+              elem.appendChild(contents);
+              range.deleteContents();
+              range.insertNode(elem);
+
+              // Update selection's value to encompass
+              // the contents of new element. this makes
+              // it possible to immediately remove the new tip
+              // wrapper by clicking the tip button again.
+              var newRange = document.createRange();
+              newRange.setStart(elem, 0);
+              newRange.setEnd(elem, elem.childNodes.length); 
+              sel.removeAllRanges();
+              sel.addRange(newRange);
+
+              // Clear any empty elements created by this process.
+              $(range.commonAncestorContainer).find('*').each(function() {
+                if ($(this).is(':empty') || $(this).text() === '') {
+                  $(this).remove();
+                }
+              });
+            }
 
             return _this.options.editable.element.trigger('change');
           });
