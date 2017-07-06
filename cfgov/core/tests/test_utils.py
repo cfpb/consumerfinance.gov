67cbb256a1cf1fa0dcea6b3d56254d83c1456295
Andy Chosak <chosak@users.noreply.github.com>
2017-07-06 19:44:17 -0400

    remove unused slice_list function

--- cfgov/core/tests/test_utils.py
+++ cfgov/core/tests/test_utils.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from core.utils import extract_answers_from_request, slice_list
+from core.utils import extract_answers_from_request
 
 
 class FakeRequest(object):
@@ -23,26 +23,3 @@ class ExtractAnswersTest(TestCase):
         result = extract_answers_from_request(request)
         assert sorted(result) == [('another', 'another_answer'),
                                   ('first', 'some_answer')]
-
-
-class TestSliceList(TestCase):
-    def test_empty_list(self):
-        self.assertEqual(list(slice_list([], 10)), [])
-
-    def test_single_slice(self):
-        self.assertEqual(
-            list(slice_list([1, 2, 3], 1)),
-            [[1, 2, 3]]
-        )
-
-    def test_equally_sized_slices(self):
-        self.assertEqual(
-            list(slice_list([1, 2, 3, 4, 5, 6], 3)),
-            [[1, 2], [3, 4], [5, 6]]
-        )
-
-    def test_unequally_sized_slices(self):
-        self.assertEqual(
-            list(slice_list([1, 2, 3, 4, 5], 3)),
-            [[1, 2], [3, 4], [5]]
-        )
