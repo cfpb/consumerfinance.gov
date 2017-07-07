da230ede35f04c528008c361f54e87d46bb78c7a
Andy Chosak <chosak@users.noreply.github.com>
2017-07-06 22:09:27 -0400

    fixing flaky unit tests
    These unit tests were failing on Travis due to inconsistencies with
    floating point numbers and file ordering on different operating systems.

--- cfgov/legacy/tests/housing_counselor/test_generator.py
+++ cfgov/legacy/tests/housing_counselor/test_generator.py
@@ -36,8 +36,8 @@ class TestGeneratorCounselorJson(TestCase):
 
     def test_generate_creates_json_files(self):
         generate_counselor_json(self.counselors, self.zipcodes, self.tempdir)
-        self.assertEqual(
-            os.listdir(self.tempdir),
+        self.assertItemsEqual(
+            os.listdir(self.tempdir),  # os.listdir order not guaranteed.
             ['20001.json', '20002.json']
         )
 
@@ -46,25 +46,33 @@ class TestGeneratorCounselorJson(TestCase):
         with open(os.path.join(self.tempdir, '20001.json')) as f:
             data = json.load(f)
 
-        self.assertEqual(data, {
-            'zip': {
-                'lat': 115.5,
-                'lng': 97.5,
-                'zipcode': '20001',
-            },
-            'counseling_agencies': [
-                {
-                    'agc_ADDR_LATITUDE': 120,
-                    'agc_ADDR_LONGITUDE': 98,
-                    'distance': 311.35243783709234,
-                },
-                {
-                    'agc_ADDR_LATITUDE': 125,
-                    'agc_ADDR_LONGITUDE': 99,
-                    'distance': 658.4536705140896,
-                },
-            ],
+        self.assertEqual(data['zip'], {
+            'lat': 115.5,
+            'lng': 97.5,
+            'zipcode': '20001',
         })
+
+        agencies = data['counseling_agencies']
+        self.assertEqual(len(agencies), 2)
+
+        expected_agencies = [
+            {
+                'agc_ADDR_LATITUDE': 120,
+                'agc_ADDR_LONGITUDE': 98,
+                'distance': 311.35243783709234,
+            },
+            {
+                'agc_ADDR_LATITUDE': 125,
+                'agc_ADDR_LONGITUDE': 99,
+                'distance': 658.4536705140896,
+            },
+        ]
+
+        for a, b in zip(agencies, expected_agencies):
+            self.assertItemsEqual(a.keys(), b.keys())
+
+            for k in a:
+                self.assertAlmostEqual(a[k], b[k])
 
 
 class TestGetCounselorJsonFiles(TestCase):
