49bebf926ee6c8b5362f30ec0cdc66a891a23907
Ross Karchner <ross.karchner@cfpb.gov>
2017-07-11 13:09:15 -0400

    add a history model, permissions, and more tests!

--- cfgov/v1/admin_views.py
+++ cfgov/v1/admin_views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.contrib import messages
 from django.conf import settings
+from django.http.response import HttpResponseForbidden
 
 from wagtail.contrib.wagtailfrontendcache.utils import purge_url_from_cache
 
 from requests.exceptions import HTTPError
 from v1.admin_forms import AkamaiFlushForm
-from v1.models.akamai_backend import AkamaiBackend
+from v1.models.akamai_backend import AkamaiBackend, AkamaiHistory
 
 
 def cdn_is_configured():
@@ -14,39 +15,55 @@ def cdn_is_configured():
             settings.WAGTAILFRONTENDCACHE)
 
 
-def purge(request, url=None):
+def purge(url=None):
     if url:
         purge_url_from_cache(url)
-        messages.success(request,
-                         "flushing %s" % url)
+        return "flushed %s" % url
     else:
         config = settings.WAGTAILFRONTENDCACHE['akamai']
         backend = AkamaiBackend(config)
         backend.purge_all()
-        messages.success(request,
-                         "flushed entire site")
+        return "flushed entire site"
 
 
 def manage_cdn(request):
     if not cdn_is_configured():
         return render(request, 'cdnadmin/disabled.html')
 
+    user_can_purge = request.user.has_perm('v1.add_akamaihistory')
+
     if request.method == 'GET':
         form = AkamaiFlushForm()
     elif request.method == 'POST':
+        if not user_can_purge:
+            return HttpResponseForbidden()
+
         form = AkamaiFlushForm(request.POST)
         if form.is_valid():
+            url = form.cleaned_data['url']
+            history_item = AkamaiHistory(subject=url or "entire site",
+                                         user=request.user)
             try:
-                purge(request, form.cleaned_data['url'])
+                message = purge(url)
+                history_item.message = message
+                history_item.save()
+                messages.success(request, message)
+
             except Exception as e:
                 if isinstance(e, HTTPError):
                     error_info = e.response.json()
                     error_message = "{title}: {detail}".format(**error_info)
                 else:
                     error_message = repr(e)
+                history_item.message = error_message
+                history_item.save()
                 messages.error(request, error_message)
         else:
             for field, error_list in form.errors.iteritems():
                 for error in error_list:
                     messages.error(request, "Error in %s: %s" % (field, error))
-    return render(request, 'cdnadmin/index.html', context={'form': form})
+    history = AkamaiHistory.objects.all().order_by('-created')[:20]
+    return render(request, 'cdnadmin/index.html',
+                  context={'form': form,
+                           'user_can_purge': user_can_purge,
+                           'history': history})
