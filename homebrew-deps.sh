a60f033a6e99ea0d12d03744c41786bd1c98a549
Ross Karchner <ross.karchner@cfpb.gov>
2017-08-02 09:12:49 -0400

    homebrew deps improvement

--- homebrew-deps.sh
+++ homebrew-deps.sh
@@ -4,4 +4,6 @@
 # https://github.com/python-pillow/Pillow/blob/master/docs/installation.rst#building-on-macos
 # http://docs.wand-py.org/en/0.4.1/guide/install.html#install-imagemagick-on-mac
 
+brew update
 brew install docker-compose docker-machine docker imagemagick python node
+$(brew --prefix)/bin/pip2.7 install -U pip virtualenv virtualenvwrapper
