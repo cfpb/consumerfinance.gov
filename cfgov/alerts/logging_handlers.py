33bb7e325429939c103770e99af067997640a384
Richa Agarwal <richa.agarwal85@gmail.com>
2017-09-15 00:21:38 -0700

    Add test for logging_handlers

--- cfgov/alerts/logging_handlers.py
+++ cfgov/alerts/logging_handlers.py
@@ -4,9 +4,6 @@ from alerts.github_alert import GithubAlert
 
 
 class CFGovErrorHandler(logging.Handler):
-    def __init__(self):
-        logging.Handler.__init__(self)
-
     def emit(self, record):
         GithubAlert({}).post(
             title=record.message[:30],
