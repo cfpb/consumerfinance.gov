3d08a288e74e202d44e49e8a53e2146cebbc32ea
Hillary Jeffrey <hillary.jeffrey@cfpb.gov>
2017-10-24 16:50:48 -0400

    Combines install and run travis files
    Combines these for clarity during the conversion process

--- run_travis.sh
+++ run_travis.sh
@@ -1,18 +1,46 @@
 #!/usr/bin/env bash
 
+# Install frontend dependencies
+frontend() {
+    export CXX=clang++
+
+    if [[ "$(node -v)" != 'v8.'* ]]; then
+        curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+        source $HOME/.nvm/nvm.sh
+        nvm install 8.0.0
+    fi
+
+    npm install -g gulp-cli
+
+    # Added to fix an issue with gulp not being accessible in run_travis.sh
+    echo 'PATH=/home/travis/.nvm/versions/node/v8.0.0/lib/node_modules/gulp-cli/bin/gulp.js:$PATH' >> ~/.bash_profile
+
+    chmod +x ./frontend.sh
+    ./frontend.sh test
+}
+
+# Install backend dependencies
+backend() {
+    pip install -r requirements/travis.txt
+}
+
 # Fail if any command fails.
 set -e
 
 echo "running $RUNTEST tests"
 if [ "$RUNTEST" == "frontend" ]; then
+    frontend
     source $HOME/.nvm/nvm.sh
     nvm use 8.0.0
     gulp "test" --travis
 elif [ "$RUNTEST" == "backend" ]; then
+    backend
     flake8
     tox -e fast
     tox -e missing-migrations
 elif [ "$RUNTEST" == "acceptance" ]; then
+    frontend
+    backend
     source $HOME/.nvm/nvm.sh
     nvm use 8.0.0
     export DISPLAY=:99.0
