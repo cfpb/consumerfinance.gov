5fb61eb8ae6c2e0b2db3345b0a47a556a2d55ff2
Steven Bellamy <steven.bellamy@cfpb.gov>
2017-11-06 11:23:23 -0500

    Adding webpack stats

--- config/webpack-config.js
+++ config/webpack-config.js
@@ -17,9 +17,23 @@ const JS_ROUTES_PATH = '/js/routes';
 const OAH_COMMON_BUNDLE_ROUTE = '/js/routes/owning-a-home/';
 const COMMON_BUNDLE_NAME = 'common.js';
 
+
+const STATS = {
+  // Add chunk information (setting this to `false` allows for a less verbose output)
+  chunks: true,
+  // Add built modules information to chunk information
+  chunkModules: true,
+  // Add the origins of chunks and chunk merging info
+  chunkOrigins: true,
+  // Add errors
+  errors: true,
+  // Add timing information
+  timings: true,
+};
+
 // Commmon webpack 'module' option used in each configuration.
 // Runs code through Babel and uses global supported browser list.
-const COMMON_MODULE_CONFIG = {
+const COMMON_MODULE = {
   loaders: [ {
     test: /\.js$/,
     loaders: [ {
@@ -44,7 +58,7 @@ const COMMON_MODULE_CONFIG = {
 
  // Set warnings to true to show linter-style warnings.
  // Set mangle to false and beautify to true to debug the output code.
-const COMMON_UGLIFY_CONFIG = new UglifyWebpackPlugin( {
+const COMMON_UGLIFY = new UglifyWebpackPlugin( {
   parallel: true,
   uglifyOptions: {
     ie8: false,
@@ -58,11 +72,12 @@ const COMMON_UGLIFY_CONFIG = new UglifyWebpackPlugin( {
   }
 } );
 
-const modernConf = {
+const MODERN = {
   cache: true,
+  profile: true,
   context: path.join( __dirname, '/../', paths.unprocessed, JS_ROUTES_PATH ),
   entry: scriptsManifest.getDirectoryMap( paths.unprocessed + JS_ROUTES_PATH ),
-  module: COMMON_MODULE_CONFIG,
+  module: COMMON_MODULE,
   output: {
     path: path.join( __dirname, 'js' ),
     filename: '[name]'
@@ -71,63 +86,63 @@ const modernConf = {
     new webpack.optimize.CommonsChunkPlugin( {
       name: COMMON_BUNDLE_NAME
     } ),
-    COMMON_UGLIFY_CONFIG
+    COMMON_UGLIFY
   ]
 };
 
-const ieConf = {
+const IE = {
   entry: paths.unprocessed + '/js/ie/common.ie.js',
-  module: COMMON_MODULE_CONFIG,
+  module: COMMON_MODULE,
   output: {
     filename: 'common.ie.js'
   },
-  plugins: [ COMMON_UGLIFY_CONFIG ]
+  plugins: [ COMMON_UGLIFY ]
 };
 
-const externalConf = {
+const EXTERNAL = {
   entry: paths.unprocessed + JS_ROUTES_PATH + '/external-site/index.js',
-  module: COMMON_MODULE_CONFIG,
+  module: COMMON_MODULE,
   output: {
     filename: 'external-site.js'
   },
-  plugins: [ COMMON_UGLIFY_CONFIG ]
+  plugins: [ COMMON_UGLIFY ]
 };
 
-const onDemandConf = {
+const ON_DEMAND = {
   context: path.join( __dirname, '/../', paths.unprocessed,
                       JS_ROUTES_PATH + '/on-demand' ),
   entry:   scriptsManifest.getDirectoryMap( paths.unprocessed +
                                             JS_ROUTES_PATH + '/on-demand' ),
-  module: COMMON_MODULE_CONFIG,
+  module: COMMON_MODULE,
   output: {
     path:     path.join( __dirname, 'js' ),
     filename: '[name]'
   },
-  plugins: [ COMMON_UGLIFY_CONFIG ]
+  plugins: [ COMMON_UGLIFY ]
 };
 
-const onDemandHeaderRawConf = {
+const ON_DEMAND_HEADER_RAW = {
   context: path.join( __dirname, '/../', paths.unprocessed,
                       JS_ROUTES_PATH + '/on-demand' ),
   entry:  './header.js',
-  module: COMMON_MODULE_CONFIG,
+  module: COMMON_MODULE,
   output: {
     path:     path.join( __dirname, 'js' ),
     filename: '[name]'
   }
 };
 
-const spanishConf = {
+const SPANISH = {
   entry: paths.unprocessed +
          JS_ROUTES_PATH + '/es/obtener-respuestas/single.js',
-  module: COMMON_MODULE_CONFIG,
+  module: COMMON_MODULE,
   output: {
     filename: 'spanish.js'
   },
-  plugins: [ COMMON_UGLIFY_CONFIG ]
+  plugins: [ COMMON_UGLIFY ]
 };
 
-const owningAHomeConf = {
+const OWNING_A_HOME = {
   cache: true,
   context: path.join( __dirname, '/../',
     paths.unprocessed, OAH_COMMON_BUNDLE_ROUTE
@@ -135,7 +150,7 @@ const owningAHomeConf = {
   entry: scriptsManifest.getDirectoryMap(
     paths.unprocessed + OAH_COMMON_BUNDLE_ROUTE
   ),
-  module: COMMON_MODULE_CONFIG,
+  module: COMMON_MODULE,
   output: {
     path: path.join( __dirname, 'js' ),
     filename: '[name]',
@@ -145,15 +160,16 @@ const owningAHomeConf = {
     new webpack.optimize.CommonsChunkPlugin( {
       name: COMMON_BUNDLE_NAME
     } ),
-    COMMON_UGLIFY_CONFIG ]
+    COMMON_UGLIFY ]
 };
 
 module.exports = {
-  onDemandHeaderRawConf: onDemandHeaderRawConf,
-  onDemandConf:          onDemandConf,
-  owningAHomeConf:       owningAHomeConf,
-  ieConf:                ieConf,
-  modernConf:            modernConf,
-  externalConf:          externalConf,
-  spanishConf:           spanishConf
+  EXTERNAL,
+  IE,
+  MODERN,
+  ON_DEMAND,
+  ON_DEMAND_HEADER_RAW,
+  OWNING_A_HOME,
+  SPANISH,
+  STATS
 };
