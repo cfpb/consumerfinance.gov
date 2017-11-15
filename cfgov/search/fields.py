0dbc55d9c57f10bb8aa21d5052ba4a059fae542c
Will Barton <william.barton@cfpb.gov>
2017-11-15 00:36:17 -0500

    Require the synonym analyzer for CharFieldWithSynonyms

--- cfgov/search/fields.py
+++ cfgov/search/fields.py
@@ -1,7 +1,18 @@
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+
 from haystack.fields import CharField as BaseCharField
 
 
 class CharFieldWithSynonyms(BaseCharField):
+
     def __init__(self, **kwargs):
+        # Ensure that the synonym analyzer is available
+        index_settings = getattr(settings, 'ELASTICSEARCH_INDEX_SETTINGS')
+        if 'synonym' not in index_settings['settings']['analysis']['analyzer']:
+            raise ImproperlyConfigured(
+                "CharFieldWithSynonyms requires a synonym analyzer to be "
+                "configured")
+
         self.analyzer = 'synonym'
         super(CharFieldWithSynonyms, self).__init__(**kwargs)
