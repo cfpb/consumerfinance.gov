c334be4eea0e05bac3ccbee8c75d3de4d007f621
Richa Agarwal <richa.agarwal85@gmail.com>
2017-12-04 17:26:16 -0800

    Modify how we know if there are 'active filters' on a page

--- cfgov/v1/tests/test_filterable_list.py
+++ cfgov/v1/tests/test_filterable_list.py
@@ -15,58 +15,25 @@ class TestFilterableListMixin(TestCase):
     def test_per_page_limit_returns_integer(self):
         assert isinstance(self.mixin.per_page_limit(), int)
 
-    def test_get_form_specific_filter_data_returns_GET_data(self):
+    def test_get_form_data_returns_GET_data(self):
         request_string = '/?title=test'
-        data = self.mixin.get_form_specific_filter_data(self.factory.get(request_string).GET)
+        data = self.mixin.get_form_data(self.factory.get(request_string).GET)
         assert data[0]['title'] == 'test'
 
-    def test_get_form_specific_filter_data_returns_GET_data_as_list_for_multiple_values(self):
+    def test_get_form_data_returns_GET_data_as_list_for_multiple_values(self):
         request_string = '/?categories=test1&categories=test2'
-        data = self.mixin.get_form_specific_filter_data(self.factory.get(request_string).GET)
+        data = self.mixin.get_form_data(self.factory.get(request_string).GET)
         assert data[0]['categories'] == ['test1', 'test2']
 
     # FilterableListMixin.get_context tests
     def test_get_context_raises_exception_for_super_obj_has_no_get_context(self):
         self.assertRaises(AttributeError, self.mixin.get_context, request=self.factory.get('/'))
 
-    @mock.patch('__builtin__.super')
-    @mock.patch('v1.util.util.get_secondary_nav_items')
-    @mock.patch('v1.util.filterable_list.FilterableListMixin.process_forms')
-    @mock.patch('v1.util.filterable_list.FilterableListMixin.get_forms')
-    def test_get_context_calls_super(self, mock_get_forms, mock_process_forms, mock_nav, mock_super):
-        self.mixin.get_context(self.factory.get('/'))
-        assert mock_super.called
-
-    @mock.patch('__builtin__.super')
-    @mock.patch('v1.util.util.get_secondary_nav_items')
-    @mock.patch('v1.util.filterable_list.FilterableListMixin.process_forms')
-    @mock.patch('v1.util.filterable_list.FilterableListMixin.get_forms')
-    def test_get_context_adds_get_secondary_nav_items_into_context(self, mock_get_forms, mock_process_forms, mock_nav, mock_super):
-        mock_super().get_context.return_value = {}
-        assert 'get_secondary_nav_items' in self.mixin.get_context(self.factory.get('/'))
-
-    @mock.patch('__builtin__.super')
-    @mock.patch('v1.util.util.get_secondary_nav_items')
-    @mock.patch('v1.util.filterable_list.FilterableListMixin.process_forms')
-    @mock.patch('v1.util.filterable_list.FilterableListMixin.get_forms')
-    def test_get_context_adds_filter_data_into_context(self, mock_get_forms, mock_process_forms, mock_nav, mock_super):
-        mock_super().get_context.return_value = {}
-        assert 'filter_data' in self.mixin.get_context(self.factory.get('/'))
-
-    # FilterableListMixin.process_forms tests
-    @mock.patch('v1.util.filterable_list.FilterableListMixin.per_page_limit')
-    def test_process_forms_returns_dictionary_of_forms_and_page_sets(self, mock_per_page_limit):
-        data = self.mixin.process_forms(self.factory.get('/'), [])
-        assert 'forms' in data
-        assert 'page_sets' in data
-
-
     @mock.patch('v1.util.filterable_list.Paginator')
     @mock.patch('v1.util.filterable_list.FilterableListMixin.per_page_limit')
-    def test_process_forms_calls_is_valid_on_each_form(self, mock_per_page_limit, mock_paginator):
+    def test_process_form_calls_is_valid_on_each_form(self, mock_per_page_limit, mock_paginator):
         mock_request = mock.Mock()
         mock_request.GET = self.factory.get('/').GET
-        mock_forms = [mock.Mock(), mock.Mock()]
-        data = self.mixin.process_forms(mock_request, mock_forms)
-        assert mock_forms[0].is_valid.called
-        assert mock_forms[1].is_valid.called
+        mock_form = mock.Mock()
+        data = self.mixin.process_form(mock_request, mock_form)
+        assert mock_form.is_valid.called
--- cfgov/v1/util/filterable_list.py
+++ cfgov/v1/util/filterable_list.py
@@ -16,59 +16,53 @@ class FilterableListMixin(object):
         except AttributeError as e:
             raise e
         context['get_secondary_nav_items'] = get_secondary_nav_items
-        filter_data = self.process_forms(request, self.get_forms(request))
-        context['filter_data'] = filter_data
+        form_data, has_active_filters = self.get_form_data(request.GET)
+        form = FilterableListForm(form_data, base_query=self.base_query())
+        context['has_active_filters'] = has_active_filters
+        context['filter_data'] = self.process_form(request, form)
         return context
 
     def base_query(self):
         return AbstractFilterPage.objects.live().filter(
             CFGOVPage.objects.child_of_q(self))
 
-    def process_forms(self, request, forms):
-        filter_data = {'forms': [], 'page_sets': []}
-        for form in forms:
-            if form.is_valid():
-                paginator = Paginator(form.get_page_set(),
-                                      self.per_page_limit())
-                page = request.GET.get('page')
+    def process_form(self, request, form):
+        filter_data = {}
+        if form.is_valid():
+            paginator = Paginator(form.get_page_set(),
+                                  self.per_page_limit())
+            page = request.GET.get('page')
 
-                # Get the page number in the request and get the page from the
-                # paginator to serve.
-                try:
-                    pages = paginator.page(page)
-                except PageNotAnInteger:
-                    pages = paginator.page(1)
-                except EmptyPage:
-                    pages = paginator.page(paginator.num_pages)
+            # Get the page number in the request and get the page from the
+            # paginator to serve.
+            try:
+                pages = paginator.page(page)
+            except PageNotAnInteger:
+                pages = paginator.page(1)
+            except EmptyPage:
+                pages = paginator.page(paginator.num_pages)
 
-                filter_data['page_sets'].append(pages)
-            else:
-                paginator = Paginator([], self.per_page_limit())
-                filter_data['page_sets'].append(paginator.page(1))
-            filter_data['forms'].append(form)
+            filter_data['page_set'] = pages
+        else:
+            paginator = Paginator([], self.per_page_limit())
+            filter_data['page_set'] = paginator.page(1)
+        filter_data['form'] = form
         return filter_data
 
-    def get_forms(self, request):
-        for form_data in self.get_form_specific_filter_data(
-                request_dict=request.GET):
-            yield FilterableListForm(
-                form_data,
-                base_query=self.base_query()
-            )
-
-    # Transform each GET parameter key from unique ID for the form in the
-    # request and assign it to a dictionary under the form ID from where it
-    # came from.
-    def get_form_specific_filter_data(self, request_dict):
-        filters_data = []
-        data = {}
+    # Set up the form's data either with values from the GET request
+    # or with defaults based on whether it's a dropdown/list or a text field
+    def get_form_data(self, request_dict):
+        form_data = {}
+        has_active_filters = False
         for field in FilterableListForm.declared_fields:
             if field in ['categories', 'topics', 'authors']:
-                data[field] = request_dict.getlist(field, [])
+                value = request_dict.getlist(field, [])
             else:
-                data[field] = request_dict.get(field, '')
-            filters_data.append(data)
-        return filters_data
+                value = request_dict.get(field, '')
+            form_data[field] = value
+            if value:
+                has_active_filters = True
+        return form_data, has_active_filters
 
     def per_page_limit(self):
         return 10
