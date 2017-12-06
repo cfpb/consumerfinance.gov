6d1b553250268d4622961d0248127b7123ff3812
Ross Karchner <ross.karchner@cfpb.gov>
2017-12-06 12:13:38 -0500

    update tests

--- cfgov/v1/tests/test_db_router.py
+++ cfgov/v1/tests/test_db_router.py
@@ -8,41 +8,64 @@ class CFGOVRouterTestCase(TestCase):
 
     def setUp(self):
         self.router = CFGOVRouter()
-        self.models = apps.get_app_config('v1').get_models()
+        self.v1_models = apps.get_app_config('v1').get_models()
+        self.ask_models = apps.get_app_config('ask_cfpb').get_models()
 
 
-@override_settings(DATABASES={'default': {}})
-class CFGOVRouterNoReplicaTestCase(CFGOVRouterTestCase):
+@override_settings(DATABASES={'default': {}},
+                   POSTGRES_APPS=['ask_cfpb'])
+class CFGOVRouterNoPostgresTestCase(CFGOVRouterTestCase):
 
-    def test_cfgov_apps_read_from_default_db(self):
+    def test_non_pg_apps_read_from_default_db(self):
         results = [self.router.db_for_read(m) == 'default'
-                   for m in self.models]
+                   for m in self.v1_models]
         self.assertTrue(all(results))
 
-    def test_cfgov_apps_write_to_default_db(self):
+    def test_non_pg_apps_write_to_default_db(self):
         results = [self.router.db_for_write(m) == 'default'
-                   for m in self.models]
+                   for m in self.v1_models]
+        self.assertTrue(all(results))
+
+    def test_pg_apps_read_from_default_db(self):
+        results = [self.router.db_for_read(m) == 'default'
+                   for m in self.ask_models]
+        self.assertTrue(all(results))
+
+    def test_pg_apps_write_to_default_db(self):
+        results = [self.router.db_for_write(m) == 'default'
+                   for m in self.ask_models]
         self.assertTrue(all(results))
 
     def test_cfgov_apps_allow_migrate_default_db(self):
         self.assertTrue(self.router.allow_migrate('default', 'v1'))
 
 
-@override_settings(DATABASES={'default': {}, 'replica': {}})
-class CFGOVRouterReplicaTestCase(CFGOVRouterTestCase):
+@override_settings(DATABASES={'default': {}, 'postgres': {}},
+                   POSTGRES_APPS=['ask_cfpb'])
+class CFGOVRouterWithPostgresTestCase(CFGOVRouterTestCase):
 
-    def test_cfgov_apps_read_from_replica_db(self):
-        results = [self.router.db_for_read(m) == 'replica'
-                   for m in self.models]
+    def test_non_pg_apps_read_from_default_db(self):
+        results = [self.router.db_for_read(m) == 'default'
+                   for m in self.v1_models]
         self.assertTrue(all(results))
 
-    def test_cfgov_apps_write_to_default_db(self):
+    def test_non_pg_apps_write_to_default_db(self):
         results = [self.router.db_for_write(m) == 'default'
-                   for m in self.models]
+                   for m in self.v1_models]
         self.assertTrue(all(results))
 
-    def test_cfgov_apps_allow_migrate_default_db(self):
+    def test_pg_apps_read_from_pg_db(self):
+        results = [self.router.db_for_read(m) == 'postgres'
+                   for m in self.ask_models]
+        self.assertTrue(all(results))
+
+    def test_pg_apps_write_to_default_db(self):
+        results = [self.router.db_for_write(m) == 'postgres'
+                   for m in self.ask_models]
+        self.assertTrue(all(results))
+
+    def test_non_pg_apps_allow_migrate_default_db(self):
         self.assertTrue(self.router.allow_migrate('default', 'v1'))
 
-    def test_cfgov_apps_disallow_migrate_replica_db(self):
-        self.assertFalse(self.router.allow_migrate('replica', 'v1'))
+    def test_pg_apps_disallow_migrate_replica_db(self):
+        self.assertTrue(self.router.allow_migrate('postgres', 'v1'))
