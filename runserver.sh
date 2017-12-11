32012ef4bb58f1663d73b1e64e30bb1bcdadc1b2
Anselm Bradford <Anselm.Bradford@cfpb.gov>
2017-12-11 11:00:56 -0500

    Move docker check

--- runserver.sh
+++ runserver.sh
@@ -10,44 +10,35 @@
 set -e
 
 standalone() {
-    mysql(){
-    if ! mysql.server status; then
-        mysql.server start
-    fi
-    }
+  mysql(){
+  if ! mysql.server status; then
+    mysql.server start
+  fi
+  }
 
-    # Run tasks to build the project for distribution.
-    server(){
-    if [ "$1" == "ssl" ]; then
-        echo '\033[0;32mStarting SSL Django server on port' $DJANGO_HTTP_PORT '...'
-        python cfgov/manage.py runsslserver $DJANGO_HTT_PORT
-    else
-        echo '\033[0;32mStarting the Django server on port' $DJANGO_HTTP_PORT '...'
-        python cfgov/manage.py runserver $DJANGO_HTTP_PORT
-    fi
-    }
+  # Run tasks to build the project for distribution.
+  server(){
+  if [ "$1" == "ssl" ]; then
+    echo '\033[0;32mStarting SSL Django server on port' $DJANGO_HTTP_PORT '...'
+    python cfgov/manage.py runsslserver $DJANGO_HTT_PORT
+  else
+    echo '\033[0;32mStarting the Django server on port' $DJANGO_HTTP_PORT '...'
+    python cfgov/manage.py runserver $DJANGO_HTTP_PORT
+  fi
+  }
 
-    mysql
-    server "$1"
+  mysql
+  server "$1"
 }
 
 dockerized() {
-    source mac-virtualbox-init.sh
-    docker-compose up
-}
-
-check_if_docker_installed() {
-    if [ -x "$(command -v docker)" ]; then
-        echo "Docker is installed."
-    else
-        echo "Docker not installed. See installation.md for installing docker."
-    fi
+  source mac-virtualbox-init.sh
+  docker-compose up
 }
 
 # Execute requested (or all) functions.
 if [ "$1" == "docker" ]; then
-    check_if_docker_installed
-    dockerized
+  dockerized
 else
-    standalone
+  standalone
 fi
