0cf8ebf69b14b676ce113e5d703151bbbfa21fc6
Anselm Bradford <Anselm.Bradford@cfpb.gov>
2017-12-14 16:48:59 -0500

    Remove ESLint ignore file

--- cfgov/unprocessed/js/routes/on-demand/filterable-list-controls.js
+++ cfgov/unprocessed/js/routes/on-demand/filterable-list-controls.js
@@ -2,9 +2,8 @@
    Scripts for Filterable List Controls organism
    ========================================================================== */
 
-'use strict';
 
-var atomicHelpers = require( '../../modules/util/atomic-helpers' );
-var FilterableListControls = require( '../../organisms/FilterableListControls' );
+const atomicHelpers = require( '../../modules/util/atomic-helpers' );
+const FilterableListControls = require( '../../organisms/FilterableListControls' );
 
 atomicHelpers.instantiateAll( '.o-filterable-list-controls', FilterableListControls );
--- cfgov/unprocessed/js/routes/on-demand/secondary-navigation.js
+++ cfgov/unprocessed/js/routes/on-demand/secondary-navigation.js
@@ -2,13 +2,13 @@
    Scripts for Secondary Navigation organism
    ========================================================================== */
 
-'use strict';
 
-var SecondaryNavigation = require( '../../organisms/SecondaryNavigation' );
+const SecondaryNavigation = require( '../../organisms/SecondaryNavigation' );
 
-var dom = document.querySelector( '.o-secondary-navigation' );
-// Check that this script has been delivered to a page that actually
-// has secondary navigation markup.
+const dom = document.querySelector( '.o-secondary-navigation' );
+
+/* Check that this script has been delivered to a page that actually
+   has secondary navigation markup. */
 if ( dom ) {
-  var secondaryNavigation = new SecondaryNavigation( dom ).init();
+  const secondaryNavigation = new SecondaryNavigation( dom ).init();
 }
