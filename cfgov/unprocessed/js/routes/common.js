abb505f0d25424c852d7a8c6438708e26e661393
Anselm Bradford <Anselm.Bradford@cfpb.gov>
2017-12-15 10:38:43 -0500

    Fix for variable reference on spanish.js

--- cfgov/unprocessed/js/routes/common.js
+++ cfgov/unprocessed/js/routes/common.js
@@ -2,7 +2,6 @@
    Common application-wide scripts that are used across the whole site.
    ========================================================================== */
 
-
 // Global modules.
 require( '../modules/focus-target' ).init();
 
