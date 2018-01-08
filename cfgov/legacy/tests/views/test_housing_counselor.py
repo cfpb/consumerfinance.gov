ef1eaa37c025aa36fb47032822cfc5f4acd5de21
Will Barton <william.barton@cfpb.gov>
2018-01-08 10:54:21 -0500

    Serve HUD API PDFs from S3 and remove unused PDFreactor-dependent view

--- cfgov/legacy/tests/views/test_housing_counselor.py
+++ cfgov/legacy/tests/views/test_housing_counselor.py
@@ -1,18 +1,70 @@
 from django.test import TestCase, override_settings
 
-from legacy.views.housing_counselor import HousingCounselorView
+import mock
+import requests
+
+from legacy.views.housing_counselor import HousingCouncelorS3URLMixin
 
 
 @override_settings(AWS_STORAGE_BUCKET_NAME='foo.bucket')
-class TestHousingCounselorView(TestCase):
+class HousingCouncelorS3URLMixinTestCase(TestCase):
     def test_s3_json_url(self):
         self.assertEqual(
-            HousingCounselorView.s3_json_url(20001),
+            HousingCouncelorS3URLMixin.s3_json_url(20001),
             'https://s3.amazonaws.com/foo.bucket/a/assets/hud/jsons/20001.json'
         )
 
     def test_s3_pdf_url(self):
         self.assertEqual(
-            HousingCounselorView.s3_pdf_url(20009),
+            HousingCouncelorS3URLMixin.s3_pdf_url(20009),
             'https://s3.amazonaws.com/foo.bucket/a/assets/hud/pdfs/20009.pdf'
         )
+
+
+@override_settings(AWS_STORAGE_BUCKET_NAME='foo.bucket')
+class HousingCounselorViewTestCase(TestCase):
+
+    @mock.patch('requests.get')
+    def test_get_counselors_failed_s3_request(self, mock_requests_get):
+        mock_requests_get.side_effect = requests.HTTPError
+        response = self.client.get('/find-a-housing-counselor/')
+        self.assertNotIn('zipcode_valid', response.context)
+        self.assertNotIn('api_json', response.context)
+        self.assertNotIn('pdf_url', response.context)
+
+    @mock.patch('requests.get')
+    def test_get_counselors_invalid_zipcode(self, mock_requests_get):
+        self.client.get('/find-a-housing-counselor/', {'zipcode': 'abcdef'})
+        mock_requests_get.assert_not_called()
+
+    @mock.patch('requests.get')
+    def test_get_counselors_valid_zipcode(self, mock_requests_get):
+        mock_requests_get.return_value.json.return_value = {}
+        response = self.client.get('/find-a-housing-counselor/',
+                                   {'zipcode': '12345'})
+        self.assertTrue(response.context['zipcode_valid'])
+        self.assertIn('12345.pdf', response.context['pdf_url'])
+
+    @mock.patch('requests.get')
+    def test_get_counselors_pdf_template(self, mock_requests_get):
+        mock_requests_get.return_value.json.return_value = {}
+        response = self.client.get('/find-a-housing-counselor/', {'pdf': ''})
+        self.assertIn('pdf', response.templates[0].name)
+
+
+@override_settings(AWS_STORAGE_BUCKET_NAME='foo.bucket')
+class HousingCounselorPDFViewTestCase(TestCase):
+
+    def test_get_invalid_form(self):
+        response = self.client.get('/save-hud-counselors-list/', {})
+        self.assertEqual(response.status_code, 400)
+
+    @mock.patch('requests.get')
+    def test_get_valid_form(self, mock_requests_get):
+        response = self.client.get('/save-hud-counselors-list/',
+                                   {'zip': '12345'})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.get('content-type'),
+                         'application/pdf')
+        self.assertEqual(response.get('Content-Disposition'),
+                         'attachment; filename=12345.pdf')
