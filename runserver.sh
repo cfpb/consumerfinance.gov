ba48bdf229b7d95931a1d64242ec02068d36addb
Will Barton <william.barton@cfpb.gov>
2018-01-12 09:49:05 -0500

    Undockerize setup.sh/runserver.sh — it doesn't work

--- runserver.sh
+++ runserver.sh
@@ -9,36 +9,22 @@
 # Set script to exit on any errors.
 set -e
 
-standalone() {
-  mysql(){
-  if ! mysql.server status; then
-    mysql.server start
-  fi
-  }
-
-  # Run tasks to build the project for distribution.
-  server(){
-  if [ "$1" == "ssl" ]; then
-    echo '\033[0;32mStarting SSL Django server on port' $DJANGO_HTTP_PORT '...'
-    python cfgov/manage.py runsslserver $DJANGO_HTT_PORT
-  else
-    echo '\033[0;32mStarting the Django server on port' $DJANGO_HTTP_PORT '...'
-    python cfgov/manage.py runserver $DJANGO_HTTP_PORT
-  fi
-  }
-
-  mysql
-  server "$1"
+mysql(){
+    if ! mysql.server status; then
+        mysql.server start
+    fi
 }
 
-dockerized() {
-  source mac-virtualbox-init.sh
-  docker-compose up
+# Run tasks to build the project for distribution.
+server(){
+    if [ "$1" == "ssl" ]; then
+        echo '\033[0;32mStarting SSL Django server on port' $DJANGO_HTTP_PORT '...'
+        python cfgov/manage.py runsslserver $DJANGO_HTT_PORT
+    else
+        echo '\033[0;32mStarting the Django server on port' $DJANGO_HTTP_PORT '...'
+        python cfgov/manage.py runserver $DJANGO_HTTP_PORT
+    fi
 }
 
-# Execute requested (or all) functions.
-if [ "$1" == "docker" ]; then
-  dockerized
-else
-  standalone
-fi
+mysql
+server "$1"
