8dcef609b4c27c148a175d0eaa73709cffd24cc8
Ross Karchner <ross.karchner@cfpb.gov>
2018-02-12 15:42:22 -0500

    Add test for group page

--- cfgov/permissions_viewer/tests/test_views.py
+++ cfgov/permissions_viewer/tests/test_views.py
@@ -1,17 +1,21 @@
 from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 from django.core.urlresolvers import reverse
 
 User = get_user_model()
 
 
-class TestUserPermissionsView(TestCase):
+class TestPermissionsViews(TestCase):
     def setUp(self):
         self.testuser = User.objects.create_superuser(
             username='foo',
             password='bar',
             email='foo@cfpb.gov')
         self.testuser.save()
+
+        self.testgroup = Group.objects.create(name='Developers')
+        self.testgroup.user_set.add(self.testuser)
 
     def test_user_has_page(self):
         client = Client()
@@ -20,4 +24,14 @@ class TestUserPermissionsView(TestCase):
 
         response = client.get(url)
 
-        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Developers')
+
+    def test_user_on_group_page(self):
+        client = Client()
+        client.login(username='foo', password='bar')
+
+        url = reverse('permissions:group', args=[self.testgroup.pk])
+
+        response = client.get(url)
+
+        self.assertContains(response, 'foo')
