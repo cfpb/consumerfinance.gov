58256be57d79aa2b52ea4263e98565ae96ba1ce0
Ross Karchner <ross.karchner@cfpb.gov>
2018-02-12 14:58:54 -0500

    linter: happier, more productive

--- cfgov/permissions_viewer/tests/test_views.py
+++ cfgov/permissions_viewer/tests/test_views.py
@@ -1,13 +1,16 @@
-from django.test import RequestFactory, TestCase, Client
+from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from django.core.urlresolvers import reverse
 
 User = get_user_model()
 
+
 class TestUserPermissionsView(TestCase):
     def setUp(self):
-        self.testuser = User.objects.create_superuser(username='foo',
-                             password='bar', email='foo@cfpb.gov')
+        self.testuser = User.objects.create_superuser(
+            username='foo',
+            password='bar',
+            email='foo@cfpb.gov')
         self.testuser.save()
 
     def test_user_has_page(self):
@@ -15,6 +18,6 @@ class TestUserPermissionsView(TestCase):
         client.login(username='foo', password='bar')
         url = reverse('permissions:user', args=[self.testuser.pk])
 
-        response =client.get(url)
+        response = client.get(url)
 
         self.assertEqual(response.status_code, 200)
