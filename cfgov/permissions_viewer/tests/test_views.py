6f9835447a5efc70691a58db8e88e375e2b65b13
Ross Karchner <ross.karchner@cfpb.gov>
2018-02-12 14:50:27 -0500

    remove PDB

--- cfgov/permissions_viewer/tests/test_views.py
+++ cfgov/permissions_viewer/tests/test_views.py
@@ -4,20 +4,17 @@ from django.core.urlresolvers import reverse
 
 User = get_user_model()
 
-
 class TestUserPermissionsView(TestCase):
     def setUp(self):
         self.testuser = User.objects.create_superuser(username='foo',
-                                                      password='bar', email='foo@cfpb.gov')
+                             password='bar', email='foo@cfpb.gov')
         self.testuser.save()
 
     def test_user_has_page(self):
         client = Client()
-        login_response = client.login(username='foo', password='bar')
+        client.login(username='foo', password='bar')
         url = reverse('permissions:user', args=[self.testuser.pk])
 
-        response = client.get(url)
+        response =client.get(url)
 
-        import pdb
-        pdb.set_trace()
         self.assertEqual(response.status_code, 200)
