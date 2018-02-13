91043aa558504728cd662a5faca32bea76e71074
Virginia Czosek <Virginia.Czosek@cfpb.gov>
2018-02-13 13:23:22 -0800

    Review fixes

--- cfgov/unprocessed/js/routes/on-demand/read-more.js
+++ cfgov/unprocessed/js/routes/on-demand/read-more.js
@@ -1,11 +1,16 @@
 const Expandable = require( '../../organisms/Expandable' );
 const getBreakpointState = require( '../../modules/util/breakpoint-state' ).get;
 
-const readMoreContainer = document.querySelector( '.o-expandable__read-more' );
-if ( readMoreContainer && getBreakpointState().isBpXS ) {
-  const readMoreExpandable = new Expandable( readMoreContainer ).init();
-  readMoreExpandable.addEventListener( 'expandEnd', function() {
-    readMoreExpandable.destroy();
-    readMoreContainer.querySelector( '.o-expandable_content' ).style.height = '';
-  } );
+const readMoreContainers = document.querySelectorAll( '.o-expandable__read-more' );
+
+if ( readMoreContainers && getBreakpointState().isBpXS ) {
+	for ( let i = 0; i < readMoreContainers.length; i++ ) {
+		const container = readMoreContainers[i];
+    const readMoreExpandable = new Expandable( container ).init();
+	  readMoreExpandable.addEventListener( 'expandEnd', function() {
+	    readMoreExpandable.destroy();
+	    container.querySelector( '.o-expandable_content' ).style.height = '';
+	  } );
+  }
 }
+
