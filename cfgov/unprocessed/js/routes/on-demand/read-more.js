a1c96f189be33493165302f32ebcd55b081e7d88
Virginia Czosek <Virginia.Czosek@cfpb.gov>
2018-02-22 09:42:21 -0800

    Fix spacing in read-more.js

--- cfgov/unprocessed/js/routes/on-demand/read-more.js
+++ cfgov/unprocessed/js/routes/on-demand/read-more.js
@@ -4,13 +4,13 @@ const getBreakpointState = require( '../../modules/util/breakpoint-state' ).get;
 const readMoreContainers = document.querySelectorAll( '.o-expandable__read-more' );
 
 if ( readMoreContainers && getBreakpointState().isBpXS ) {
-	for ( let i = 0; i < readMoreContainers.length; i++ ) {
-		const container = readMoreContainers[i];
+  for ( let i = 0; i < readMoreContainers.length; i++ ) {
+    const container = readMoreContainers[i];
     const readMoreExpandable = new Expandable( container ).init();
-	  readMoreExpandable.addEventListener( 'expandEnd', function() {
-	    readMoreExpandable.destroy();
-	    container.querySelector( '.o-expandable_content' ).style.height = '';
-	  } );
+    readMoreExpandable.addEventListener( 'expandEnd', function() {
+      readMoreExpandable.destroy();
+      container.querySelector( '.o-expandable_content' ).style.height = '';
+    } );
   }
 }
 
