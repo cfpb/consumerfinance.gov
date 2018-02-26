8049c1b5fa5ef39192f305f4a99cf580bf7c5c1d
Steven Bellamy <steven.bellamy@cfpb.gov>
2018-02-26 14:48:56 -0500

    Moving loan estimate into cfgov-refresh

---
+++ cfgov/unprocessed/js/routes/owning-a-home/loan-estimate/index.js
@@ -0,0 +1,7 @@
+import FormExplainer from '../../../../apps/owning-a-home/js/form-explainer';
+
+const formExplainerElement = document.querySelector( '.form-explainer' );
+
+if ( formExplainerElement ) {
+  const formExplainer = new FormExplainer( formExplainerElement );
+}
