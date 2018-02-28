8934cb1875e3735a80b148e0b87403eec388ce03
Steven Bellamy <steven.bellamy@cfpb.gov>
2018-02-28 11:11:23 -0500

    Flagging OAH routes

--- cfgov/cfgov/settings/base.py
+++ cfgov/cfgov/settings/base.py
@@ -609,11 +609,9 @@ FLAGS = {
     # To be enabled when owning-a-home/explore-rates is de-sheered.
     'OAH_EXPLORE_RATES': {},
 
-    # To be enabled when owning-a-home/mortgage-closing is de-sheered.
-    'OAH_MORTGAGE_CLOSING': {},
-
-    # To be enabled when owning-a-home/mortgage-estimate is de-sheered.
-    'OAH_MORTGAGE_ESTIMATE': {},
+    # To be enabled when owning-a-home/closing-disclosure/
+    # and owning-a-home/loan-estimate/ are de-sheered.
+    'OAH_FORM_EXPLAINERS': {},
 
     # Google Optimize code snippets for A/B testing
     # When enabled this flag will add various Google Optimize code snippets.
