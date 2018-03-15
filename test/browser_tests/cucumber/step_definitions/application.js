9b2fa5317656f51c62ea332619c32e79354c9f59
Anselm Bradford <Anselm.Bradford@cfpb.gov>
2018-03-15 17:41:49 -0400

    Updates test reps + removes extraneous

--- test/browser_tests/cucumber/step_definitions/application.js
+++ test/browser_tests/cucumber/step_definitions/application.js
@@ -1,33 +1,29 @@
 const BasePage = require( '../../page_objects/base-page.js' );
 const basePage = new BasePage();
 
-const { defineSupportCode } = require( 'cucumber' );
+const { Then, When } = require( 'cucumber' );
 const { expect } = require( 'chai' );
 
-defineSupportCode( function( { Then, When } ) {
-
-  When( /I goto URL "(.*)"/, { timeout: 60 * 1000 }, function( url ) {
-    return basePage.gotoURL( url ).then( function() {
-      return expect( browser.getCurrentUrl() ).to.eventually.contain( url );
-    } );
+When( /I goto URL "(.*)"/, { timeout: 60 * 1000 }, function( url ) {
+  return basePage.gotoURL( url ).then( function() {
+    return expect( browser.getCurrentUrl() ).to.eventually.contain( url );
   } );
+} );
 
-  When( /I navigate back*/, function() {
+When( /I navigate back*/, function() {
 
-    return browser.navigate().back();
+  return browser.navigate().back();
+} );
+
+When( /I click away*/, function() {
+
+  return element( by.css( 'body' ) ).click( );
+} );
+
+Then( /I should see page title "(.*)"/, function( pageTitle ) {
+
+  return browser.getTitle().then( function( title ) {
+
+    return expect( title ).to.equal( pageTitle );
   } );
-
-  When( /I click away*/, function() {
-
-    return element( by.css( 'body' ) ).click( );
-  } );
-
-  Then( /I should see page title "(.*)"/, function( pageTitle ) {
-
-    return browser.getTitle().then( function( title ) {
-
-      return expect( title ).to.equal( pageTitle );
-    } );
-  } );
-
 } );
