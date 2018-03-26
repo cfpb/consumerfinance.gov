c42d10fedb74e74615c37ea21e114655df36bac1
higs4281 <higs4281@gmail.com>
2018-03-26 10:26:00 -0400

    add page models and initial migrations

--- cfgov/regulations3k/models/django.py
+++ cfgov/regulations3k/models/django.py
@@ -1,40 +1,30 @@
 # -*- coding: utf-8 -*-
-# from __future__ import absolute_import, unicode_literals
-from six.moves import html_parser as HTMLParser
+from __future__ import absolute_import, unicode_literals
 
 from django.db import models
-# from django import forms
-# from django.apps import apps
-# from django.contrib.auth.models import User
-# from django.core.exceptions import ObjectDoesNotExist
-# from django.utils.text import slugify
-# from django.utils import html
-# from django.utils.functional import cached_property
 
-# from wagtail.wagtailadmin.edit_handlers import (
-#     FieldPanel, FieldRowPanel, MultiFieldPanel
-# )
-# from wagtail.wagtailcore.blocks.stream_block import StreamValue
-# from wagtail.wagtailcore.fields import RichTextField
-# from wagtail.wagtailcore.models import Page
-# from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
-
-# from v1.util.migrations import get_or_create_page
-
-html_parser = HTMLParser.HTMLParser()
+from regulations3k.models.fields import RegDownTextField
 
 
 class Section(models.Model):
     label = models.CharField(max_length=255, blank=True)
     title = models.CharField(max_length=255, blank=True)
-    contents = models.TextField(blank=True)
+    contents = RegDownTextField(blank=True)
+
+    def __str__(self):
+        return "{} {}".format(self.label, self.title)
 
 
 class Subpart(models.Model):
     label = models.CharField(max_length=255, blank=True)
     title = models.CharField(max_length=255, blank=True)
-    version = models.ForeignKey('EffectiveVersion')
+    version = models.ForeignKey(
+        'EffectiveVersion', blank=True, null=True,
+        related_name='subpart_version')
     sections = models.ForeignKey(Section, blank=True, null=True)
+
+    def __str__(self):
+        return "{} {} ({})".format(self.label, self.title, self.version)
 
 
 class EffectiveVersion(models.Model):
@@ -44,6 +34,10 @@ class EffectiveVersion(models.Model):
     part = models.ForeignKey('Part', blank=True, null=True)
     subparts = models.ForeignKey(Subpart, blank=True, null=True)
 
+    def __str__(self):
+        return "{} {} ({})".format(
+            self.part, self.subparts, self.effecitve_date)
+
 
 class Part(models.Model):
     cfr_title = models.CharField(max_length=255)
@@ -51,7 +45,12 @@ class Part(models.Model):
     part_number = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
     letter_code = models.CharField(max_length=10)
-    versions = models.ForeignKey(EffectiveVersion, blank=True, null=True)
+    versions = models.ForeignKey(
+        EffectiveVersion, blank=True, null=True,
+        related_name='part_version')
+
+    def __str__(self):
+        return self.crf_title
 
     def get_parts_with_effective_version(self):
         pass
