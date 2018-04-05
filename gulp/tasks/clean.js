86f0e6570c1d93409df2dbf672bbe5ff87f2ab91
Anselm Bradford <Anselm.Bradford@cfpb.gov>
2018-04-05 13:17:25 -0400

    Add return statements to clean task for signaling async completion

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -4,15 +4,15 @@ const gulp = require( 'gulp' );
 
 gulp.task( 'clean:css', () => {
   // Clean CSS out of /cfgov/static_built/css/
-  del( configClean.css + '/**/*' );
+  return del( configClean.css + '/**/*' );
 } );
 
 gulp.task( 'clean:js', () => {
   // Clean JavaScript out of /cfgov/static_built/js/
-  del( configClean.js + '/**/*' );
+  return del( configClean.js + '/**/*' );
 } );
 
 gulp.task( 'clean', () => {
   // Clean everything out of /cfgov/static_built/
-  del( configClean.dest + '/**/*' );
+  return del( configClean.dest + '/**/*' );
 } );
