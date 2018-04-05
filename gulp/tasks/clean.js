54c55a9db7651f62084634d1407cf69e984999ad
Anselm Bradford <Anselm.Bradford@cfpb.gov>
2018-04-05 13:22:32 -0400

    Fix gulp clean syntax

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -2,17 +2,11 @@ const configClean = require( '../config' ).clean;
 const del = require( 'del' );
 const gulp = require( 'gulp' );
 
-gulp.task( 'clean:css', () => {
-  // Clean CSS out of /cfgov/static_built/css/
-  return del( configClean.css + '/**/*' );
-} );
+// Clean CSS out of /cfgov/static_built/css/
+gulp.task( 'clean:css', () => del( configClean.css + '/**/*' ) );
 
-gulp.task( 'clean:js', () => {
-  // Clean JavaScript out of /cfgov/static_built/js/
-  return del( configClean.js + '/**/*' );
-} );
+// Clean JavaScript out of /cfgov/static_built/js/
+gulp.task( 'clean:js', () => del( configClean.js + '/**/*' ) );
 
-gulp.task( 'clean', () => {
-  // Clean everything out of /cfgov/static_built/
-  return del( configClean.dest + '/**/*' );
-} );
+// Clean everything out of /cfgov/static_built/
+gulp.task( 'clean', () => del( configClean.dest + '/**/*' ) );
