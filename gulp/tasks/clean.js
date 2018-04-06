22c4ba76616dfa416ca3547a3835c26321ce9493
Chris Contolini <chris@contolini.com>
2018-04-06 10:42:45 -0400

    Have gulp clean tasks return promises

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -3,18 +3,16 @@ const gulp = require( 'gulp' );
 const paths = require( '../../config/environment' ).paths;
 
 // Clean CSS out of /cfgov/static_built/css/
-gulp.task( 'clean:css', cb => {
-  del( paths.processed + '/css/**/*' );
-  del( paths.processed + '/apps/**/css/*' );
-  cb();
-} );
+gulp.task( 'clean:css', cb => del( [
+  paths.processed + '/css/**/*',
+  paths.processed + '/apps/**/css/*'
+] ) );
 
 // Clean JavaScript out of /cfgov/static_built/js/
-gulp.task( 'clean:js', cb => {
-  del( paths.processed + '/js/**/*' );
-  del( paths.processed + '/apps/**/js/*' );
-  cb();
-} );
+gulp.task( 'clean:js', cb => del( [
+  paths.processed + '/js/**/*',
+  paths.processed + '/apps/**/js/*'
+] ) );
 
 // Clean everything out of /cfgov/static_built/
 gulp.task( 'clean', () => del( paths.processed + '/**/*' ) );
