215f0a2466b5bad8250ce7a06862df0b1f866f5e
Anselm Bradford <Anselm.Bradford@cfpb.gov>
2018-04-06 17:01:49 -0400

    Fix watch task for gulp 4

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,20 +1,36 @@
 /* Notes:
    - gulp/tasks/browserSync.js watches and reloads compiled files. */
-
+const fancyLog = require( 'fancy-log' );
 const gulp = require( 'gulp' );
-const config = require( '../config' );
 const paths = require( '../../config/environment' ).paths;
 
+/**
+ * Add reporting output to watched files.
+ * @param {FSWatcher} watcher - Output from gulp.watch.
+ */
+function _addChangeListener( watcher ) {
+  watcher.on( 'change', function( path, stats ) {
+    fancyLog.info( 'File ' + path + ' was changed' );
+  } );
+
+  watcher.on('unlink', function( path ) {
+    fancyLog.info( 'File ' + path + ' was removed' );
+  } );
+}
+
 gulp.task( 'watch',
-  gulp.series( 'browsersync', () => {
-    gulp.watch( [
-      config.scripts.src,
+  gulp.parallel( 'browsersync', () => {
+    const jsWatcher = gulp.watch( [
+      paths.unprocessed + '/js/**/*.js',
       paths.unprocessed + '/apps/**/js/**/*.js'
-    ], [ 'scripts' ] );
-    gulp.watch( [
-      config.styles.cwd + '/**/*.less',
-      config.legacy.cwd + '/*/less/*.less',
-      paths.unprocessed + '/apps/**/css/**/*.less'
-    ], [ 'styles' ] );
+    ], gulp.parallel( 'scripts' ) );
+    _addChangeListener( jsWatcher );
+
+    const cssWatcher = gulp.watch( [
+      paths.unprocessed + '/css/**/*.less',
+      paths.unprocessed + '/apps/**/css/**/*.less',
+      paths.legacy + '/*/less/*.less'
+    ], gulp.parallel( 'scripts' ) );
+    _addChangeListener( cssWatcher );
   } )
 );
