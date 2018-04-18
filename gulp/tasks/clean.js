6851ca85afca81aeeb063a0f65815544b2580725
Anselm Bradford <Anselm.Bradford@cfpb.gov>
2018-04-18 18:38:24 -0400

    Minor linter cleanup

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -3,13 +3,13 @@ const gulp = require( 'gulp' );
 const paths = require( '../../config/environment' ).paths;
 
 // Clean CSS out of /cfgov/static_built/css/
-gulp.task( 'clean:css', cb => del( [
+gulp.task( 'clean:css', () => del( [
   paths.processed + '/css/**/*',
   paths.processed + '/apps/**/css/*'
 ] ) );
 
 // Clean JavaScript out of /cfgov/static_built/js/
-gulp.task( 'clean:js', cb => del( [
+gulp.task( 'clean:js', () => del( [
   paths.processed + '/js/**/*',
   paths.processed + '/apps/**/js/*'
 ] ) );
