cedda1da928149b697a7bfd4af3302827358c093
higs4281 <higs4281@gmail.com>
2018-05-10 12:14:30 -0400

    let test handle multiple timeouts

--- cfgov/scripts/tests/test_smoke_tests.py
+++ cfgov/scripts/tests/test_smoke_tests.py
@@ -86,8 +86,9 @@ class HttpTests(unittest.TestCase):
         mock_ok_response = mock.Mock()
         mock_ok_response.status_code = 200
         short_run_remainder = len(http_smoke_test.SHORT_RUN) - allowed
+        allowed_list = [requests.exceptions.Timeout] * allowed
         ok_list = [mock_ok_response] * short_run_remainder
-        side_effect_list = [requests.exceptions.Timeout] + ok_list
+        side_effect_list = allowed_list + ok_list
         mock_get.side_effect = side_effect_list
         self.assertTrue(http_smoke_test.check_urls('pro1'))
 
