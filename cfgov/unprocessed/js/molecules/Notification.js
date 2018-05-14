d79f493867ab466c74388709d9e42a968f04435e
James Wilson <jimmynotjim@me.com>
2018-05-14 16:25:57 -0500

    Remove debugging code

--- cfgov/unprocessed/js/molecules/Notification.js
+++ cfgov/unprocessed/js/molecules/Notification.js
@@ -110,7 +110,6 @@ function Notification( element ) {
   function _setType( type ) {
     // If type hasn't changed, return.
     if ( _currentType === type ) {
-      console.log(this);
       return this;
     }
 
