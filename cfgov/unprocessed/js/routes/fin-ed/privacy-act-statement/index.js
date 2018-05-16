756d89312b356811e1d92837139d6fb0fbea3b7f
Anselm Bradford <Anselm.Bradford@cfpb.gov>
2018-05-16 17:07:07 -0400

    Remove console.log

--- cfgov/unprocessed/js/routes/fin-ed/privacy-act-statement/index.js
+++ cfgov/unprocessed/js/routes/fin-ed/privacy-act-statement/index.js
@@ -4,9 +4,6 @@
 
 const atomicHelpers = require( '../../../modules/util/atomic-helpers' );
 const Expandable = require( '../../../organisms/Expandable' );
-console.log( document.querySelector(
-  '.o-secondary-navigation .o-expandable'
-) );
 const expandable = new Expandable( document.querySelector(
   '.o-secondary-navigation .o-expandable'
 ) );
