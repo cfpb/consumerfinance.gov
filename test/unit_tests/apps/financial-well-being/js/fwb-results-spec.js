214097b6b608daab1cff14adfc8c41ebcc89cc82
Anselm Bradford <Anselm.Bradford@cfpb.gov>
2018-05-16 17:06:11 -0400

    Update additional expandable instances

--- test/unit_tests/apps/financial-well-being/js/fwb-results-spec.js
+++ test/unit_tests/apps/financial-well-being/js/fwb-results-spec.js
@@ -28,10 +28,10 @@ const HTML_SNIPPET = `
           </span>
           <span class="o-expandable_header-right o-expandable_link">
             <span class="o-expandable_cue o-expandable_cue-open">
-              <span class="cf-icon cf-icon-plus-round"></span>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1200" class="cf-icon-svg"><path d="M500 105.2c-276.1 0-500 223.9-500 500s223.9 500 500 500 500-223.9 500-500-223.9-500-500-500zm263.1 550.7H549.6v213.6c0 27.6-22.4 50-50 50s-50-22.4-50-50V655.9H236c-27.6 0-50-22.4-50-50s22.4-50 50-50h213.6V342.3c0-27.6 22.4-50 50-50s50 22.4 50 50v213.6h213.6c27.6 0 50 22.4 50 50s-22.5 50-50.1 50z"/></svg>
             </span>
             <span class="o-expandable_cue o-expandable_cue-close">
-              <span class="cf-icon cf-icon-minus-round"></span>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1200" class="cf-icon-svg"><path d="M500 105.2c-276.1 0-500 223.9-500 500s223.9 500 500 500 500-223.9 500-500-223.9-500-500-500zm263.1 550.7H236c-27.6 0-50-22.4-50-50s22.4-50 50-50h527.1c27.6 0 50 22.4 50 50s-22.4 50-50 50z"/></svg>
             </span>
           </span>
         </div>
