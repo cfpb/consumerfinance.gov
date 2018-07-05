ee28288b71aed0cc3f29636856f450d755baca34
James Wilson <jimmynotjim@me.com>
2018-07-05 12:34:28 -0500

    Reduce Travis queue by moving docs deploy to it's own stage
    The master branch doesn't need to run the test suites on ever PR merge,
    but the docs deploy currently relies on the master branch. Additionally,
    the docs should only deploy when a tag is released, otherwise the docs
    don't represent what's on production. This PR moves the docs to it's own
    stage that relies on a tagged release condition.

--- travis_run.sh
+++ travis_run.sh
@@ -15,7 +15,6 @@ elif [ "$RUNTEST" == "backend" ]; then
     TEST_DATABASE_URL=postgres://travis:travis@localhost:5433/travis tox -e fast
     tox -e missing-migrations
     bash <(curl -s https://codecov.io/bash) -F backend
-
-    # pip install -r requirements/manual.txt
-    # mkdocs build
+elif [ "$RUNTEST" == "docs" ]; then
+    mkdocs build
 fi
