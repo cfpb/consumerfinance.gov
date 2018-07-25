74689a01b8b45b635ed37eb4336c02ffecf33df1
Will Barton <william.barton@cfpb.gov>
2018-07-25 11:11:59 -0400

    Add support for copying Effective Versions

--- cfgov/regulations3k/wagtail_hooks.py
+++ cfgov/regulations3k/wagtail_hooks.py
@@ -1,9 +1,12 @@
 from __future__ import unicode_literals
 
+from datetime import date
+
 from wagtail.contrib.modeladmin.options import modeladmin_register
 
 from treemodeladmin.options import TreeModelAdmin
 
+from regulations3k.copyable_modeladmin import CopyableModelAdmin
 from regulations3k.models import EffectiveVersion, Part, Section, Subpart
 
 
@@ -33,7 +36,7 @@ class SubpartModelAdmin(TreeModelAdmin):
     ordering = ['subpart_type', 'title']
 
 
-class EffectiveVersionModelAdmin(TreeModelAdmin):
+class EffectiveVersionModelAdmin(CopyableModelAdmin):
     model = EffectiveVersion
     menu_label = 'Regulation effective versions'
     menu_icon = 'list-ul'
@@ -44,6 +47,34 @@ class EffectiveVersionModelAdmin(TreeModelAdmin):
     child_field = 'subparts'
     child_model_admin = SubpartModelAdmin
     parent_field = 'part'
+
+    def copy(self, instance):
+        # Create a new instance
+        new_version = EffectiveVersion(
+            authority=instance.authority,
+            source=instance.source,
+            part=instance.part,
+            effective_date=date.today(),
+            acquired=date.today(),
+            draft=True,
+        )
+        new_version.save()
+
+        # Copy subparts and sections
+        for subpart in instance.subparts.all():
+            sections = subpart.sections.all()
+            new_subpart = subpart
+            new_subpart.pk = None
+            new_subpart.version = new_version
+            new_subpart.save()
+
+            for section in sections:
+                new_section = section
+                new_section.pk = None
+                new_section.subpart = new_subpart
+                new_section.save()
+
+        return new_version
 
 
 @modeladmin_register
