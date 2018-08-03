b21f500e7054b5faa67afd06748d622da6ae8b45
Will Barton <william.barton@cfpb.gov>
2018-08-03 10:10:34 -0400

    WIP

--- cfgov/regulations3k/regdown.py
+++ cfgov/regulations3k/regdown.py
@@ -54,7 +54,7 @@ from __future__ import unicode_literals
 
 import re
 
-from markdown import markdown, util
+from markdown import Markdown, markdown, util
 from markdown.blockprocessors import BlockProcessor, ParagraphProcessor
 from markdown.extensions import Extension
 from markdown.inlinepatterns import DoubleTagPattern, Pattern, SimpleTagPattern
@@ -286,8 +286,7 @@ def regdown(text, **kwargs):
     """ Regdown convenience function
     Takes text and parses it with the RegulationsExtention configured with
     the given keyword arguments. """
-    return markdown(
-        text,
+    md = Markdown(
         extensions=[
             'markdown.extensions.tables',
             RegulationsExtension(**kwargs),
@@ -295,6 +294,9 @@ def regdown(text, **kwargs):
         ],
         **kwargs
     )
+    md.stripTopLevelTags = False
+    return md.convert(text)
+
 
 
 def extract_labeled_paragraph(label, text, exact=True):
