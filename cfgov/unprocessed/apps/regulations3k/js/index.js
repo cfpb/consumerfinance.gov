4c2db920cdd178f908f4572b6452ff7fa0940400
Chris Contolini <chris@contolini.com>
2018-08-09 22:34:45 -0400

    Add Regs3K expandables analytics event

--- cfgov/unprocessed/apps/regulations3k/js/index.js
+++ cfgov/unprocessed/apps/regulations3k/js/index.js
@@ -1,42 +1,46 @@
 import Expandable from 'cf-expandables/src/Expandable';
 import { bindEvent } from '../../../js/modules/util/dom-events';
 import { queryOne as find } from '../../../js/modules/util/dom-traverse';
-import { sendEvent } from './analytics';
+import { handleContentClick, handleNavClick } from './analytics';
 
 const navHeader = find( '.o-regs3k-navigation_header' );
 const navItems = find( '.o-regs3k-sections' );
+const regContent = find( '.content_main.regulations3k' );
 
+
+/**
+ * toggleSecondaryNav - Show/hide the secondary nav on smaller screens
+ */
 const toggleSecondaryNav = () => {
   navHeader.classList.toggle( 'o-expandable_target__collapsed' );
   navHeader.classList.toggle( 'o-expandable_target__expanded' );
   navItems.classList.toggle( 'u-hide-on-stacked' );
 };
 
-const handleNavClick = event => {
-  if ( !event.target.href ) {
-    return;
-  }
-  // Double check that the URL ends in regulations/1234/XXXXXX
-  let section = event.target.href.match( /regulations\/(\d+\/[\w\-]+)\/?$/ );
-  if ( !section ) {
-    return;
-  }
-  section = section[1].replace( '/', '-' );
-  sendEvent( 'toc:click', section );
-};
-
+/**
+ * bindSecondaryNav - Set up secondary nav toggling.
+ */
 const bindSecondaryNav = () => {
   bindEvent( navHeader, {
     click: toggleSecondaryNav
   } );
 };
 
+/**
+ * bindAnalytics - Set up analytics reporting.
+ */
 const bindAnalytics = () => {
   bindEvent( navItems, {
     click: handleNavClick
   } );
+  bindEvent( regContent, {
+    click: handleContentClick
+  } );
 };
 
+/**
+ * init - Initialize everything on page load.
+ */
 const init = () => {
   if ( 'serviceWorker' in navigator ) {
     navigator.serviceWorker.register( '/regulations3k-service-worker.js' ).catch( err => {
