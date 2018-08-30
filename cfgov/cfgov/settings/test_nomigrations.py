ad3ad1340ceae24401f552718d9df0214e40daae
Andy Chosak <andy.chosak@cfpb.gov>
2018-08-30 10:49:33 -0400

    properly disable migrations in Django 1.11
    PR #4444 (014df5a7) attempted to add the ability to run Python tests
    against Django 1.11, but did not properly disable migrations when
    running with `settings.test_nomigrations`.
    
    This commit fixes testing without migrations (e.g. with `tox -e
    unittest-py27-dj111-wag113-fast`) in Django 1.11.
    
    To test this change, you can run that command and notice how much faster
    the tests run, because the migrations aren't being run.

--- cfgov/cfgov/settings/test_nomigrations.py
+++ cfgov/cfgov/settings/test_nomigrations.py
@@ -1,13 +1,6 @@
-from collections import defaultdict
-
-import django
-
 from core.utils import NoMigrations
 
 from .test import *
 
 
-if django.VERSION[:2] < (1, 9):
-    MIGRATION_MODULES = NoMigrations()
-else:
-    MIGRATION_MODULES = defaultdict(None)
+MIGRATION_MODULES = NoMigrations()
