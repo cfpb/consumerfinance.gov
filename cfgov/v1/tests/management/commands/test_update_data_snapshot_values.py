a42f4ccaca84638f918fc00e73b097efec556839
Hillary <hillaryj@users.noreply.github.com>
2018-09-24 12:29:37 -0400

    Consumer Credit Trends data snapshot upgrade for inquiry/denial data (#4541)
    Adds support for new inquiry index and inferred denial data sets for Consumer Credit Trends. Includes stakeholder approval/finalization of template wording.
    
    ## Changes
    
    - DataSnapshot Wagtail block
        - Adds fields for inquiry and denial data
        - Updates block test and adds test for correct inquiry/denial display when present
    - Management command `update_data_snapshot_values`
        - Supports inquiry and denial data modification for applicable markets
        - Updates management command test and adds test for loading inquiry/denial data
        - Backwards-compatible with prior `data_snapshot.json` files
    - Modifies fixture data for DataSnapshot and management command tests
    - Includes migration for block changes
    
    ## Testing
    
    Management command:
    1. Run `tox -e fast` and note no failures occur
    
    Wagtail block:
    1. Run `cfgov-refresh` on localhost
    1. Run the migration via `manage.py migrate`
    1. Visit http://localhost:8000/data-research/consumer-credit-trends/auto-loans/
    1. Verify it contains three entries under "Data Snapshot for..." header
    1. Edit the following fields in the DataSnapshot block via the admin interface at http://localhost:8000/admin/pages/4477/edit/
        - Inquiry month: `2018-05-01`
        - Inquiry year over year change: `5.6% increase`
        - Inquiry year over year change text: `In year-over-year applications`
        - Denial month: `2018-05-01`
        - Denial year over year change: `5.6% decrease`
        - Denial year over year change text: `In year-over-year unsuccessful applications`
    1. Publish your changes
    1. Reload http://localhost:8000/data-research/consumer-credit-trends/auto-loans/
    1. Verify the Data Snapshot now displays the inquiry and denial entries
    
    ## Screenshots
    
    Showing inquiry and denial entries - please note that there should be an image but my local images are currently broken
    ![Auto loan market page showing inquiry and denial entries](https://user-images.githubusercontent.com/10235730/45891096-aa030680-bd92-11e8-869c-d6c157049fe4.png)
    
    ## Checklist
    
    - [x] PR has an informative and human-readable title
    - [x] Changes are limited to a single goal (no scope creep)
    - [x] Code can be automatically merged (no conflicts)
    - [x] Code follows the standards laid out in the [development playbook](https://github.com/cfpb/development)
    - [x] Passes all existing automated tests
    - [x] Any _change_ in functionality is tested
    - [x] New functions are documented (with a description, list of inputs, and expected output)
    - [x] Placeholder code is flagged / future todos are captured in comments
    - [x] Visually tested in supported browsers and devices (see checklist below :point_down:)
    - [x] Project documentation has been updated
    - [x] Reviewers requested with the [Reviewers tool](https://help.github.com/articles/requesting-a-pull-request-review/) :arrow_right:

--- cfgov/v1/tests/management/commands/test_update_data_snapshot_values.py
+++ cfgov/v1/tests/management/commands/test_update_data_snapshot_values.py
@@ -20,7 +20,7 @@ class UpdateDataSnapshotValuesTestCase(TestCase):
                 slug='browse',
             )
 
-            # Adds a AUT market to a browse page
+            # Adds a STU market to a browse page
             browse_page.content = StreamValue(
                 browse_page.content.stream_block,
                 [atomic.data_snapshot],
@@ -38,10 +38,60 @@ class UpdateDataSnapshotValuesTestCase(TestCase):
                 '--snapshot_file={}'.format(filename)
             )
             response = self.client.get('/browse/')
+            self.assertContains(response, '458,407')
+            self.assertContains(response, '$8.0 billion')
+            self.assertContains(response, '32.6% increase')
+            self.assertContains(response, 'March&nbsp;2017')
+            self.assertContains(response, 'Loans originated')
+            self.assertContains(response, 'Dollar value of new loans')
+            self.assertContains(response, 'In year-over-year originations')
+            # Should not contain inquiry and denial values
+            self.assertNotContains(response, '3.2% increase')
+            self.assertNotContains(response, '7.0% increase')
+            self.assertNotContains(response, 'In year-over-year inquiries')
+            self.assertNotContains(
+                response,
+                'In year-over-year credit tightness'
+            )
+
+        def test_data_snapshot_with_inquiry_and_denial(self):
+            """ Management command correctly updates data snapshot values
+            for market that contains inquiry and denial data"""
+            browse_page = BrowsePage(
+                title='Browse Page',
+                slug='browse',
+            )
+
+            # Adds a AUT market to a browse page
+            browse_page.content = StreamValue(
+                browse_page.content.stream_block,
+                [atomic.data_snapshot_with_optional_fields],
+                True
+            )
+            publish_page(child=browse_page)
+
+            # Call management command to update values
+            filename = os.path.join(
+                settings.PROJECT_ROOT,
+                'v1/tests/fixtures/data_snapshots.json'
+            )
+            call_command(
+                'update_data_snapshot_values',
+                '--snapshot_file={}'.format(filename)
+            )
+            response = self.client.get('/browse/')
             self.assertContains(response, '2.1 million')
             self.assertContains(response, '$46.4 billion')
             self.assertContains(response, '5.8% increase')
-            self.assertContains(response, 'March 2017')
-            self.assertContains(response, 'Auto loans originated')
+            self.assertContains(response, 'March&nbsp;2017')
+            self.assertContains(response, 'Loans originated')
             self.assertContains(response, 'Dollar value of new loans')
             self.assertContains(response, 'In year-over-year originations')
+            # Inquiry and denial values
+            self.assertContains(response, '3.2% increase')
+            self.assertContains(response, '7.0% increase')
+            self.assertContains(response, 'In year-over-year inquiries')
+            self.assertContains(
+                response,
+                'In year-over-year credit tightness'
+            )
