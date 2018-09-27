01fd21927ee3b6f26ee4d0ed65b456b6eb9a0aa8
Andy Chosak <andy.chosak@cfpb.gov>
2018-09-27 13:29:43 -0400

    add tests to custom makemessages command
    This change adds a Python test for the v1 app's custom `makemessages`
    Django management command, which overrides the default one documented at
    
    https://docs.djangoproject.com/en/1.11/ref/django-admin/#django-admin-makemessages
    
    Our custom makemessages command adds support for block tags like this:
    
    {% trans %}
    Multi-line
    statement
    here
    {% endtrans %}
    
    which is not supported in Django templates.
    
    This change also removes support in this command for Django 1.8 now that
    this project is solely targeted at Django 1.11.
    
    To test this locally, you can verify that the unit tests passes, and
    then remove our custom makemessages command (including the .pyc file if
    on Python 2). The built-in command will fail as it does not detect the
    multi-line Jinja2 tag.

---
+++ cfgov/v1/tests/management/commands/test_makemessages.py
@@ -0,0 +1,81 @@
+import os
+import shutil
+import tempfile
+
+from django.core.management import call_command
+from django.test import SimpleTestCase
+from django.utils._os import upath
+
+
+class TestCustomMakeMessages(SimpleTestCase):
+    DATA_DIR = 'test_makemessages_data'
+
+    LOCALE = 'de'
+
+    PO_FILE = 'locale/%s/LC_MESSAGES/django.po' % LOCALE
+
+    def setUp(self):
+        # https://github.com/django/django/blob/1.11.15/tests/i18n/utils.py#L33
+        self._cwd = os.getcwd()
+
+        # Create a temporary test directory to extract messages from.
+        self.temp_dir = tempfile.mkdtemp()
+
+        # Copy test data to temporary test directory.
+        test_dir = os.path.join(self.temp_dir, self.DATA_DIR)
+        shutil.copytree(
+            os.path.join(os.path.dirname(__file__), self.DATA_DIR),
+            test_dir,
+            ignore=shutil.ignore_patterns('*.pyc', '__pycache__')
+        )
+
+        # Create a destination locale directory in the temp directory. We don't
+        # include this in the source data being copied because we don't want it
+        # to be in the source tree when makemigrations is run normally.
+        os.mkdir(os.path.join(test_dir, 'locale'))
+
+        # Remove the temporary test directory on cleanup.
+        self.addCleanup(self._rmrf, self.temp_dir)
+
+        # Change back to the previous working directory on cleanup.
+        self.addCleanup(os.chdir, self._cwd)
+
+        # Change to the test directory so that running makemessages both reads
+        # the files located there and writes its output there.
+        os.chdir(test_dir)
+
+    def _rmrf(self, dname):
+        # Only remove this location if we're really deleting the right thing.
+        # https://github.com/django/django/blob/1.11.15/tests/i18n/utils.py
+        if (
+            os.path.commonprefix([self.temp_dir, os.path.abspath(dname)]) !=
+            self.temp_dir
+        ):
+            return
+
+        shutil.rmtree(dname)
+
+    def test_extraction_works_as_expected_including_jinja2_block(self):
+        call_command('makemessages', locale=[self.LOCALE], verbosity=0)
+
+        with open(self.PO_FILE, 'r') as f:
+            contents = f.read()
+
+        expected = '''
+#: __init__.py:4
+msgid "Test string from Python file."
+msgstr ""
+
+#: jinja2/test.html:1
+msgid ""
+"\\n"
+"Test string from Jinja template.\\n"
+"This is in a multi-line block.\\n"
+msgstr ""
+
+#: templates/test.html:2
+msgid "Test string from Django template."
+msgstr ""
+'''
+
+        self.assertIn(expected, contents)
