6a866ef6c5b255d3de699a20e6221771f922bc8e
Richa Agarwal <richa.agarwal85@gmail.com>
2018-10-04 00:04:36 -0700

    Add new view for searching Wagtail pages
      - Search content, sidefoot, and header sections of pages
      - Search body, archive_body, live_stream_url, flickr_url, youtube_url,
        future_body, and agenda_items of EventPage
      - Search snippet and answer of ask_cfpb pages
      - Search body of Contacts
      - Search link and alternate_link of Resources
      - Search text of ReusableTexts

--- cfgov/search/views.py
+++ cfgov/search/views.py
@@ -1,6 +1,57 @@
+from django.shortcuts import render
 from django.template.response import TemplateResponse
+from django.views.generic import View
+
+from wagtail.wagtailcore.models import get_page_models
 
 from search import dotgov
+from search.forms import ExternalLinksForm
+from v1.models.resources import Resource
+from v1.models.snippets import Contact, ReusableText
+
+
+class SearchView(View):
+    template_name = 'search/external_links.html'
+
+    def get(self, request):
+        return render(request, self.template_name, {
+            'form': ExternalLinksForm()
+        })
+
+    def post(self, request):
+        form = ExternalLinksForm(request.POST)
+        if not form.is_valid():
+            return render(request, self.template_name, {
+                'form': form
+            })
+        url = form.cleaned_data['url']
+        pages = []
+
+        for cls in get_page_models():
+            pages += list(cls.objects.search(url))
+        pages = sorted(pages, key=lambda k: k.title)
+
+        contacts = list(
+            Contact.objects.filter(body__contains=url).order_by('heading'))
+        resources = sorted(list(
+            Resource.objects.filter(link__contains=url)) + list(
+            Resource.objects.filter(alternate_link__contains=url)),
+            key=lambda k: k.title)
+        reusable_texts = list(
+            ReusableText.objects.filter(text__contains=url).order_by('title'))
+
+        num_page_results = len(pages)
+        num_snippet_results = len(contacts + resources + reusable_texts)
+
+        return render(request, self.template_name, {
+            'form': form,
+            'pages': pages,
+            'contacts': contacts,
+            'resources': resources,
+            'reusable_texts': reusable_texts,
+            'num_page_results': num_page_results,
+            'num_snippet_results': num_snippet_results,
+        })
 
 
 def results_view(request):
