2f68152e6f7868242d86f2cee3e94b1e44b5acc5
Ross Karchner <ross.karchner@cfpb.gov>
2018-10-19 14:06:01 -0400

    revert changes to s3utils and related tests

--- cfgov/legacy/tests/views/test_housing_counselor.py
+++ cfgov/legacy/tests/views/test_housing_counselor.py
@@ -11,13 +11,13 @@ class HousingCounselorS3URLMixinTestCase(TestCase):
     def test_s3_json_url(self):
         self.assertEqual(
             HousingCounselorS3URLMixin.s3_json_url(20001),
-            'https://foo.bucket/a/assets/hud/jsons/20001.json'
+            'https://s3.amazonaws.com/foo.bucket/a/assets/hud/jsons/20001.json'
         )
 
     def test_s3_pdf_url(self):
         self.assertEqual(
             HousingCounselorS3URLMixin.s3_pdf_url(20009),
-            'https://foo.bucket/a/assets/hud/pdfs/20009.pdf'
+            'https://s3.amazonaws.com/foo.bucket/a/assets/hud/pdfs/20009.pdf'
         )
 
 
--- cfgov/v1/s3utils.py
+++ cfgov/v1/s3utils.py
@@ -1,10 +1,21 @@
 from django.conf import settings
 
 
-def https_s3_url_prefix():
+def get_s3_url_prefix(https):
     s3_url = getattr(settings, 'AWS_STORAGE_BUCKET_NAME', None)
 
     if not s3_url:
         raise RuntimeError('AWS_STORAGE_BUCKET_NAME is not defined')
 
-    return 'https://' + s3_url + '/'
+    if not https:
+        return 'http://' + s3_url + '/'
+    else:
+        return 'https://s3.amazonaws.com/' + s3_url + '/'
+
+
+def http_s3_url_prefix():
+    return get_s3_url_prefix(https=False)
+
+
+def https_s3_url_prefix():
+    return get_s3_url_prefix(https=True)
