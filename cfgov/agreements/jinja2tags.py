b5599b384a1a5f344856e3b3cec9a15b179080de
Anselm Bradford <Anselm.Bradford@cfpb.gov>
2019-02-14 17:27:42 -0500

    Move credit card agreements to jinja

---
+++ cfgov/agreements/jinja2tags.py
@@ -0,0 +1,36 @@
+from agreements.models import Agreement, Issuer
+from jinja2 import Markup
+from jinja2.ext import Extension
+
+
+def issuer_select(selected=None):
+    issuers = Issuer.objects.all().order_by('name')
+    issuers_filtered = []
+    for issuer in issuers:
+        if Agreement.objects.filter(issuer=issuer).exists():
+            issuers_filtered.append(issuer)
+
+    markup = '<select data-placeholder="Choose an issuer"'
+    markup += ' class="chzn-select" tabindex="2" id="issuer_select">'
+
+    for i in issuers_filtered:
+        markup += '<option value="' + i.slug + '"'
+        if selected == i.slug:
+            markup += ' selected'
+        markup += '>' + i.name + '</option>'
+    markup += '</select>'
+    return Markup(markup)
+
+
+class AgreementsExtension(Extension):
+    """
+    This will give us a {% issuer_select %} tag.
+    """
+    def __init__(self, environment):
+        super(AgreementsExtension, self).__init__(environment)
+        self.environment.globals.update({
+            'issuer_select': issuer_select,
+        })
+
+
+agreements = AgreementsExtension
