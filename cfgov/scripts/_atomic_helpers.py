fff0d689aa6159e525bd9272ec74fabe5951c3b3
Catherine Farman <Catherine.Farman@cfpb.gov>
2019-02-22 16:44:30 -0500

    test date values on page match data snapshot

--- cfgov/scripts/_atomic_helpers.py
+++ cfgov/scripts/_atomic_helpers.py
@@ -438,6 +438,33 @@ chart_block = {
     }
 }
 
+chart_block_inquiry_activity = {
+    'type': u'chart_block',
+    'value': {
+        'title': u'Indexed number of consumers with inquiries (beta)',
+        'chart_type': u'line-index',
+        'color_scheme': u'Purple',
+        'data_source': u'consumer-credit-trends/credit-cards/inq_data_CRC.csv',
+        'date_published': u'2001-01-01', # should get overwritten by data_snapshot.json
+        'description': u'Indexed number of people with credit card inquiries.',
+        'note': 'Data from the last four months are not final.',
+    }
+}
+
+chart_block_credit_tightness = {
+    'type': u'chart_block',
+    'value': {
+        'title': u'Indexed number of consumers with credit tightness (beta)',
+        'chart_type': u'line-index',
+        'color_scheme': u'Purple',
+        'data_source': u'consumer-credit-trends/credit-cards/crt_data_CRC.csv',
+        'date_published': u'2001-01-01', # should get overwritten by data_snapshot.json
+        'description': u'Indexed number of people who applied for credit cards but did not open a new account.',
+        'note': 'Data from the last four months are not final.',
+    }
+}
+
+
 filter_controls = {
     'type': u'filter_controls',
     'value': {
--- cfgov/v1/tests/management/commands/test_update_chart_block_dates.py
+++ cfgov/v1/tests/management/commands/test_update_chart_block_dates.py
@@ -45,7 +45,6 @@ class UpdateChartBlockDatesTestCase(TestCase):
                 }
             ]
             
-
         def test_chart_block(self):
             """ Management command correctly updates chart block dates"""
             browse_page = BrowsePage(
@@ -76,6 +75,76 @@ class UpdateChartBlockDatesTestCase(TestCase):
             self.assertContains(
                 response,
                 'The most recent data available in this visualization are for July 2018'
+            )
+
+            # Tests date_published is correct
+            self.assertContains(response, 'October 2018')
+
+        def test_chart_block_inquiry_activity(self):
+            """ Management command correctly updates chart block dates for inquiry index charts"""
+            browse_page = BrowsePage(
+                title='Browse Page',
+                slug='browse',
+            )
+
+            # Adds a Chart Block to a browse page
+            browse_page.content = StreamValue(
+                browse_page.content.stream_block,
+                [atomic.chart_block_inquiry_activity],
+                True
+            )
+            publish_page(child=browse_page)
+
+            # Call management command to update values
+            filename = os.path.join(
+                settings.PROJECT_ROOT,
+                'v1/tests/fixtures/data_snapshot.json'
+            )
+            call_command(
+                'update_chart_block_dates',
+                '--snapshot_file={}'.format(filename)
+            )
+            response = self.client.get('/browse/')
+
+            # Tests last_updated_projected_data is correct
+            self.assertContains(
+                response,
+                'The most recent data available in this visualization are for June 2018'
+            )
+
+            # Tests date_published is correct
+            self.assertContains(response, 'October 2018')
+
+        def test_chart_block_credit_tightness(self):
+            """ Management command correctly updates chart block dates for credit tightness charts"""
+            browse_page = BrowsePage(
+                title='Browse Page',
+                slug='browse',
+            )
+
+            # Adds a Chart Block to a browse page
+            browse_page.content = StreamValue(
+                browse_page.content.stream_block,
+                [atomic.chart_block_credit_tightness],
+                True
+            )
+            publish_page(child=browse_page)
+
+            # Call management command to update values
+            filename = os.path.join(
+                settings.PROJECT_ROOT,
+                'v1/tests/fixtures/data_snapshot.json'
+            )
+            call_command(
+                'update_chart_block_dates',
+                '--snapshot_file={}'.format(filename)
+            )
+            response = self.client.get('/browse/')
+
+            # Tests last_updated_projected_data is correct
+            self.assertContains(
+                response,
+                'The most recent data available in this visualization are for February 2018'
             )
 
             # Tests date_published is correct
