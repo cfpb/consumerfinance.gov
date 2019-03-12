257887bedf9b8bb2a04a277f91bd181d75fed189
Chris Contolini <chris@contolini.com>
2019-03-12 00:29:10 -0400

    HMDA historic data page design improvements
    - Right-align record counts
    - Add visual separator in geo dropdown
    - Give tables and filters a max width
    - Renamed the institutions template file

--- cfgov/unprocessed/apps/hmda/js/index.js
+++ cfgov/unprocessed/apps/hmda/js/index.js
@@ -1,28 +1,3 @@
 import Expandable from 'cf-expandables/src/Expandable';
-import Multiselect from '../../../js/molecules/Multiselect';
-import { instantiateAll } from '../../../js/modules/util/atomic-helpers';
-
-const multiSelects = instantiateAll(
-  '.o-filterable-list-controls select[multiple]',
-  Multiselect
-);
 
 const expandables = Expandable.init();
-const expandable = expandables[0];
-
-// If multiselects exist on the form, iterate over them.
-multiSelects.forEach( multiSelect => {
-  multiSelect.addEventListener( 'expandBegin', function refresh() {
-    window.setTimeout(
-      expandable.transition.expand.bind( expandable.transition ),
-      250
-    );
-  } );
-
-  multiSelect.addEventListener( 'expandEnd', function refresh() {
-    window.setTimeout(
-      expandable.transition.expand.bind( expandable.transition ),
-      250
-    );
-  } );
-} );
