660cd10acfdc2a5e0146e9073fab1bd288342f06
Will Barton <william.barton@cfpb.gov>
2019-03-14 09:59:50 -0400

    Add dynamically generated latest_updates to Home Page context
    This change adds a `latest_updates` list of pages to the Home Page template context. This list has six pages in it and is intended to populate the "Latest Updates" section of the page.

--- cfgov/v1/models/home_page.py
+++ cfgov/v1/models/home_page.py
@@ -1,13 +1,19 @@
+from django.db import models
+from django.db.models import Q
+
 from wagtail.wagtailadmin.edit_handlers import (
-    ObjectList, StreamFieldPanel, TabbedInterface
+    InlinePanel, ObjectList, PageChooserPanel, StreamFieldPanel,
+    TabbedInterface
 )
 from wagtail.wagtailcore import blocks
 from wagtail.wagtailcore.fields import StreamField
 from wagtail.wagtailcore.models import PageManager
 from wagtail.wagtailsearch import index
 
+from modelcluster.fields import ParentalKey
+
 from v1.atomic_elements import atoms, molecules
-from v1.models.base import CFGOVPage
+from v1.models.base import CFGOVPage, CFGOVPageCategory
 from v1.util import ref
 
 
@@ -30,6 +36,9 @@ class HomePage(CFGOVPage):
     content_panels = CFGOVPage.content_panels + [
         StreamFieldPanel('header'),
         StreamFieldPanel('latest_updates'),
+        InlinePanel('excluded_updates',
+                    label='Pages excluded from Latest Updates',
+                    help_text='Some help text here'),
     ]
 
     # Tab handler interface
@@ -54,4 +63,46 @@ class HomePage(CFGOVPage):
 
     def get_context(self, request):
         context = super(HomePage, self).get_context(request)
+        context['latest_updates'] = self.get_latest_updates(request)
         return context
+
+    def get_latest_updates(self, request):
+        activity_log_qs = CFGOVPageCategory.objects.filter(
+            name='activity-log'
+        )
+        # TODO: There should be a way to express this as part of the query
+        # rather than evaluating it in Python.
+        excluded_updates_pks = [
+            e.excluded_page.pk for e in self.excluded_updates.all()
+        ]
+
+        latest_pages = CFGOVPage.objects.in_site(
+            request.site
+        ).exclude(
+            pk__in=excluded_updates_pks
+        ).filter(
+            # categories__in=activity_log_qs,
+            Q(content_type__app_label='v1') & (
+                Q(content_type__model='blogpage') |
+                Q(content_type__model='eventpage') |
+                Q(content_type__model='newsroompage')
+            ),
+            live=True,
+        ).distinct().order_by(
+            '-first_published_at'
+        )[:6]
+
+        return latest_pages
+
+
+class HomePageExcludedUpdates(models.Model):
+    page = ParentalKey(
+        HomePage, on_delete=models.CASCADE, related_name='excluded_updates'
+    )
+    excluded_page = models.ForeignKey(
+        CFGOVPage, on_delete=models.CASCADE, related_name='+'
+    )
+
+    panels = [
+        PageChooserPanel('excluded_page'),
+    ]
