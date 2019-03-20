413e99af78bc2ce99c9591afe3b1b3c55f59d407
higs4281 <higs4281@gmail.com>
2019-03-20 02:10:21 -0400

    Add routine to generate Ask IDs and self-healing URLs
    When an answer page is created, it needs to have an Ask ID to share
    with its sister language and to use in creating a self-healing URL
    in the form of `slug-language-id`.
    
    The legacy Answer model previously handled Ask ID creation, and can
    continue to do so even though all other content and operations have
    moved to the publishable page.
    
    Testing
    **For English**
    * Navigate to the Ask CFPB landing page (http://localhost:8000/admin/pages/5702/)
    * Click `ADD A CHILD PAGE`, choose `Answer page` as the page type, and enter a new
    question in the page's title field, and click `SAVE DRAFT`.
    * You should get back a page with '-en-[an Ask ID]' appended to the title,
    and on the CONFIGURATION tab, the url should be [slugified-question]-en-[ASK ID]
    * If you check the page in the Django admin, the new answer object should
    show up when you call the page's `answer_base` value.
    
    ```python
    from ask_cfpb.models import AnswerPage
    page = AnswerPage.objects.get(pk=[NEW PAGE'S ID])
    page.answer_base
    ```
    
    **For Spanish**
    * Navigate to the Obtener respuestas landing page (http://localhost:8000/admin/pages/5704/)
    * Click `ADD A CHILD PAGE`, choose `Answer page` as the page type, and enter a new
    question in the page's title field, and click `SAVE DRAFT`.
    * You should get back a page with '-es-[an Ask ID]' appended to the title,
    and on the CONFIGURATION tab, the url should be [slugified-question]-es-[ASK ID]

--- cfgov/ask_cfpb/tests/test_hooks.py
+++ cfgov/ask_cfpb/tests/test_hooks.py
@@ -1,14 +1,38 @@
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
+from django.http import HttpRequest
 from django.test import TestCase
 
-from ask_cfpb.models import Category, SubCategory
+from wagtail.wagtailcore.models import Page, Site
+
+from ask_cfpb.models import (
+    Answer, AnswerLandingPage, AnswerPage, Category, SubCategory
+)
 from ask_cfpb.wagtail_hooks import (
-    CategoryModelAdmin, SubCategoryModelAdmin, editor_css
+    CategoryModelAdmin, SubCategoryModelAdmin, create_answer_id, editor_css
 )
 
 
 class TestAskHooks(TestCase):
+    fixtures = ['ask_tests.json']
+
+    def setUp(self):
+        self.user = User.objects.get(pk=1)
+        self.default_site = Site.objects.get(is_default_site=True)
+        self.site_root = Page.objects.get(slug='root')
+        self.spanish_landing_page = AnswerLandingPage(
+            title="Obtener respuestas",
+            slug='obtener-respuestas')
+        self.site_root.add_child(instance=self.spanish_landing_page)
+        self.spanish_landing_page.save()
+        self.spanish_landing_page.save_revision(user=self.user).publish()
+        self.english_landing_page = AnswerLandingPage(
+            title="Ask CFPB",
+            slug='ask-cfpb')
+        self.site_root.add_child(instance=self.english_landing_page)
+        self.english_landing_page.save()
+        self.english_landing_page.save_revision(user=self.user).publish()
 
     def test_ask_hooks(self):
         self.assertEqual(SubCategoryModelAdmin.model, SubCategory)
@@ -16,3 +40,33 @@ class TestAskHooks(TestCase):
 
     def test_js_functions(self):
         self.assertIn("css/question-tips.css", editor_css())
+
+    def test_create_answer_id_english(self):
+        """Test that English page creation generates an Ask ID and pages."""
+        request = HttpRequest
+        request.user = self.user
+        test_page = AnswerPage(
+            slug='test-page', title='Test page')
+        self.english_landing_page.add_child(instance=test_page)
+        test_page.save()
+        create_answer_id(request, test_page)
+        self.assertEqual(test_page.slug, 'test-page-en-{}'.format(
+            Answer.objects.order_by('pk').last().pk))
+        self.assertIsNotNone(test_page.answer_base)
+        self.assertIsNotNone(test_page.answer_base.english_page)
+        self.assertIsNotNone(test_page.answer_base.spanish_page)
+
+    def test_create_answer_id_spanish(self):
+        """Test that Spanish page creation generates an Ask ID and pages."""
+        request = HttpRequest
+        request.user = self.user
+        test_page = AnswerPage(
+            slug='spanish-page', title='Spanish page')
+        self.spanish_landing_page.add_child(instance=test_page)
+        test_page.save()
+        create_answer_id(request, test_page)
+        self.assertEqual(test_page.slug, 'spanish-page-es-{}'.format(
+            Answer.objects.order_by('pk').last().pk))
+        self.assertIsNotNone(test_page.answer_base)
+        self.assertIsNotNone(test_page.answer_base.english_page)
+        self.assertIsNotNone(test_page.answer_base.spanish_page)
--- cfgov/ask_cfpb/wagtail_hooks.py
+++ cfgov/ask_cfpb/wagtail_hooks.py
@@ -12,8 +12,9 @@ from wagtail.contrib.modeladmin.options import (
 from wagtail.wagtailadmin.menu import MenuItem
 from wagtail.wagtailadmin.rich_text import HalloPlugin
 from wagtail.wagtailcore import hooks
+from wagtail.wagtailcore.models import Page
 
-from ask_cfpb.models import Category, SubCategory
+from ask_cfpb.models import Answer, AnswerPage, Category, SubCategory
 from ask_cfpb.scripts import export_ask_data
 
 
@@ -97,3 +98,44 @@ def register_export_menu_item():
 @hooks.register('register_admin_urls')
 def register_export_url():
     return [url('export-ask', export_data, name='export-ask')]
+
+
+@hooks.register('after_create_page')
+def create_answer_id(request, page):
+    """
+    Create an incremented Answer ID for a new AnswerPage and attach it.
+
+    Also create a sister-language page to keep languages in sync.
+    """
+    if isinstance(page, AnswerPage) and page.answer_base is None:
+        new_answer_base = Answer(
+            last_user=request.user,
+            question=page.title)
+        new_answer_base.save()
+        new_id = new_answer_base.pk
+        if page.get_parent().slug == 'obtener-respuestas':
+            page.language = 'es'
+            sister_page = AnswerPage(
+                live=False,
+                language='en',
+                title="English draft of {}-en-{}".format(page.title, new_id),
+                answer_base=new_answer_base)
+            Page.objects.get(slug='ask-cfpb').specific.add_child(
+                instance=sister_page)
+        else:
+            sister_page = AnswerPage(
+                live=False,
+                language='es',
+                title="Spanish draft of {}-es-{}".format(page.title, new_id),
+                answer_base=new_answer_base)
+            Page.objects.get(slug='obtener-respuestas').specific.add_child(
+                instance=sister_page)
+        sister_page.save()
+        sister_page.save_revision(user=request.user)
+        page.answer_base = new_answer_base
+        page.title = "{}-{}-{}".format(
+            page.title, page.language, new_id)
+        page.slug = "{}-{}-{}".format(
+            page.slug, page.language, new_id)
+        page.save()
+        page.save_revision(user=request.user)
