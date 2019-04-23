eb0c0485977a54517087de6ceeb59be3e7e7df3d
Will Barton <william.barton@cfpb.gov>
2019-04-23 17:04:56 -0400

    Drop the attempt to test specific URLs

--- cfgov/v1/tests/models/test_caching.py
+++ cfgov/v1/tests/models/test_caching.py
@@ -49,22 +49,10 @@ class TestAkamaiBackend(TestCase):
             'BACKEND': 'core.testutils.mock_cache_backend.MockCacheBackend',
         },
     },
-    AWS_LOCATION='root',
-    AWS_S3_CUSTOM_DOMAIN='test_s3_bucket_url',
-    AWS_S3_ACCESS_KEY_ID='test',
-    AWS_S3_SECRET_ACCESS_KEY='test',
-    AWS_STORAGE_BUCKET_NAME='test_s3_bucket',
-    DEFAULT_FILE_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
 )
 class CloudfrontInvalidationTest(TestCase):
 
     def setUp(self):
-        self.mock_s3 = moto.mock_s3()
-        self.mock_s3.start()
-
-        s3 = boto3.client('s3')
-        s3.create_bucket(Bucket='test_s3_bucket')
-
         self.document = Document(title="Test document")
         self.document_without_file = Document(title="Document without file")
         self.document.file.save(
@@ -81,8 +69,6 @@ class CloudfrontInvalidationTest(TestCase):
 
     def tearDown(self):
         self.document.file.delete()
-
-        self.mock_s3.stop()
 
     def test_rendition_saved_cache_purge_disabled(self):
         cloudfront_cache_invalidation(None, self.rendition)
@@ -101,10 +87,8 @@ class CloudfrontInvalidationTest(TestCase):
     def test_rendition_saved_cache_invalidation(self):
         cloudfront_cache_invalidation(None, self.rendition)
         self.assertIn(self.rendition.file.url, CACHE_PURGED_URLS)
-        self.assertIn('https://test_s3_bucket_url/root', CACHE_PURGED_URLS[0])
 
     @override_settings(ENABLE_CLOUDFRONT_CACHE_PURGE=True)
     def test_document_saved_cache_invalidation(self):
         cloudfront_cache_invalidation(None, self.document)
         self.assertIn(self.document.file.url, CACHE_PURGED_URLS)
-        self.assertIn('https://test_s3_bucket_url/root', CACHE_PURGED_URLS[0])
