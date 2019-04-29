686f410c1dc08f2e5ef2a31ede71e74deffe24e2
william higgins <higs4281@users.noreply.github.com>
2019-04-29 14:04:56 -0400

    Make seach terms safe by disallowing unsafe characters. (#4974)
    * Make seach terms safe by disallowing unsafe characters.
    
    This satisfies internal issue #50.
    
    * Added percent sign to test

--- cfgov/ask_cfpb/models/search.py
+++ cfgov/ask_cfpb/models/search.py
@@ -1,14 +1,25 @@
-from haystack.inputs import Clean
 from haystack.query import SearchQuerySet
 
 from flags.state import flag_enabled
+
+
+UNSAFE_CHARACTERS = [
+    '#', '%', ';', '^', '~', '`', '|',
+    '<', '>', '[', ']', '{', '}', '\\'
+]
+
+
+def make_safe(term):
+    for char in UNSAFE_CHARACTERS:
+        term = term.replace(char, '')
+    return term
 
 
 class AskSearch:
     def __init__(self, search_term, query_base=None, language='en'):
         self.query_base = query_base or SearchQuerySet().filter(
             language=language)
-        self.search_term = Clean(search_term).query_string.strip()
+        self.search_term = make_safe(search_term).strip()
         self.queryset = self.query_base.filter(content=self.search_term)
         self.suggestion = None
 
