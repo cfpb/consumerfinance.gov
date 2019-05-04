60f78c65671b546c6d1904a28e33626b8df584b3
Virginia Czosek <Virginia.Czosek@cfpb.gov>
2019-05-04 08:55:53 -0700

    Update Ask block tests.

--- cfgov/ask_cfpb/tests/test_blocks.py
+++ cfgov/ask_cfpb/tests/test_blocks.py
@@ -1,74 +1,61 @@
 from __future__ import unicode_literals
+
 from django.test import TestCase, override_settings
 
-from ask_cfpb.models.blocks import Tip, AskAnswerContent
+from ask_cfpb.models.blocks import AskAnswerContent, Tip
 
 
 @override_settings(LANGUAGE_CODE='en-US', LANGUAGES=(('en', 'English'),))
-class TipTestCase(TestCase):
-    def test_tip_html(self):
-        block = Tip()
-        value = {
-            'content': 'Test tip'
+class AskBlocksTestCase(TestCase):
+    def setUp(self):
+        self.tip_content = {
+            'content': 'Tip content'
         }
-        html = block.render(value)
-        self.assertIn('<aside class="m-inset', html)
-        self.assertIn('<h4>Tip</h4>', html)
-        self.assertIn('<div class="rich-text">Test tip</div>', html)
-        self.assertIn('</aside>', html)
-
-
-@override_settings(LANGUAGE_CODE='en-US', LANGUAGES=(('en', 'English'),))
-class ContentBlockTestCase(TestCase):
-    def test_inset_wrapper_with_tip(self):
-        tip = {
+        self.tip_data = {
             'type': 'tip',
-            'value': {
-                'content': 'Content'
-            }
+            'value': self.tip_content
         }
-        block = AskAnswerContent()
-        value = block.to_python([tip])
-        html = block.render(value)
-        self.assertIn('<div class="inset-row">', html)
-        self.assertIn('<aside class="m-inset', html)
-        self.assertIn('<h4>Tip</h4>', html)
-        self.assertIn('<div class="rich-text">Content</div>', html)
-        self.assertIn('</aside></div>', html)
-
-    def test_inset_wrapper_with_tip_and_text(self):
-        tip = {
-            'type': 'tip',
-            'value': {
-                'content': 'Tip content'
-            }
-        }
-        text = {
+        self.expected_tip_html = (
+            '<aside class="m-inset m-inset__bordered">'
+            '<h4>Tip</h4>'
+            '<div class="rich-text">Tip content</div>'
+            '</aside>'
+        )
+        self.text_data = {
             'type': 'text',
             'value': {
                 'content': 'text'
             }
         }
-        block = AskAnswerContent()
-        value = block.to_python([tip, text])
-        html = block.render(value)
-        self.assertIn('<div class="inset-row">', html)
-        self.assertIn('<aside class="m-inset', html)
-        self.assertIn('<h4>Tip</h4>', html)
-        self.assertIn('<div class="rich-text">Tip content</div>', html)
-        self.assertIn('</aside>', html)
-        self.assertIn('<div class="rich-text">text</div></div>', html)
+        self.expected_text_html = '<div class="rich-text">text</div>'
 
-    def test_no_wrapper_without_tip(self):
-        text = {
-            'type': 'text',
-            'value': {
-                'content': 'Test text'
-            }
-        }
+    def test_tip_block_renders_html(self):
+        block = Tip()
+        html = block.render(self.tip_content)
+        self.assertHTMLEqual(html, self.expected_tip_html)
+
+    def test_content_block_applies_wrapper_to_tip(self):
         block = AskAnswerContent()
-        value = block.to_python([text])
+        value = block.to_python([self.tip_data])
+        html = block.render(value)
+        expected_html = '<div class="inset-row">{}</div>'.format(
+            self.expected_tip_html
+        )
+        self.assertHTMLEqual(html, expected_html)
+
+    def test_content_block_applies_wrapper_to_tip_and_next_block(self):
+        block = AskAnswerContent()
+        value = block.to_python([self.tip_data, self.text_data])
+        html = block.render(value)
+        expected_html = '<div class="inset-row">{}{}</div>'.format(
+            self.expected_tip_html,
+            self.expected_text_html
+        )
+        self.assertHTMLEqual(html, expected_html)
+
+    def test_content_block_does_not_apply_wrapper_without_tip(self):
+        block = AskAnswerContent()
+        value = block.to_python([self.text_data])
         html = block.render(value)
         self.assertNotIn('<div class="inset-row">', html)
-        self.assertNotIn('<div class="rich-text">Test text</div></div>', html)
-        self.assertIn('<div class="rich-text">Test text</div>', html)
+        self.assertHTMLEqual(html, self.expected_text_html)
