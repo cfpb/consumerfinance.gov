615bfc6497342f96a76091bb78c655199cae2336
Will Barton <william.barton@cfpb.gov>
2019-05-09 13:04:52 -0400

    Ensure that `python` in the Python3 container is 3.6

--- docker/python3/entrypoint.sh
+++ docker/python3/entrypoint.sh
@@ -1,3 +1,4 @@
 source /src/cfgov-refresh/.env
 source /etc/profile.d/extend-environment.sh
+ln -sf /usr/bin/python3.6 /usr/local/bin/python
 python3.6 /src/cfgov-refresh/cfgov/manage.py runserver 0.0.0.0:8000
