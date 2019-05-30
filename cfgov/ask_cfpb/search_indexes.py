d9939e65e14af9201773fe7a00ebebebb4402f4d
higs4281 <higs4281@gmail.com>
2019-05-30 17:55:36 -0400

    Fix indexing for Ask CFPB answers
    Indexing broke when we moved answer content into streamfields.
    This adjusts the indexing routine to extract content out of the
    streamfield blocks.
    
    Testing
    
    With a recent db load (and a running elasticsearch instance), pull in
    the branch and update the elasticsearch index:
    ```bash
    ./cfgov/manage.py update_index ask_cfpb --remove
    ```
    
    The index run should complete, and you shoudl get results on a local
    search page: <http://localhost:8000/ask-cfpb/search/?q=negotiate>

--- cfgov/ask_cfpb/search_indexes.py
+++ cfgov/ask_cfpb/search_indexes.py
@@ -1,4 +1,9 @@
+from __future__ import unicode_literals
+
+from django.utils.html import strip_tags
 from haystack import indexes
+
+import unidecode
 
 from ask_cfpb.models.pages import AnswerPage
 from search import fields
@@ -9,6 +14,8 @@ class AnswerPageIndex(indexes.SearchIndex, indexes.Indexable):
         document=True,
         use_template=True,
         boost=10.0)
+    answer_text = indexes.CharField()
+    answer_unaccented = indexes.CharField()
     autocomplete = indexes.EdgeNgramField(
         use_template=True)
     url = indexes.CharField(
@@ -22,12 +29,6 @@ class AnswerPageIndex(indexes.SearchIndex, indexes.Indexable):
     portal_categories = indexes.MultiValueField()
     suggestions = indexes.FacetCharField()
 
-    def prepare_answer(self, obj):
-        data = super(AnswerPageIndex, self).prepare(obj)
-        if obj.question.lower().startswith('what is'):
-            data['boost'] = 2.0
-        return data
-
     def prepare_tags(self, obj):
         return obj.clean_search_tags
 
@@ -36,6 +37,39 @@ class AnswerPageIndex(indexes.SearchIndex, indexes.Indexable):
 
     def prepare_portal_categories(self, obj):
         return [topic.heading for topic in obj.portal_category.all()]
+
+    def extract_raw_text(self, stream_data):
+        # We want answer text to come first, to show up in result snippets.
+        text_chunks = [
+            block.get('value').get('content')
+            for block in stream_data
+            if block.get('type') == 'text'
+        ]
+        extra_chunks = [
+            block.get('value').get('content')
+            for block in stream_data
+            if block.get('type') in ['tip', 'table']
+        ]
+        chunks = text_chunks + extra_chunks
+        return " ".join(chunks)
+
+    def prepare_answer_text(self, obj):
+        stream = obj.answer_content
+        raw_text = self.extract_raw_text(stream.stream_data)
+        return strip_tags(raw_text)
+
+    def prepare_answer_unaccented(self, obj):
+        text = ''
+        if obj.language == 'en':
+            return text
+        stream = obj.answer_content
+        raw_text = self.extract_raw_text(stream.stream_data)
+        stripped = strip_tags(raw_text)
+        try:
+            text = unidecode.unidecode(stripped)
+        except Exception:
+            pass
+        return text
 
     def prepare(self, obj):
         data = super(AnswerPageIndex, self).prepare(obj)
