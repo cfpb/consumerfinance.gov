2bb2f4cacc3ba2fe1fec2022cb33060ea96289b8
higs4281 <higs4281@gmail.com>
2019-05-31 01:19:30 -0400

    make use of template block rendering for index

--- cfgov/ask_cfpb/search_indexes.py
+++ cfgov/ask_cfpb/search_indexes.py
@@ -1,12 +1,38 @@
 from __future__ import unicode_literals
 
 from django.utils.html import strip_tags
+from django.utils.text import Truncator
 from haystack import indexes
-
-import unidecode
 
 from ask_cfpb.models.pages import AnswerPage
 from search import fields
+
+
+def truncatissimo(text):
+    """Limit preview text to 40 words AND to 255 characters."""
+    word_limit = 40
+    while word_limit:
+        test = Truncator(text).words(word_limit, truncate=' ...')
+        if len(test) <= 255:
+            return test
+        else:
+            word_limit -= 1
+
+
+def extract_raw_text(stream_data):
+    # Extract text from stream_data, starting with the answer text.
+    text_chunks = [
+        block.get('value').get('content')
+        for block in stream_data
+        if block.get('type') == 'text'
+    ]
+    extra_chunks = [
+        block.get('value').get('content')
+        for block in stream_data
+        if block.get('type') in ['tip', 'table']
+    ]
+    chunks = text_chunks + extra_chunks
+    return " ".join(chunks)
 
 
 class AnswerPageIndex(indexes.SearchIndex, indexes.Indexable):
@@ -14,8 +40,6 @@ class AnswerPageIndex(indexes.SearchIndex, indexes.Indexable):
         document=True,
         use_template=True,
         boost=10.0)
-    answer_text = indexes.CharField()
-    answer_unaccented = indexes.CharField()
     autocomplete = indexes.EdgeNgramField(
         use_template=True)
     url = indexes.CharField(
@@ -28,53 +52,26 @@ class AnswerPageIndex(indexes.SearchIndex, indexes.Indexable):
     portal_topics = indexes.MultiValueField()
     portal_categories = indexes.MultiValueField()
     suggestions = indexes.FacetCharField()
+    preview = indexes.CharField(indexed=False)
 
-    def prepare_tags(self, obj):
-        return obj.clean_search_tags
+    def prepare_preview(self, page):
+        raw_text = extract_raw_text(page.answer_content.stream_data)
+        full_text = strip_tags(" ".join([page.short_answer, raw_text]))
+        return truncatissimo(full_text)
 
-    def prepare_portal_topics(self, obj):
-        return [topic.heading for topic in obj.portal_topic.all()]
+    def prepare_tags(self, page):
+        return page.clean_search_tags
 
-    def prepare_portal_categories(self, obj):
-        return [topic.heading for topic in obj.portal_category.all()]
+    def prepare_portal_topics(self, page):
+        return [topic.heading for topic in page.portal_topic.all()]
 
-    def extract_raw_text(self, stream_data):
-        # We want answer text to come first, to show up in result snippets.
-        text_chunks = [
-            block.get('value').get('content')
-            for block in stream_data
-            if block.get('type') == 'text'
-        ]
-        extra_chunks = [
-            block.get('value').get('content')
-            for block in stream_data
-            if block.get('type') in ['tip', 'table']
-        ]
-        chunks = text_chunks + extra_chunks
-        return " ".join(chunks)
+    def prepare_portal_categories(self, page):
+        return [topic.heading for topic in page.portal_category.all()]
 
-    def prepare_answer_text(self, obj):
-        stream = obj.answer_content
-        raw_text = self.extract_raw_text(stream.stream_data)
-        return strip_tags(raw_text)
-
-    def prepare_answer_unaccented(self, obj):
-        text = ''
-        if obj.language == 'en':
-            return text
-        stream = obj.answer_content
-        raw_text = self.extract_raw_text(stream.stream_data)
-        stripped = strip_tags(raw_text)
-        try:
-            text = unidecode.unidecode(stripped)
-        except Exception:
-            pass
-        return text
-
-    def prepare(self, obj):
-        data = super(AnswerPageIndex, self).prepare(obj)
-        data['suggestions'] = data['text']
-        return data
+    def prepare(self, page):
+        self.prepared_data = super(AnswerPageIndex, self).prepare(page)
+        self.prepared_data['suggestions'] = self.prepared_data['text']
+        return self.prepared_data
 
     def get_model(self):
         return AnswerPage
