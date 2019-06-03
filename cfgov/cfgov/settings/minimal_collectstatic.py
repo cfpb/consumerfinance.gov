d4358882a894de1bfaa7ce1ae0dbd2d2c5b3b18e
Ross Karchner <ross.karchner@cfpb.gov>
2019-06-03 15:11:11 -0400

    fix import order

--- cfgov/cfgov/settings/minimal_collectstatic.py
+++ cfgov/cfgov/settings/minimal_collectstatic.py
@@ -5,4 +5,5 @@ from unipath import DIRS
 from .base import *
 from .production import STATICFILES_STORAGE
 
+
 STATICFILES_DIRS += [str(d) for d in REPOSITORY_ROOT.child('static.in').listdir(filter=DIRS)]
