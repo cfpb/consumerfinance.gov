6ad97f3f29037a3899fc68400d94abb37849b4ef
Scott Cranfill <scott.cranfill@cfpb.gov>
2019-06-19 11:57:43 -0400

    Final Python linting fix :fingers_crossed:

--- cfgov/v1/util/__init__.py
+++ cfgov/v1/util/__init__.py
@@ -4,9 +4,8 @@ from v1.util.password_policy import (
     validate_password_history, validate_password_rule
 )
 from v1.util.ref import (
-    category_label, choices_for_page_type,
-    filterable_list_page_types, is_blog, is_report, page_type_choices,
-    related_posts_category_lookup
+    category_label, choices_for_page_type, filterable_list_page_types, is_blog,
+    is_report, page_type_choices, related_posts_category_lookup
 )
 from v1.util.util import (
     ERROR_MESSAGES, all_valid_destinations_for_request,
