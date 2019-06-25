dbae07973a13901912d0d87b28e5b993f73a509f
higs4281 <higs4281@gmail.com>
2019-06-25 11:11:46 -0400

    remove js processing

--- cfgov/unprocessed/apps/paying-for-college/js/dispatchers/get-financial-values.js
+++
@@ -1,9 +0,0 @@
-const financialModel = require( '../models/financial-model' );
-
-const getFinancialValues = {
-  values: function() {
-    return financialModel.values;
-  }
-};
-
-module.exports = getFinancialValues;
