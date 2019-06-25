a90b192bd281eb45eae42dc6b097668afa90a816
higs4281 <higs4281@gmail.com>
2019-06-25 00:31:39 -0400

    remove duplicate assets

--- cfgov/unprocessed/apps/paying-for-college/disclosures/js/dispatchers/get-expenses-values.js
+++
@@ -1,9 +0,0 @@
-const expensesModel = require( '../models/expenses-model' );
-
-const getExpensesValues = {
-  values: function() {
-    return expensesModel.values;
-  }
-};
-
-module.exports = getExpensesValues;
--- cfgov/unprocessed/apps/paying-for-college/disclosures/js/dispatchers/get-school-values.js
+++
@@ -1,9 +0,0 @@
-const schoolModel = require( '../models/school-model' );
-
-const getSchoolValues = {
-  values: function() {
-    return schoolModel.values;
-  }
-};
-
-module.exports = getSchoolValues;
