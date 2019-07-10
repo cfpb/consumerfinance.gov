16460a4f14b82ce2c369836d37f5350da5064ea4
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2019-07-10 15:16:16 -0700

    apache typo in alias

--- docker/alias-static.sh
+++ docker/alias-static.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp cfgov/apche/conf.d/alias.tmpl cfgov/apche/conf.d/alias.conf
+cp cfgov/apache/conf.d/alias.tmpl cfgov/apache/conf.d/alias.conf
 if [ ! -z ${DJANGO_SETTINGS_MODULE+x} ] && [ "$DJANGO_DEBUG" != "true" ]; then
   echo "Alias /static/ ${STATIC_PATH}" >> cfgov/apache/conf.d/alias.conf
 fi
