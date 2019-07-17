167e34ed9279ca58b2cb43d8941a588a68a11ddb
Andy Chosak <andy.chosak@cfpb.gov>
2019-07-17 16:07:18 -0400

    Run Docker DFD build in Travis in detached mode
    The drama-free-django build using a Docker container that runs in Travis
    seems to fail strangely sometimes, e.g.
    
    https://travis-ci.org/cfpb/cfgov-refresh/jobs/560138901#L1314
    
    This works locally. I suspect this is some kind of timeout, but am not
    sure. This change modifies the way that the Travis script works, so that
    it runs the container in detached mode and then attaches to the logs.
    I've verified that this works (at least once) on my fork.

--- docker/drama-free-django/travis.sh
+++ docker/drama-free-django/travis.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-docker run \
-    -v `pwd`:/cfgov \
-    centos:6 \
-    /bin/bash -c "/cfgov/docker/drama-free-django/_build.sh && /cfgov/docker/drama-free-django/_test.sh"
+container_id=$(
+    docker run \
+        -d \
+        -v `pwd`:/cfgov \
+        centos:6 \
+        /bin/bash -c "/cfgov/docker/drama-free-django/_build.sh && /cfgov/docker/drama-free-django/_test.sh"
+)
+
+docker logs -f $container_id
