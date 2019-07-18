1dd511e2c24e611828297f01ade114c02f40e855
Andy Chosak <andy.chosak@cfpb.gov>
2019-07-18 06:50:48 -0400

    Revert drama-free-django build on Travis
    Travis builds of drama-free-django, introduced in #5110, have been
    failing often in a strange way, potentially due to the error described
    here:
    
    https://github.com/travis-ci/travis-ci/issues/4704
    
    Some of the failures contain the error message "write /dev/stdout:
    resource temporarily unavailable", and some have no error at all. The
    Docker-based DFD build works fine locally, but seems to fail on Travis,
    which blocks PR checks from passing.
    
    I'm reverting the addition of drama-free-django to the Travis checks to
    allow our builds to continue working.

--- docker/drama-free-django/travis.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-docker run \
-    -v `pwd`:/cfgov \
-    -e PYTHONUNBUFFERED=1 \
-    centos:6 \
-    /bin/bash -c "/cfgov/docker/drama-free-django/_build.sh && /cfgov/docker/drama-free-django/_test.sh"
