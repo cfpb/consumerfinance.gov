0bac8bb2e0dbeeb9f226ef7971980f8aadc991cf
Ross Karchner <ross.karchner@cfpb.gov>
2019-07-22 12:46:18 -0400

    first step to removing DFD runtime dependencies:
    use django-dotenv to load environment variables

--- cfgov/cfgov/wsgi.py
+++ cfgov/cfgov/wsgi.py
@@ -10,6 +10,24 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 
 
+import dotenv
+
+this_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+def initialize_environment():
+    environment_candidates = [
+        os.path.join(this_dir, '../.env'),
+        os.path.join(this_dir, '../../.env'),
+        os.path.join(os.getcwd(), '.env')
+    ]
+
+    for candidate in environment_candidates:
+        if os.path.exists(candidate):
+                dotenv.read_dotenv(candidate)
+                return
+
+
 def initialize_new_relic():
     if os.getenv('NEW_RELIC_LICENSE_KEY'):
         new_relic_config_file = os.path.abspath(
@@ -23,6 +41,7 @@ def initialize_new_relic():
         newrelic.agent.initialize(new_relic_config_file)
 
 
+initialize_environment()
 initialize_new_relic()
 
 
--- cfgov/manage.py
+++ cfgov/manage.py
@@ -2,8 +2,10 @@
 import os
 import sys
 
+import dotenv
 
 if __name__ == '__main__':
+    dotenv.read_dotenv()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cfgov.settings.local')
 
     from django.core.management import execute_from_command_line
