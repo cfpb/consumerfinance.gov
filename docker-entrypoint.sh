62263674eb092169d77777321ac3adcfbcb71057
Hans Keeler <hans.keeler@cfpb.gov>
2019-07-26 14:41:10 -0400

    bash env improvements to docker-entrypoint.sh
    - Switches from --login to -i (interactive), which
        seems more appropriate.
    - Requires a `.env` file to be present, and exits
        without one.
    - Copies .env to /etc/profile.d so that all future
        shells will have the .env variables automatically.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,20 +1,18 @@
-#!/bin/bash --login
-# NOTE: By using --login, it guarantees /etc/profile is always sourced, unlike the
-# non-login, non-interactive shell you get by default with `docker run`.
+#!/bin/bash -i
+# NOTE: By using -i (interactive), it guarantees /etc/profile is always sourced,
+# unlike the non-interactive shell you get by default with `docker run`.
 
 set -e
 
 echo "Using '$(python --version 2>&1)' from '$(which python)'"
 
-# FIXME: Could we replace this with a Docker Compose env_file?
-# SEE: https://docs.docker.com/compose/compose-file/#env_file
-if [ -f "${APP_HOME}/.env" ]; then
-    source ${APP_HOME}/.env
-else
-    echo "WARN: ${APP_HOME}/.env is not present."
+if [ ! -f "${APP_HOME}/.env" ]; then
+    echo "ERROR: ${APP_HOME}/.env not found."
+    exit 1
 fi
 
-env | sort
+cp ${APP_HOME}/.env /etc/profile.d/cfgov-env.sh
+source /etc/profile
 
 # Execute the Docker CMD
 exec "$@"
