ea038e82c6820118f532dfd97efdfa852f2e8db9
Hans Keeler <hans.keeler@cfpb.gov>
2019-08-13 19:05:05 -0400

    Remove unneed --login from docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,18 @@
-#!/bin/bash --login
-# NOTE: By forcing a login shell, /etc/profile is always sourced,
-# unlike the non-interactive shell you get by default with `docker run`.
+#!/bin/bash
 
 set -e
 
-echo "Using '$(python --version 2>&1)' from '$(which python)'"
-
+# Add .env to to /etc/profile
 if [ ! -f "${APP_HOME}/.env" ]; then
     echo "ERROR: ${APP_HOME}/.env not found."
     exit 1
 fi
-
 cp ${APP_HOME}/.env /etc/profile.d/cfgov-env.sh
+
+# source all the env scripts added to /etc/profile.d/ at build and runtime
 source /etc/profile
+
+echo "Using '$(python --version 2>&1)' from '$(which python)'"
 
 # Execute the Docker CMD
 exec "$@"
