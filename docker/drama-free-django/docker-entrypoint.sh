c0f463570ee8048605e6df6f25abb389c98d4b07
Andy Chosak <andy.chosak@cfpb.gov>
2019-08-28 10:41:49 -0400

    Remove drama-free-django-based Docker build
    Our use of drama-free-django is deprecated; we now use the build
    approach that can be tested using the docker/deployable-zipfile
    directory.

--- docker/drama-free-django/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash --login
-# This entrypoint is used primarily as means of setting up a consistent
-# shell environment no matter which user the process runs as.  By using
-# --login, it guarantees /etc/profile is always sourced, unlike the
-# non-login, non-interactive shell you get by default with `docker run`.
-
-exec "$@"
