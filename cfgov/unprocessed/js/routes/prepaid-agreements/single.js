df438820458c73aa1ddf5a68a710942166290a02
Virginia Czosek <Virginia.Czosek@cfpb.gov>
2019-08-30 17:33:30 -0700

    Fix linting errors.

--- cfgov/unprocessed/js/routes/prepaid-agreements/single.js
+++ cfgov/unprocessed/js/routes/prepaid-agreements/single.js
@@ -1,18 +1,11 @@
-import {
-	checkDom,
-	instantiateAll
-} from '../../modules/util/atomic-helpers';
+import { instantiateAll } from '../../modules/util/atomic-helpers';
 import Expandable from 'cf-expandables/src/Expandable';
 import Multiselect from '../../molecules/Multiselect';
 
-const BASE_CLASS = 'filters';
-const element = document.querySelector('.search_results');
+const multiSelectsSelector = `.${ Multiselect.BASE_CLASS }`;
 
-if (element) {
-	const _dom = checkDom( element, BASE_CLASS );
-	const multiSelectsSelector = `.${ BASE_CLASS } .${ Multiselect.BASE_CLASS }`;
-
-	instantiateAll( multiSelectsSelector, Multiselect );
+if ( document.querySelector( multiSelectsSelector ) ) {
+  instantiateAll( multiSelectsSelector, Multiselect );
 }
 
 Expandable.init();
