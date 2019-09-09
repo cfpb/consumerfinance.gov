6d23d157cdb291f5b6b9675bec26ca5c2aa5086d
Hans Keeler <hans.keeler@cfpb.gov>
2019-09-09 03:27:23 -0400

    Docker Compose improvements
    1. Mount `.env` right into `/etc/profile` instead of copying
        it in at container startup.
    1. Use yaml anchors in Compose files for dupe data
    1. Fix path on `develop-apps` volume
    1. Add APACHE_UPLOADS_F_ALIAS, pointing at local
        filesystem for Wagtail uploads.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,16 +2,6 @@
 
 set -e
 
-# Add .env to to /etc/profile
-if [ ! -f "${APP_HOME}/.env" ]; then
-    echo "WARNING: ${APP_HOME}/.env not found."
-else
-    cp ${APP_HOME}/.env /etc/profile.d/cfgov-env.sh
-fi
-
-# source all the env scripts added to /etc/profile.d/ at build and runtime
-source /etc/profile
-
 echo "Using '$(python --version 2>&1)' from '$(which python)'"
 
 # Execute the Docker CMD
