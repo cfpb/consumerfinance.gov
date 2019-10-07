e3a34bb225ff2c07b8b3a899d66f263ecfd4d1c8
el-mapache <adam.biagianti@gmail.com>
2019-10-07 14:36:52 -0700

    Adds alert in no-js env, refines print behavior

--- test/unit_tests/apps/youth-employment-success/js/views/print-button-spec.js
+++ test/unit_tests/apps/youth-employment-success/js/views/print-button-spec.js
@@ -2,22 +2,55 @@ import { simulateEvent } from '../../../../../util/simulate-event';
 import printButton from '../../../../../../cfgov/unprocessed/apps/youth-employment-success/js/views/print-button';
 
 const CLASSES = printButton.CLASSES;
-const HTML = `<button class="${ CLASSES.BUTTON }"></button>`;
+const HTML = `
+  <div class="${ CLASSES.NO_PRINT }"></div>
+  <button class="${ CLASSES.BUTTON }"></button>
+`;
 
 describe( 'printButtonView', () => {
   const printMock = jest.fn();
+  let addEventSpy;
+  let removeEventSpy;
   let dom;
+  let view;
 
   beforeEach( () => {
     window.print = printMock;
+    addEventSpy = jest.spyOn( window, 'addEventListener' );
+    removeEventSpy = jest.spyOn( window, 'removeEventListener' );
     document.body.innerHTML = HTML;
     dom = document.querySelector( `.${ CLASSES.BUTTON }` );
-    printButton( dom ).init();
+    view = printButton( dom );
+    view.init();
+  } );
+
+  afterEach( () => {
+    view = null;
+    printMock.mockReset();
   } );
 
   it( 'calls the system print dialog when clicked', () => {
     simulateEvent( 'click', dom );
 
     expect( printMock.mock.calls.length ).toBe( 1 );
+    expect( addEventSpy ).toHaveBeenCalled();
+
+    simulateEvent( 'focus', window );
+
+    expect( removeEventSpy ).toHaveBeenCalled();
+  } );
+
+  it( 'hides elements with the no print class on print, and shows them afterwards', () => {
+    const elToToggle = document.querySelector( `.${ CLASSES.NO_PRINT }` );
+
+    expect( elToToggle.classList.contains( CLASSES.HIDE ) ).toBeFalsy();
+
+    simulateEvent( 'click', dom );
+
+    expect( elToToggle.classList.contains( CLASSES.HIDE ) ).toBeTruthy();
+
+    simulateEvent( 'focus', window );
+
+    expect( elToToggle.classList.contains( CLASSES.HIDE ) ).toBeFalsy();
   } );
 } );
