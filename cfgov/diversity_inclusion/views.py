75da00e6494298937849017d35eafeee468ea16f
Scott Cranfill <scott.cranfill@cfpb.gov>
2019-10-16 12:30:10 -0400

    Reinstate the dedicated success page
    With the `FormView` implementation, this is easier than redirecting to a 
    modified `voluntary-assessment-form.html`.

--- cfgov/diversity_inclusion/views.py
+++ cfgov/diversity_inclusion/views.py
@@ -7,7 +7,7 @@ from diversity_inclusion.forms import VoluntaryAssessmentForm
 class GetAssessmentForm(FormView):
     template_name = 'diversity_inclusion/voluntary-assessment-form.html'
     form_class = VoluntaryAssessmentForm
-    success_url = reverse_lazy('diversity_inclusion:voluntary_assessment_form')
+    success_url = reverse_lazy('diversity_inclusion:form_submitted')
 
     def form_valid(self, form):
         # This method is called when valid form data has been POSTed.
