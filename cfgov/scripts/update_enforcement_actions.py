ff00d4f68bb3fb0981548bc067a092fd0cd3d83e
Catherine Farman <Catherine.Farman@cfpb.gov>
2019-10-30 19:13:56 -0400

    add publish step

--- cfgov/scripts/update_enforcement_actions.py
+++ cfgov/scripts/update_enforcement_actions.py
@@ -1,27 +1,11 @@
 from __future__ import unicode_literals
 
-import datetime
-import re
-from six.moves import html_parser as HTMLParser
-
-from django.http import HttpResponse
-from django.utils import html
-
-import unicodecsv
-
 from v1.models import DocumentDetailPage
 from v1.util.migrations import get_stream_data
+from v1.tests.wagtail_pages.helpers import publish_changes
 
 
-html_parser = HTMLParser.HTMLParser()
-
-
-def clean_and_strip(data):
-    unescaped = html_parser.unescape(data)
-    return html.strip_tags(unescaped).strip()
-
-
-def assemble_output():
+def update_field_name():
     for page in DocumentDetailPage.objects.all():
         if not page.live:
             continue
@@ -35,10 +19,12 @@ def assemble_output():
                 for block in field_content:
                     if block['value'].get('heading', '') == 'File number':
                         block['value']['heading'] = 'Docket number'
-                        print(clean_and_strip(block['value'].get('heading', '')))
+                        print(block['value'].get('heading', ''))
+                        print(block['value'].get('blob', ''))
             break
+        publish_changes(page.specific)
         break
 
 
 def run():
-    assemble_output()
+    update_field_name()
