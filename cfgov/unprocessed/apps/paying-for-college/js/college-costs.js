2154146ff7586f71b04295a7288e21612f4ceb96
Chuck Werner <chuck.werner@gmail.com>
2019-10-30 11:52:59 -0400

    Paying for College - Basic costs tool navigation
    - Adds JS, HTMl, and CSS to allow basic navigation of the user through the
    tool's sections
    - Adds state tracking for the application state

--- cfgov/unprocessed/apps/paying-for-college/js/college-costs.js
+++ cfgov/unprocessed/apps/paying-for-college/js/college-costs.js
@@ -4,6 +4,7 @@ import studentDebtCalculator from 'student-debt-calc';
 import { schoolModel } from './models/school-model.js';
 import { debtModel } from './models/debt-model.js';
 import { expensesModel } from './models/expenses-model.js';
+import { stateModel } from './models/state-model.js';
 
 import { navigationView } from './views/navigation-view.js';
 import { financialView } from './views/financial-view.js';
@@ -14,6 +15,7 @@ import { searchView } from './views/search-view.js';
 
 const init = function() {
   const body = document.querySelector( 'body' );
+  stateModel.init();
   searchView.init( body );
   financialView.init( body );
   navigationView.init( body );
---
+++ cfgov/unprocessed/apps/paying-for-college/js/dispatchers/update-state.js
@@ -0,0 +1,29 @@
+/**
+ * Update the application state model, then trigger updates in views
+ */
+import { stateModel } from '../models/state-model.js';
+
+import { navigationView } from '../views/navigation-view.js';
+import { financialView } from '../views/financial-view.js';
+import { searchView } from '../views/search-view.js';
+
+const updateState = {
+
+  /**
+   * activeSection - Change the active navigation view, trigger an update
+   * for the navigationView
+   *
+   * @param {string} item - Value of 'data-nav_item' attribute
+   */
+  activeSection: item => {
+    stateModel.activeSection = item;
+
+    navigationView.update();
+    financialView.updateSection();
+  }
+
+};
+
+export {
+  updateState
+};
