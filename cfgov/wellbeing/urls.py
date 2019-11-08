478f55e62133a4dbcf66e56f9810372e3f498d2f
Will Barton <william.barton@cfpb.gov>
2019-11-08 12:26:33 -0500

    Support translated content in the Financial Well Being tool

--- cfgov/wellbeing/urls.py
+++ cfgov/wellbeing/urls.py
@@ -1,5 +1,6 @@
-from django.views.generic import TemplateView
+from django.conf.urls import url
 
+from core.views import TranslatedTemplateView
 from wellbeing.views import ResultsView
 
 
@@ -10,17 +11,45 @@ except ImportError:
 
 
 urlpatterns = [
-    re_path(
-        r'^$(?i)',
-        TemplateView.as_view(template_name='wellbeing/home.html'),
-        name='fwb_home'
+    url(
+        r'^$',
+        TranslatedTemplateView.as_view(
+            template_name='wellbeing/home.html'
+        ),
+        name='fwb_home_en'
     ),
-    re_path(
-        r'^results/$(?i)', ResultsView.as_view(), name='fwb_results'
+    url(
+        r'^results/$',
+        ResultsView.as_view(),
+        name='fwb_results_en'
     ),
-    re_path(
-        r'^about/$(?i)',
-        TemplateView.as_view(template_name='wellbeing/about.html'),
-        name='fwb_about'
+    url(
+        r'^about/$',
+        TranslatedTemplateView.as_view(
+            template_name='wellbeing/about.html'
+        ),
+        name='fwb_about_en'
+    ),
+
+    url(
+        r'^es/$',
+        TranslatedTemplateView.as_view(
+            template_name='wellbeing/home.html',
+            language='es'
+        ),
+        name='fwb_home_es'
+    ),
+    url(
+        r'^results/es/$',
+        ResultsView.as_view(language='es'),
+        name='fwb_results_es'
+    ),
+    url(
+        r'^about/es/$',
+        TranslatedTemplateView.as_view(
+            template_name='wellbeing/about.html',
+            language='es'
+        ),
+        name='fwb_about_es'
     ),
 ]
