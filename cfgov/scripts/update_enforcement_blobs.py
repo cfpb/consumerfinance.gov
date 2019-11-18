5450586957d197ac62bdb8957a773c66e15afdcf
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2019-11-18 10:55:02 -0800

    Use page categories for related-metadata sidefoot
    Adds a block to to the RelatedMetadata molecule to allow
    editors to trigger whether they want to display page categories
    Also, updates the related-metadata template to use the page
    categories when this is selected
    As a bonus, adds the new enforcement action categories that
    will be assigned to pages in a separate script after this
    migration is applied

--- cfgov/scripts/update_enforcement_blobs.py
+++ cfgov/scripts/update_enforcement_blobs.py
@@ -5,18 +5,6 @@ from v1.models import DocumentDetailPage
 from v1.util.migrations import get_stream_data, set_stream_data
 from v1.tests.wagtail_pages.helpers import publish_changes
 
-def update_category(current):
-    stip = search('Stipulation and consent order', current, IGNORECASE)
-    admin = search('Administrative adjudication', current, IGNORECASE)
-
-    if stip or admin:
-        return 'Administrative Proceeding'
-    elif search('Federal district court case', current, IGNORECASE):
-        return 'Civil Action'
-    else:
-      return current
-
-
 def update_sidefoot():
     draft_pages = []
     for page in DocumentDetailPage.objects.all():
@@ -35,11 +23,15 @@ def update_sidefoot():
         for field in stream_data:
             if field['type'] == 'related_metadata':
                 field_content = field['value']['content']
+                new_content = []
                 for block in field_content:
+                    # Switch File number to Docker number
                     if block['value'].get('heading', '') == 'File number':
                         block['value']['heading'] = 'Docket number'
-                    if block['value'].get('heading', '') == 'Category':
-                        block['value']['blob'] = update_category(block['value']['blob'])
+                    # Remove inline Category, use the page category instead
+                    if block['value'].get('heading', '') != 'Category':
+                        new_content.append(block)
+                field['value']['content'] = new_content
             break
         set_stream_data(page.specific, 'sidefoot', stream_data)
     if len(draft_pages) > 0:
