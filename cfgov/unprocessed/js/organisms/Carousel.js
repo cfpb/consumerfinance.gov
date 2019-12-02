89c88d3e587301a71595401eff8b8c460c343ce9
Anselm Bradford <anselmbradford@users.noreply.github.com>
2019-12-02 18:55:31 -0500

    Add analytics code to carousel

--- cfgov/unprocessed/js/organisms/Carousel.js
+++ cfgov/unprocessed/js/organisms/Carousel.js
@@ -1,4 +1,5 @@
 // Required modules.
+import Analytics from '../modules/Analytics';
 import { checkDom, setInitFlag } from '../modules/util/atomic-helpers';
 
 const BASE_CLASS = 'o-carousel';
@@ -22,6 +23,10 @@ function Carousel( element ) {
   const _thumbnails = _dom.querySelectorAll( `.${ BASE_CLASS }_thumbnail` );
   const _items = _dom.querySelectorAll( `.${ BASE_CLASS }_item` );
   let _currItemIndex = 0;
+
+  // Track how many unique slides the user has seen in the carousel.
+  let _slideVisitCount = 1;
+  const ALL_SLIDES_VISITED = 15;
 
   /**
    * @returns {Carousel} An instance.
@@ -57,14 +62,14 @@ function Carousel( element ) {
    * Handle clicks of the previous button.
    */
   function _btnPrevClicked() {
-    _updateDisplay( _currItemIndex, _currItemIndex - 1 );
+    _updateDisplay( _currItemIndex, _currItemIndex - 1, 'Previous Arrow' );
   }
 
   /**
    * Handle clicks of the next button.
    */
   function _btnNextClicked() {
-    _updateDisplay( _currItemIndex, _currItemIndex + 1 );
+    _updateDisplay( _currItemIndex, _currItemIndex + 1, 'Next Arrow' );
   }
 
   /**
@@ -80,17 +85,22 @@ function Carousel( element ) {
         ++i;
       }
     }
-    if ( _currItemIndex !== i ) {
-      _updateDisplay( _currItemIndex, i );
-    }
+
+    _updateDisplay( _currItemIndex, i, 'Tab' );
   }
 
   /**
    * Update which carousel item is being displayed.
    * @param {number} oldCurrItemIndex - The previous index value of the selected item.
    * @param {number} newCurrItemIndex - The new index value of the selected item.
+   * @param {string} interactionLabel - A label for what called this method.
+   * @returns {boolean} True if the display was updated, false otherwise.
    */
-  function _updateDisplay( oldCurrItemIndex, newCurrItemIndex ) {
+  function _updateDisplay( oldCurrItemIndex, newCurrItemIndex, interactionLabel ) {
+    if ( oldCurrItemIndex === newCurrItemIndex ) {
+      return false;
+    }
+
     const lastItem = _items[oldCurrItemIndex];
     const lastThumbnail = _thumbnails[oldCurrItemIndex];
 
@@ -100,11 +110,42 @@ function Carousel( element ) {
     } else if ( _currItemIndex > _items.length - 1 ) {
       _currItemIndex = 0;
     }
+
+    // Update visible slides.
     _items[_currItemIndex].classList.remove( 'u-alpha-0' );
     lastItem.classList.add( 'u-alpha-0' );
 
     _thumbnails[_currItemIndex].classList.add( `${ BASE_CLASS }_thumbnail-selected` );
     lastThumbnail.classList.remove( `${ BASE_CLASS }_thumbnail-selected` );
+
+
+    /*
+     Send off analytics.
+
+     We use a bitmask to see if all slides have been visited:
+     0001 = item one (1 decimal).
+     0010 = item two (2 decimal).
+     0100 = item three (4 decimal).
+     1000 = item four (8 decimal).
+     1111 = all visited (15 decimal).
+     We need to map 0, 1, 2, 3 item indices to bitwise digits to 1, 2, 4, 8.
+     */
+    _slideVisitCount |= [ 1, 2, 4, 8 ][newCurrItemIndex];
+    if ( _slideVisitCount === ALL_SLIDES_VISITED ) {
+      Analytics.sendEvent( {
+        event: 'Page Interaction',
+        action: 'Carousel',
+        label: 'All Items Seen'
+      } );
+    }
+
+    Analytics.sendEvent( {
+      event: 'Page Interaction',
+      action: 'Carousel',
+      label: `${ interactionLabel } Clicked: Page ${ _currItemIndex + 1 }`
+    } );
+
+    return true;
   }
 
   this.init = init;
