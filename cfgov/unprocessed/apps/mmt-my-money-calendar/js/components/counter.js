7d39a3449e30c5732ecc993c38433baa08f0f832
Mike Green <mike.green@simplethread.com>
2020-01-08 12:46:44 -0500

    Add simple counter component to mmc react app stub

---
+++ cfgov/unprocessed/apps/mmt-my-money-calendar/js/components/counter.js
@@ -0,0 +1,25 @@
+import { useState, useCallback } from 'react';
+
+function Counter({ initial = 0 }) {
+  const [count, setCount] = useState(initial);
+
+  const increment = useCallback((evt) => {
+    evt.preventDefault();
+    setCount(count + 1);
+  });
+  const decrement = useCallback((evt) => {
+    evt.preventDefault();
+    setCount(count - 1);
+  });
+
+  return (
+    <div className="counter">
+      <h2>Counter</h2>
+      <button onClick={decrement}>--</button>
+      <strong>{count}</strong>
+      <button onClick={increment}>++</button>
+    </div>
+  );
+}
+
+export default Counter;
