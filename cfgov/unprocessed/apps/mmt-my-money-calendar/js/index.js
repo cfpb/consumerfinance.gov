6d82c1edbd9732fc5409be25e79494b43986ad8b
Mike Green <mike.green@simplethread.com>
2020-01-08 12:46:44 -0500

    Add simple counter component to mmc react app stub

--- cfgov/unprocessed/apps/mmt-my-money-calendar/js/index.js
+++ cfgov/unprocessed/apps/mmt-my-money-calendar/js/index.js
@@ -1,9 +1,11 @@
 import { render } from 'react-dom';
 import Greeting from './components/greeting';
+import Counter from './components/counter';
 
 const App = () => (
   <div className="hello">
     <Greeting />
+    <Counter />
   </div>
 );
 
