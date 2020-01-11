78c09c5107b4651c8495edefa8bb7618571235ac
Craig Davies <craig.davies@cfpb.gov>
2020-01-10 22:03:39 -0500

    Resolve redirect_stdout imported but unused

--- cfgov/cfgov/tests/test_test.py
+++ cfgov/cfgov/tests/test_test.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 from io import StringIO
 from unittest import TestCase, TestSuite, defaultTestLoader
 
-from cfgov.test import StdoutCapturingTestRunner, redirect_stdout
+from cfgov.test import StdoutCapturingTestRunner
 
 
 class StderrSuppressingStdoutCapturingTestRunner(StdoutCapturingTestRunner):
