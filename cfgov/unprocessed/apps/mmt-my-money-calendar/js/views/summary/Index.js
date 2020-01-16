c0a3a338b211c304d967739df3b158238756be57
Missy Maloney <mmaloney@healthstarInfo.com>
2020-01-15 19:17:43 -0500

    fixed Month error

--- cfgov/unprocessed/apps/mmt-my-money-calendar/js/views/summary/Index.js
+++ cfgov/unprocessed/apps/mmt-my-money-calendar/js/views/summary/Index.js
@@ -1,24 +1,24 @@
-import React from "react";
 import { Route, Redirect, Switch, Link, useRouteMatch } from "react-router-dom";
-
-import { Month } from "./Month";
+import { Interval } from "./interval";
 
 export default function Summary() {
+  const match = useRouteMatch("/summary/interval/:interval");
+
   return (
     <section id="summary-view">
-      <h1>Here is your Summary Screen</h1>
+      <h1>Summary Screen</h1>
+
       <Link to="/">Back Home</Link>
 
       <h3>Route match debug</h3>
       <pre className="debug">{JSON.stringify(match, null, 2)}</pre>
+
       <Switch>
         <Redirect exact from="/summary" to="/summary/month" />
         <Route path="/summary/:interval">
           <Interval />
         </Route>
       </Switch>
-      <Month />
-      <div className="strategy">Strategy List Link</div>
     </section>
   );
 }
