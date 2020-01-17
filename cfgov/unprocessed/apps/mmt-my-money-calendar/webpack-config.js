8337c34dc99d585be1e2e1222de95a942318a3ff
Mike Green <mike.green@simplethread.com>
2020-01-17 10:02:06 -0500

    output service worker into jinja templates folder to myMoney Calendar app

--- cfgov/unprocessed/apps/mmt-my-money-calendar/webpack-config.js
+++ cfgov/unprocessed/apps/mmt-my-money-calendar/webpack-config.js
@@ -15,6 +15,7 @@ const {
 } = process.env;
 
 const COMMON_BUNDLE_NAME = 'common.js';
+const SERVICE_WORKER_DESTINATION = '../../../../jinja2/v1/mmt-my-money-calendar/mmt-my-money-calendar-service-worker.js';
 
 const AUTOLOAD_REACT = new webpack.ProvidePlugin({
   React: 'react',
@@ -28,6 +29,8 @@ const COPY_PWA_MANIFEST = new CopyPlugin([
 ]);
 
 const GENERATE_SERVICE_WORKER = new WorkboxPlugin.GenerateSW({
+  swDest: SERVICE_WORKER_DESTINATION,
+
   // Do not precache images
   exclude: [
     /\.(?:png|jpe?g|svg|gif)$/,
