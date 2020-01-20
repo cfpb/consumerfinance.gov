d66e907bc2efc49bcbd4796e6bae28ec96603f93
Mike Green <mike.green@simplethread.com>
2020-01-20 07:13:39 -0500

    Add inline documentation to service worker view class dispatch

--- cfgov/mmt_my_money_calendar/views.py
+++ cfgov/mmt_my_money_calendar/views.py
@@ -6,6 +6,13 @@ class ServiceWorkerView(TemplateView):
     content_type = 'application/javascript'
     scope = '/'
 
+    """
+    Adds a Service-Worker-Allowed HTTP response header when clients request a service worker script.
+    Can be used to properly set the service worker's allowed path scope since it comes from a different
+    location (/static) than whichever app it's meant to operate on.
+
+    See: https://www.w3.org/TR/service-workers/#service-worker-allowed
+    """
     def dispatch(self, request, *args, **kwargs):
         response = super(ServiceWorkerView).dispatch(self, request, *args, **kwargs)
         response['Service-Worker-Allowed'] = self.scope
