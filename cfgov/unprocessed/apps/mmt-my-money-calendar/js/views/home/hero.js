88aeffa797033d0c30e36b11517fce4007ccc76b
Mike Green <mike.green@simplethread.com>
2020-01-22 12:03:30 -0500

    Install clsx classname library, update hero component to be an arrow func

--- cfgov/unprocessed/apps/mmt-my-money-calendar/js/views/home/hero.js
+++ cfgov/unprocessed/apps/mmt-my-money-calendar/js/views/home/hero.js
@@ -9,20 +9,17 @@ export default function Hero() {
         <div className="m-hero_text">
           <h1 className="m-hero_heading">My Money Calendar</h1>
           <div className="m-hero_subhead">
-            Visualize your spending and learn strategies to manage your weekly
-            and monthly budget{" "}
+            Visualize your spending and learn strategies to manage your weekly and monthly budget{' '}
           </div>
         </div>
         <div className="m-hero_image-wrapper">
           <div className="m-hero_image">
-            <img
-              src={heroImg}
-              alt=""
-              className="u-hide-on-print"
-            />
+            <img src={heroImg} alt="" className="u-hide-on-print" />
           </div>
         </div>
       </div>
     </section>
   );
 }
+
+export default Hero;
