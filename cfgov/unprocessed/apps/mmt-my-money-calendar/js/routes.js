397ef399f8571f3e78b94bc67c8438c73c936cec
Mike Green <mike.green@simplethread.com>
2020-01-27 16:39:48 -0500

    Wire up UI state to starting balance component, setup preliminary wizard route nesting

--- cfgov/unprocessed/apps/mmt-my-money-calendar/js/routes.js
+++ cfgov/unprocessed/apps/mmt-my-money-calendar/js/routes.js
@@ -1,8 +1,6 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./views/home";
-import StartingBalance from "./views/wizard/steps/starting-balance";
-import Income from "./views/wizard/steps/income";
-import Expenses from "./views/wizard/steps/expenses";
+import Wizard from './views/wizard';
 import Summary from "./views/summary";
 
 const Routes = () => (
@@ -13,16 +11,8 @@ const Routes = () => (
           <Home />
         </Route>
 
-        <Route path="/wizard/steps/starting-balance">
-          <StartingBalance />
-        </Route>
-
-        <Route path="/wizard/steps/income">
-          <Income />
-        </Route>
-
-        <Route path="/wizard/steps/expenses">
-          <Expenses />
+        <Route path="/wizard">
+          <Wizard />
         </Route>
 
         <Route path="/summary">
