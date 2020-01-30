ae77de3865f8dcc903cf83475875da7439460eb1
Mike Green <mike.green@simplethread.com>
2020-01-30 13:29:06 -0500

    add styles for pos/neg balances and month nav buttons
    Add year to calendar subtitle, lighten days not in current month
    
    add jsdoc comments to cash flow store

--- cfgov/unprocessed/apps/mmt-my-money-calendar/js/views/calendar/index.js
+++ cfgov/unprocessed/apps/mmt-my-money-calendar/js/views/calendar/index.js
@@ -1,8 +1,12 @@
-import { useEffect } from 'react';
+import { useEffect, useCallback } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 import { observer } from 'mobx-react';
 import { useStore } from '../../stores';
 import Day from './day';
+import Button from '../../components/button';
+
+import arrowRight from 'cf-icons/src/icons/arrow-right.svg';
+import arrowLeft from 'cf-icons/src/icons/arrow-left.svg';
 
 const CalendarWeekRow = ({ days }) => (
   <div className="calendar__row">
@@ -15,15 +19,34 @@ function Calendar() {
   const location = useLocation();
   const params = useParams();
 
+  const nextMonth = useCallback((evt) => {
+    evt.preventDefault();
+    uiStore.nextMonth();
+  }, [uiStore.currentMonth]);
+
+  const prevMonth = useCallback((evt) => {
+    evt.preventDefault();
+    uiStore.prevMonth();
+  }, [uiStore.currentMonth]);
+
   useEffect(() => {
     uiStore.setPageTitle('myMoney Calendar');
-    uiStore.setSubtitle(uiStore.currentMonth.get('monthLong'));
-  }, [location, params]);
+    uiStore.setSubtitle(uiStore.currentMonth.toFormat('MMMM, y'));
+  }, [location, params, uiStore.currentMonth]);
 
   return (
     <section className="calendar">
       <h1>{uiStore.pageTitle}</h1>
       <h2>{uiStore.subtitle}</h2>
+
+      <nav className="calendar__nav">
+        <Button icon={arrowLeft} iconSide="left" onClick={prevMonth}>
+          Previous
+        </Button>
+        <Button icon={arrowRight} iconSide="right" onClick={nextMonth}>
+          Next
+        </Button>
+      </nav>
 
       <div className="calendar__rows">
         {uiStore.monthCalendarRows.map(({ days, weekNumber }) => <CalendarWeekRow days={days} key={`week-${weekNumber}`} />)}
