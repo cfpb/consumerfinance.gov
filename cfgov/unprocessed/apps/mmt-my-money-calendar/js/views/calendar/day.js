3109d1ed62746d7104b9002d88e63cf5a33b7352
Mike Green <mike.green@simplethread.com>
2020-01-30 09:49:13 -0500

    Add calendar view and supporting code
    Calculating balances is not accurate yet

---
+++ cfgov/unprocessed/apps/mmt-my-money-calendar/js/views/calendar/day.js
@@ -0,0 +1,39 @@
+import clsx from 'clsx';
+import { useCallback } from 'react';
+import { observer } from 'mobx-react';
+import { DateTime } from 'luxon';
+import { useStore } from '../../stores';
+import { formatCurrency } from '../../lib/currency-helpers';
+
+function Day({ day, dateFormat = 'd' }) {
+  const { uiStore, eventStore } = useStore();
+
+  const handleClick = useCallback(
+    (evt) => {
+      evt.preventDefault();
+      console.log('day clicked: %O', day);
+    },
+    [day]
+  );
+
+  const balance = eventStore.getBalanceForDate(day);
+
+  const classes = clsx('calendar__day', {
+    today: day.hasSame(DateTime.local(), 'day'),
+    selected: uiStore.selectedDate && day.hasSame(uiStore.selectedDate, 'day'),
+    'current-month': day.hasSame(uiStore.currentMonth, 'month'),
+    'pos-balance': balance > 0,
+    'neg-balance': balance < 0,
+  });
+
+  return (
+    <div className={classes} aria-role="button" onClick={handleClick}>
+      <div className="calendar__day-number">
+        <time datetime={day.toFormat('y-MM-dd')} className="calendar__day-datetime">{day.toFormat(dateFormat)}</time>
+      </div>
+      <div className="calendar__day-balance">{formatCurrency(balance)}</div>
+    </div>
+  );
+}
+
+export default observer(Day);
