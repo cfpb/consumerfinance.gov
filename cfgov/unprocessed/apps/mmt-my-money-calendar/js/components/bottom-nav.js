4c43496bec8ca7a15d1aac4ad876868295361e63
Mike Green <mike.green@simplethread.com>
2020-02-12 14:07:52 -0500

    Add bottom nav component markup

---
+++ cfgov/unprocessed/apps/mmt-my-money-calendar/js/components/bottom-nav.js
@@ -0,0 +1,31 @@
+import { useCallback } from 'react';
+import { useHistory, useLocation, NavLink } from 'react-router-dom';
+
+import calendar from '@cfpb/cfpb-icons/src/icons/calendar.svg';
+import add from '@cfpb/cfpb-icons/src/icons/add.svg';
+import lightbulb from '@cfpb/cfpb-icons/src/icons/lightbulb.svg';
+import menu from '@cfpb/cfpb-icons/src/icons/menu.svg';
+
+const NavItem = ({ href, icon, label, ...params }) => (
+  <li className="bottom-nav__item">
+    <NavLink className="bottom-nav__link" to={href} {...params}>
+      <div className="bottom-nav__link-icon" dangerouslySetInnerHTML={{ __html: icon }} />
+      <div className="bottom-nav__link-label">{label}</div>
+    </NavLink>
+  </li>
+);
+
+export default function BottomNav() {
+  return (
+    <footer className="bottom-nav">
+      <nav className="bottom-nav__nav">
+        <ul className="bottom-nav__items">
+          <NavItem href="/calendar" icon={calendar} label="Calendar" />
+          <NavItem href="/calendar/add" icon={add} label="Income/Expense" />
+          <NavItem href="/strategies" icon={lightbulb} label="Strategies" />
+          <NavItem href="/more" icon={menu} label="More" />
+        </ul>
+      </nav>
+    </footer>
+  );
+}
