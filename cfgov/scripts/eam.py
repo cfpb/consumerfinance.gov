eb3a8db9f14d8168a4397063426266fd0b4fc0be
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2020-02-12 17:53:49 -0800

    kwargs new page

--- cfgov/scripts/eam.py
+++ cfgov/scripts/eam.py
@@ -1,7 +1,10 @@
 from __future__ import unicode_literals
 
 from v1.models import DocumentDetailPage
+from v1.models.learn_page import EnforcementActionPage
 from v1.util.migrations import get_stream_data, set_stream_data
+
+import json
 
 
 def update_sidefoot():
@@ -13,9 +16,17 @@ def update_sidefoot():
         if 'policy-compliance/enforcement/actions' not in url:
             continue
 
-        stream_data = get_stream_data(page, 'sidefoot')
+        keys = vars(page)
+        del keys['_state']
+        del keys['_wagtail_cached_site_root_paths']
 
-        print('\n'.join(vars(page).keys()))
+        page.delete()
+
+        eap = EnforcementActionPage.objects.create(**keys)
+        print(eap)
+
+
+        #print('\n'.join(vars(page).keys()))
         break
 
 def run():
