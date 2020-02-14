9b1ca6b069b6b71552179d2bdfcdb492ad278963
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2020-02-13 21:22:50 -0800

    content and variable functionality

--- cfgov/scripts/eam.py
+++ cfgov/scripts/eam.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 from django.contrib.contenttypes.models import ContentType
-from v1.models import DocumentDetailPage
+from v1.models import DocumentDetailPage, BrowseFilterablePage
 from v1.models.learn_page import EnforcementActionPage
 from v1.util.migrations import get_stream_data, set_stream_data
 
@@ -9,21 +9,22 @@ import json
 
 
 def update_sidefoot():
+    enforcement_actions = BrowseFilterablePage.objects.get(pk=1327)
     for page in DocumentDetailPage.objects.all():
         url = page.get_url()
 
-        if not page.live:
+        if not url or 'policy-compliance/enforcement/actions' not in url:
             continue
-        if 'policy-compliance/enforcement/actions' not in url:
+        if 'enforcement-action-definitions' in url:
             continue
 
         keys = vars(page)
         
         page.delete()
 
-        eap = EnforcementActionPage.objects.create(
-            path = keys['path'],
+        eap = EnforcementActionPage(
             depth = keys['depth'],
+            live = keys['live'],
             latest_revision_created_at = keys['latest_revision_created_at'],
             slug = keys['slug'],
             title = keys['title'],
@@ -36,6 +37,15 @@ def update_sidefoot():
             status = 'Post Order/Post Judgment',
             institution_type = 'Nonbank'
         )
+
+        enforcement_actions.add_child(instance=eap)
+        
+        rev = eap.save_revision()
+        eap.latest_revision_created_at = keys['latest_revision_created_at']
+        eap.save()
+
+        if keys['live']:
+          rev.publish()
 
 
 def run():
