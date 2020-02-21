dd4ddaa9e9146ebc556fa3d54edb0348432ab769
Richard Dinh <richard.dinh@cfpb.gov>
2020-02-21 12:54:37 -0500

    eslint nag fix

--- cfgov/unprocessed/apps/ccdb-landing-map/js/TileMap.js
+++ cfgov/unprocessed/apps/ccdb-landing-map/js/TileMap.js
@@ -4,9 +4,8 @@ import Highcharts from 'highcharts/highmaps';
 import accessibility from 'highcharts/modules/accessibility';
 import getTileMapState from 'cfpb-chart-builder/src/js/utils/get-tile-map-state';
 
-// ----------------------------------------------------------------------------
-// Utility Functions
-
+/* ----------------------------------------------------------------------------
+   Utility Functions */
 /**
 * A reducer function to process the maximum value in the state complaint data
 *
@@ -20,9 +19,9 @@ function findMaxComplaints( accum, stateComplaint ) {
 
 /**
  * helper function to get the bins for legend and colors, etc.
- * @param data
+ * @param {Array} data all of the states w/ displayValue, complaintCount, raw
  * @param {Array} colors an array of colors
- * @returns {[]|Array}
+ * @returns {Array} the bins with bounds, name, and color
  */
 function getBins( data, colors ) {
   const binCount = colors.length;
@@ -32,12 +31,12 @@ function getBins( data, colors ) {
   // Early exit
   if ( max === 0 ) return [];
 
-  const bins = [ { from: 0, to: 1, color: '#fff', name: 'N/A' } ];
+  const bins = [];
   const step = ( max - min + 1 ) / binCount;
 
   for ( let i = 0, curr = min; i < binCount; i++, curr += step ) {
     const minValue = Math.round( curr );
-    const displayValue = Math.round( curr/1000 );
+    const displayValue = Math.round( curr / 1000 );
 
     bins.push( {
       from: minValue,
@@ -48,16 +47,17 @@ function getBins( data, colors ) {
   }
 
   // The last bin is unbounded
+  // eslint-disable-next-line no-undefined
   bins[binCount - 1].to = undefined;
 
   return bins;
 }
 
 /**
- * helper function to get the bins for legend and colors, etc.
- * @param data
+ * helper function to get the per Capita bins for legend and colors, etc.
+ * @param {Array} data all of the states w/ displayValue, complaintCount, raw
  * @param {Array} colors an array of colors
- * @returns {[]|Array}
+ * @returns {Array} contains bins with bounds, colors, name, and color
  */
 function getPerCapitaBins( data, colors ) {
   const binCount = colors.length;
@@ -71,60 +71,68 @@ function getPerCapitaBins( data, colors ) {
   const step = ( max - min ) / binCount;
 
   for ( let i = 0, curr = min; i < binCount; i++, curr += step ) {
-    curr = parseFloat(curr.toFixed(2))
+    curr = parseFloat( curr.toFixed( 2 ) );
     const minValue = curr;
     const displayValue = curr;
 
     bins.push( {
       from: minValue,
-      to: parseFloat((curr + step).toFixed(2)),
+      to: parseFloat( ( curr + step ).toFixed( 2 ) ),
       color: colors[i],
       name: displayValue > 0 ? `≥ ${ displayValue }` : '≥ 0'
     } );
   }
 
   // The last bin is unbounded
+  // eslint-disable-next-line no-undefined
   bins[binCount - 1].to = undefined;
 
   return bins;
 }
 
-// ----------------------------------------------------------------------------
-// Utility Functions 2
+/* ----------------------------------------------------------------------------
+   Utility Functions 2 */
 /**
  * @param {Object} data - Data to process. add in state paths to the data obj
+ * @param {Array} bins - contains different buckets for the values
  * @returns {Object} The processed data.
  */
-function processMapData( data , bins ) {
+function processMapData( data, bins ) {
   // Filter out any empty values just in case
   data = data.filter( function( row ) {
     return Boolean( row.name );
   } );
 
-  data = data.map( function( obj, i ) {
+  data = data.map( function( obj ) {
     const state = getTileMapState[obj.name];
     return {
       ...obj,
-      color: getColorByValue(obj.displayValue, bins),
+      color: getColorByValue( obj.displayValue, bins ),
       path: state.path
-
     };
   } );
 
   return data;
 }
 
-function getColorByValue(value, bins) {
+/**
+ * helper function to search color in the bins
+ * @param {number} value the number of complaints or perCapita
+ * @param {array} bins contains bin objects
+ * @returns {string} color hex or rgb code for a color
+ */
+function getColorByValue( value, bins ) {
   let color = '#ffffff';
-  for (let i = 0; i < bins.length; i++) {
-    if (value > bins[i].from) {
+  for ( let i = 0; i < bins.length; i++ ) {
+    if ( value > bins[i].from ) {
       color = bins[i].color;
     }
   }
   return color;
 }
-// ----------------------------------------------------------------------------
-// Highcharts callbacks
+
+/* ----------------------------------------------------------------------------
+   Highcharts callbacks */
 
 /**
  * Draw a legend on a chart.
@@ -138,26 +146,27 @@ function _drawLegend( chart ) {
   const boxHeight = 15;
   const boxPadding = 1;
 
-  // args: (str, x, y, shape, anchorX, anchorY, useHTML, baseline, className)
-  // const labelTx = 'Map shading: Complaints';
-  // chart.renderer
-  //   .label( labelTx, 5, 5, null, null, null, true, false, 'label__tile-map' )
-  //   .add();
-
+  /* args: (str, x, y, shape, anchorX, anchorY, useHTML, baseline, className)
+     const labelTx = 'Map shading: Complaints';
+     chart.renderer
+       .label( labelTx, 5, 5, null, null, null, true, false, 'label__tile-map' )
+       .add(); */
+  /* https://api.highcharts.com/class-reference/Highcharts.SVGRenderer#label
+     boxes and labels for legend buckets */
   const legend = chart.renderer.g( 'legend__tile-map' )
-    .translate(10, marginTop)
+    .translate( 10, marginTop )
     .add();
 
-  for (let i = 0; i < bins.length; i++) {
+  for ( let i = 0; i < bins.length; i++ ) {
     const g = chart.renderer.g( `g${ i }` )
-      .translate(i * (boxWidth + boxPadding), 0)
-      .add(legend);
+      .translate( i * ( boxWidth + boxPadding ), 0 )
+      .add( legend );
 
     const bin = bins[i];
 
     chart.renderer
       .rect( 0, 0, boxWidth, boxHeight )
-      .attr( { 'fill': bin.color } )
+      .attr( { fill: bin.color } )
       .addClass( 'legend-box' )
       .add( g );
 
@@ -168,8 +177,52 @@ function _drawLegend( chart ) {
   }
 }
 
-// ----------------------------------------------------------------------------
-// Tile Map class
+/**
+ * callback function to format the individual tiles in HTML
+ * @returns {string} html output
+ */
+function tileFormatter() {
+  const value = this.point.displayValue.toLocaleString();
+  return '<div class="highcharts-data-label-state">' +
+    '<span class="abbr">' + this.point.name + '</span>' +
+    '<br />' +
+    '<span class="value">' + value + '</span>' +
+    '</div>';
+}
+
+/**
+ * callback function to format the tooltip in HTML
+ * @returns {string} html output
+ */
+function tooltipFormatter() {
+  const product = this.product ? '<div class="row u-clearfix">' +
+    '<p class="u-float-left">Product with highest complaint volume</p>' +
+    '<p class="u-right">' + this.product + '</p>' +
+    '</div>' : '';
+
+  const issue = this.issue ? '<div class="row u-clearfix">' +
+    '<p class="u-float-left">Issue with highest complaint volume</p>' +
+    '<p class="u-right">' + this.issue + '</p>' +
+    '</div>' : '';
+
+  const value = this.value.toLocaleString();
+  const perCapita = this.perCapita ? '<div class="row u-clearfix">' +
+    '<p class="u-float-left">Per capita</p>' +
+    '<p class="u-right">' + this.perCapita + '</p>' +
+    '</div>' : '';
+
+  return '<div class="title">' + this.fullName + '</div>' +
+    '<div class="row u-clearfix">' +
+    '<p class="u-float-left">Complaints</p>' +
+    '<p class="u-right">' + value + '</p>' +
+    '</div>' +
+    perCapita +
+    product +
+    issue;
+}
+
+/* ----------------------------------------------------------------------------
+   Tile Map class */
 
 accessibility( Highcharts );
 
@@ -179,25 +232,23 @@ Highcharts.setOptions( {
   }
 } );
 
-// ----------------------------------------------------------------------------
-// Tile Map class
+const colors = [
+  'rgba(247, 248, 249, 0.5)',
+  'rgba(212, 231, 230, 0.5)',
+  'rgba(180, 210, 209, 0.5)',
+  'rgba(137, 182, 181, 0.5)',
+  'rgba(86, 149, 148, 0.5)',
+  'rgba(37, 116, 115, 0.5)'
+];
+
+/* ----------------------------------------------------------------------------
+   Tile Map class */
 
 class TileMap {
-  constructor( { el, description, data } ) {
-    // console.log(data);
-    const colors = [
-      'rgba(247, 248, 249, 0.5)',
-      'rgba(212, 231, 230, 0.5)',
-      'rgba(180, 210, 209, 0.5)',
-      'rgba(137, 182, 181, 0.5)',
-      'rgba(86, 149, 148, 0.5)',
-      'rgba(37, 116, 115, 0.5)'
-    ];
-    //const bins = getBins( data, colors );
-    const bins = getPerCapitaBins( data, colors );
-    data = processMapData(data, bins);
-
-    const localize = true;
+  constructor( { el, description, data, isPerCapita } ) {
+    const bins = isPerCapita ? getPerCapitaBins( data, colors ) :
+      getBins( data, colors );
+    data = processMapData( data, bins );
 
     const options = {
       bins,
@@ -219,47 +270,14 @@ class TileMap {
         className: 'tooltip',
         enabled: true,
         headerFormat: '',
-        pointFormatter: function() {
-          const product = this.product ? '<div class="row u-clearfix">' +
-            '<p class="u-float-left">Product with highest complaint volume</p>' +
-            '<p class="u-right">' + this.product + '</p>' +
-            '</div>' : '';
-
-          const issue = this.issue ? '<div class="row u-clearfix">' +
-            '<p class="u-float-left">Issue with highest complaint volume</p>' +
-            '<p class="u-right">' + this.issue + '</p>' +
-            '</div>' : '';
-
-          const value = localize ? this.value.toLocaleString() : this.value;
-          const perCapita = this.perCapita ? '<div class="row u-clearfix">' +
-            '<p class="u-float-left">Per capita</p>' +
-            '<p class="u-right">' + this.perCapita + '</p>' +
-            '</div>' : '';
-
-          return '<div class="title">' + this.fullName + '</div>' +
-            '<div class="row u-clearfix">' +
-              '<p class="u-float-left">Complaints</p>' +
-              '<p class="u-right">' + value + '</p>' +
-            '</div>' +
-            perCapita +
-            product +
-            issue;
-        },
+        pointFormatter: tooltipFormatter,
         useHTML: true
       },
       plotOptions: {
         series: {
           dataLabels: {
             enabled: true,
-            formatter: function() {
-              // are we using perCapita or value?
-              const value = this.point.displayValue.toLocaleString();
-              return '<div class="highcharts-data-label-state">' +
-                '<span class="abbr">' + this.point.name + '</span>' +
-                '<br />' +
-                '<span class="value">' + value + '</span>' +
-                '</div>';
-            },
+            formatter: tileFormatter,
             useHTML: true
           }
         }
--- cfgov/unprocessed/apps/ccdb-landing-map/js/landing-map.js
+++ cfgov/unprocessed/apps/ccdb-landing-map/js/landing-map.js
@@ -5,28 +5,26 @@ import TileMap from './TileMap.js';
  *  Replace the functionality in cfpb-chart-builder/src/js/index.js
  */
 class Chart {
-
   constructor( chartOptions ) {
     this.chartOptions = chartOptions;
 
     fetch( chartOptions.source )
-    .then( response=> {
-      return response.json();
-    })
-    .then( data => {
-      data = data.map(o => {
-        const perCapita = parseFloat(o.perCapita.toFixed(2));
-        const displayValue = this.chartOptions.perCapita ? perCapita : o.value;
-        return {
-          ...o,
-          displayValue,
-          perCapita
-        };
-      });
+      .then( response => response.json() )
+      .then( data => {
+        data = data.map( o => {
+          const perCapita = parseFloat( o.perCapita.toFixed( 2 ) );
+          const displayValue = this.chartOptions.perCapita ?
+            perCapita : o.value;
+          return {
+            ...o,
+            displayValue,
+            perCapita
+          };
+        } );
 
-      this.chartOptions.data = data;
-      this.draw( this.chartOptions );
-    } );
+        this.chartOptions.data = data;
+        this.draw( this.chartOptions );
+      } );
   }
 
   draw( chartOptions ) {
@@ -34,33 +32,29 @@ class Chart {
   }
 }
 
-
 /**
  * main
  */
 
-function start(perCapita ) {
-  const el = document.getElementById('landing-map');
-  el.querySelectorAll('*').forEach(n => n.remove());
+function start( isPerCapita ) {
+  const el = document.getElementById( 'landing-map' );
+  el.querySelectorAll( '*' ).forEach( n => n.remove() );
 
   const dataUrl = 'https://files.consumerfinance.gov/ccdb/hero-map-3y.json';
   // eslint-disable-next-line no-unused-vars
-  const chart = new Chart({
+  const chart = new Chart( {
     el: el,
     source: dataUrl,
-    title: 'Complaints by State',
-    description: 'The complaints by state for...',
-    perCapita
-  });
+    isPerCapita
+  } );
 }
 
-
-document.getElementsByClassName('capita')[0].onclick = () => {
-  start(true);
+document.getElementsByClassName( 'capita' )[0].onclick = () => {
+  start( true );
 };
 
-document.getElementsByClassName('raw')[0].onclick = () => {
-  start(false);
+document.getElementsByClassName( 'raw' )[0].onclick = () => {
+  start( false );
 };
 
 start( false );
