ac6359c9536dd5b90d54f0150378127e5667e195
Anselm Bradford <anselmbradford@users.noreply.github.com>
2020-03-11 13:24:20 -0400

    Mock window resizing

--- test/unit_tests/js/modules/util/breakpoint-state-spec.js
+++ test/unit_tests/js/modules/util/breakpoint-state-spec.js
@@ -9,6 +9,20 @@ import varsBreakpoints from '@cfpb/cfpb-core/src/vars-breakpoints';
 
 let configKeys;
 
+/**
+ * Change the viewport to width x height. Mocks window.resizeTo( w, h ).
+ * @param  {number} width - width in pixels.
+ * @param  {number} height - height in pixels.
+ */
+function windowResizeTo( width, height ) {
+  // Change the viewport to width x height. Mocks window.resizeTo( w, h ).
+  global.innerWidth = width;
+  global.innerHeight = height;
+
+  // Trigger the window resize event.
+  global.dispatchEvent( new Event( 'resize' ) );
+}
+
 describe( 'breakpoint-state', () => {
   beforeEach( () => {
     configKeys = Object.keys( varsBreakpoints );
@@ -61,30 +75,24 @@ describe( 'breakpoint-state', () => {
 
   describe( '.viewportIsIn()', () => {
     it( 'should determine whether inside desktop breakpoint threshold', () => {
+      windowResizeTo( 1200, 800 );
       expect( viewportIsIn( DESKTOP ) ).toBe( true );
-
-      /* TODO:
-         Mock getBreakpointState() to return a small breakpoint size,
-         so that viewportIsIn( DESKTOP ) returns false and can be tested with
-         expect( viewportIsIn( DESKTOP ) ).toBe( false ); */
+      windowResizeTo( 599, 800 );
+      expect( viewportIsIn( DESKTOP ) ).toBe( false );
     } );
 
     it( 'should determine whether inside tablet breakpoint threshold', () => {
+      windowResizeTo( 1200, 800 );
       expect( viewportIsIn( TABLET ) ).toBe( false );
-
-      /* TODO:
-         Mock getBreakpointState() to return a tablet breakpoint size,
-         so that viewportIsIn( TABLET ) returns true and can be tested with
-         expect( viewportIsIn( TABLET ) ).toBe( true ); */
+      windowResizeTo( 899, 800 );
+      expect( viewportIsIn( TABLET ) ).toBe( true );
     } );
 
     it( 'should determine whether inside tablet breakpoint threshold', () => {
+      windowResizeTo( 1200, 800 );
       expect( viewportIsIn( MOBILE ) ).toBe( false );
-
-      /* TODO:
-         Mock getBreakpointState() to return a mobile breakpoint size,
-         so that viewportIsIn( MOBILE ) returns true and can be tested with
-         expect( viewportIsIn( MOBILE ) ).toBe( true ); */
+      windowResizeTo( 599, 800 );
+      expect( viewportIsIn( MOBILE ) ).toBe( true );
     } );
   } );
 
