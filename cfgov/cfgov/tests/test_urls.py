ed68001e786aef0c7fd2e009a110295e9d007df2
Craig Davies <craig.davies@cfpb.gov>
2020-03-24 13:21:31 -0400

    Fix test urls

--- cfgov/cfgov/tests/test_urls.py
+++ cfgov/cfgov/tests/test_urls.py
@@ -8,7 +8,7 @@ from cfgov import urls
 
 
 try:
-    from django.urls import re_path, RegexURLPattern, RegexURLResolver
+    from django.urls import re_path
 except ImportError:
     from django.conf.urls import url as re_path
     from django.core.urlresolvers import RegexURLPattern, RegexURLResolver
