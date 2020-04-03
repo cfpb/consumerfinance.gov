2111300328f3674990687818737a37f210795b59
Craig Davies <craig.davies@cfpb.gov>
2020-04-03 18:20:51 -0400

    Update local settings for Django 2

--- cfgov/cfgov/settings/local.py
+++ cfgov/cfgov/settings/local.py
@@ -18,95 +18,89 @@ if sys.version_info[0] < 3:
     )
 
 DEBUG = True
-SECRET_KEY = 'not-secret-key-for-testing'
+SECRET_KEY = "not-secret-key-for-testing"
 
 INSTALLED_APPS += (
-    'sslserver',
-    'wagtail.contrib.styleguide',
+    "sslserver",
+    "wagtail.contrib.styleguide",
 )
 
-STATIC_ROOT = REPOSITORY_ROOT.child('collectstatic')
+STATIC_ROOT = REPOSITORY_ROOT.child("collectstatic")
 
-ALLOW_ADMIN_URL = DEBUG or os.environ.get('ALLOW_ADMIN_URL', False)
+ALLOW_ADMIN_URL = DEBUG or os.environ.get("ALLOW_ADMIN_URL", False)
 
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-        }
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {"level": "DEBUG", "class": "logging.StreamHandler",}
     },
-    'loggers': {
-        '': {
-            'handlers': ['console'],
-            'level': 'INFO',
-            'propagate': True,
-        }
-    }
+    "loggers": {
+        "": {"handlers": ["console"], "level": "INFO", "propagate": True,}
+    },
 }
 
-# Log database queries.
-if os.environ.get('ENABLE_SQL_LOGGING'):
-    LOGGING['loggers']['django.db.backends'] = {
-        'handlers': ['console'],
-        'level': 'DEBUG',
-        'propagate': False,
+# Log database queries
+if os.environ.get("ENABLE_SQL_LOGGING"):
+    LOGGING["loggers"]["django.db.backends"] = {
+        "handlers": ["console"],
+        "level": "DEBUG",
+        "propagate": False,
     }
 
 # Django Debug Toolbar
-if os.environ.get('ENABLE_DEBUG_TOOLBAR'):
-    INSTALLED_APPS += ('debug_toolbar',)
+if os.environ.get("ENABLE_DEBUG_TOOLBAR"):
+    INSTALLED_APPS += ("debug_toolbar",)
 
     if django.VERSION < (2, 0):
-        MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+        MIDDLEWARE_CLASSES += (
+            "debug_toolbar.middleware.DebugToolbarMiddleware",
+        )
         MIDDLEWARE_CLASSES += CSP_MIDDLEWARE
     else:
-        MIDDLEWARE += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+        MIDDLEWARE += ("debug_toolbar.middleware.DebugToolbarMiddleware",)
         MIDDLEWARE += CSP_MIDDLEWARE
 
     DEBUG_TOOLBAR_PANELS = [
-        'debug_toolbar.panels.versions.VersionsPanel',
-        'debug_toolbar.panels.timer.TimerPanel',
-        'debug_toolbar.panels.settings.SettingsPanel',
-        'debug_toolbar.panels.headers.HeadersPanel',
-        'debug_toolbar.panels.request.RequestPanel',
-        'debug_toolbar.panels.sql.SQLPanel',
-        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-        'debug_toolbar.panels.templates.TemplatesPanel',
-        'debug_toolbar.panels.cache.CachePanel',
-        'debug_toolbar.panels.signals.SignalsPanel',
-        'debug_toolbar.panels.logging.LoggingPanel',
-        'debug_toolbar.panels.redirects.RedirectsPanel',
-        'flags.panels.FlagsPanel',
-        'flags.panels.FlagChecksPanel',
+        "debug_toolbar.panels.versions.VersionsPanel",
+        "debug_toolbar.panels.timer.TimerPanel",
+        "debug_toolbar.panels.settings.SettingsPanel",
+        "debug_toolbar.panels.headers.HeadersPanel",
+        "debug_toolbar.panels.request.RequestPanel",
+        "debug_toolbar.panels.sql.SQLPanel",
+        "debug_toolbar.panels.staticfiles.StaticFilesPanel",
+        "debug_toolbar.panels.templates.TemplatesPanel",
+        "debug_toolbar.panels.cache.CachePanel",
+        "debug_toolbar.panels.signals.SignalsPanel",
+        "debug_toolbar.panels.logging.LoggingPanel",
+        "debug_toolbar.panels.redirects.RedirectsPanel",
+        "flags.panels.FlagsPanel",
+        "flags.panels.FlagChecksPanel",
     ]
 
     DEBUG_TOOLBAR_CONFIG = {
-        'SHOW_COLLAPSED': True,
-        'SHOW_TOOLBAR_CALLBACK': lambda request: DEBUG,
+        "SHOW_COLLAPSED": True,
+        "SHOW_TOOLBAR_CALLBACK": lambda request: DEBUG,
     }
 
-# Disable caching when working locally.
+# Disable caching when working locally
 CACHES = {
     k: {
-        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
-        'TIMEOUT': 0,
-    } for k in (
-        'default', 'post_preview'
-    )
+        "BACKEND": "django.core.cache.backends.dummy.DummyCache",
+        "TIMEOUT": 0,
+    }
+    for k in ("default", "post_preview")
 }
 
 # Optionally enable cache for post_preview
-if os.environ.get('ENABLE_POST_PREVIEW_CACHE'):
-    CACHES['post_preview'] = {
-        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
-        'LOCATION': 'post_preview_cache',
-        'TIMEOUT': None,
+if os.environ.get("ENABLE_POST_PREVIEW_CACHE"):
+    CACHES["post_preview"] = {
+        "BACKEND": "django.core.cache.backends.db.DatabaseCache",
+        "LOCATION": "post_preview_cache",
+        "TIMEOUT": None,
     }
 
 # Use a mock GovDelivery API instead of the real thing,
 # unless the GOVDELIVERY_BASE_URL environment variable is set.
-if not os.environ.get('GOVDELIVERY_BASE_URL'):
-    GOVDELIVERY_API = 'core.govdelivery.LoggingMockGovDelivery'
+if not os.environ.get("GOVDELIVERY_BASE_URL"):
+    GOVDELIVERY_API = "core.govdelivery.LoggingMockGovDelivery"
