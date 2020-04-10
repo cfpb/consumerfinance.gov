e057c7e6407cfb3eb2f31d1e72c81a63187ffcb6
Richard Dinh <richard.dinh@cfpb.gov>
2020-04-10 12:50:35 -0400

    update test coverage

--- test/unit_tests/apps/ccdb-landing-map/js/TileMap-spec.js
+++ test/unit_tests/apps/ccdb-landing-map/js/TileMap-spec.js
@@ -99,6 +99,23 @@ describe( 'Tile map', () => {
     const result = sut.tileFormatter();
     expect( result )
       .toEqual( '<div class="highcharts-data-label-state tile-FA ">' +
+      '<span class="abbr">FA</span>' +
+      '<span class="value">10,000</span></div>' );
+  } );
+
+  it( 'formats a map tile in Internet Explorer', () => {
+    sut.point = {
+      className: 'selected',
+      displayValue: 10000,
+      name: 'FA'
+    };
+
+    Object.defineProperty( window.navigator, 'userAgent',
+      { value: 'MSIE' } )
+
+    const result = sut.tileFormatter();
+    expect( result )
+    .toEqual( '<div class="highcharts-data-label-state tile-FA ">' +
       '<span class="abbr">FA</span><br />' +
       '<span class="value">10,000</span></div>' );
   } );
@@ -271,4 +288,151 @@ describe( 'Tile map', () => {
 
     expect( drawSpy ).toHaveBeenCalled();
   } );
+
+  it( 'shortens long numbers' , () => {
+    const cases = [
+      { n: 0, e: '0' },
+      { n: 7, e: '7' },
+      { n: 42, e: '42' },
+      { n: 567, e: '567' },
+      { n: 3456, e: '3.4K' },
+      { n: 9873, e: '9.8K' },
+      { n: 23456, e: '23K' },
+      { n: 98765, e: '98K' },
+      { n: 234567, e: '234K' },
+      { n: 782345, e: '782K' },
+      { n: 1450000, e: '1.4M' },
+      { n: 9870000, e: '9.8M' },
+    ]
+
+    cases.forEach( x => {
+      expect(sut.makeShortName(x.n)).toEqual(x.e);
+    })
+  } );
+
+  it( 'formats a series point for voice-over reading' , () => {
+    const point = {
+      fullName: 'Foo',
+      displayValue: '13'
+    }
+    const actual = sut.pointDescriptionFormatter( point );
+    expect( actual ).toEqual( 'Foo 13' );
+  } );
+
+
+  describe( 'generating bins', () => {
+    let scaleFn
+    beforeEach( () => {
+      scaleFn = jest.fn( x => x )
+    } );
+
+    it( 'gets complaints bins - All', () => {
+      const quantiles = [
+        880.2857142857142,
+        1622.5714285714282,
+        3064.9999999999995,
+        6136.714285714284,
+        7788.142857142858,
+        13909.714285714286
+      ]
+      const expected = [
+        { from: 0, color: "#ffffff", name: "≥ 0", shortName: "≥ 0"},
+        { from: 880, color: 881, name: "> 880", shortName: "> 880"},
+        { from: 1623, color: 1623, name: "≥ 1,623", shortName: "≥ 1.6K"},
+        { from: 3065, color: 3065, name: "≥ 3,065", shortName: "≥ 3.0K"},
+        { from: 6137, color: 6137, name: "≥ 6,137", shortName: "≥ 6.1K"},
+        { from: 7788, color: 7789, name: "> 7,788", shortName: "> 7.7K"},
+        { from: 13910, color: 13910, name: "≥ 13,910", shortName: "≥ 13K"}
+      ]
+
+      const result = sut.getBins( quantiles, scaleFn );
+      expect( result ).toEqual( expected )
+    } );
+
+    it( 'gets complaints bins - one zip code', () => {
+      const quantiles = [
+        0.2857142857142857,
+        0.5714285714285714,
+        0.8571428571428571,
+        183.99999999999991,
+        550,
+        915.9999999999999,
+      ]
+      const expected = [
+        { from: 0, color: "#ffffff", name: "≥ 0", shortName: "≥ 0"},
+        { from: 1, color: 1, name: "≥ 1", shortName: "≥ 1"},
+        { from: 184, color: 184, name: "≥ 184", shortName: "≥ 184"},
+        { from: 550, color: 550, name: "≥ 550", shortName: "≥ 550"},
+        { from: 916, color: 916, name: "≥ 916", shortName: "≥ 916"},
+      ]
+
+      const result = sut.getBins( quantiles, scaleFn );
+      expect( result ).toEqual( expected )
+    } );
+
+    it( 'gets complaints bins - max 2 complaints', () => {
+      const quantiles = [
+        0.2857142857142857,
+        0.5714285714285714,
+        0.8571428571428571,
+        1.1428571428571428,
+        1.4285714285714286,
+        1.7142857142857142
+      ]
+      const expected = [
+        { from: 0, color: "#ffffff", name: "≥ 0", shortName: "≥ 0"},
+        { from: 1, color: 1, name: "≥ 1", shortName: "≥ 1"},
+        { from: 2, color: 2, name: "≥ 2", shortName: "≥ 2"},
+      ]
+
+      const result = sut.getBins( quantiles, scaleFn );
+      expect( result ).toEqual( expected )
+    } );
+
+    it( 'gets complaints bins - max 1 complaint', () => {
+      const quantiles = [
+        0.14285714285714285,
+        0.2857142857142857,
+        0.42857142857142855,
+        0.5714285714285714,
+        0.7142857142857143,
+        0.8571428571428571
+      ]
+      const expected = [
+        { from: 0, color: "#ffffff", name: "≥ 0", shortName: "≥ 0"},
+        { from: 1, color: 1, name: "≥ 1", shortName: "≥ 1"}
+      ]
+
+      const result = sut.getBins( quantiles, scaleFn );
+      expect( result ).toEqual( expected )
+    } );
+
+    it( 'gets Per 1000 population bins', () => {
+      const quantiles = [
+        1.1928571428571428,
+        1.4657142857142857,
+        1.81,
+        2.0357142857142856,
+        2.33,
+        2.845714285714285
+      ]
+      const expected = [
+        { from: 0, color: "#ffffff", name: "≥ 0", shortName: "≥ 0"},
+        { from: 1.19, color: quantiles[0], name: "> 1.19", shortName: "> 1.19"},
+        { from: 1.46, color: quantiles[1], name: "> 1.46", shortName: "> 1.46"},
+        { from: 1.81, color: quantiles[2], name: "≥ 1.81", shortName: "≥ 1.81"},
+        { from: 2.03, color: quantiles[3], name: "> 2.03", shortName: "> 2.03"},
+        { from: 2.33, color: quantiles[4], name: "≥ 2.33", shortName: "≥ 2.33"},
+        { from: 2.84, color: quantiles[5], name: "> 2.84", shortName: "> 2.84"}
+      ]
+
+      const result = sut.getPerCapitaBins( quantiles, scaleFn );
+      expect( result ).toEqual( expected )
+    } );
+  })
+
+  it('handles no values for colors', ()=>{
+    const color = sut.getColorByValue(null, null)
+    expect(color).toEqual('#ffffff')
+  })
 } );
