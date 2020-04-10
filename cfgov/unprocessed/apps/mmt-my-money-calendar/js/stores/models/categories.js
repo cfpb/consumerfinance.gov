c295e91174969a5579228b21253c1d4add681bf9
Mike Green <mike.green@simplethread.com>
2020-04-10 09:41:40 -0400

    Add id property to strategies to allow deduplication of identical recommendations in different categories

--- cfgov/unprocessed/apps/mmt-my-money-calendar/js/stores/models/categories.js
+++ cfgov/unprocessed/apps/mmt-my-money-calendar/js/stores/models/categories.js
@@ -78,6 +78,7 @@ export const Categories = new CategoryTree({
       description: 'Income from employment',
       recurrenceTypes: ['weekly', 'biweekly', 'monthly', 'semimonthly'],
       strategy: {
+        id: 'directDeposit',
         title: 'Sign Up for Direct Deposit',
         body: 'Direct deposit may help you to avoid fees and interest associated with Check Cashing.',
         link:  {
@@ -92,6 +93,7 @@ export const Categories = new CategoryTree({
         name: 'Veterans Benefits',
         recurrenceTypes: ['monthly'],
         strategy: {
+          id: 'vetBenefits',
           title: 'Explore CFPB\'s Military Financial Resources',
           body: 'These tools, designed specifically for service members, can help you manage financial challenges at every step of your military career.',
           link: {
@@ -104,6 +106,7 @@ export const Categories = new CategoryTree({
         name: 'Disability Benefits',
         recurrenceTypes: ['monthly'],
         strategy: {
+          id: 'disabilityBenefits',
           title: 'Explore the Focus on People with Disabilities Companion Guides',
           body: 'These guides contains tips, information, tools, and skill-building resources for people with disabilities and from organizations that serve the disability community.',
           link: {
@@ -120,6 +123,7 @@ export const Categories = new CategoryTree({
         name: 'Unemployment',
         recurrenceTypes: ['monthly'],
         strategy: {
+          id: 'jobTraining',
           title: 'Explore CareerOneStop Job Training Opportunities',
           link: {
             href: 'https://www.careeronestop.org/localhelp/americanjobcenters/find-american-job-centers.aspx',
@@ -151,6 +155,7 @@ export const Categories = new CategoryTree({
         recurrenceTypes: ['monthly'],
         hasBill: true,
         strategy: {
+          id: 'refinanceMortgage',
           title: 'Refinance your mortgage',
           body: 'Check with your mortgage lender to see if you qualify, then enter the new expense into the calendar to see how it affects your cash flow.',
           link: {
@@ -187,6 +192,7 @@ export const Categories = new CategoryTree({
         recurrenceTypes: ['monthly'],
         hasBill: true,
         strategy: {
+          id: 'utilityPaymentPlans',
           title: 'Explore Level Payment Plans for Utilities',
           body: 'Also known as budget billing, these plans average your bills out over the year. Check with your utility providers to see if you qualify, then enter the new monthly average into the calendar to see how it affects your cash flow.',
         },
@@ -196,6 +202,7 @@ export const Categories = new CategoryTree({
         recurrenceTypes: ['monthly'],
         hasBill: true,
         strategy: {
+          id: 'utilityPaymentPlans',
           title: 'Explore Level Payment Plans for Utilities',
           body: 'Also known as budget billing, these plans average your bills out over the year. Check with your utility providers to see if you qualify, then enter the new monthly average into the calendar to see how it affects your cash flow.',
         },
@@ -205,6 +212,7 @@ export const Categories = new CategoryTree({
         recurrenceTypes: ['monthly'],
         hasBill: true,
         strategy: {
+          id: 'utilityPaymentPlans',
           title: 'Explore Level Payment Plans for Utilities',
           body: 'Also known as budget billing, these plans average your bills out over the year. Check with your utility providers to see if you qualify, then enter the new monthly average into the calendar to see how it affects your cash flow.',
         },
@@ -219,6 +227,7 @@ export const Categories = new CategoryTree({
         recurrenceTypes: ['monthly'],
         hasBill: true,
         strategy: {
+          id: 'cablePlans',
           title: 'Consider Entertainment Alternatives',
           body: 'Many cable providers offer multiple options for new and existing customers. Contact your provider and ask about lower-cost plans or consider a cheaper streaming service.',
           link: {
@@ -232,6 +241,7 @@ export const Categories = new CategoryTree({
         recurrenceTypes: ['monthly'],
         hasBill: true,
         strategy: {
+          id: 'lifelinePhoneInternet',
           title: 'Explore Low Cost Phone and Internet Services',
           body: 'If you qualify for the FCC\'s "Lifeline" phone rate you could lower the monthly cost of phone and internet service.',
           link: {
@@ -245,6 +255,7 @@ export const Categories = new CategoryTree({
         recurrenceTypes: ['monthly'],
         hasBill: true,
         strategy: {
+          id: 'lifelinePhoneInternet',
           title: 'Explore Low Cost Phone and Internet Services',
           body: 'If you qualify for the FCC\'s "Lifeline" phone rate you could lower the monthly cost of phone and internet service.',
           link: {
@@ -261,6 +272,7 @@ export const Categories = new CategoryTree({
         recurrenceTypes: ['monthly'],
         hasBill: true,
         strategy: {
+          id: 'refinanceCarLoan',
           title: 'Refinance your car loan',
           body: 'Check with your auto lender to see if you qualify, then enter the new expense into the calendar to see how it affects your cash flow.',
           link: {
@@ -273,6 +285,7 @@ export const Categories = new CategoryTree({
         name: 'Car Maintenance',
         hasBill: false,
         strategy: {
+          id: 'carMaintenance',
           title: 'Regularly maintain your car to cut repair costs',
           body: 'Preventive measures (e.g., regularly changing your oil, maintaining proper tire pressure) can help you avoid car repair expenses.',
         },
@@ -282,6 +295,7 @@ export const Categories = new CategoryTree({
         recurrenceTypes: ['monthly'],
         hasBill: true,
         strategy: {
+          id: 'compareInsuranceRates',
           title: 'Compare the Rates of Other Insurance Companies',
           body: 'Most car Insurance providers offer quotes over the phone or online. Research other providers and reenter their quotes into the calendar to see how much you could save.',
         },
@@ -291,6 +305,7 @@ export const Categories = new CategoryTree({
         recurrenceTypes: ['weekly'],
         hasBill: false,
         strategy: {
+          id: 'carpoolRideShare',
           title: 'Carpool or Ride Share',
           body: 'Carpooling or ridesharing can save a lot on fuel costs as well as allow you access to HOV lanes, freeing up money for your budget and shortening your commute.',
         },
@@ -300,6 +315,7 @@ export const Categories = new CategoryTree({
         recurrenceTypes: ['weekly', 'monthly'],
         hasBill: false,
         strategy: {
+          id: 'compareTransportationOptions',
           title: 'Compare Transportation Options',
           body: 'Evaluate your avaliable modes of transportation to discover cheaper alternatives that can put more money in your budget.',
         },
@@ -312,6 +328,7 @@ export const Categories = new CategoryTree({
         recurrenceTypes: ['weekly', 'monthly'],
         hasBill: false,
         strategy: {
+          id: 'reduceFoodExpenses',
           title: 'Reduce your Expenses while eating out',
           body: 'Preparing your lunch, avoiding fountain drinks and even finding local restaurants with specials, like "kids eat free" nights, can help reduce this expense.',
         },
@@ -321,6 +338,7 @@ export const Categories = new CategoryTree({
         recurrenceTypes: ['weekly', 'monthly'],
         hasBill: false,
         strategy: {
+          id: 'reduceGroceryExpenses',
           title: 'Reduce your Grocery Expenses',
           body: 'Using coupons and buying groceries and supplies in bulk with other family or friends can help reduce your grocery costs and put more money in your budget.',
         },
@@ -343,6 +361,7 @@ export const Categories = new CategoryTree({
         recurrenceTypes: ['weekly', 'monthly'],
         hasBill: true,
         strategy: {
+          id: 'cancelSubscriptions',
           title: 'Cancel Unnecessary Subscriptions',
           body: 'Remove auto-renew for subscriptions and cancel those you no longer use or need.',
         },
@@ -351,6 +370,7 @@ export const Categories = new CategoryTree({
         name: 'Clothing',
         hasBill: false,
         strategy: {
+          id: 'secondHandClothing',
           title: 'Consider Second-hand Shops',
           body: 'Buying clothes and accessorits through classifieds ads, thrift shops, and consignment stores are much more cost effective alternatives to retail.',
           link: {
@@ -364,6 +384,7 @@ export const Categories = new CategoryTree({
         recurrenceTypes: ['weekly', 'monthly'],
         hasBill: false,
         strategy: {
+          id: 'trackDonations',
           title: 'Keep track of Your Donations',
           body: 'Your charitable donations my be tax deductible. Keep records and receipts of these exchanges to lower the cost of your annual taxes.',
           link: {
@@ -382,6 +403,7 @@ export const Categories = new CategoryTree({
         recurrenceTypes: ['weekly', 'monthly'],
         hasBill: true,
         strategy: {
+          id: 'childCareAssistance',
           title: 'Get Child Care assistance',
           body: 'There are a number of financial aid programs designed to help parents struggling with childcare costs. Enrolling in these programs could free more money in your budget.',
           link: {
@@ -414,6 +436,7 @@ export const Categories = new CategoryTree({
     debt: {
       name: 'Debt',
       strategy: {
+        id: 'dealWithDebt',
         title: 'Explore CFPB\'s Resources for Dealing With Debt',
         body: 'Whether you\'re about to receive a medical procedure or are having trouble paying your medical bills, there are things you can do to help keep medical debt in check.',
         link: {
@@ -426,6 +449,7 @@ export const Categories = new CategoryTree({
         recurrenceTypes: ['weekly', 'monthly'],
         hasBill: true,
         strategy: {
+          id: 'medicaidCHIP',
           title: 'Sign up for Medicaid and CHIP',
           body: 'Seek help with paying medical bills, because receiving timely medical care can help you maintain your earning potential.',
           link: {
@@ -454,6 +478,7 @@ export const Categories = new CategoryTree({
         recurrenceTypes: ['monthly'],
         hasBill: true,
         strategy: {
+          id: 'studentLoanRepayment',
           title: 'Explore Repayment Options',
           body: 'You have choices when it comes to repaying student loans. Make sure you have the repayment plan that works best for you.',
           link: {
