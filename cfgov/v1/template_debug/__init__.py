121d23aa7b9e5ea8bc90bc253f540115cb042072
Andy Chosak <andy.chosak@cfpb.gov>
2020-04-14 12:08:51 -0400

    Nicer way to register template_debug views

---
+++ cfgov/v1/template_debug/__init__.py
@@ -0,0 +1,24 @@
+from wagtail.core import hooks
+
+from v1.views.template_debug import TemplateDebugView
+
+
+try:
+    from django.urls import re_path
+except ImportError:  # pragma: nocover
+    from django.conf.urls import url as re_path
+
+
+def register_template_debug(app_name, url_path, template_name, test_cases):
+    @hooks.register('register_admin_urls')
+    def register_template_debug_url():
+        return [
+            re_path(
+                rf'^template_debug/{app_name}/{url_path}/',
+                TemplateDebugView.as_view(
+                    debug_template_name=template_name,
+                    debug_test_cases=test_cases
+                ),
+                name=f'template_debug_{app_name}_{url_path}'
+            ),
+        ]
