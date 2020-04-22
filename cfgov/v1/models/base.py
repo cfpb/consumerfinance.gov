3da8920a811f1feb198c915fa93c51fa1699fa57
Craig Davies <craig.davies@cfpb.gov>
2020-04-22 13:05:18 -0400

    Fix DeprecationWarning: The modelcluster.tags module has been moved to modelcluster.contrib.taggit

--- cfgov/v1/models/base.py
+++ cfgov/v1/models/base.py
@@ -20,7 +20,7 @@ from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
 
 from modelcluster.fields import ParentalKey
-from modelcluster.tags import ClusterTaggableManager
+from modelcluster.contrib.taggit import ClusterTaggableManager
 from taggit.models import TaggedItemBase
 from wagtailinventory.helpers import get_page_blocks
 
