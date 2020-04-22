50ca80d2df8f88e122d5ab2c76ced9d643c2c565
Naa Marteki Reed <marteki@users.noreply.github.com>
2020-04-22 11:27:37 -0400

    Apply suggestions from code review
    Co-Authored-By: Scott Cranfill <scott.cranfill@cfpb.gov>

--- cfgov/wellbeing/urls.py
+++ cfgov/wellbeing/urls.py
@@ -8,12 +8,6 @@ except ImportError:
     from django.conf.urls import url as re_path
 
 
-try:
-    from django.urls import re_path
-except ImportError:
-    from django.conf.urls import url as re_path
-
-
 urlpatterns = [
     re_path(
         r'^$',
