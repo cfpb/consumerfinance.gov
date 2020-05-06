037d3ed0a61611960532935ef6a1025daa1d4f4f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-06 15:50:40 +0000

    Deploy cfpb/cfgov-refresh to github.com/cfpb/cfgov-refresh.git:gh-pages

---
+++ samples/sample-spec.js
@@ -0,0 +1,10 @@
+import sample from '../../../../cfgov/unprocessed/js/modules/sample.js';
+
+describe( 'sample', () => {
+
+  it.skip( 'should return a string with expected value', () => {
+    const sampleString = 'Shredder';
+    expect( sample.init() ).toBe( sampleString );
+  } );
+
+} );
