78ee222b09e3c8921b1d46acb92eeb1657fdd36b
Craig Davies <craig.davies@cfpb.gov>
2020-05-22 11:00:09 -0400

    Update Django urls import

--- cfgov/teachers_digital_platform/urls.py
+++ cfgov/teachers_digital_platform/urls.py
@@ -1,12 +1,7 @@
+from django.urls import re_path
 from django.views.generic import TemplateView
 
 from wagtailsharing.views import ServeView
-
-
-try:
-    from django.urls import re_path
-except ImportError:
-    from django.conf.urls import url as re_path
 
 
 urlpatterns = [
