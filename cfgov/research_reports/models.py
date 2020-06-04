fae30cdbeade3f125b782932d543d274deeb3f58
Betsy Lorton <schbetsy@users.noreply.github.com>
2020-06-04 17:23:50 -0400

    Create new sub-app for research reports (#5766)
    Co-authored-by: Betsy Lorton <elizabeth.lorton@cfpb.gov>

---
+++ cfgov/research_reports/models.py
@@ -0,0 +1,3 @@
+# new page models and any supporting models will go here
+# Once model code is in this file, generate migrations with:
+#    cfgov/manage.py makemigrations research_reports -n create_research_report_models
