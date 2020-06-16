38756b726cd8819ec73894e62b917696a50196ce
Jack Bevis <jack.bevis@gmail.com>
2020-06-16 16:20:22 -0600

    WIP...add narrative modal to calendar view

--- cfgov/unprocessed/apps/mmt-my-money-calendar/js/views/calendar/index/index.js
+++ cfgov/unprocessed/apps/mmt-my-money-calendar/js/views/calendar/index/index.js
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import { useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useLocation, useParams, Redirect } from 'react-router-dom';
 import { observer } from 'mobx-react';
 import { useStore } from '../../../stores';
@@ -8,8 +8,10 @@ import Day from './day';
 import Details from './details';
 import { useScrollToTop } from '../../../components/scroll-to-top';
 import { DAY_LABELS, dayjs } from '../../../lib/calendar-helpers';
+import Modal from 'react-modal';
+import { useBEM } from '../../../lib/hooks';
 
-import { arrowLeft, arrowRight } from '../../../lib/icons';
+import { arrowLeft, arrowRight, downArrow } from '../../../lib/icons';
 
 const IconButton = ({ icon, ...props }) => <button dangerouslySetInnerHTML={{ __html: icon }} {...props} />;
 
@@ -29,10 +31,23 @@ function Calendar() {
   const { uiStore, eventStore } = useStore();
   const location = useLocation();
   const params = useParams();
+  const [showModal, setShowModal] = useState();
+  const bem = useBEM('modal-dialog');
+
+  const handleModalSession = () => {
+    let visited = localStorage.getItem('visitedPage');
+    console.log(visited)
+    if (visited) {
+      setShowModal(false);
+    } else {
+      setShowModal(true);
+    }
+  }
 
   useEffect(() => {
     uiStore.setPageTitle('myMoney Calendar');
     uiStore.setSubtitle(uiStore.currentMonth.format('MMMM YYYY'));
+    handleModalSession()
   }, [location, params, uiStore.currentMonth]);
 
   const dayLabels = useMemo(
@@ -47,6 +62,37 @@ function Calendar() {
     ),
     []
   );
+  
+  //TODO: Extract this out into it's own component to be reused at different screens
+  const NarrativeModal = () => {
+    return (
+      <Modal isOpen={showModal}
+             className={bem()}
+             overlayClassName="modal-overlay"
+             appElement={document.querySelector('#mmt-my-money-calendar')}
+             style={
+               { content: {
+                  textAlign: 'center',
+                  padding: '15px'
+                 }
+               }
+            }
+      >
+        <div className='narrative-modal'>
+          <h4>Welcome to your Budget Calendar</h4>
+          <p>Start adding your weekly Expenses and Income by clicking on the Add Income and Expenses Button in the menu below.</p>
+          <div dangerouslySetInnerHTML={{__html: downArrow}}></div>
+          <button onClick={(e) => handleToggleModal(e)}>OK</button>
+        </div>
+      </Modal>
+    )
+  };
+
+  const handleToggleModal = (event) => {
+    event.preventDefault();
+    localStorage.setItem('visitedPage', true);
+    setShowModal(!showModal);
+  };
 
   useScrollToTop();
 
@@ -54,6 +100,9 @@ function Calendar() {
 
   return (
     <section className="calendar">
+      {showModal && 
+        <NarrativeModal />
+      }
       <header className="calendar__header">
         <IconButton
           className="calendar__nav-button"
