113dc678c2617691d8d0a25d06c04c98a9556031
Jack Bevis <jack.bevis@gmail.com>
2020-06-17 14:32:39 -0600

    wip...modals

--- cfgov/unprocessed/apps/mmt-my-money-calendar/js/views/calendar/add/form.js
+++ cfgov/unprocessed/apps/mmt-my-money-calendar/js/views/calendar/add/form.js
@@ -154,6 +154,7 @@ function Form() {
           payday2: paydaySchema,
         })}
         onSubmit={(values) => {
+          console.log('SAVE THAT SHIT')
           if (!values.name) values.name = category.name;
 
           logger.debug('Event form submission: %O', values);
@@ -180,6 +181,12 @@ function Form() {
             localStorage.setItem('enteredData', 'initial');
           } else {
             localStorage.setItem('enteredData', 'subsequent')
+          }
+
+          if (!localStorage.getItem('firstEntry')) {
+            localStorage.setItem('firstEntry', true);
+          } else {
+            localStorage.setItem('firstEntry')
           }
 
           return saveEvent(values);
