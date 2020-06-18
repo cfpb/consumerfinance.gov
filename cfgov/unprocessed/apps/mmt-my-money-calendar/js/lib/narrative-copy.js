5863754d14168d3be1d545d95ad6f814f7eb0538
Jack Bevis <jack.bevis@gmail.com>
2020-06-18 14:24:44 -0600

    add narrative notifications as modals to guide user through app on first visit.

--- cfgov/unprocessed/apps/mmt-my-money-calendar/js/lib/narrative-copy.js
+++ cfgov/unprocessed/apps/mmt-my-money-calendar/js/lib/narrative-copy.js
@@ -6,5 +6,13 @@ export const narrativeCopy = {
     step2: {
         headline: 'Congratulations!',
         body: 'You’ve just entered your first transaction into the calendar. The more transactions you enter the more this tool can help you manage your budget. '
+    },
+    step3: {
+        headline: 'Have you entered all of your income and expenses?',
+        body: 'Fix-It Strategies are tips to help you stay cash positive based upon the transactions you entered into the calendar. So, make sure you’ve entered all income and expenses before exploring the strategies on this page for the most accurate information.'
+    },
+    step4: {
+        headline: 'SNAP Benefits',
+        body: 'Your SNAP Benefits can only be applied to your groceries, so the amount that you enter here will only contribute to your Groceries expense.'
     }
 }
