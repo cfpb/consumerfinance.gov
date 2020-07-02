fdc9bb4c8831267946a6ed51c9bd8c3586b453b6
Jack Bevis <jack.bevis@gmail.com>
2020-07-02 14:32:07 -0600

    updates to narrative notifications

--- cfgov/unprocessed/apps/mmt-my-money-calendar/js/lib/narrative-copy.js
+++ cfgov/unprocessed/apps/mmt-my-money-calendar/js/lib/narrative-copy.js
@@ -5,14 +5,14 @@ export const narrativeCopy = {
     },
     step2: {
         headline: 'Congratulations!',
-        body: 'You’ve just entered your first transaction. Add more to see a more complete picture of your weekly money and strategies to help you make decisions!'
+        body: 'You’ve just entered your first transaction. Add more To see a more complete picture of your weekly money and strategies to help you make decisions!'
     },
     step3: {
         headline: 'Have you entered all of your income and expenses?',
-        body: 'Fix-it Strategies improve your weekly cashflow based on what you enter on the calendar. Enter all income (including government benefits) and expenses to get more personalized strategies.'
+        body: 'Fix-it Strategies improve your weekly cashflow based on what you enter on the calendar.  Enter all income (including government benefits) and expenses to get more personalized strategies.'
     },
     step4: {
         headline: 'SNAP Benefits',
-        body: 'Your SNAP benefits can only be applied to a Grocery expense. Any Grocery expense that you add will come out of your SNAP benefit first.'
+        body: 'Any Grocery expense entered will come out of your SNAP benefit first.'
     }
 }
