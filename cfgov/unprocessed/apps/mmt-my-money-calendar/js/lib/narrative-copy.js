72aa33a57d16441bb10d106c594e461de9e61246
Jack Bevis <jack.bevis@gmail.com>
2020-07-02 14:44:17 -0600

    fix typos

--- cfgov/unprocessed/apps/mmt-my-money-calendar/js/lib/narrative-copy.js
+++ cfgov/unprocessed/apps/mmt-my-money-calendar/js/lib/narrative-copy.js
@@ -5,11 +5,11 @@ export const narrativeCopy = {
     },
     step2: {
         headline: 'Congratulations!',
-        body: 'You’ve just entered your first transaction. Add more To see a more complete picture of your weekly money and strategies to help you make decisions!'
+        body: 'You’ve just entered your first transaction. Add more to see a more complete picture of your weekly money and strategies to help you make decisions!'
     },
     step3: {
         headline: 'Have you entered all of your income and expenses?',
-        body: 'Fix-it Strategies improve your weekly cashflow based on what you enter on the calendar.  Enter all income (including government benefits) and expenses to get more personalized strategies.'
+        body: 'Fix-it Strategies improve your weekly cashflow based on what you enter on the calendar. Enter all income (including government benefits) and expenses to get more personalized strategies.'
     },
     step4: {
         headline: 'SNAP Benefits',
