b8de8f678d1c336485bf563bcebd3aa26e42055a
Alex Martin <alexander.martin@cfpb.gov>
2020-07-22 10:45:22 -0400

    Adding CCT Charts

---
+++ test/cypress/integration/components/cct-charts.js
@@ -0,0 +1,11 @@
+import { CCTCharts } from '../../components/cct-charts';
+
+const chart = new CCTCharts();
+
+describe('CCT Charts', () => {
+  it('should adjust based on the year range selected', () => {
+    chart.open();
+    chart.selectTimeRange('3y');
+    chart.currentTimeRange().should('have.text', '3y');
+  });
+});
