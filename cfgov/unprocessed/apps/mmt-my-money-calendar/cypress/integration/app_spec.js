99e14a12c82bbc8d7376307887bec04c2926ed1e
Jack Bevis <jack.bevis@gmail.com>
2020-07-23 16:10:24 -0600

    cypress testing for money on hand wizard

--- cfgov/unprocessed/apps/mmt-my-money-calendar/cypress/integration/app_spec.js
+++ cfgov/unprocessed/apps/mmt-my-money-calendar/cypress/integration/app_spec.js
@@ -1,4 +1,10 @@
 describe('myMoneyCalendar E2E testing', () => {
+
+  beforeEach(() => {
+    cy.clearLocalStorage()
+    indexedDB.deleteDatabase('myMoneyCalendar')
+  })
+
   it('Arrives at landing page', () => {
     cy.visit('http://localhost:8000/mmt-my-money-calendar')
 
@@ -13,6 +19,36 @@ describe('myMoneyCalendar E2E testing', () => {
     cy.contains('Get Started')
   })
 
+  it('Can move back between screens', () => {
+    cy.visit('http://localhost:8000/mmt-my-money-calendar')
+
+    cy.get('.a-btn')
+      .contains('Get Started')
+      .click()
+    
+    cy.url()
+      .should('include', '/money-on-hand/sources')
+
+    cy.contains('Money on Hand')
+      
+    cy.contains('Checking Account')
+      .click()
+
+    cy.get('.a-btn')
+      .contains('Next')
+      .click()
+
+    cy.url()
+      .should('include', '/balances/checking')
+
+    cy.get('.a-btn')
+      .contains('Back')
+      .click()
+
+    cy.url()
+      .should('include', '/money-on-hand/sources')
+  })
+
   it('Steps through the get started wizard', () => {
     cy.visit('http://localhost:8000/mmt-my-money-calendar')
 
@@ -24,9 +60,6 @@ describe('myMoneyCalendar E2E testing', () => {
       .should('include', '/money-on-hand/sources')
 
     cy.contains('Money on Hand')
-
-    cy.get('.a-label')
-      .contains('Checking Account')
       
     cy.contains('Checking Account')
       .click()
@@ -34,5 +67,35 @@ describe('myMoneyCalendar E2E testing', () => {
     cy.get('.a-btn')
       .contains('Next')
       .click()
+
+    cy.url()
+      .should('include', '/balances/checking')
+
+    cy.get('#checking')
+      .type('40000')
+      .should('have.value', '400.00')
+
+    cy.get('.a-btn')
+      .contains('Next')
+      .click()
+
+    cy.url()
+      .should('include', '/money-on-hand/summary')
+
+    cy.get('.funding-source__name')
+      .contains('Checking Account')
+
+    cy.get('.funding-source__balance')
+      .contains('$400.00')
+
+    cy.get('p')
+      .contains('Total Starting Balance: $400.00')
+
+    cy.get('.a-btn')
+      .contains('Go to Calendar')
+      .click()
+
+    cy.url()
+      .should('include', '/calendar')
   })
 })
