97f08c90b17171e57d11bf461d4a95fd56bff422
Craig Davies <craig.davies@cfpb.gov>
2020-07-23 16:17:38 -0400

    Remove wagtail admin rich text editor Hallo in wagtail_hooks.py

--- cfgov/v1/wagtail_hooks.py
+++ cfgov/v1/wagtail_hooks.py
@@ -90,31 +90,6 @@ def log_page_deletion(request, page):
 def editor_js():
     js_files = ['js/table-block.js']
 
-    # Temporarily adding Hallo-related JavaScript files to all admin pages
-    # to support the continued use of Hallo in our RichTextTableInput
-    # until we can take more time to migrate that to Draftail.
-    js_files.insert(0, 'wagtailadmin/js/vendor/hallo.js')
-    js_files.insert(0, 'wagtailadmin/js/hallo-plugins/hallo-hr.js')
-    js_files.insert(
-        0,
-        'wagtailadmin/js/hallo-plugins/hallo-requireparagraphs.js'
-    )
-    js_files.insert(
-        0, 'wagtailadmin/js/hallo-plugins/hallo-wagtaillink.js'
-    )
-    js_files.insert(
-        0,
-        'wagtaildocs/js/hallo-plugins/hallo-wagtaildoclink.js'
-    )
-    js_files.insert(
-        0,
-        'wagtailembeds/js/hallo-plugins/hallo-wagtailembeds.js'
-    )
-    js_files.insert(
-        0,
-        'wagtailimages/js/hallo-plugins/hallo-wagtailimage.js'
-    )
-
     js_includes = format_html_join(
         '\n',
         '<script src="{0}{1}"></script>',
@@ -133,11 +108,6 @@ def editor_css():
         'css/heading-block.css',
         'css/table-block.css',
     ]
-
-    # Temporarily adding Hallo CSS to all admin pages
-    # to support the continued use of Hallo in our RichTextTableInput
-    # until we can take more time to migrate that to Draftail.
-    css_files.insert(0, 'wagtailadmin/css/panels/hallo.css')
 
     css_includes = format_html_join(
         '\n',
