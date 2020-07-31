f78a1c159f6d25e549dbaa6fd32ee387fa868b2c
Alex Martin <alexander.martin@cfpb.gov>
2020-07-31 12:02:14 -0400

    Bump node to v12.18.3

--- docker/deployable-zipfile/build.sh
+++ docker/deployable-zipfile/build.sh
@@ -6,6 +6,7 @@ docker build -t cfgov-deployable-zipfile-builder docker/deployable-zipfile
 
 docker run \
   --rm \
+  -it \
   -u $(id -u):$(id -g) \
   -v $(pwd):/cfgov \
   cfgov-deployable-zipfile-builder ./_build.sh
