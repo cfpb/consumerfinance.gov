084660673216efb84ca6b420f6c95fe0dffa6830
Alex Martin <alexander.martin@cfpb.gov>
2020-07-31 13:08:45 -0400

    Remove -it

--- docker/deployable-zipfile/build.sh
+++ docker/deployable-zipfile/build.sh
@@ -6,7 +6,6 @@ docker build -t cfgov-deployable-zipfile-builder docker/deployable-zipfile
 
 docker run \
   --rm \
-  -it \
   -u $(id -u):$(id -g) \
   -v $(pwd):/cfgov \
   cfgov-deployable-zipfile-builder ./_build.sh
