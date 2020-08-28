a34daa836db3fee9f6b6846df2046f8ae79f81e5
Alex Hoffman <alexander.hoffman@cfpb.gov>
2020-08-28 14:51:02 -0400

    updates to partially fix linting for now compiled files

--- cfgov/unprocessed/js/admin/ask_cfpb_tips.js
+++ cfgov/unprocessed/js/admin/ask_cfpb_tips.js
@@ -1,59 +1,59 @@
-(function () {
-  (function (jQuery) {
-    return jQuery.widget("IKS.answermodule", {
+( function() {
+  ( function( jQuery ) {
+    return jQuery.widget( 'IKS.answermodule', {
       options: {
         editable: null,
         toolbar: null,
         uuid: '',
         buttonCssClass: null
       },
-      populateToolbar: function (toolbar) {
-          var buttonset,
-        baseClass = "answer-module",
-        textInputClass = 'richtext',
-        _this = this;
-        buttonset = jQuery("<span class=\"" + this.widgetName + "\"></span>");
-        var buttonElement;
-        buttonElement = jQuery('<span></span>');
-        buttonElement.Draftail-ToolbarButton({
+      populateToolbar: function( toolbar ) {
+        let buttonset,
+            baseClass = 'answer-module',
+            textInputClass = 'richtext',
+            _this = this;
+        buttonset = jQuery( '<span class="' + this.widgetName + '"></span>' );
+        let buttonElement;
+        buttonElement = jQuery( '<span></span>' );
+        buttonElement.Draftail - ToolbarButton( {
           uuid: _this.options.uuid,
           editable: _this.options.editable,
-          label: "Apply Ask CFPB tip style to selection",
+          label: 'Apply Ask CFPB tip style to selection',
           icon: 'icon-edit',
           command: null
-        });
+        } );
 
-        buttonset.append(buttonElement);
-        
-        buttonElement.on('click', function (event) {
-          var sel = window.getSelection();
-          var node = sel.baseNode || sel.anchorNode;
-          var tip = $(node).closest( '.' + baseClass )[0];
+        buttonset.append( buttonElement );
 
-          // If selection is currently styled as a tip,
-          // remove the tip wrapper element.
+        buttonElement.on( 'click', function( event ) {
+          const sel = window.getSelection();
+          const node = sel.baseNode || sel.anchorNode;
+          const tip = $( node ).closest( '.' + baseClass )[0];
+
+          /* If selection is currently styled as a tip,
+             remove the tip wrapper element. */
           if ( tip ) {
-            var docFrag = document.createDocumentFragment();
+            const docFrag = document.createDocumentFragment();
             while ( tip.firstChild ) {
-              var child = tip.removeChild( tip.firstChild );
-              docFrag.appendChild(child);
+              const child = tip.removeChild( tip.firstChild );
+              docFrag.appendChild( child );
             }
-            tip.parentNode.replaceChild(docFrag, tip);
+            tip.parentNode.replaceChild( docFrag, tip );
             _this.options.editable.setModified();
             return;
           }
 
-          // Otherwise, get the selected range. If it starts or ends
-          // with a text node or inline element (a, strong, i), 
-          // expand the selection either to that node's 
-          // start or end bound, or the start or end bound of its parent
-          // if it's not a direct child of the text input div.
-          var range = rangy.getSelection().getRangeAt(0);
-          
+          /* Otherwise, get the selected range. If it starts or ends
+             with a text node or inline element (a, strong, i),
+             expand the selection either to that node's
+             start or end bound, or the start or end bound of its parent
+             if it's not a direct child of the text input div. */
+          const range = rangy.getSelection().getRangeAt( 0 );
+
           function expandSelection( node, start ) {
-            while ( node.nodeType === 3 
-                    || ['A', 'I', 'Strong'].indexOf( node.nodeName ) > -1 ) {
-              var parent = node.parentNode;
+            while ( node.nodeType === 3 ||
+                    [ 'A', 'I', 'Strong' ].indexOf( node.nodeName ) > -1 ) {
+              const parent = node.parentNode;
               if ( parent && !parent.classList.contains( textInputClass ) ) {
                 range[start ? 'setStartBefore' : 'setEndAfter']( parent );
                 node = parent;
@@ -68,21 +68,21 @@
           expandSelection( range.endContainer );
 
 
-          // Check if range can be surrounded with a wrapper element.
-          // If not, try to find the least common ancestor of the 
-          // start and end containers and expand the start/end of the
-          // range to encompas it so the range can be surrounded.
+          /* Check if range can be surrounded with a wrapper element.
+             If not, try to find the least common ancestor of the
+             start and end containers and expand the start/end of the
+             range to encompas it so the range can be surrounded. */
           function getDepth( ancestor, node ) {
-            return node === ancestor 
-                        ? 0 : 
-                        $( node ).parentsUntil( common ).length + 1;
+            return node === ancestor ?
+              0 :
+              $( node ).parentsUntil( common ).length + 1;
           }
 
           if ( !range.canSurroundContents() ) {
             var common = range.commonAncestorContainer;
-            var start_depth = getDepth( common, range.startContainer );
-            var end_depth = getDepth( common, range.endContainer );
-            
+            let start_depth = getDepth( common, range.startContainer );
+            let end_depth = getDepth( common, range.endContainer );
+
             while ( !range.canSurroundContents() ) {
               if ( start_depth > end_depth ) {
                 if ( range.startContainer.classList.contains( textInputClass ) ) {
@@ -91,31 +91,29 @@
                   range.setStartBefore( range.startContainer );
                   start_depth = getDepth( common, range.startContainer );
                 }
+              } else if ( range.endContainer.classList.contains( textInputClass ) ) {
+                break;
               } else {
-                if ( range.endContainer.classList.contains( textInputClass ) ) {
-                  break;
-                } else {
-                  range.setEndAfter( range.endContainer );
-                  end_depth = getDepth( common, range.endContainer );
-                }
+                range.setEndAfter( range.endContainer );
+                end_depth = getDepth( common, range.endContainer );
               }
             }
           }
-          
-          // If range can be surrounded, wrap it in an element 
-          // that applies the tip class.
+
+          /* If range can be surrounded, wrap it in an element
+             that applies the tip class. */
           if ( range.canSurroundContents() ) {
-            var newNode = document.createElement( 'aside' );
+            const newNode = document.createElement( 'aside' );
             newNode.className = baseClass;
             range.surroundContents( newNode );
             return _this.options.editable.element.trigger( 'change' );
           }
 
-        });
-        
-        buttonset.Draftail-ToolbarButtonset();
-        return toolbar.append(buttonset);
+        } );
+
+        buttonset.Draftail - ToolbarButtonset();
+        return toolbar.append( buttonset );
       }
-    });
-})(jQuery);
-}).call(this);
+    } );
+  } )( jQuery );
+} ).call( this );
--- cfgov/unprocessed/js/admin/html_editor.js
+++ cfgov/unprocessed/js/admin/html_editor.js
@@ -1,77 +1,77 @@
-(function() {
-  (function($) {
-    return $.widget('IKS.editHtmlButton', {
+( function() {
+  ( function( $ ) {
+    return $.widget( 'IKS.editHtmlButton', {
       options: {
         uuid: '',
         editable: null
       },
-      populateToolbar: function(toolbar) {
-        var button, widget;
+      populateToolbar: function( toolbar ) {
+        let button, widget;
 
-        var getEnclosing = function(tag) {
-          var node;
+        const getEnclosing = function( tag ) {
+          let node;
           node = widget.options.editable.getSelection().commonAncestorContainer;
-          return $(node).parents(tag).get(0);
+          return $( node ).parents( tag ).get( 0 );
         };
 
         widget = this;
 
-        button = $('<span></span>');
-        button.Draftail-ToolbarButton({
+        button = $( '<span></span>' );
+        button.Draftail - ToolbarButton( {
           uuid: this.options.uuid,
           editable: this.options.editable,
           label: 'Edit HTML',
           icon: 'icon-code',
           command: null
-        });
+        } );
 
-        toolbar.append(button);
+        toolbar.append( button );
 
-        button.on('click', function(event) {
-          $('body > .modal').remove();
-          var contents = '<div class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">' + 
+        button.on( 'click', function( event ) {
+          $( 'body > .modal' ).remove();
+          const contents = '<div class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">' +
                             '<div class="modal-dialog">' +
-                              '<div class="modal-content">'+
-                                '<button type="button" class="close icon text-replace icon-cross" '+
+                              '<div class="modal-content">' +
+                                '<button type="button" class="close icon text-replace icon-cross" ' +
                                          'data-dismiss="modal" aria-hidden="true">' +
-                                    '&times;' + 
-                                '</button>' +         
+                                    '&times;' +
+                                '</button>' +
                                 '<div class="modal-body">' +
                                   '<header class="nice-padding hasform">' +
                                     '<div class="row">' +
                                       '<div class="left">' +
-                                        '<div class="col">' + 
+                                        '<div class="col">' +
                                           '<h1><i class="fa fa-file-code-o"></i>Edit HTML Code</h1>' +
                                         '</div>' +
                                       '</div>' +
                                     '</div>' +
                                   '</header>' +
                                   '<div class="modal-body-body" style="padding:15px;"></div>' +
-                                '</div> <!-- /.modal-body -->' +         
-                              '</div><!-- /.modal-content -->' +    
+                                '</div> <!-- /.modal-body -->' +
+                              '</div><!-- /.modal-content -->' +
                             '</div><!-- /.modal-dialog -->' +
                           '</div>';
-          var container = $(contents);
+          const container = $( contents );
 
-          // add container to body and hide it, so content can be added to it before display                                                                             
-          $('body').append(container);
-          container.modal('hide');
-          var modalBody = container.find('.modal-body-body');
-          var bodyContents =  '<textarea style="height: 400px; border: 1px solid black" id="wagtail-edit-html-content">' +
+          // add container to body and hide it, so content can be added to it before display
+          $( 'body' ).append( container );
+          container.modal( 'hide' );
+          const modalBody = container.find( '.modal-body-body' );
+          const bodyContents = '<textarea style="height: 400px; border: 1px solid black" id="wagtail-edit-html-content">' +
                                 widget.options.editable.element.html() +
                               '</textarea>' +
                               '<button id="wagtail-edit-html-save" type="button" class="button" style="margin-top:15px;">' +
-                                'Save' + 
+                                'Save' +
                               '</button>';
-          modalBody.html(bodyContents);
-          $("#wagtail-edit-html-save").on("click", function() {
-              widget.options.editable.setContents($("#wagtail-edit-html-content").val());
-              widget.options.editable.setModified();
-              container.modal('hide');
-          });
-          container.modal('show');
-        });
+          modalBody.html( bodyContents );
+          $( '#wagtail-edit-html-save' ).on( 'click', function() {
+            widget.options.editable.setContents( $( '#wagtail-edit-html-content' ).val() );
+            widget.options.editable.setModified();
+            container.modal( 'hide' );
+          } );
+          container.modal( 'show' );
+        } );
       }
-    });
-  })(jQuery);
-}).call(this);
+    } );
+  } )( jQuery );
+} ).call( this );
