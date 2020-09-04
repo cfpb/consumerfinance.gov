870e3cd19fa89d7ea58033a6276b365926e1066b
Charles Werner <charles.werner@cfpb.gov>
2020-09-04 09:26:17 -0400

    - Change url parameters code to obey a single truth

--- cfgov/unprocessed/apps/paying-for-college/js/dispatchers/update-models.js
+++ cfgov/unprocessed/apps/paying-for-college/js/dispatchers/update-models.js
@@ -13,96 +13,7 @@ import { isNumeric, stringToNum } from '../util/number-utils.js';
 import { getConstantsValue, getProgramInfo, getSchoolValue, getStateValue } from '../dispatchers/get-model-values.js';
 import { updateGradMeterChart, updateRepaymentMeterChart, updateSchoolView } from './update-view.js';
 import { updateUrlQueryString } from '../dispatchers/update-view.js';
-
-
-const _urlParamsToModelVars = {
-  'iped': 'schoolModel.schoolID',
-  'oid': 'schoolModel.oid',
-
-  'pid': 'stateModel.pid',
-  'houp': 'stateModel.programHousing',
-  'typp': 'stateModel.programType',
-  'lenp': 'stateModel.programLength',
-  'ratp': 'stateModel.programRate',
-  'depp': 'stateModel.programStudentType',
-  'cobs': 'stateModel.costsQuestion',
-  'regs': 'stateModel.expensesRegion',
-  'iqof': 'stateModel.impactOffer',
-  'iqlo': 'stateModel.impactLoans',
-
-  'utm_source': 'stateModel.utmSource',
-  'utm_medium': 'stateModel.utm_medium',
-  'utm_campaign': 'stateModel.utm_campaign',
-
-  'tuit': 'financialModel.dirCost_tuition',
-  'hous': 'financialModel.dirCost_housing',
-  'diro': 'financialModel.dirCost_other',
-
-  'book': 'financialModel.indiCost_books',
-  'indo': 'financialModel.indiCost_other',
-  'nda':  'financialModel.indiCost_added',
-  'tran': 'financialModel.indiCost_transportation',
-
-  'pelg': 'financialModel.grant_pell',
-  'seog': 'financialModel.grant_seog',
-  'fedg': 'financialModel.grant_federal',
-  'stag': 'financialModel.grant_state',
-  'schg': 'financialModel.grant_school',
-  'othg': 'financialModel.grant_other',
-
-  'mta': 'financialModel.mil_milTuitAssist',
-  'gi': 'financialModel.mil_GIBill',
-  'othm': 'financialModel.mil_other',
-
-  'stas': 'financialModel.scholarship_state',
-  'schs': 'financialModel.scholarship_school',
-  'oths': 'financialModel.scholarship_other',
-
-  'wkst': 'financialModel.workStudy_workStudy',
-
-  'fell': 'financialModel.fund_fellowship',
-  'asst': 'financialModel.fund_assistantship',
-
-  'subl': 'financialModel.fedLoan_directSub',
-  'unsl': 'financialModel.fedLoan_directUnsub',
-
-  'insl': 'financialModel.publicLoan_institutional',
-  'insr': 'financialModel.rate_institutional',
-  'insf': 'financialModel.fee_institutional',
-  'stal': 'financialModel.publicLoan_state',
-  'star': 'financialModel.rate_stateLoan',
-  'staf': 'financialModel.fee_stateLoan',
-  'npol': 'financialModel.publicLoan_nonprofit',
-  'npor': 'financialModel.rate_nonprofit',
-  'npof': 'financialModel.fee_nonprofit',
-
-  'pers': 'financialModel.savings_personal',
-  'fams': 'financialModel.savings_family',
-  '529p': 'financialModel.savings_collegeSavings',
-
-  'offj': 'financialModel.income_jobOffCampus',
-  'onj': 'financialModel.income_jobOnCampus',
-  'eta': 'financialModel.income_employerAssist',
-  'othf': 'financialModel.income_otherFunding',
-
-  'pvl1': 'financialModel.privLoan_privLoan1',
-  'pvr1': 'financialModel.privloan_privLoanRate1',
-  'pvf1': 'financialModel.privloan_privLoanFee1',
-
-  'plus': 'financialModel.fedLoan_parentPlus',
-
-  'houx': 'expensesModel.item_housing',
-  'fdx': 'expensesModel.item_food',
-  'clhx': 'expensesModel.item_clothing',
-  'trnx': 'expensesModel.item_transportation',
-  'hltx': 'expensesModel.item_healthcare',
-  'entx': 'expensesModel.item_entertainment',
-  'retx': 'expensesModel.item_retirement',
-  'taxx': 'expensesModel.item_taxes',
-  'chcx': 'expensesModel.item_childcare',
-  'othx': 'expensesModel.item_other',
-  'dbtx': 'expensesModel.item_currentDebt'
-};
+import { urlParameters } from '../util/url-parameter-utils.js';
 
 /**
  * initializeFinancialModel - Create financial model values based on the input
@@ -165,6 +76,15 @@ function recalculateExpenses() {
 function updateRegion( region ) {
   expensesModel.setValuesByRegion( region );
 }
+
+/**
+ * refreshExpenses - Update the expenses with stored values
+ */
+function refreshExpenses() {
+  expensesModel.setValuesByRegion( schoolModel.values.region );
+}
+
+
 
 /**
   * updateSchoolData - Fetch API data for school and update the model
@@ -265,8 +185,8 @@ function updateModelsFromQueryString( queryObj ) {
   }
 
   for ( const key in queryObj ) {
-    if ( _urlParamsToModelVars.hasOwnProperty( key ) ) {
-      const match = _urlParamsToModelVars[key].split( '.' );
+    if ( urlParameters.hasOwnProperty( key ) ) {
+      const match = urlParameters[key].split( '.' );
       modelMatch[match[0]]( match[1], queryObj[key], false );
 
       // plus can mean either type of loan (they are mutually exclusive)
@@ -291,6 +211,7 @@ export {
   initializeFinancialValues,
   recalculateExpenses,
   recalculateFinancials,
+  refreshExpenses,
   updateExpense,
   updateFinancial,
   updateFinancialsFromSchool,
