747501b679289fdec6ef01d9d06d20a20eeadecb
Scott Cranfill <scott.cranfill@cfpb.gov>
2020-09-11 10:31:52 -0400

    Tidy up archive_events management command and add tests

--- cfgov/v1/management/commands/archive_events.py
+++ cfgov/v1/management/commands/archive_events.py
@@ -1,7 +1,5 @@
-import logging
-
 from django.core.exceptions import ValidationError
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from django.utils import timezone
 
 from v1.models.browse_filterable_page import (
@@ -10,14 +8,11 @@ from v1.models.browse_filterable_page import (
 from v1.models.learn_page import EventPage
 
 
-logger = logging.getLogger(__name__)
-
-
 class Command(BaseCommand):
     help = 'Move event pages whose end date has passed into the archive'
 
     def handle(self, *args, **options):
-        logger.info('Searching for events to archive…')
+        self.stdout.write('Searching for events to archive…')
         event_page_exists = BrowseFilterablePage.objects.filter(
             title='Events'
         ).exists()
@@ -25,33 +20,43 @@ class Command(BaseCommand):
             title__icontains='Archive'
         ).exists()
 
-        if event_page_exists and archive_event_page_exists:
-            events = BrowseFilterablePage.objects.get(title='Events')
-            archive = EventArchivePage.objects.get(
-                title__icontains='Archive')
-
-            live_event_pages = events.get_children().live().type(EventPage)
-
-            if len(live_event_pages) > 1:
-                for event in live_event_pages:
-                    event = event.specific
-                    if event.end_dt:
-                        if event.end_dt < timezone.now():
-                            if event.can_move_to(archive):
-                                try:
-                                    event.move(archive, pos='last-child')
-                                except ValidationError:
-                                    isodate = event.start_dt.date().isoformat()
-                                    event.slug = event.slug + '-' + isodate
-                                    event.save()
-                                    event.move(archive, pos='last-child')
-                                # No longer logging here because event.move has
-                                # its own log statement.
-            else:
-                logger.info('No past events to be archived were found.')
-        elif not event_page_exists:
-            logger.info('BrowseFilterablePage titled "Events" does not exist.')
-        elif not archive_event_page_exists:
-            logger.info(
-                'EventArchivePage with "Archive" in the title does not exist.'
+        if not event_page_exists or not archive_event_page_exists:
+            raise CommandError(
+                'Error: Main events page and/or archive page not found. '
+                'Please ensure that there is a BrowseFilterablePage named '
+                '"Events" and an EventArchivePage with "Archive" in the name '
+                'in order for this script to run correctly.'
             )
+
+        events = BrowseFilterablePage.objects.get(title='Events')
+        archive = EventArchivePage.objects.get(title__icontains='Archive')
+        live_event_pages = events.get_children().live().type(EventPage)
+        events_archived = False
+
+        if live_event_pages:
+            for event in live_event_pages:
+                event = event.specific
+
+                if event.end_dt:
+                    end_dt = event.end_dt
+                else:
+                    end_dt = event.start_dt
+
+                if end_dt < timezone.now():
+                    if event.can_move_to(archive):
+                        try:
+                            event.move(archive, pos='last-child')
+                        except ValidationError:
+                            iso_date = event.start_dt.date().isoformat()
+                            event.slug = event.slug + '-' + iso_date
+                            event.save()
+                            event.move(archive, pos='last-child')
+                        # Not logging here because event.move writes its
+                        # own log to stdout.
+                        events_archived = True
+
+            if not events_archived:
+                self.stdout.write('No past events found to be archived.')
+
+        else:
+            self.stdout.write('No live event pages found.')
