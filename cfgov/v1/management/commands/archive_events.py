8b5538259dfefdc5ded59173a2820eaaeab118c4
Scott Cranfill <scott.cranfill@cfpb.gov>
2020-09-11 13:26:19 -0400

    Only check live EventPages for archiving; append date to duplicate slugs (#5997)
    * Only check live EventPages for archiving; append date to duplicate slugs
    
    Drafts that got archived were causing errors when, later, pages with the 
    same slug were attempted to be archived again. This update only checks 
    for live pages to archive (and only EventPages now), and if it finds a 
    duplicate slug, it will append the event's start date to the slug before 
    moving it.
    
    * Add archive_events management command
    
    * Tidy up archive_events management command and add tests
    
    * Remove old archive_events raw script
    
    (now fully replaced by the Django management command)
    
    * Add test for appending date to duplicate slug

---
+++ cfgov/v1/management/commands/archive_events.py
@@ -0,0 +1,62 @@
+from django.core.exceptions import ValidationError
+from django.core.management.base import BaseCommand, CommandError
+from django.utils import timezone
+
+from v1.models.browse_filterable_page import (
+    BrowseFilterablePage, EventArchivePage
+)
+from v1.models.learn_page import EventPage
+
+
+class Command(BaseCommand):
+    help = 'Move event pages whose end date has passed into the archive'
+
+    def handle(self, *args, **options):
+        self.stdout.write('Searching for events to archive…')
+        event_page_exists = BrowseFilterablePage.objects.filter(
+            title='Events'
+        ).exists()
+        archive_event_page_exists = EventArchivePage.objects.filter(
+            title__icontains='Archive'
+        ).exists()
+
+        if not event_page_exists or not archive_event_page_exists:
+            raise CommandError(
+                'Error: Main events page and/or archive page not found. '
+                'Please ensure that there is a BrowseFilterablePage named '
+                '"Events" and an EventArchivePage with "Archive" in the name '
+                'in order for this script to run correctly.'
+            )
+
+        events = BrowseFilterablePage.objects.get(title='Events')
+        archive = EventArchivePage.objects.get(title__icontains='Archive')
+        live_event_pages = events.get_children().live().type(EventPage)
+        events_archived = False
+
+        if live_event_pages:
+            for event in live_event_pages:
+                event = event.specific
+
+                if event.end_dt:
+                    end_dt = event.end_dt
+                else:
+                    end_dt = event.start_dt
+
+                if end_dt < timezone.now():
+                    if event.can_move_to(archive):
+                        try:
+                            event.move(archive, pos='last-child')
+                        except ValidationError:
+                            iso_date = event.start_dt.date().isoformat()
+                            event.slug = event.slug + '-' + iso_date
+                            event.save()
+                            event.move(archive, pos='last-child')
+                        # Not logging here because event.move writes its
+                        # own log to stdout.
+                        events_archived = True
+
+            if not events_archived:
+                self.stdout.write('No past events found to be archived.')
+
+        else:
+            self.stdout.write('No live event pages found.')
