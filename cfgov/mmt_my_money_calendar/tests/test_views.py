d14f2c665ff2cf7e930eb73c01e9042fe90d1553
Barak Stout <barakstout@gmail.com>
2020-09-14 14:48:54 -0400

    updated mmt flag

--- cfgov/mmt_my_money_calendar/tests/test_views.py
+++ cfgov/mmt_my_money_calendar/tests/test_views.py
@@ -3,9 +3,7 @@ from django.urls import reverse
 
 
 class TestServiceWorkerView(TestCase):
-
     @override_settings(FLAGS={"MMT_MY_MONEY_CALENDAR": [("boolean", True)]})
-
     def test_service_worker(self):
         response = self.client.get(reverse('mmt-my-money-calendar'))
         self.assertEqual(response.status_code, 404)
