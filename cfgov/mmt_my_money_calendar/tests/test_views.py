828d19ee8fc4238a2f824dcd3dc6da76be73f599
Barak Stout <barakstout@gmail.com>
2020-09-14 12:25:48 -0400

    fix client url

--- cfgov/mmt_my_money_calendar/tests/test_views.py
+++ cfgov/mmt_my_money_calendar/tests/test_views.py
@@ -4,5 +4,5 @@ from django.urls import reverse
 
 class TestServiceWorkerView(TestCase):
     def test_service_worker(self):
-        response = self.client.get(reverse('mmt-my-money-calendar'))
+        response = self.client.get(reverse('my-money-calendar'))
         self.assertEqual(response.status_code, 200)
