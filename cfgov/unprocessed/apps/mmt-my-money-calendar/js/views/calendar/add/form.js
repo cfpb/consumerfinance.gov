6d4584d3fd1a3be522af835e2a8283e2a33a147c
Missy Maloney <missymaloney1@gmail.com>
2020-09-16 10:51:46 -0400

    removed all basic issues from form.js

--- cfgov/unprocessed/apps/mmt-my-money-calendar/js/views/calendar/add/form.js
+++ cfgov/unprocessed/apps/mmt-my-money-calendar/js/views/calendar/add/form.js
@@ -27,37 +27,36 @@ function Form() {
   const [showModal, setShowModal] = useState();
   const logger = useMemo(() => Logger.addGroup('eventForm'), []);
   const monthDayOptions = useMemo(
-    () => [...range(1, 30)].map((num) => ({ label: numberWithOrdinal(num), value: num })),
+    () => [...range(1, 30)].map( num => ({ label: numberWithOrdinal(num), value: num })),
     []
   );
   const paydaySchema = useMemo(
     () =>
-      yup.number().when(['recurs', 'recurrenceType'], {
-        is: (recurs, recurrenceType) => recurs && recurrenceType === 'semimonthly',
+      yup.number().when(['recurs', 'recurrenceType'], {is: (recurs, recurrenceType) => recurs && recurrenceType === 'semimonthly',
         then: yup
           .number()
           .integer()
           .required('Day of month is required for semimonthly recurrences')
           .cast(),
-        otherwise: yup.number(),
+        otherwise: yup.number()
       }),
     []
   );
-  const handleCatName = (category) => category === 'TANF' || category === 'SNAP' ? category : category.toLowerCase();
+  const handleCatName = category => category === 'TANF' || category === 'SNAP' ? category : category.toLowerCase();
 
   useEffect(() => {
     handleModalSession();
-  }, [])
+  }, []);
 
   // Toggle bottom nav bar when inputs are focused, to prevent it from obscuring text on mobile screens:
   const focusHandler = useCallback(
-    (evt) => {
+    evt => {
       uiStore.toggleBottomNav(false);
     },
     [uiStore]
   );
   const blurHandler = useCallback(
-    (cb) => (evt) => {
+     cb => evt => {
       uiStore.toggleBottomNav(true);
       cb(evt);
     },
@@ -82,13 +81,13 @@ function Form() {
   // Should eventually return a loading spinner here:
   if (id && !eventStore.eventsLoaded) return null;
 
-  if (isNew && !category) return <Redirect to="/calendar/add" />;
+  if (isNew && !category) return <Redirect to='/calendar/add' />;
 
   const event = id
     ? eventStore.getEvent(id)
     : new CashFlowEvent({
-        category: categoryPath,
-        dateTime: uiStore.selectedDate,
+      category: categoryPath,
+      dateTime: uiStore.selectedDate
       });
 
       if (id && event) {
@@ -113,35 +112,31 @@ function Form() {
     }
   };
 
-  const handleToggleModal = (event) => {
+  const handleToggleModal = event => {
     event.preventDefault();
     localStorage.setItem('snapVisit', true);
     setShowModal(!showModal);
   };
 
   return (
-    <section className="add-event">
+    <section className='add-event'>
       { showModal && 
         <NarrativeModal showModal={showModal} 
-                        handleOkClick={handleToggleModal}
-                        copy={narrativeCopy.step4}
+          handleOkClick={handleToggleModal}
+          copy={narrativeCopy.step4}
         />
       }
-      <BackButton variant="secondary" onClick={() => history.goBack()}>
+      <BackButton variant='secondary' onClick={() => history.goBack()}>
         Back
       </BackButton>
 
-      <h2 className="add-event__title">{category.name}</h2>
+      <h2 className='add-event__title'>{category.name}</h2>
       {category.name === 'Job' ? (
-          <p className="add-event__intro"> Enter your paycheck information.</p>
+        <p className='add-event__intro'> Enter your paycheck information.</p>
       ) : (
-        <p className="add-event__intro">Enter the details of your {handleCatName(category.name)} {eventType}.</p>
-       
+        <p className='add-event__intro'>Enter the details of your {handleCatName(category.name)} {eventType}.</p>
       )}
-        
-        {!!category.description && <p className="add-event__description">{category.description}</p>} 
-     
-      
+        {!!category.description && <p className='add-event__description'>{category.description}</p>} 
 
       <Formik
         initialValues={event.toFormValues()}
@@ -156,10 +151,10 @@ function Form() {
           recurrenceType: yup.string().when('recurs', {
             is: true,
             then: yup.string().required('Frequency is required for recurring transactions'),
-            otherwise: yup.string(),
+            otherwise: yup.string()
           }),
           payday1: paydaySchema,
-          payday2: paydaySchema,
+          payday2: paydaySchema
         })}
         onSubmit={(values) => {
           if (!values.name) values.name = category.name;
@@ -199,32 +194,32 @@ function Form() {
         {(formik) => (
           <form onSubmit={formik.handleSubmit}>
             <TextField
-              name="name"
-              id="name"
-              label="Description"
+              name='name'
+              id='name'
+              label='Description'
               onChange={formik.handleChange}
               onFocus={focusHandler}
               onBlur={blurHandler(formik.handleBlur)}
               value={formik.values.name}
               errors={formik.errors.name}
               touched={formik.touched.name}
-              tabIndex="0"
+              tabIndex='0'
               placeholder={`For example: ${category.name}`}
             />
             <Checkbox
-              id="recurs"
-              name="recurs"
-              label="Recurring?"
+              id='recurs'
+              name='recurs'
+              label='Recurring?'
               checked={formik.values.recurs}
               onChange={formik.handleChange}
-              tabIndex="0"
+              tabIndex='0'
             />
 
             {formik.values.recurs && (
               <SelectField
-                id="recurrenceType"
-                name="recurrenceType"
-                label="Frequency"
+                id='recurrenceType'
+                name='recurrenceType'
+                label='Frequency'
                 options={recurrenceOptions}
                 value={formik.values.recurrenceType}
                 onChange={formik.handleChange}
@@ -232,66 +227,66 @@ function Form() {
                 errors={formik.errors.recurrenceType}
                 touched={formik.touched.recurrenceType}
                 required={formik.values.recurs}
-                tabIndex="0"
+                tabIndex='0'
               />
             )}
 
             {formik.values.recurs && formik.values.recurrenceType === 'semimonthly' && (
               <>
                 <SelectField
-                  id="payday1"
-                  name="payday1"
-                  label="First Payday"
+                  id='payday1'
+                  name='payday1'
+                  label='First Payday'
                   options={monthDayOptions}
                   onChange={formik.handleChange}
                   onBlur={formik.handleBlur}
                   value={formik.values.payday1}
                   required={formik.values.recurrenceType === 'semimonthly'}
-                  tabIndex="0"
+                  tabIndex='0'
                 />
                 <SelectField
-                  id="payday2"
-                  name="payday2"
-                  label="Second Payday"
+                  id='payday2'
+                  name='payday2'
+                  label='Second Payday'
                   options={monthDayOptions}
                   onChange={formik.handleChange}
                   onBlur={formik.handleBlur}
                   value={formik.values.payday2}
                   required={formik.values.recurrenceType === 'semimonthly'}
-                  tabIndex="0"
+                  tabIndex='0'
                 />
               </>
             )}
             <CurrencyField
-              id="totalCents"
-              name="totalCents"
-              label="Amount"
+              id='totalCents'
+              name='totalCents'
+              label='Amount'
               onChange={formik.handleChange}
               onFocus={focusHandler}
               onBlur={blurHandler(formik.handleBlur)}
               value={formik.values.totalCents}
               errors={formik.errors.totalCents}
               touched={formik.touched.totalCents}
-              tabIndex="0"
+              tabIndex='0'
               required
             />
 
             <DateField
-              id="dateTime"
-              name="dateTime"
-              label="Date"
+              id='dateTime'
+              name='dateTime'
+              label='Date'
               onChange={formik.handleChange}
               onFocus={focusHandler}
               onBlur={blurHandler(formik.handleBlur)}
               value={formik.values.dateTime || ''}
               errors={formik.errors.dateTime}
               touched={formik.touched.dateTime}
-              tabIndex="0"
+              tabIndex='0'
               required
             />
             
 
-            <Button fullWidth disabled={!formik.dirty && !formik.isValid} type="submit" tabIndex="0">
+            <Button fullWidth disabled={!formik.dirty && !formik.isValid} type='submit' tabIndex='0'>
               Save
             </Button>
           </form>
@@ -299,18 +294,18 @@ function Form() {
       </Formik>
 
       <ModalDialog
-        contentLabel="Recurring event update options"
+        contentLabel='Recurring event update options'
         isOpen={recurrenceUpdateModalState}
         onRequestClose={() => showRecurrenceUpdateModal(false)}
-        id="recurrence-update-dialog"
-        prompt="Update the totals of future occurrences of this event?"
+        id='recurrence-update-dialog'
+        prompt='Update the totals of future occurrences of this event?'
         actions={[
           {
             label: 'Yes',
             onClick: async () => {
               showRecurrenceUpdateModal(false);
               await saveEvent(formValues.current, true);
-            },
+            }
           },
           {
             label: 'No',
@@ -318,7 +313,7 @@ function Form() {
               showRecurrenceUpdateModal(false);
               await saveEvent(formValues.current);
             },
-          },
+          }
         ]}
         showCancel={false}
       />
