1203afa6a96658fc089d506b5e0863b47a3f50b3
Missy Maloney <missymaloney1@gmail.com>
2020-09-17 11:36:07 -0400

    added eslint

--- cfgov/unprocessed/apps/mmt-my-money-calendar/js/lib/logger.js
+++ cfgov/unprocessed/apps/mmt-my-money-calendar/js/lib/logger.js
@@ -17,61 +17,63 @@ import { useEffect } from 'react';
  * @property {Function} groupCollapsed
  */
 
-class Logger {
-  /** The methods that will be present in the Console API wrapper object returned by addGroup
-   * @type {string[]}
-   */
-  static wrappedMethods = [
-    'debug',
-    'log',
-    'info',
-    'warn',
-    'error',
-    'time',
-    'timeEnd',
-    'count',
-    'group',
-    'groupEnd',
-    'groupCollapsed'
-  ];
+/** The methods that will be present in the Console API wrapper object returned by addGroup
+ * @type {string[]}
+ */
 
-  /** The localStorage key under which a JSON object of persistent settings will be saved
+const wrappedMethods = [
+  'debug',
+  'log',
+  'info',
+  'warn',
+  'error',
+  'time',
+  'timeEnd',
+  'count',
+  'group',
+  'groupEnd',
+  'groupCollapsed'
+];
+
+/** The localStorage key under which a JSON object of persistent settings will be saved
    * @type {string}
    */
-  static storageKey = 'mmtLogger';
+const storageKey = 'mmtLogger';
 
-  static defaults = {
-    active: [],
-  };
+const defaults = {
+  active: []
+};
 
-  /** An array of background/foreground hex string pairs used in color coding group messages
-   * @type {Array<string[]>}
-   */
-  colors = [
-    ['#a6cee3', '#000'],
-    ['#1f78b4', '#fff'],
-    ['#b2df8a', '#000'],
-    ['#33a02c', '#fff'],
-    ['#fb9a99', '#fff'],
-    ['#e31a1c', '#fff'],
-    ['#fdbf6f', '#000'],
-    ['#ff7f00', '#fff'],
-    ['#cab2d6', '#fff'],
-    ['#6a3d9a', '#fff'],
-    ['#ffff99', '#000'],
-    ['#b15928', '#fff'],
-    ['#e41a1c', '#fff'],
-    ['#377eb8', '#fff'],
-    ['#4daf4a', '#fff'],
-    ['#984ea3', '#fff'],
-    ['#ff7f00', '#fff'],
-    ['#ffff33', '#000'],
-    ['#a65628', '#fff'],
-    ['#f781bf', '#fff'],
-    ['#999999', '#fff'],
-  ];
+/** An array of background/foreground hex string pairs used in color coding group messages
+ * @type {Array<string[]>}
+ */
+const colors = [
+  ['#a6cee3', '#000'],
+  ['#1f78b4', '#fff'],
+  ['#b2df8a', '#000'],
+  ['#33a02c', '#fff'],
+  ['#fb9a99', '#fff'],
+  ['#e31a1c', '#fff'],
+  ['#fdbf6f', '#000'],
+  ['#ff7f00', '#fff'],
+  ['#cab2d6', '#fff'],
+  ['#6a3d9a', '#fff'],
+  ['#ffff99', '#000'],
+  ['#b15928', '#fff'],
+  ['#e41a1c', '#fff'],
+  ['#377eb8', '#fff'],
+  ['#4daf4a', '#fff'],
+  ['#984ea3', '#fff'],
+  ['#ff7f00', '#fff'],
+  ['#ffff33', '#000'],
+  ['#a65628', '#fff'],
+  ['#f781bf', '#fff'],
+  ['#999999', '#fff']
+];
 
-  usedColors = [];
+let usedColors = [];
+
+class Logger {
 
   /** Computed property returning either localStorage or an ephemeral object in memory if
    * localStorage is not available (e.g. in incognito mode)
@@ -93,7 +95,6 @@ class Logger {
     }
   }
 
-  
   /** Configuration object, auto loaded from localStorage
    * @type {Object}
    */
@@ -104,15 +105,14 @@ class Logger {
     return this._config;
   }
 
-  
   /** Setter that automatically saves config object to localStorage
-   */
+   * @type {object} an object
+  */
   set config(obj) {
-    this.storage[this.constructor.storageKey] = JSON.stringify(obj);
+    this.storage[storageKey] = JSON.stringify(obj);
     this._config = obj;
   }
 
-  
   /** An array of registered group names
    * @type {string[]}
    */
@@ -120,7 +120,6 @@ class Logger {
     return [...this.groups.keys()];
   }
 
-  
   /** An array of currently enabled groups
    * @type {string[]}
    */
@@ -129,51 +128,46 @@ class Logger {
   }
 
   constructor() {
-    const { storageKey, defaults } = this.constructor;
     this._config = null;
-    //this.groups = {};
     this.groups = new Map();
 
     if (!this.storage[storageKey]) this.storage[storageKey] = JSON.stringify(defaults);
   }
 
-  
   /** Get a random color pair
    *
-   * @returns {string[]}
+   * @returns {string[]} an array
    */
   randomColor() {
-    return this.colors[Math.floor(Math.random() * this.colors.length)];
+    return colors[Math.floor(Math.random() * colors.length)];
   }
 
   unusedColorPair() {
-    if (this.usedColors.length === this.colors.length) {
-      this.usedColors = [];
+    if (usedColors.length === colors.length) {
+      usedColors = [];
     }
 
-    const index = Math.floor(Math.random() * this.colors.length);
-    const colorPair = this.colors[index];
+    const index = Math.floor(Math.random() * colors.length);
+    const colorPair = colors[index];
 
-    if (!this.usedColors.includes(index)) {
-      this.usedColors.push(index);
+    if (!usedColors.includes(index)) {
+      usedColors.push(index);
       return colorPair;
     }
 
     return this.unusedColorPair();
   }
 
-  
   /** Load and parse JSON configuration from localStorage
    *
-   * @returns {Object}
+   * @returns {Object} an object
    */
   loadConfig() {
-    const config = this.storage[this.constructor.storageKey];
+    const config = this.storage[storageKey];
     const parsed = config ? JSON.parse(config) : {};
     return parsed;
   }
 
-  
   /** Updates runtime config and persists it to localStorage
    *
    * @param {Object} params New configuration parameters
@@ -184,11 +178,10 @@ class Logger {
     this._info('Logger config updated: %O', this.config);
   }
 
-  
   /** Registers a new message group and returns an object of wrapped console methods.
    *
    * @param {string} name The name of the new group
-   * @returns {ConsoleWrapper}
+   * @returns {ConsoleWrapper} an object
    */
   addGroup(name) {
     if (this.groups.has(name)) return this.groups.get(name);
@@ -207,7 +200,7 @@ class Logger {
       padding: 0;
     `;
 
-    const group = this.constructor.wrappedMethods.reduce((obj, method) => {
+    const group = wrappedMethods.reduce((obj, method) => {
       obj[method] = (...args) => {
         if (!this.config.active.includes(name)) return;
 
@@ -215,7 +208,6 @@ class Logger {
           args[0] = `%c${name}%c ${args[0]}`;
           args.splice(1, 0, labelStyle, resetStyle);
         }
-
         return console[method](...args);
       };
 
@@ -229,7 +221,6 @@ class Logger {
     return group;
   }
 
-  
   /** Makes messages from specified groups print to console
    *
    * @param  {...string} groupNames Names of groups to enable
@@ -253,7 +244,6 @@ class Logger {
     }
   }
 
-  
   /** Hides messages from specified groups and prevents them from printing to console.
    *
    * @param  {...any} groupNames Names of groups to disable
@@ -268,25 +258,23 @@ class Logger {
       }
 
       this.updateConfig({
-        active: active.filter((name) => name !== groupName),
+        active: active.filter( name => name !== groupName)
       });
       this._info('Disabled logging for group %s', groupName);
     }
   }
 
-  
   /** Enables all registered groups
    *
    * @returns {undefined}
    */
   enableAll() {
     this.updateConfig({
-      active: this.groupNames,
+      active: this.groupNames
     });
     this._info('Enabled all groups (%O)', this.groupNames);
   }
 
-  
   /** Disables all registered groups
    *
    * @returns {undefined}
