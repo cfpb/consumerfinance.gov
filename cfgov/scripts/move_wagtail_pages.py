174a88bdc77919b2081c7f6ea1684b82ade59157
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2020-09-21 13:46:18 -0400

    Add script to move pages within Wagtail
    Also, if the page should be moved to a new slug, make that change too.

---
+++ cfgov/scripts/move_wagtail_pages.py
@@ -0,0 +1,34 @@
+import csv
+import logging
+
+from wagtail.contrib.redirects.models import Redirect
+from wagtail.core.models import Page
+
+
+logger = logging.getLogger(__name__)
+
+
+# Run this from the command line with this:
+#   cfgov/manage.py runscript move_wagtail_pages --script-args [PATH]
+def run(*args):
+    if not args:
+        logger.error("error. Use --script-args [PATH] to specify the " +
+                     "location of the csv.")
+    else:
+        page_moves_file = args[0]
+
+        successes = 0
+
+        with open(page_moves_file, "r") as csv_file:
+            page_list = csv.reader(csv_file, delimiter=',')
+            next(page_list)  # skip the header row
+            for [page_id, destination_id, new_slug] in page_list:
+                page = Page.objects.get(id=page_id)
+                if new_slug:
+                    page.slug = new_slug
+                    page.save()
+                new_parent = Page.objects.get(id=destination_id)
+                page.move(new_parent, pos='first-child')
+                successes += 1
+
+        logger.info(f"Done! Moved {successes} top-level pages")
