4304114972cc02b309b5045d1dafd65cb2484e18
Missy Maloney <missymaloney1@gmail.com>
2020-09-21 16:05:38 -0400

    removed comments

--- cfgov/unprocessed/apps/mmt-my-money-calendar/js/components/scroll-to-top.js
+++ cfgov/unprocessed/apps/mmt-my-money-calendar/js/components/scroll-to-top.js
@@ -2,25 +2,21 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 export default function ScrollToTop() {
-  /* istanbul ignore next */
   useScrollToTop();
-  /* istanbul ignore next */
   return null;
 }
 
 export function useScrollToTop() {
-  /* istanbul ignore next */
+
   const { pathname } = useLocation();
 
-  /* istanbul ignore next */
+
   useEffect( () => {
-  /* istanbul ignore next */
     window.scrollTo( 0, 0 );
   }, [ pathname ] );
 }
 
 export function useScrollToTopOnMount() {
-  /* istanbul ignore next */
   useEffect( () => {
     window.scrollTo( 0, 0 );
   }, [] );
