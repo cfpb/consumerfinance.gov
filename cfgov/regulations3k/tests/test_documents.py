a7ddc765bfba0213c2256dad2f30bb1515298c24
Alex Martin <alexander.martin@cfpb.gov>
2020-10-01 16:13:31 -0400

    Flake8 fixes

--- cfgov/regulations3k/tests/test_documents.py
+++ cfgov/regulations3k/tests/test_documents.py
@@ -9,11 +9,24 @@ from regulations3k.models import (
 class SectionParagraphDocumentTest(TestCase):
 
     def test_prepare(self):
-        part = Part(cfr_title_number="0001", chapter="1", part_number="1234", title="Test Title", short_name="short name")
-        version = EffectiveVersion(authority="authority", source="Source", part=part)
-        subpart = Subpart(label="Subpart Label", title="Subpart Title", version=version)
-        section = Section(label="Section Label", title="Section Title", subpart=subpart, sortable_label="Sortable Label")
-        section_paragraph = SectionParagraph(paragraph="Paragraph", paragraph_id="1", section=section)
+        part = Part(cfr_title_number="0001",
+                    chapter="1",
+                    part_number="1234",
+                    title="Test Title",
+                    short_name="short name")
+        version = EffectiveVersion(authority="authority",
+                                   source="Source",
+                                   part=part)
+        subpart = Subpart(label="Subpart Label",
+                          title="Subpart Title",
+                          version=version)
+        section = Section(label="Section Label",
+                          title="Section Title",
+                          subpart=subpart,
+                          sortable_label="Sortable Label")
+        section_paragraph = SectionParagraph(paragraph="Paragraph",
+                                             paragraph_id="1",
+                                             section=section)
 
         doc = SectionParagraphDocument()
 
