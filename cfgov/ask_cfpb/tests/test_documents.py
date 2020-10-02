1e8023befc67cb1c5b4660ea7ba61f6ca2032065
Craig Davies <craig.davies@cfpb.gov>
2020-10-01 21:59:12 -0400

    Updated AnswerPage fields in test_documents.py

--- cfgov/ask_cfpb/tests/test_documents.py
+++ cfgov/ask_cfpb/tests/test_documents.py
@@ -31,64 +31,54 @@ class AnswerPageDocumentTest(TestCase):
             set(mapping.properties.properties.to_dict().keys()),
             set(
                 [
-                    'autocomplete', 'portal_categories', 'text',
-                    'url', 'preview', 'search_tags', 'language'
+                    'autocomplete', 'portal_categories', 'portal_topics',
+                    'text', 'url', 'preview', 'search_tags', 'language'
                 ]
             )
         )
 
-    def test_related_models_added(self):
-        related_models = AnswerPageDocument.django.related_models
-        self.assertEqual([Answer], related_models)
-
     def test_to_field(self):
         doc = DocType()
-        answer_page_text_fields = [
-            'autocomplete',
-            'portal_categories',
-            'text',
-            'url',
-            'preview',
-            'search_tags',
-            'language'
-        ]
-        for f in answer_page_text_fields:
+        for f in ['question', 'statement']:
             nameField = doc.to_field(f, AnswerPage._meta.get_field(f))
             self.assertIsInstance(nameField, fields.TextField)
             self.assertEqual(nameField._path, [f])
-        topicField = doc.to_field(
-            'portal_topics', AnswerPage._meta.get_field('portal_topics')
+        dateField = doc.to_field(
+            'last_edited', AnswerPage._meta.get_field('last_edited')
         )
-        self.assertIsInstance(topicField, fields.KeywordField)
-        self.assertEqual(topicField._path, ['portal_topics'])
+        self.assertIsInstance(dateField, fields.DateField)
+        self.assertEqual(dateField._path, ['last_edited'])
+        for f in ['featured', 'share_and_print']:
+            boolField = doc.to_field(f, AnswerPage._meta.get_field(f))
+            self.assertIsInstance(boolField, fields.BooleanField)
+            self.assertEqual(boolField._path, [f])
+        intField = doc.to_field(
+            'featured_rank', AnswerPage._meta.get_field('featured_rank')
+        )
+        self.assertIsInstance(intField, fields.IntegerField)
+        self.assertEqual(intField._path, ['featured_rank'])
 
     def test_to_field_with_unknown_field(self):
         doc = DocType()
         with self.assertRaises(ModelFieldNotMappedError):
-            doc.to_field('document', AnswerPage._meta.get_field('document'))
+            doc.to_field(
+                'answer_base', AnswerPage._meta.get_field('answer_base')
+            )
 
     def test_mapping(self):
         self.assertEqual(
             AnswerPageDocument._doc_type.mapping.to_dict(), {
                 'properties': {
                     'autocomplete': {
-                        'type': 'text'
+                        'analyzer': 'label_autocomplete', 'type': 'text'
                     },
-                    'portal_categories': {
-                        'type': 'text'
-                    },
-                    'url': {
-                        'type': 'text'
-                    },
-                    'preview': {
-                        'type': 'text'
-                    },
-                    'search_tags': {
-                        'type': 'text'
-                    },
-                    'language': {
-                        'type': 'text'
-                    }
+                    'language': {'type': 'text'},
+                    'portal_categories': {'type': 'text'},
+                    'portal_topics': {'type': 'keyword'},
+                    'preview': {'type': 'text'},
+                    'search_tags': {'type': 'text'},
+                    'text': {'analyzer': 'synonym_analyzer_en', 'type': 'text'},
+                    'url': {'type': 'text'}
                 }
             }
         )
@@ -125,9 +115,20 @@ class AnswerPageDocumentTest(TestCase):
         )
 
     def test_model_instance_update_no_refresh(self):
+        answer = Answer(id=1234)
+        answer.save()
+        page = AnswerPage(
+            slug="mock-question-en-1234", title="Mock question"
+        )
+        page.answer_base = answer
+        page.question = "Mock question"
+        page.answer_content = StreamValue(
+            page.answer_content.stream_block,
+            [{"type": "text", "value": {"content": "Mock answer"}}],
+            True,
+        )
         doc = AnswerPageDocument()
         doc.django.auto_refresh = False
-        page = AnswerPage()
         with patch('django_elasticsearch_dsl.documents.bulk') as mock:
             doc.update(page)
             self.assertNotIn('refresh', mock.call_args_list[0][1])
