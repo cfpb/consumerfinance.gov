47be5fc0ef0d0c1d8b69f673d2d3d16b53b44c89
Craig Davies <craig.davies@cfpb.gov>
2020-10-06 18:08:39 -0400

    Add super to MockSearchResult in ask_cfpb test_views.py

--- cfgov/ask_cfpb/tests/test_views.py
+++ cfgov/ask_cfpb/tests/test_views.py
@@ -32,6 +32,9 @@ class MockSearchResult:
         self.url = "/ask-cfpb/mock-question-en-{}/".format(pk)
         self.text = "Mock answer text for question {}.".format(pk)
         self.preview = "Mock preview ..."
+        super(MockSearchResult, self).__init__(
+            app_label, model_name, pk, score, **kwargs
+        )
 
 
 def mock_es7_queryset(count=0):
