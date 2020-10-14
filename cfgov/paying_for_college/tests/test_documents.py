564e609aa61978f0ff3bca98ad4c683942c19b1f
Craig Davies <craig.davies@cfpb.gov>
2020-10-14 18:18:36 -0400

    Add SchoolDocument class for paying_for_college

---
+++ cfgov/paying_for_college/tests/test_documents.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+
+from paying_for_college.documents import SchoolDocument
+from paying_for_college.models import School
+
+
+class SchoolDocumentTest(TestCase):
+
+    def test_prepare(self):
+        school = School(school_id="999999",
+                        settlement_school="Example University",
+                        city="Example City", state="VA")
+        doc = SchoolDocument()
+
+        prepared_data = doc.prepare(school)
+        self.assertEqual(prepared_data, {
+            'autocomplete': doc.prepare_autocomplete(school),
+            'city': doc.prepare_city(school),
+            'nicknames': doc.prepare_nicknames(school),
+            'school_id': school.school_id,
+            'state': doc.prepare_state(school),
+            'text': school.settlement_school,
+        })
