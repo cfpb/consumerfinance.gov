08e9f4ba047cae29e30c71758f741db45d1d0172
Craig Davies <craig.davies@cfpb.gov>
2020-10-21 14:50:30 -0400

    Remove base_query from paying_for_college search.py

--- cfgov/paying_for_college/models/search.py
+++ cfgov/paying_for_college/models/search.py
@@ -1,22 +1,10 @@
 from paying_for_college.documents import SchoolDocument
-
-
-UNSAFE_CHARACTERS = [
-    '#', '%', ';', '^', '~', '`', '|',
-    '<', '>', '[', ']', '{', '}', '\\'
-]
-
-
-def make_safe(term):
-    for char in UNSAFE_CHARACTERS:
-        term = term.replace(char, '')
-    return term
+from search.elasticsearch_helpers import make_safe
 
 
 class SchoolSearch:
-    def __init__(self, search_term, base_query=None):
+    def __init__(self, search_term):
         self.search_term = make_safe(search_term).strip()
-        self.base_query = base_query
         self.results = []
 
     def autocomplete(self):
@@ -29,10 +17,7 @@ class SchoolSearch:
         return self.results
 
     def search(self):
-        if not self.base_query:
-            search = SchoolDocument.search().filter('term')
-        else:
-            search = self.base_query.filter('term')
+        search = SchoolDocument.search().filter('term')
         if self.search_term != '':
             search = search.query('match', text=self.search_term)
         total_results = search.count()
