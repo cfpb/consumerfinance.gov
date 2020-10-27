fc8a075fc0e7dfadc172c16acc7953d19953a3ea
Craig Davies <craig.davies@cfpb.gov>
2020-10-27 15:42:14 -0400

    Add autocomplete tests for school id, nickname, city, and state

--- cfgov/paying_for_college/tests/test_search.py
+++ cfgov/paying_for_college/tests/test_search.py
@@ -32,7 +32,6 @@ class SchoolSearchTest(TestCase):
         term = "Kansas"
         school = School.objects.get(pk=155317)
         school.save()
-        # mock the search returned value
         mock_return = mock.Mock()
         mock_return.text = school.primary_alias
         mock_return.school_id = school.school_id
@@ -64,6 +63,86 @@ class SchoolSearchTest(TestCase):
         self.assertEqual(155317, mock_return.school_id)
         self.assertTrue("Jayhawks" in mock_return.nicknames)
         self.assertTrue("Lawrence" in mock_return.city)
+        self.assertTrue("KS" in mock_return.state)
+
+    @override_settings(FLAGS={"ELASTICSEARCH_DSL_PFC": [("boolean", True)]})
+    @mock.patch.object(SchoolDocument, 'search')
+    def test_autocomplete_school_id(self, mock_search):
+        school_id = "155317"
+        school = School.objects.get(pk=155317)
+        school.save()
+        mock_return = mock.Mock()
+        mock_return.search_term = school_id
+        mock_return.school_id = school.school_id
+        mock_queryset = mock.Mock()
+        mock_queryset.__iter__ = mock.Mock(return_value=iter([mock_return]))
+        mock_queryset.count.return_value = 1
+        response = self.client.get(
+            reverse("paying_for_college:disclosures:school_search"),
+            {"q": school_id}
+        )
+        self.assertEqual(mock_search.call_count, 1)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(155317, mock_return.school_id)
+
+    @override_settings(FLAGS={"ELASTICSEARCH_DSL_PFC": [("boolean", True)]})
+    @mock.patch.object(SchoolDocument, 'search')
+    def test_autocomplete_nickname(self, mock_search):
+        nickname = "Jayhawks"
+        school = School.objects.get(pk=155317)
+        school.save()
+        mock_return = mock.Mock()
+        mock_return.search_term = nickname
+        mock_return.nicknames = nickname
+        mock_queryset = mock.Mock()
+        mock_queryset.__iter__ = mock.Mock(return_value=iter([mock_return]))
+        mock_queryset.count.return_value = 1
+        response = self.client.get(
+            reverse("paying_for_college:disclosures:school_search"),
+            {"q": nickname}
+        )
+        self.assertEqual(mock_search.call_count, 1)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("Jayhawks" in mock_return.nicknames)
+
+    @override_settings(FLAGS={"ELASTICSEARCH_DSL_PFC": [("boolean", True)]})
+    @mock.patch.object(SchoolDocument, 'search')
+    def test_autocomplete_city(self, mock_search):
+        city = "Lawrence"
+        school = School.objects.get(pk=155317)
+        school.save()
+        mock_return = mock.Mock()
+        mock_return.search_term = city
+        mock_return.city = school.city
+        mock_queryset = mock.Mock()
+        mock_queryset.__iter__ = mock.Mock(return_value=iter([mock_return]))
+        mock_queryset.count.return_value = 1
+        response = self.client.get(
+            reverse("paying_for_college:disclosures:school_search"),
+            {"q": city}
+        )
+        self.assertEqual(mock_search.call_count, 1)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("Lawrence" in mock_return.city)
+
+    @override_settings(FLAGS={"ELASTICSEARCH_DSL_PFC": [("boolean", True)]})
+    @mock.patch.object(SchoolDocument, 'search')
+    def test_autocomplete_state(self, mock_search):
+        state = "KS"
+        school = School.objects.get(pk=155317)
+        school.save()
+        mock_return = mock.Mock()
+        mock_return.search_term = state
+        mock_return.state = state
+        mock_queryset = mock.Mock()
+        mock_queryset.__iter__ = mock.Mock(return_value=iter([mock_return]))
+        mock_queryset.count.return_value = 1
+        response = self.client.get(
+            reverse("paying_for_college:disclosures:school_search"),
+            {"q": state}
+        )
+        self.assertEqual(mock_search.call_count, 1)
+        self.assertEqual(response.status_code, 200)
         self.assertTrue("KS" in mock_return.state)
 
     @override_settings(FLAGS={"ELASTICSEARCH_DSL_PFC": [("boolean", True)]})
