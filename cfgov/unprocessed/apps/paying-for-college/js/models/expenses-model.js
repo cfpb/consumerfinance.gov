24e4660c89040ade38760bc8f594fd5dc2d652ad
Chuck Werner <mistergone@gmail.com>
2020-10-29 13:21:56 -0400

    Paying for College - Bug Fixes (#6090)
    * Paying for College - Bug Fixes
    - Fix bug which breaks URL querystring updates
    - Fix content issue
    - Remove monthly loan payment from "Average Monthly Spending" on budget page

--- cfgov/unprocessed/apps/paying-for-college/js/models/expenses-model.js
+++ cfgov/unprocessed/apps/paying-for-college/js/models/expenses-model.js
@@ -29,9 +29,9 @@ const expensesModel = {
       }
     }
 
-    totalExpenses += getFinancialValue( 'debt_tenYearMonthly' );
     expensesModel.values.total_expenses = totalExpenses;
-    expensesModel.values.total_remainder = getFinancialValue( 'salary_monthly' ) - totalExpenses;
+    expensesModel.values.total_remainder = getFinancialValue( 'salary_monthly' ) - totalExpenses -
+      getFinancialValue( 'debt_tenYearMonthly' );
     if ( expensesModel.values.total_remainder > 0 ) {
       updateState.byProperty( 'expensesRemainder', 'surplus' );
     } else if ( expensesModel.values.total_remainder < 0 ) {
