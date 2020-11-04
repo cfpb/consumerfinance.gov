482eb9ca58c9df89cf43e57e5a3b7f4a0c9e4ea1
Alex Martin <alexander.martin@cfpb.gov>
2020-11-04 12:34:33 -0500

    Setup synonyms

--- cfgov/search/elasticsearch_helpers.py
+++ cfgov/search/elasticsearch_helpers.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+
 from elasticsearch_dsl import analyzer, token_filter, tokenizer
 
 
@@ -26,11 +27,13 @@ label_autocomplete = analyzer(
     filter=['lowercase', token_filter('ascii_fold', 'asciifolding')]
 )
 
-synonym_file_en = open(f'{settings.ELASTICSEARCH_SYNONYMS_HOME}/synonyms_en.txt')
+synonym_file_en = open(
+    f'{settings.ELASTICSEARCH_SYNONYMS_HOME}/synonyms_en.txt')
 synonyms_en = [line.rstrip('\n') for line in synonym_file_en]
 synonym_file_en.close()
 
-synonym_file_es = open(f'{settings.ELASTICSEARCH_SYNONYMS_HOME}/synonyms_es.txt')
+synonym_file_es = open(
+    f'{settings.ELASTICSEARCH_SYNONYMS_HOME}/synonyms_es.txt')
 synonyms_es = [line.rstrip('\n') for line in synonym_file_es]
 synonym_file_es.close()
 
