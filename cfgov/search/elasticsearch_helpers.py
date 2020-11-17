654312b9822c704dd4f84401de739bb8b9e7cb14
Alex Martin <alexm118@vt.edu>
2020-11-17 13:55:56 -0500

    Update cfgov/search/elasticsearch_helpers.py
    Co-authored-by: Andy Chosak <andy.chosak@cfpb.gov>

--- cfgov/search/elasticsearch_helpers.py
+++ cfgov/search/elasticsearch_helpers.py
@@ -54,7 +54,7 @@ synonym_analyzer = analyzer(
 
 
 def environment_specific_index(base_name):
-    if settings.DEPLOY_ENVIRONMENT == 'production' or settings.DEPLOY_ENVIRONMENT == 'local' or settings.DEPLOY_ENVIRONMENT is None:  # noqa: E501
+    if settings.DEPLOY_ENVIRONMENT in (None, '', 'local', 'production'):
         return base_name
     else:
         return f'{settings.DEPLOY_ENVIRONMENT}-{base_name}'
