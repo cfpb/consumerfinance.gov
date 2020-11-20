16e362ed47a9cd18d00b763bde10f38f9e0efa87
Craig Davies <craig.davies@cfpb.gov>
2020-11-20 18:40:23 -0500

    Update prepaid CSV write test to add product wit an underscore

--- cfgov/prepaid_agreements/tests/test_scripts_write_prepaid_agreements.py
+++ cfgov/prepaid_agreements/tests/test_scripts_write_prepaid_agreements.py
@@ -13,38 +13,75 @@ from prepaid_agreements.scripts.write_prepaid_agreements_data_to_csv import (
 class TestViews(TestCase):
 
     def setUp(self):
+        self.issuer_name1 = 'Bank of CFPB'
+        self.issuer_name2 = 'CFPB Bank'
+        self.issuer_name3 = self.issuer_name1.replace(' ', '_')
+
+        self.product_name1 = self.issuer_name2
+        self.product_name2 = self.issuer_name1
+        self.product_name3 = self.issuer_name2.replace(' ', '_')
+
         self.product1 = PrepaidProduct(
-            issuer_name='Bank of CFPB',
+            name=self.product_name1,
+            issuer_name=self.issuer_name1,
             prepaid_type='Tax'
         )
         self.product1.save()
         self.product2 = PrepaidProduct(
-            issuer_name='CFPB Bank',
+            name=self.product_name2,
+            issuer_name=self.issuer_name2,
             prepaid_type='Tax',
             other_relevant_parties='Party'
         )
         self.product2.save()
+        self.product3 = PrepaidProduct(
+            name=self.product_name3,
+            issuer_name=self.issuer_name3,
+            prepaid_type='Tax',
+            other_relevant_parties='Party'
+        )
+        self.product3.save()
 
         effective_date = date(month=2, day=3, year=2019)
         created_date = make_aware(datetime(month=4, day=1, year=2020))
+        self.filename1 = '20200401.pdf'
+        self.filename2 = '20200402.pdf'
+        self.filename3 = '20200403.pdf'
+        self.path1 = 'CFPB/' + self.filename1
+        self.path2 = 'CFPB/' + self.filename2
+        self.path3 = 'CFPB/' + self.filename3
         self.agreement_old = PrepaidAgreement(
+            bulk_download_path = self.path1,
+            filename = self.filename1,
             effective_date=effective_date,
             created_time=created_date - timedelta(hours=1),
             product=self.product1,
         )
         self.agreement_old.save()
         self.agreement_older = PrepaidAgreement(
+            bulk_download_path = self.path2,
+            filename = self.filename2,
             effective_date=effective_date,
             created_time=created_date - timedelta(hours=2),
             product=self.product2,
         )
         self.agreement_older.save()
         self.agreement_new = PrepaidAgreement(
+            bulk_download_path = self.path1,
+            filename = self.filename1,
             effective_date=effective_date,
             created_time=created_date,
             product=self.product1,
         )
         self.agreement_new.save()
+        self.agreement_newer = PrepaidAgreement(
+            bulk_download_path = self.path3,
+            filename = self.filename3,
+            effective_date=effective_date,
+            created_time=created_date + timedelta(hours=1),
+            product=self.product3,
+        )
+        self.agreement_newer.save()
 
     @mock.patch("builtins.open", new_callable=mock.mock_open)
     def test_write_agreements_data(self, mock_open):
@@ -71,40 +108,68 @@ class TestViews(TestCase):
 
         # Make sure expected data rows exist
         mock_file_handle.write.assert_any_call(
-            'Bank of CFPB,,'
+            self.issuer_name1 + ','
+            + self.product_name1 + ','
             + str(self.product1.pk) +
             ',2019-02-03,'
             + str(self.agreement_old.pk) +
             ',No,2020-04-01 03:00:00,,,Tax,,,'
-            'No information provided,,\r\n'
+            'No information provided,'
+            + self.path1 + ',\r\n'
         )
         mock_file_handle.write.assert_any_call(
-            'Bank of CFPB,,'
+            self.issuer_name1 + ','
+            + self.product_name1 + ','
             + str(self.product1.pk) +
             ',2019-02-03,'
             + str(self.agreement_new.pk) +
             ',2020-04-01 04:00:00,,,Tax,,,'
-            'No information provided,,\r\n'
+            'No information provided,'
+            + self.path1 + ',\r\n'
         )
         mock_file_handle.write.assert_any_call(
-            'Bank of CFPB,,'
+            self.issuer_name1 + ','
+            + self.product_name1 + ','
             + str(self.product1.pk) +
             ',2019-02-03,'
             + str(self.agreement_new.pk) +
             ',Yes,2020-04-01 04:00:00,,,Tax,,,'
-            'No information provided,,\r\n'
+            'No information provided,'
+            + self.path1 + ',\r\n'
         )
         mock_file_handle.write.assert_any_call(
-            'CFPB Bank,,'
+            self.issuer_name2 + ','
+            + self.product_name2 + ','
             + str(self.product2.pk) +
             ',2019-02-03,'
             + str(self.agreement_older.pk) +
-            ',2020-04-01 02:00:00,,,Tax,,,Party,,\r\n'
+            ',2020-04-01 02:00:00,,,Tax,,,Party,'
+            + self.path2 + ',\r\n'
         )
         mock_file_handle.write.assert_any_call(
-            'CFPB Bank,,'
+            self.issuer_name2 + ','
+            + self.product_name2 + ','
             + str(self.product2.pk) +
             ',2019-02-03,'
             + str(self.agreement_older.pk) +
-            ',Yes,2020-04-01 02:00:00,,,Tax,,,Party,,\r\n'
+            ',Yes,2020-04-01 02:00:00,,,Tax,,,Party,'
+            + self.path2 + ',\r\n'
+        )
+        mock_file_handle.write.assert_any_call(
+            self.issuer_name3 + ','
+            + self.product_name3 + ','
+            + str(self.product3.pk) +
+            ',2019-02-03,'
+            + str(self.agreement_newer.pk) +
+            ',2020-04-01 05:00:00,,,Tax,,,Party,'
+            + self.path3 + ',\r\n'
+        )
+        mock_file_handle.write.assert_any_call(
+            self.issuer_name3 + ','
+            + self.product_name3 + ','
+            + str(self.product3.pk) +
+            ',2019-02-03,'
+            + str(self.agreement_newer.pk) +
+            ',Yes,2020-04-01 05:00:00,,,Tax,,,Party,'
+            + self.path3 + ',\r\n'
         )
