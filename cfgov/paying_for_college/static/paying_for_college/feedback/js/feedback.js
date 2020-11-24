447031d824f01556f738c436e4274dc4a781a9fe
Anselm Bradford <anselmbradford@users.noreply.github.com>
2020-11-24 10:52:12 -0500

    Add code comment

--- cfgov/paying_for_college/static/paying_for_college/feedback/js/feedback.js
+++ cfgov/paying_for_college/static/paying_for_college/feedback/js/feedback.js
@@ -4,6 +4,10 @@ const feedbackTextarea = feedbackContainer.querySelector( 'textarea' );
 feedbackButton.setAttribute( 'disabled', 'disabled' );
 feedbackTextarea.addEventListener( 'keyup' , feedbackTextareaKeyUp );
 
+/**
+ * Disable the submit button if there is no content in the textarea input.
+ * @param {KeyboardEvent} event - The keyup event object from the textarea.
+ */
 function feedbackTextareaKeyUp( event ) {
     if ( event.target.value === '' ) {
         feedbackButton.setAttribute( 'disabled', 'disabled' );
