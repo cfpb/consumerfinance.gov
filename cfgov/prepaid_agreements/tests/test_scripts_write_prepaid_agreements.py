e6106619ecf5121ef975aa3c50314aac3dfe3d0f
Craig Davies <craig.davies@cfpb.gov>
2020-11-25 07:07:06 -0500

    Update prepaid CSV write test to add S3_PATH and compressed_files_url

--- cfgov/prepaid_agreements/tests/test_scripts_write_prepaid_agreements.py
+++ cfgov/prepaid_agreements/tests/test_scripts_write_prepaid_agreements.py
@@ -10,12 +10,15 @@ from prepaid_agreements.scripts.write_prepaid_agreements_data_to_csv import (
 )
 
 
+S3_PATH = 'https://files.consumerfinance.gov/a/assets/prepaid-agreements/'
+
+
 class TestViews(TestCase):
 
     def setUp(self):
         self.issuer_name1 = 'Bank of CFPB'
         self.issuer_name2 = 'CFPB Bank'
-        self.issuer_name3 = self.issuer_name1.replace(' ', '_')
+        self.issuer_name3 = 'Bank_of CFPB'
 
         self.product_name1 = self.issuer_name2
         self.product_name2 = self.issuer_name1
@@ -44,13 +47,24 @@ class TestViews(TestCase):
 
         effective_date = date(month=2, day=3, year=2019)
         created_date = make_aware(datetime(month=4, day=1, year=2020))
-        self.filename1 = '20200401.pdf'
-        self.filename2 = '20200402.pdf'
-        self.filename3 = '20200403.pdf'
-        self.path1 = 'CFPB/' + self.filename1
-        self.path2 = 'CFPB/' + self.filename2
-        self.path3 = 'CFPB/' + self.filename3
+        self.filename1 = \
+            self.product_name1.replace(' ', '_') + '_01_04_2020.zip'
+        self.filename2 = \
+            self.product_name2.replace(' ', '_') + '_02_04_2020.zip'
+        self.filename3 = \
+            self.product_name3.replace(' ', '_') + '_03_04_2020.zip'
+        self.path1 = \
+            self.issuer_name1 + '/' + self.product_name1 + '/20200401'
+        self.path2 = \
+            self.issuer_name2 + '/' + self.product_name2 + '/20200402'
+        self.path3 = \
+            self.issuer_name3 + '/' + self.product_name3 + '/20200403'
+        self.direct_download1 = S3_PATH + self.filename1
+        self.direct_download2 = S3_PATH + self.filename2
+        self.direct_download3 = S3_PATH + self.filename3
+
         self.agreement_old = PrepaidAgreement(
+            compressed_files_url=self.direct_download1,
             bulk_download_path=self.path1,
             filename=self.filename1,
             effective_date=effective_date,
@@ -59,6 +73,7 @@ class TestViews(TestCase):
         )
         self.agreement_old.save()
         self.agreement_older = PrepaidAgreement(
+            compressed_files_url=self.direct_download2,
             bulk_download_path=self.path2,
             filename=self.filename2,
             effective_date=effective_date,
@@ -67,6 +82,7 @@ class TestViews(TestCase):
         )
         self.agreement_older.save()
         self.agreement_new = PrepaidAgreement(
+            compressed_files_url=self.direct_download1,
             bulk_download_path=self.path1,
             filename=self.filename1,
             effective_date=effective_date,
@@ -75,6 +91,7 @@ class TestViews(TestCase):
         )
         self.agreement_new.save()
         self.agreement_newer = PrepaidAgreement(
+            compressed_files_url=self.direct_download3,
             bulk_download_path=self.path3,
             filename=self.filename3,
             effective_date=effective_date,
@@ -115,7 +132,8 @@ class TestViews(TestCase):
             + str(self.agreement_old.pk) +
             ',No,2020-04-01 03:00:00,,,Tax,,,'
             'No information provided,'
-            + self.path1 + ',\r\n'
+            + self.path1 + ','
+            + self.direct_download1 + '\r\n'
         )
         mock_file_handle.write.assert_any_call(
             self.issuer_name1 + ','
@@ -125,7 +143,8 @@ class TestViews(TestCase):
             + str(self.agreement_new.pk) +
             ',2020-04-01 04:00:00,,,Tax,,,'
             'No information provided,'
-            + self.path1 + ',\r\n'
+            + self.path1 + ','
+            + self.direct_download1 + '\r\n'
         )
         mock_file_handle.write.assert_any_call(
             self.issuer_name1 + ','
@@ -135,7 +154,8 @@ class TestViews(TestCase):
             + str(self.agreement_new.pk) +
             ',Yes,2020-04-01 04:00:00,,,Tax,,,'
             'No information provided,'
-            + self.path1 + ',\r\n'
+            + self.path1 + ','
+            + self.direct_download1 + '\r\n'
         )
         mock_file_handle.write.assert_any_call(
             self.issuer_name2 + ','
@@ -144,7 +164,8 @@ class TestViews(TestCase):
             ',2019-02-03,'
             + str(self.agreement_older.pk) +
             ',2020-04-01 02:00:00,,,Tax,,,Party,'
-            + self.path2 + ',\r\n'
+            + self.path2 + ','
+            + self.direct_download2 + '\r\n'
         )
         mock_file_handle.write.assert_any_call(
             self.issuer_name2 + ','
@@ -153,7 +174,8 @@ class TestViews(TestCase):
             ',2019-02-03,'
             + str(self.agreement_older.pk) +
             ',Yes,2020-04-01 02:00:00,,,Tax,,,Party,'
-            + self.path2 + ',\r\n'
+            + self.path2 + ','
+            + self.direct_download2 + '\r\n'
         )
         mock_file_handle.write.assert_any_call(
             self.issuer_name3 + ','
@@ -162,7 +184,8 @@ class TestViews(TestCase):
             ',2019-02-03,'
             + str(self.agreement_newer.pk) +
             ',2020-04-01 05:00:00,,,Tax,,,Party,'
-            + self.path3 + ',\r\n'
+            + self.path3 + ','
+            + self.direct_download3 + '\r\n'
         )
         mock_file_handle.write.assert_any_call(
             self.issuer_name3 + ','
@@ -171,5 +194,6 @@ class TestViews(TestCase):
             ',2019-02-03,'
             + str(self.agreement_newer.pk) +
             ',Yes,2020-04-01 05:00:00,,,Tax,,,Party,'
-            + self.path3 + ',\r\n'
+            + self.path3 + ','
+            + self.direct_download3 + '\r\n'
         )
