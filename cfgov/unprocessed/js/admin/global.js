fe0cd20f21a6d93c7188395370bdb9e802cb500e
Scott Cranfill <scott.cranfill@cfpb.gov>
2020-12-08 11:19:17 -0500

    Lint and test new global JS for environment banner

--- cfgov/unprocessed/js/admin/global.js
+++ cfgov/unprocessed/js/admin/global.js
@@ -1,4 +1,4 @@
-const env = location.hostname.split('.')[0];
+const env = location.hostname.split( '.' )[0];
 
-const body = document.querySelector('body');
-body.setAttribute('data-env', env);
+const body = document.querySelector( 'body' );
+body.setAttribute( 'data-env', env );
