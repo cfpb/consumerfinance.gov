dfe9a32a6eb9fb4eadb4931619a388681689c164
Craig Davies <craig.davies@cfpb.gov>
2020-12-11 11:34:13 -0500

    Use beforeEach with open

--- test/cypress/integration/pages/obtener-respuestas.js
+++ test/cypress/integration/pages/obtener-respuestas.js
@@ -2,27 +2,27 @@ import { ObtenerRespuestasBuscar } from '../../pages/obtener-respuestas/buscar';
 
 const buscar = new ObtenerRespuestasBuscar();
 
-describe( 'obtener-respuestas', () => {
+describe( 'Obtener Respuestas', () => {
+  beforeEach( () => {
+    buscar.open();
+  } );
   describe( 'Buscar', () => {
     it( 'should autocomplete results', () => {
-      buscar.open();
       buscar.enter( 'divulgación de cierre' );
       buscar.autocomplete().should( 'be.visible' );
     } );
 
     it( 'should return results', () => {
-      buscar.open();
       buscar.enter( 'divulgación de cierre' );
       buscar.search();
       buscar.resultsSection().should( 'be.visible' );
     } );
 
     it( 'should correct spelling', () => {
-      buscar.open();
       buscar.enter( 'vehíclo' );
       buscar.search();
-      buscar.resultsHeader().contains( 'resultados para “vehículo”' );
-      buscar.resultsHeader().siblings( 'p' ).first().contains( 'Busca de' );
+      buscar.resultsHeader().should( 'contain', 'resultados para “vehículo”' );
+      buscar.resultsHeader().siblings( 'p' ).first().should( 'contain', 'Busca de vehíclo' );
     } );
   } );
 } );
