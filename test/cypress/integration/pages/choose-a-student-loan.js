46685688c783db4cbce0ded3e32e572f90308f45
Craig Davies <craig.davies@cfpb.gov>
2020-12-14 11:50:15 -0500

    Removed checking for text that's hard-coded in a template

--- test/cypress/integration/pages/choose-a-student-loan.js
+++ test/cypress/integration/pages/choose-a-student-loan.js
@@ -10,21 +10,15 @@ describe( 'Paying for College', () => {
     it( 'should display answers to each question', () => {
       cy.get( '#_o1' ).click();
       cy.get( '#_o1' ).should( 'be.visible' )
-        .and( 'contain', 'I have to borrow money for school. What are my options?' );
       cy.get( '#answer1' ).should( 'be.visible' )
-        .and( 'contain', 'If you have to take out student loans' );
 
       cy.get( '#_o2' ).click();
       cy.get( '#_o2' ).should( 'be.visible' )
-        .and( 'contain', "What if my grants and federal loans don't cover the cost of attendance?" );
       cy.get( '#answer2' ).should( 'be.visible' )
-        .and( 'contain', 'If your grants and federal loans are not enough to cover the cost of your education' );
 
       cy.get( '#_o3' ).click();
       cy.get( '#_o3' ).should( 'be.visible' )
-        .and( 'contain', 'What should I consider when shopping for a private loan?' );
       cy.get( '#answer3' ).should( 'be.visible' )
-        .and( 'contain', 'First, make sure you need a private student loan' );
     } );
     it( 'should display expandable comparison tables', () => {
       page.clickExpandable( 'Detailed comparison of Federal and Private loans' );
@@ -45,25 +39,20 @@ describe( 'Paying for College', () => {
     it( 'should display answers to each question', () => {
       page.clickBubble( "What's the difference between subsidized and unsubsidized student loans?" );
       cy.get( '.bubble-transparent-answer' ).should( 'be.visible' )
-        .and( 'contain', 'The government pays the interest on subsidized loans' );
       page.closeFirstBubble();
       cy.get( '.bubble-transparent-answer' ).should( 'not.be.visible' );
 
       page.clickBubble( 'What happened to Stafford Loans?' );
       cy.get( '.bubble-transparent-answer' ).should( 'be.visible' )
-        .and( 'contain', 'These are now called Federal Direct Loans' );
 
       page.clickBubble( 'How often do student loan rates change?' );
       cy.get( '.bubble-transparent-answer' ).should( 'be.visible' )
-        .and( 'contain', 'Congress has the authority to change federal student loan rates' );
 
       page.clickBubble( 'Should I use a credit card to cover my education costs?' );
       cy.get( '.bubble-transparent-answer' ).should( 'be.visible' )
-        .and( 'contain', "Don't replace student loan debt with credit card debt" );
 
       page.clickBubble( "What if I can't repay my private student loan?" );
       cy.get( '.bubble-transparent-answer' ).should( 'be.visible' )
-        .and( 'contain', 'Contact the company that services your student loan immediately' );
       page.closeLastBubble();
 
       page.closeAllBubbles();
--- test/cypress/integration/pages/manage-your-college-money.js
+++ test/cypress/integration/pages/manage-your-college-money.js
@@ -10,21 +10,15 @@ describe( 'Paying for College', () => {
     it( 'should display answers to each question', () => {
       cy.get( '#_o1' ).click();
       cy.get( '#_o1' ).should( 'be.visible' )
-        .and( 'contain', 'When should I get a bank account?' );
       cy.get( '#answer1' ).should( 'be.visible' )
-        .and( 'contain', 'Choose an account as soon as possible' );
 
       cy.get( '#_o2' ).click();
       cy.get( '#_o2' ).should( 'be.visible' )
-        .and( 'contain', 'How do I avoid paying unexpected fees?' );
       cy.get( '#answer2' ).should( 'be.visible' )
-        .and( 'contain', 'very few accounts charge no fees at all' );
 
       cy.get( '#_o3' ).click();
       cy.get( '#_o3' ).should( 'be.visible' )
-        .and( 'contain', 'Do I have to get an account with the bank at my school?' );
       cy.get( '#answer3' ).should( 'be.visible' )
-        .and( 'contain', 'Schools cannot require you to use their bank' );
     } );
     it( 'should display expandable comparison tables', () => {
       page.clickExpandable( 'View Banking Options' );
@@ -42,14 +36,12 @@ describe( 'Paying for College', () => {
     it( 'should display answers to each question', () => {
       page.clickBubble( 'What is a financial aid disbursement?' );
       cy.get( '.bubble-transparent-answer' ).should( 'be.visible' )
-        .and( 'contain', 'Your financial aid disbursement is the money left' );
 
       page.closeFirstBubble();
       cy.get( '.bubble-transparent-answer' ).should( 'not.be.visible' );
 
       page.clickBubble( 'What are overdraft fees and how can I avoid them?' );
       cy.get( '.bubble-transparent-answer' ).should( 'be.visible' )
-        .and( 'contain', 'When you spend more money than you have in your account' );
 
       page.closeLastBubble();
       cy.get( '.bubble-transparent-answer' ).should( 'not.be.visible' );
