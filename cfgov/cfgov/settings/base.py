2bd260924ca058a346082c52e964759b92fa5320
Will Barton <william.barton@cfpb.gov>
2021-01-11 11:55:03 -0500

    Remove old clock drift check
    This change removes the Django-Watchman checks we have that we're not using.

--- cfgov/cfgov/settings/base.py
+++ cfgov/cfgov/settings/base.py
@@ -770,18 +770,7 @@ WATCHMAN_TOKENS = os.environ.get("WATCHMAN_TOKENS", os.urandom(32))
 # This specifies what checks Watchman should run and include in its output
 # https://github.com/mwarkentin/django-watchman#custom-checks
 WATCHMAN_CHECKS = (
-    "watchman.checks.databases",
-    "watchman.checks.storage",
-    "watchman.checks.caches",
-    "alerts.checks.check_clock_drift",
 )
-
-# Used to check server's time against in check_clock_drift
-NTP_TIME_SERVER = "north-america.pool.ntp.org"
-
-# If server's clock drifts from NTP by more than specified offset
-# (in seconds), check_clock_drift will fail
-MAX_ALLOWED_TIME_OFFSET = 5
 
 # Search.gov values
 SEARCH_DOT_GOV_AFFILIATE = os.environ.get("SEARCH_DOT_GOV_AFFILIATE")
