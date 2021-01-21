44cb6e9f030eba728d48d47d0729f91e8ff45002
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2021-01-21 16:09:24 -0500

    New Enforcement Actions API endpoint
    New API endpoint at /api/enforcement_actions/v1/ returns a JSON
    serialization of all Enforcement Action Pages that are children of the
    EnforcementActionFilterPage in the page tree.
    
    This will be used to power the charts displaying data about
    enforcement actions over time.
    
    Script to generate chart data json
    
    return array reverse sorted by initial filing date
    
    Filter chart implementation
    
    frontend simple chart caching
    
    cache promise then data for simple chart
    
    fix linting
    
    Chart migration
    
    Match API response and fixup bar categories on selection
    
    Style updates
    
    generalize chart hook updates on filter
    
    Remove conflicting migration
    
    Further style updates
    
    Further style updates
    
    additional spacing adjustments
    
    size based off content box
    
    simple chart h3 small screen spacing
    
    more chart spacing
    
    Trim subtitle paragraph spacing on mobile
    
    include papaparse
    
    In progress csv+multi series
    
    sub-data types and working series/x-axis-data for datetime
    
    allow storing of functions in simple-chart style objects
    
    hover behavior, legend, series color
    
    multi-series styles
    
    reorder dash styles
    
    Limit enforcement-db to bar charts
    
    dashstyle in css and adjusted colors
    
    add migration
    
    tie line marker css to highcharts line classes
    
    navigator and rangeselector for simplechart (and highcharts 9.0.1)
    
    style updates
    
    revert highcharts upgrade
    
    time-based selects
    
    in prog legend
    
    decent legend alignment at all screen sizes and series counts

--- cfgov/unprocessed/js/routes/on-demand/simple-chart/chart-hooks.js
+++ cfgov/unprocessed/js/routes/on-demand/simple-chart/chart-hooks.js
@@ -1,4 +1,13 @@
 const hooks = {
+  filter( data, filterProp, filterVal ) {
+    if ( !filterVal ) return data;
+    return data.filter( d => {
+      const match = d[filterProp];
+      if ( Array.isArray( match ) ) return match.indexOf( filterVal ) >= 0;
+      return match === filterVal;
+    } );
+  },
+
   monotonicY( data ) {
     return data.map( ( item, i ) => ( {
       ...item,
@@ -6,33 +15,123 @@ const hooks = {
     } ) );
   },
 
-  enforcement_barCount( data ) {
-    const years = {};
-    data.forEach( d => {
-      const year = new Date( d.x ).getFullYear();
-      if ( years[year] ) years[year]++;
-      else years[year] = 1;
+  enforcement_relief( data ) {
+    const dispositions = [];
+    data.forEach( val => {
+      const disp = val.enforcement_dispositions;
+      if ( disp ) {
+        disp.forEach( v => {
+          if ( v.final_order_date ) {
+            v.url = val.url;
+            dispositions.push( v );
+          }
+        } );
+      }
     } );
-    return Object.keys( years )
-      .sort()
-      .map( k => ( { name: k, y: years[k] } ) );
+    dispositions.sort( ( a, b ) => Number( new Date( a.final_order_date ) ) -
+      Number( new Date( b.final_order_date ) ) );
+
+    let total = 0;
+    return dispositions.map( v => {
+      const relief = v.final_order_consumer_redress +
+        v.final_order_other_consumer_relief;
+      total += relief;
+
+      return {
+        x: Number( new Date( v.final_order_date ) ),
+        y: total,
+        name: v.final_disposition,
+        relief: relief,
+        url: v.url
+      };
+    } );
+  },
+
+
+  csv_test( data ) {
+    return data.map( d => ( {
+      name: d.Quarter,
+      y: Number( d['18-30'] )
+    } ) );
+  },
+
+  csv_datetime( data ) {
+    const q2m = { 1: '01', 2: '04', 3: '07', 4: '10' };
+    return data.map( d => {
+      const qtr = d.Quarter;
+      return {
+        x: new Date( `${ qtr.slice( 0, 4 ) }-${ q2m[qtr.slice( -1 )] }-01` ),
+        y: Number( d['18-30'] )
+      };
+    } );
+  },
+
+  csv_t2( data ) {
+    return data.map( d => Number( d['18-30'] )
+    );
+  },
+
+  csv_cats( data ) {
+    return data.map( d => d.name );
+  },
+
+  enforcement_counts( data ) {
+    const d = data.slice().sort(
+      ( a, b ) => Number( new Date( a.initial_filing_date ) ) -
+          Number( new Date( b.initial_filing_date ) )
+    );
+
+    let total = 0;
+    return d.map( v => {
+      total++;
+
+      return {
+        x: Number( new Date( v.initial_filing_date ) ),
+        y: total,
+        name: v.public_enforcement_action,
+        url: v.url
+      };
+    } );
   },
 
   enforcement_reliefCount( data ) {
     const years = {};
     data.forEach( d => {
-      const year = new Date( d.x ).getFullYear();
-      const relief = Number( d.relief.replace( /[,\.]/g, '' ) ) / 100;
-      if ( years[year] ) years[year] += relief;
-      else years[year] = relief;
+      const disp = d.enforcement_dispositions;
+      if ( disp ) {
+        disp.forEach( v => {
+          if ( v.final_order_date ) {
+            const year = new Date( v.final_order_date ).getFullYear();
+            const relief = v.final_order_consumer_redress +
+              v.final_order_other_consumer_relief;
+            if ( years[year] ) years[year] += relief;
+            else years[year] = relief;
+          }
+        } );
+      }
     } );
     return Object.keys( years )
       .sort()
       .map( k => ( { name: k, y: years[k] } ) );
   },
 
-  enforcement_barCategories( data ) {
-    return data.map( d => d.name );
+  enforcement_barCount( data ) {
+    const years = {};
+
+    data.forEach( d => {
+      const year = new Date( d.initial_filing_date ).getFullYear();
+      if ( years[year] ) years[year]++;
+      else years[year] = 1;
+    } );
+
+    return Object.keys( years )
+      .sort()
+      .map( k => ( { name: k, y: years[k] } ) );
+  },
+
+
+  enforcement_barCategories( { transformed } ) {
+    return transformed.map( d => d.name );
   },
 
   enforcement_yAxisLabelsFormatter() {
