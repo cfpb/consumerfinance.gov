7f2cca8db2f1015a8a641eb51f2cf006e4a5674e
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2021-02-11 15:17:46 -0500

    Limit enforcement-db to bar charts

--- cfgov/unprocessed/js/routes/on-demand/simple-chart/chart-hooks.js
+++ cfgov/unprocessed/js/routes/on-demand/simple-chart/chart-hooks.js
@@ -35,49 +35,16 @@ const hooks = {
     return data.map( d => d.name );
   },
 
-  enforcement_reliefTooltipFormatter() {
-    const { x, y, name, relief, url } = this.points[0].point.options;
-    return (
-      `<a style="padding: 15px;" rel="noopener noreferrer" target="_blank" href="${ url }">` +
-      `<div style="margin-bottom:0.5em"><b>${ new Date( x ).toLocaleDateString(
-        'en-US',
-        {
-          dateStyle: 'medium'
-        }
-      ) }</b><br/>` +
-      `Total relief to date: <b>$${ y.toLocaleString() }</b></div>` +
-      `<span class="a-link" style="max-width: 290px; font-weight:500;">${ name }</span><br/>` +
-      `Relief from action: <b>$${ relief }</b></a>`
-    );
-  },
-
   enforcement_yAxisLabelsFormatter() {
     return `$${ Math.round( this.value / 1e9 ) }B`;
   },
 
-  enforcement_actionsTooltipFormatter() {
-    const { x, y, name, url } = this.points[0].point.options;
-    return (
-      `<a style="padding: 15px;" rel="noopener noreferrer" target="_blank" href="${ url }">` +
-      `<div style="margin-bottom:0.5em"><b>${ new Date( x ).toLocaleDateString(
-        'en-US',
-        {
-          dateStyle: 'medium'
-        }
-      ) }</b><br/>` +
-      `Total actions to date: <b>${ y }</b></div>` +
-      `<span class="a-link" style="max-width: 290px; font-weight:500;">${ name }</span></a>`
-    );
-  },
-
   enforcement_barTooltipFormatter() {
-    const { y, name } = this.points[0].point.options;
-    return `<b>${ name }</b><br/>Total enforcement actions: <b>${ y }</b>`;
+    return `<b>${ this.x }</b><br/>Total enforcement actions: <b>${ this.y }</b>`;
   },
 
   enforcement_reliefBarTooltipFormatter() {
-    const { y, name } = this.points[0].point.options;
-    return `<b>${ name }</b><br/>Total relief: <b>$${ y.toLocaleString() }</b>`;
+    return `<b>${ this.x }</b><br/>Total relief: <b>$${ this.y.toLocaleString() }</b>`;
   }
 };
 
