750c8c05a829a2dc4358a9edf078de424d5a28ef
Alex Martin <alexander.martin@cfpb.gov>
2021-02-11 11:49:49 -0500

    Cleanup document

--- cfgov/v1/documents.py
+++ cfgov/v1/documents.py
@@ -1,6 +1,8 @@
 from django_elasticsearch_dsl import Document, fields
 from django_elasticsearch_dsl.registries import registry
 
+from django.core.exceptions import FieldDoesNotExist
+
 from v1.models.blog_page import BlogPage, LegacyBlogPage
 from v1.models.enforcement_action_page import EnforcementActionPage
 from v1.models.learn_page import (
@@ -42,37 +44,27 @@ class FilterablePagesDocument(Document):
             content = content_field.get_searchable_content(value)
             content = content.pop()
             return content
-        except Exception:
+        except FieldDoesNotExist:
             return None
 
     def prepare_url(self, instance):
         return instance.url
 
     def prepare_start_dt(self, instance):
-        try:
-            return instance.specific.start_dt
-        except Exception:
-            return None
+        return getattr(instance.specific, 'start_dt', None)
 
     def prepare_end_dt(self, instance):
-        try:
-            return instance.specific.end_dt
-        except Exception:
-            return None
+        return getattr(instance.specific, 'end_dt', None)
 
     def prepare_statuses(self, instance):
-        try:
-            return [status.status
-                    for status in
-                    instance.specific.statuses.all()]
-        except Exception:
+        statuses = getattr(instance.specific, 'statuses', None)
+        if statuses is not None:
+            return [status.status for status in statuses.all()]
+        else:
             return None
 
     def prepare_initial_filing_date(self, instance):
-        try:
-            return instance.specific.initial_filing_date
-        except Exception:
-            return None
+        return getattr(instance.specific, 'initial_filing_date', None)
 
     def get_instances_from_related(self, related_instance):
         # Related instances all inherit from AbstractFilerPage
@@ -93,7 +85,7 @@ class FilterablePagesDocument(Document):
         ]
 
     class Index:
-        name = 'test'
+        name = 'filterable-pages'
 
 
 class FilterablePagesDocumentSearch:
@@ -177,15 +169,9 @@ class EventFilterablePagesDocumentSearch(FilterablePagesDocumentSearch):
 
 class EnforcementActionFilterablePagesDocumentSearch(FilterablePagesDocumentSearch):  # noqa: E501
 
-    def __init__(self,
-                 prefix='/', topics=[], categories=[],
-                 authors=[], to_date=None, from_date=None,
-                 title='', archived=None, statuses=[]):
-        super(EnforcementActionFilterablePagesDocumentSearch, self).__init__(
-            prefix, topics, categories, authors,
-            to_date, from_date, title, archived
-        )
-        self.statuses = statuses
+    def __init__(self, **kwargs):
+        self.statuses = kwargs.pop('statuses')
+        super().__init__(**kwargs)
 
     def filter_date(self, search):
         return search.filter(
