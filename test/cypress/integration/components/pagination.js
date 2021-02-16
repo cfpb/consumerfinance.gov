ca43183b47d313753cf105bc8df01615a8357380
Craig Davies <craig.davies@cfpb.gov>
2021-02-16 16:33:15 -0500

    Move navigate to first page in pagination cypress test

--- test/cypress/integration/components/pagination.js
+++ test/cypress/integration/components/pagination.js
@@ -6,6 +6,16 @@ describe( 'Pagination molecule to navigate on the filterable pages', () => {
   beforeEach( () => {
     cy.visit( '/about-us/blog/' );
   } );
+  it( 'should be able to navigate to the first page', () => {
+    // When I click on the "next" button
+    page.clickButton( 'next' );
+    // When I enter "1" in the page input field
+    page.enter( '1' );
+    // And I click on the "submit" button
+    page.clickButton( 'submit' );
+    // Then the page url should contain "page=1"
+    cy.url().should( 'include', 'page=1' );
+  } );
   it( 'should be able to navigate to the next page', () => {
     // When I click on the "next" button
     page.clickButton( 'next' );
@@ -25,16 +35,6 @@ describe( 'Pagination molecule to navigate on the filterable pages', () => {
     page.clickButton( 'next' );
     // And I click on the "previous" button
     page.clickButton( 'prev' );
-    // Then the page url should contain "page=1"
-    cy.url().should( 'include', 'page=1' );
-  } );
-  it( 'should be able to navigate to the first page', () => {
-    // When I click on the "next" button
-    page.clickButton( 'next' );
-    // When I enter "1" in the page input field
-    page.enter( '1' );
-    // And I click on the "submit" button
-    page.clickButton( 'submit' );
     // Then the page url should contain "page=1"
     cy.url().should( 'include', 'page=1' );
   } );
