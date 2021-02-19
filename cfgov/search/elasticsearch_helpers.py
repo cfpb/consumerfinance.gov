598e24da01dfc7f2d6771368e6fdaee5f3d3fe18
Andy Chosak <andy.chosak@cfpb.gov>
2021-02-19 13:34:48 -0500

    Fix test coverage

--- cfgov/search/elasticsearch_helpers.py
+++ cfgov/search/elasticsearch_helpers.py
@@ -95,7 +95,7 @@ class ElasticsearchTestsMixin:
         # want the tests to fail.
         try:
             socket.create_connection((settings.ES7_HOST, settings.ES_PORT))
-        except OSError as e:
+        except OSError as e:  # pragma: nocover
             if os.getenv('GITHUB_ACTIONS'):
                 raise
 
