d3065ccb765d6ec9472e319512b1d0db6022f2df
Craig Davies <craig.davies@cfpb.gov>
2021-02-24 18:25:50 -0500

    Fix accessibility issues in owning-a-home dropdown-utils unit test

--- test/unit_tests/apps/owning-a-home/js/dropdown-utils-spec.js
+++ test/unit_tests/apps/owning-a-home/js/dropdown-utils-spec.js
@@ -95,10 +95,12 @@ describe( 'Dropdown utils', () => {
   } );
 
   it( 'should select several select elements', () => {
-    document.body.innerHTML +=
-      '<select id="foo1"><option value="baz1"></option></select>';
-    document.body.innerHTML +=
-      '<select id="foo2"><option value="baz1"></option></select>';
+    document.body.innerHTML += `
+<select id="foo1"><option value="baz1"></option></select>
+`;
+    document.body.innerHTML += `
+<select id="foo2"><option value="baz1"></option></select>
+`;
     dropDownUtils( [ 'foo', 'foo2' ] ).disable();
     expect( document.querySelectorAll( 'select :disabled' ).length ).toBe( 2 );
     dropDownUtils( [ 'foo', 'foo1', 'foo2' ] ).enable();
