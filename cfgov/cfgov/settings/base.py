68c370b8aa84c6eb5b65db1dd5a810e115e14cbe
Craig Davies <30801483+cwdavies@users.noreply.github.com>
2021-02-25 16:10:16 -0500

    Hide archive filter options in the filterable UI (#6281)
    * Enable or disable archive filter options on request-dependent properties
    * Set archive filter options to be enabled by default
    * Add new class for testing context of pages with Filterable Lists
    
    Co-Authored-By: Will Barton <willbarton@users.noreply.github.com>
    Co-authored-by: Scott Cranfill <scott.cranfill@cfpb.gov>

--- cfgov/cfgov/settings/base.py
+++ cfgov/cfgov/settings/base.py
@@ -764,6 +764,8 @@ FLAGS = {
     # Controls whether or not to include Qualtrics Web Intercept code for the
     # Q42020 Ask CFPB customer satisfaction survey.
     "ASK_SURVEY_INTERCEPT": [],
+    # Hide archive filter options in the filterable UI
+    "HIDE_ARCHIVE_FILTER_OPTIONS": [],
     # Enable ES as the backend for FilterableLists
     "ELASTICSEARCH_FILTERABLE_LISTS": []
 }
