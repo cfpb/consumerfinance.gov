19eea3b2d718971e8a9b9508f52c4796b2637ada
Anselm Bradford <anselmbradford@users.noreply.github.com>
2021-03-09 09:53:45 -0500

    Change heading size | change indenting

--- cfgov/paying_for_college/forms.py
+++ cfgov/paying_for_college/forms.py
@@ -3,5 +3,5 @@ from django import forms
 
 class FeedbackForm(forms.Form):
     message = forms.CharField(widget=forms.Textarea(attrs={
-            'class': 'a-text-input',
-        }))
+        'class': 'a-text-input',
+    }))
