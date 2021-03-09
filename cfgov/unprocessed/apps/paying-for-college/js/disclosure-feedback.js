e9ea3eabea2cbb69291f55dd4dc9b64232714ad9
Anselm Bradford <anselmbradford@users.noreply.github.com>
2021-03-09 10:22:42 -0500

    Linting fixes

--- cfgov/unprocessed/apps/paying-for-college/js/disclosure-feedback.js
+++ cfgov/unprocessed/apps/paying-for-college/js/disclosure-feedback.js
@@ -2,16 +2,16 @@ const feedbackContainer = document.querySelector( '#feedback-container' );
 const feedbackButton = feedbackContainer.querySelector( 'button' );
 const feedbackTextarea = feedbackContainer.querySelector( 'textarea' );
 feedbackButton.setAttribute( 'disabled', 'disabled' );
-feedbackTextarea.addEventListener( 'keyup' , feedbackTextareaKeyUp );
+feedbackTextarea.addEventListener( 'keyup', feedbackTextareaKeyUp );
 
 /**
  * Disable the submit button if there is no content in the textarea input.
  * @param {KeyboardEvent} event - The keyup event object from the textarea.
  */
 function feedbackTextareaKeyUp( event ) {
-    if ( event.target.value === '' ) {
-        feedbackButton.setAttribute( 'disabled', 'disabled' );
-    } else {
-        feedbackButton.removeAttribute( 'disabled' );
-    }
+  if ( event.target.value === '' ) {
+    feedbackButton.setAttribute( 'disabled', 'disabled' );
+  } else {
+    feedbackButton.removeAttribute( 'disabled' );
+  }
 }
