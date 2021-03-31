778cb38153e4adddf452432b2b62929e10f83cf3
Craig Davies <30801483+cwdavies@users.noreply.github.com>
2021-03-31 18:13:24 -0400

    Organize functional Cypress tests to run in groups on Jenkins (#6303)
    * Update cypress to 6.8.0
    * Disable screenshot capture
    * Run functional tests in groups
    * Add cypress environment variables
    * Add a series of Cypress containers
    * Split functional tests into multiple groups
    * Create docker network if it does not exist
    * Add environment to Functional Test stage
    * Update test status reported back to GitHub
    * Increase timeout to 15min for all test stages
    * Add CFGOV_HOSTNAME variable in .env_SAMPLE
    * Add postGitHubStatus for each functional test stage
    * Automatically remove cypress container when it exits
    * Add parallel stages to Run Functional Tests in Jenkinsfile
    * Run multiple containers in parallel on a single docker node
    * Add cypress integration page tests into consumer-tools folder
    * Create a unique name for docker containers using stack and stage name
    * Update Functional Test stage to run tests in parallel using multiple containers
    * Fix agent is not allowed in Functional Tests stage
    * Update docker-compose.e2e test documentation
    
    * Fix TableEditor in admin.js test
    * Add retry to check css of table-block-modal
    * Update selectDocumentLink in admin.js test
    * Add test to closeTableEditor and use aliases in admin.js
    * Add beforeEach and afterEach to TableBlock admin test
    
    * Fix WorkflowScript: agent is not allowed in stage Run Tests as it contains parallel or matrix stages

--- test/cypress/integration/pages/consumer-tools/owning-a-home.js
+++ test/cypress/integration/pages/consumer-tools/owning-a-home.js
@@ -1,4 +1,4 @@
-import { ExploreRates } from '../../../pages/owning-a-home/explore-rates';
+import { ExploreRates } from '../../../pages/consumer-tools/owning-a-home/explore-rates';
 
 const exploreRates = new ExploreRates();
 
