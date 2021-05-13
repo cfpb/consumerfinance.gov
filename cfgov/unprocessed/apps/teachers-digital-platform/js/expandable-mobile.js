632ed6a6080fc69e4cd51e9d50135fae7c917717
Steve Clay <steve@mrclay.org>
2021-05-13 14:34:25 -0400

    Fix some linting issues

--- cfgov/unprocessed/apps/teachers-digital-platform/js/expandable-mobile.js
+++ cfgov/unprocessed/apps/teachers-digital-platform/js/expandable-mobile.js
@@ -5,12 +5,21 @@ const MOBILE_WIDTH = 800;
 
 let innerWidth;
 
+/**
+ * Get the viewport width
+ *
+ * @returns {number} The viewport width
+ */
 function getInnerWidth() {
   return innerWidth || window.innerWidth;
 }
 
-// For unit tests
-function setInnerWidth(width) {
+/**
+ * Fix the output of getInnerWidth() for test purposes
+ *
+ * @param {number} width Width
+ */
+function setInnerWidth( width ) {
   innerWidth = width;
 }
 
@@ -22,14 +31,14 @@ function beforeExpandableTransitionInit() {
   const nodeList = document.querySelectorAll( '.' + MOBILE_COLLAPSED_CLASS );
 
   // IE11 lacks NodeList.forEach
-  [].forEach.call(nodeList, el => {
+  [].forEach.call( nodeList, el => {
     if ( getInnerWidth() <= MOBILE_WIDTH ) {
       el.classList.remove( ExpandableTransition.CLASSES.OPEN_DEFAULT );
     }
 
     // Always clean up this class, just used at init time
     el.classList.remove( MOBILE_COLLAPSED_CLASS );
-  });
+  } );
 }
 
 export { setInnerWidth, MOBILE_COLLAPSED_CLASS };
