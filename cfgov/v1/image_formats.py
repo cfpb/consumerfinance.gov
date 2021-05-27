3c278e2d6a6df42d6dd35ac6045676e813691541
Chris Contolini <contolini@users.noreply.github.com>
2021-05-27 15:33:36 -0700

    Add new story page type (#6460)
    * Add new story page type
    
    Adds a new "Story Page" content type with a sidebar-less centered column
    and images that bleed into the left and right gutters. It's use case is
    prose-heavy landing pages designed to tell a story.
    
    To let editors choose when they want either RichTextBlock images
    or FullWidthText images to bleed into the gutters, a 'bleed' format
    option has been added to both image fields. Selecting this option
    for an image that's not on a story page will have no affect (it'll
    be treated as a standard full width image).
    
    https://docs.wagtail.io/en/stable/advanced_topics/customisation/page_editing_interface.html#rich-text-image-formats
    
    * Properly sort imports to satisfy linter
    
    * Clean up BEM formatting, class names
    
    * Update cfgov/unprocessed/css/enhancements/layout.less
    
    Co-authored-by: Scott Cranfill <scott.cranfill@cfpb.gov>

---
+++ cfgov/v1/image_formats.py
@@ -0,0 +1,11 @@
+from wagtail.images.formats import Format, register_image_format
+
+
+register_image_format(
+    Format(
+        'bleed',
+        'Bleed into left/right margins',
+        'richtext-image image-bleed',
+        'width-1170'
+    )
+)
