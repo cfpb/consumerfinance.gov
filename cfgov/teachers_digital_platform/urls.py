9eed61a25603bdf8d208fa7dcfd76b48f2bfefab
Steve Clay <steve@mrclay.org>
2021-06-10 23:30:02 -0400

    POC of survey form

--- cfgov/teachers_digital_platform/urls.py
+++ cfgov/teachers_digital_platform/urls.py
@@ -1,8 +1,19 @@
-from django.urls import re_path
+from django.urls import path, re_path
 from django.views.generic import TemplateView
+
+from teachers_digital_platform.forms import get_pages
+from teachers_digital_platform.views import SurveyWizard
 
 from wagtailsharing.views import ServeView
 
+
+pages = get_pages('gr3')
+
+contact_wizard = SurveyWizard.as_view(
+    form_list=pages['form_list'],
+    url_name='survey_step',
+    template_name='teachers_digital_platform/survey-page.html',
+)
 
 urlpatterns = [
     re_path(
@@ -11,6 +22,21 @@ urlpatterns = [
             template_name='teachers_digital_platform/bb-tool.html'
         )
     ),
+
+    path(
+        'survey/results/',
+        TemplateView.as_view(
+            template_name='teachers_digital_platform/survey-results.html'
+        )
+    ),
+
+    re_path(
+        r'^survey/(?P<step>.+)/$',
+        contact_wizard,
+        name='survey_step'
+    ),
+
+    path('survey/', contact_wizard, name='survey'),
 
     re_path(
         r'^$',
--- cfgov/teachers_digital_platform/views.py
+++ cfgov/teachers_digital_platform/views.py
@@ -1,3 +1,34 @@
-# from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
+from formtools.wizard.views import NamedUrlCookieWizardView
 
-# Create your views here.
+from teachers_digital_platform.forms import get_pages
+
+
+surveys_dict = {}
+surveys_dict['gr3'] = get_pages('gr3')['question_dict']
+
+class SurveyWizard(NamedUrlCookieWizardView):
+    def done(self, form_list, **kwargs):
+        first_page = self.get_form('page1')
+        survey_key = first_page.fields['_sk'].initial
+        
+        data = self.get_all_cleaned_data()
+        total = 0
+        answers = []
+
+        for key, question in surveys_dict[survey_key].items():
+            answer = int(data[key])
+            answers.append(str(answer))
+            total += question.scores[answer]
+
+        encoded = ''.join(answers) + ':' + str(total)
+        
+        response = HttpResponseRedirect('../results/')
+        response.set_signed_cookie('resultUrl', encoded)
+        return response
+
+    def process_step(self, form):
+        # By default, the big CSRF tokens get needlessly stored in the cookie and
+        # take up a lot of space. This is bad because cookies have a small limit.
+        dict = self.get_form_step_data(form)
+        return {key:val for key, val in dict.items() if key != 'csrfmiddlewaretoken'}
