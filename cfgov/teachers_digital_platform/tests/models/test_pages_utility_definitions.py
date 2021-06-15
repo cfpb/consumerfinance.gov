440ee93ef48886d0832fbc81ffbf7e3f2d6c80f1
Anselm Bradford <anselmbradford@users.noreply.github.com>
2021-06-15 12:57:15 -0400

    Update unit test to check for 25 instead of 5

--- cfgov/teachers_digital_platform/tests/models/test_pages_utility_definitions.py
+++ cfgov/teachers_digital_platform/tests/models/test_pages_utility_definitions.py
@@ -14,7 +14,7 @@ class PagingTestCases(TestCase, WagtailTestUtils):
 
     def test_validate_results_per_page_default_id_five(self):
         mock_request = HttpRequest()
-        default_per_page = 5
+        default_per_page = 25
         results_per_page = validate_results_per_page(mock_request)
         self.assertEqual(results_per_page, default_per_page)
 
