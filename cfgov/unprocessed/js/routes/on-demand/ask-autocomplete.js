dfa79c4bd0cd88689d690c90c975dd46ab8010f9
Nicholas Johnson <nicholas.johnson@cfpb.gov>
2021-07-27 12:02:04 -0400

    Refactor

--- cfgov/unprocessed/js/routes/on-demand/ask-autocomplete.js
+++ cfgov/unprocessed/js/routes/on-demand/ask-autocomplete.js
@@ -10,12 +10,18 @@ const URLS = {
 };
 
 const autocompleteContainer = document.querySelector( '.m-autocomplete' );
+const errorMessage = document.querySelector( '#o-search-bar-error_message' );
+
+function handleMaxCharacters() {
+  console.log( event );
+}
 
 if ( autocompleteContainer ) {
   const language = autocompleteContainer.getAttribute( 'data-language' );
 
   const autocomplete = new Autocomplete( autocompleteContainer, {
     url: language === 'es' ? URLS.es : URLS.en,
+    errorMessage: errorMessage,
     onSubmit: function( event, selected ) {
       const link = selected.querySelector( 'a' );
       const href = link.getAttribute( 'href' );
@@ -33,5 +39,6 @@ if ( autocompleteContainer ) {
       return li;
     }
   } );
+  autocomplete.addEventListener( 'maxCharacterChange', handleMaxCharacters );
   autocomplete.init();
 }
