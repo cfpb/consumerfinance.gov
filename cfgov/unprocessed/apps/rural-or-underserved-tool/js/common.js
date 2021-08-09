587d3d0deea34faffc8db5631440541edb16f367
Anselm Bradford <anselmbradford@users.noreply.github.com>
2021-08-09 11:29:12 -0400

    Remove unhandled error parameter to satisfy linter

--- cfgov/unprocessed/apps/rural-or-underserved-tool/js/common.js
+++ cfgov/unprocessed/apps/rural-or-underserved-tool/js/common.js
@@ -82,7 +82,7 @@ window.callbacks.censusAPI = function( data, rural ) {
         addressUtils.render( result );
         count.updateCount( result.type );
       } ) )
-      .catch( function( error ) {
+      .catch( function() {
         const addressElement = DT.createEl( '<li>' + result.address + '</li>' );
         DT.addEl( DT.getEl( '#process-error-desc' ), addressElement );
         DT.removeClass( '#process-error', 'u-hidden' );
