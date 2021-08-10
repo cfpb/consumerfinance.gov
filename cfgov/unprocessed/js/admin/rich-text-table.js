1a160681e74a3e3f04d5299716f8117f03ce15ea
Will Barton <william.barton@cfpb.gov>
2021-08-10 10:41:06 -0400

    WIP move some of the code around, and try to style draftail

--- cfgov/unprocessed/js/admin/rich-text-table.js
+++ cfgov/unprocessed/js/admin/rich-text-table.js
@@ -2,6 +2,7 @@ import { stateToHTML } from 'draft-js-export-html';
 
 // https://handsontable.com/docs/7.2.2/tutorial-cell-editor.html
 (function(Handsontable){
+  // We're extending the base TextEditor, https://github.com/handsontable/handsontable/blob/master/src/editors/textEditor/textEditor.js
   const RichTextEditor = Handsontable.editors.TextEditor.prototype.extend();
 
   const draftail_options = {
@@ -62,55 +63,104 @@ import { stateToHTML } from 'draft-js-export-html';
     ]
   }
 
-  RichTextEditor.prototype.init = function() {
-    Handsontable.editors.TextEditor.prototype.init.call(this);
-    this.EDITOR = null;
-    this.TEXTAREA = document.createElement('input');
+  // Do some light modification to the TextEditor's TEXTAREA element to
+  // prepare it for Draftail.
+  RichTextEditor.prototype.createElements = function() {
+    Handsontable.editors.TextEditor.prototype.createElements.call(this);
+
+    // TextEditor's TEXTAREA will hold our data for Draftail, and we'll use
+    // its style when we open the editor.
     this.TEXTAREA.id = 'rich-text-table-cell-editor';
-    this.TEXTAREA.style.visibility = 'hidden';
-    this.textareaStyle = this.TEXTAREA.style;
-    Handsontable.dom.empty(this.TEXTAREA_PARENT);
-    this.TEXTAREA_PARENT.appendChild(this.TEXTAREA);
-    this.TEXTAREA_PARENT.style.backgroundColor = 'white';
+    this.TEXTAREA.style.display = 'none';
   }
 
+  // Get the Draftail value from the input and convert it back to HTML
   RichTextEditor.prototype.getValue = function() {
-    console.log("get value");
-    const options = {};
+    const options = {
+      entityStyleFn: entity => {
+        const entityType = entity.get( 'type' ).toLowerCase();
+        if ( entityType === 'document' ) {
+          const data = entity.getData();
+          return {
+            element: 'a',
+            attributes: {
+              'href': data.url,
+              'data-linktype': 'DOCUMENT',
+              'data-id': data.id,
+              'type': 'DOCUMENT'
+            },
+            style: {
+            }
+          };
+        }
+        return null;
+      }
+    };
     const editorValue = this.TEXTAREA.value;
     const raw = JSON.parse(editorValue);
     const state = window.DraftJS.convertFromRaw( raw );
     return stateToHTML(state, options);
   }
 
+  // Convert the given value to what Draftail expects, and store it on the
+  // input element from which we'll initialize Draftail
   RichTextEditor.prototype.setValue = function(value) {
-    console.log("set value", value);
     let contentState;
+
     const blocksFromHTML = value ? window.DraftJS.convertFromHTML(value) : null;
     if (blocksFromHTML && blocksFromHTML.contentBlocks) {
       contentState = window.DraftJS.ContentState.createFromBlockArray(blocksFromHTML.contentBlocks, blocksFromHTML.entityMap);
     } else {
       contentState = window.DraftJS.ContentState.createFromText('');
     }
+
     const cellValue = window.DraftJS.convertToRaw(contentState);
+    if ( cellValue.entityMap ) {
+      for ( const entity in cellValue.entityMap ) {
+        if ( cellValue.entityMap[entity] && cellValue.entityMap[entity].data ) {
+          cellValue.entityMap[entity].data.url = cellValue.entityMap[entity].data.href;
+          if ( cellValue.entityMap[entity].data.url.startsWith( '/documents/' ) ) {
+            cellValue.entityMap[entity].type = 'DOCUMENT';
+          }
+        }
+      }
+    }
+
     this.TEXTAREA.value = JSON.stringify(cellValue);
-    console.log(cellValue);
   }
 
   RichTextEditor.prototype.open = function() {
-    console.log("open", this.TEXTAREA);
-    // Initialize Draftail
+    // Initialize the Draftail editor for this cell
+    // We seem to have to initialize a new Draftail editor for every cell,
+    // because there doesn't seem to be a way to update the data after
+    // initialization.
     window.draftail.initEditor(
       '#' + this.TEXTAREA.id,
       draftail_options,
       document.currentScript
     );
+
+    // Call TextEditor's open
     Handsontable.editors.TextEditor.prototype.open.call(this);
+
+    // Style the Draftail editor with the TextEditor's TEXTAREA's style
+    let draftailWrapper = this.TEXTAREA_PARENT.querySelector('.Draftail-Editor__wrapper');
+    draftailWrapper.style.cssText= this.TEXTAREA.style.cssText;
+    draftailWrapper.style.display = 'block';
+
+    // This is default style for input elements.
+    // TODO: Specify this in a stylesheet somewhere
+    draftailWrapper.style.backgroundColor = '#fafafa';
+    draftailWrapper.style.border = '1px solid var(--color-input-focus-border)';
+
+    // Clear the TEXTAREA's style and hide it
+    this.TEXTAREA.style.cssText = null;
+    this.TEXTAREA.style.display = 'none';
   }
 
   RichTextEditor.prototype.close = function() {
     Handsontable.editors.TextEditor.prototype.close.call(this);
-    // Remove Draftail for this cell
+    // Remove the Draftail editor for this cell
     this.TEXTAREA_PARENT.querySelector('.Draftail-Editor__wrapper').remove();
   }
 
