94ee2b4ad4d8218bc16ac102fe1bf669a9502a4a
Steve <steve@mrclay.org>
2021-08-17 12:29:12 -0400

    Clear session on entry to the grade-level page
    If we base on the click event, then a user could open a new tab
    and bypass clearing the session. There's just no downside to
    clearing on this page.

--- cfgov/unprocessed/apps/teachers-digital-platform/js/survey/grade-level-page.js
+++ cfgov/unprocessed/apps/teachers-digital-platform/js/survey/grade-level-page.js
@@ -1,20 +1,14 @@
 const Cookie = require( 'js-cookie' );
 const { ANSWERS_SESS_KEY, RESULT_COOKIE, SURVEY_COOKIE } = require( './config' );
 
-const $ = document.querySelector.bind( document );
-
 /**
  * Initialize a grade-level intro page
  */
 function gradeLevelPage() {
-  // Entry links clear session before entry
-  const link = $( '.survey-entry-link' );
-
-  link.addEventListener( 'click', () => {
-    Cookie.remove( RESULT_COOKIE );
-    Cookie.remove( SURVEY_COOKIE );
-    sessionStorage.removeItem( ANSWERS_SESS_KEY );
-  } );
+  // Clear session to prepare for fresh entry
+  Cookie.remove( RESULT_COOKIE );
+  Cookie.remove( SURVEY_COOKIE );
+  sessionStorage.removeItem( ANSWERS_SESS_KEY );
 }
 
 export { gradeLevelPage };
