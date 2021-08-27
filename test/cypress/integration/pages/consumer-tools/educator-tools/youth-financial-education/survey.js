8c9863bd56f4d615d7e5adc8f9069ca373703803
Steve <steve@mrclay.org>
2021-08-27 11:17:17 -0400

    [TDP survey] refactor cypress tests and add shared page

--- test/cypress/integration/pages/consumer-tools/educator-tools/youth-financial-education/survey.js
+++ test/cypress/integration/pages/consumer-tools/educator-tools/youth-financial-education/survey.js
@@ -1,23 +1,17 @@
-function testWithValues(test) {
-  const firsts = [ 1, 7, 9, 16, 19 ];
+import { TdpResultsPage } from '../../../../../pages/consumer-tools/educator-tools/youth-financial-education/results';
+import { TdpSurveyPage } from '../../../../../pages/consumer-tools/educator-tools/youth-financial-education/survey';
 
-  cy.visit( '/consumer-tools/educator-tools/youth-financial-education/survey/3-5/p1/' );
+const results = new TdpResultsPage();
+const survey = new TdpSurveyPage();
 
-  const firstLegendSelector = '.tdp-form > li:first-child legend';
-  const submitSelector = '.o-well + div .m-btn-group button[type="submit"]';
+function testWithValues( test ) {
+  survey.open( `${ test.grades }/p1` );
 
-  for (let i = 0; i < 5; i++) {
-    cy.get( firstLegendSelector ).should( 'include.text', `${ firsts[i] }.` );
-
-    test.answers[i].forEach( ( val, idx ) => {
-      cy.get( `.tdp-form > li:nth-child(${ idx + 1 }) .a-label` )
-        .then( labels => {
-          labels[val].click();
-        } );
-    } );
-
-    cy.get( submitSelector ).click();
-  }
+  test.firstQ.forEach( ( val, idx ) => {
+    survey.getFirstLegend().should( 'include.text', `${ val }.` );
+    survey.selectAnswers( test.answers[idx] );
+    survey.clickNext();
+  } );
 
   /**
    * The survey POSTs the answers the final time to the last page URL: p5/.
@@ -25,54 +19,55 @@ function testWithValues(test) {
    * done/ sets the cookie resultsUrl, and removes cookie wizard_survey_wizard.
    * done/ then redirects to results/.
    */
-  cy.url().should( 'include', '/3-5/results/' );
-  cy.getCookie( 'wizard_survey_wizard' ).should( 'not.exist' );
-  cy.getCookie( 'resultUrl' )
-    .then( cookie => {
-      expect( cookie.value ).to.match( test.resultsMatch );
-    } );
-  cy.get( 'svg image' )
-    .then( images => {
-      test.carImageX.forEach( ( val, idx ) => {
-        expect( images[idx].getAttribute( 'x' ) ).to.equal( String( val ) );
-      } );
-    } );
+  cy.url().should( 'include', results.getPath( test.grades ) );
+  results.checkStudentCookies( test.resultsMatch );
+  results.checkCarPositions( test.carImageX );
+
+  results.visitSharedUrl();
+  results.checkInitials();
+  results.checkCarPositions( test.carImageX );
+  results.checkNoSharing();
 }
 
 describe( 'Youth Financial Education Survey', () => {
   describe( 'Completion', () => {
-
     it( 'should grade 3-5 survey with all first options', () => testWithValues( {
+      grades: '3-5',
+      firstQ: [ 1, 7, 9, 16, 19 ],
       answers: [
-        [0, 0, 0, 0, 0, 0],
-        [0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0],
-        [0, 0]
+        [ 0, 0, 0, 0, 0, 0 ],
+        [ 0, 0 ],
+        [ 0, 0, 0, 0, 0, 0, 0 ],
+        [ 0, 0, 0 ],
+        [ 0, 0 ]
       ],
       resultsMatch: /^v1_3-5_10:z:h_/,
-      carImageX: [500, 500, 650, 300]
+      carImageX: [ 500, 500, 650, 300 ]
     } ) );
 
     it( 'should grade 3-5 survey with all last options', () => testWithValues( {
+      grades: '3-5',
+      firstQ: [ 1, 7, 9, 16, 19 ],
       answers: [
-        [3, 3, 3, 3, 3, 3],
-        [3, 3],
-        [3, 3, 3, 3, 3, 3, 3],
-        [3, 3, 3],
-        [3, 3]
+        [ 3, 3, 3, 3, 3, 3 ],
+        [ 3, 3 ],
+        [ 3, 3, 3, 3, 3, 3, 3 ],
+        [ 3, 3, 3 ],
+        [ 3, 3 ]
       ],
       resultsMatch: /^v1_3-5_g:e:a_/,
-      carImageX: [40, 40, 40, 40]
+      carImageX: [ 40, 40, 40, 40 ]
     } ) );
+  } );
 
+  describe( 'Redirects', () => {
     it( 'should reject direct requests for results', () => {
       cy.clearCookies();
 
       // Can't use visit() locally because 500
       cy.request( {
         url: '/consumer-tools/educator-tools/youth-financial-education/survey/3-5/results/',
-        followRedirect: false,
+        followRedirect: false
       } ).then( resp => {
         expect( resp.redirectedToUrl ).to.include( '/assess/survey/' );
       } );
