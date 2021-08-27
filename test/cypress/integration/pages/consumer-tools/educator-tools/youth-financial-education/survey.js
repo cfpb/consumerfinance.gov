9a329e61497d79300433aacdba35e7f9dd31794e
Steve <steve@mrclay.org>
2021-08-27 09:27:46 -0400

    [TDP survey] Add tests for direct access of some URLs

--- test/cypress/integration/pages/consumer-tools/educator-tools/youth-financial-education/survey.js
+++ test/cypress/integration/pages/consumer-tools/educator-tools/youth-financial-education/survey.js
@@ -1,30 +1,3 @@
-const tests = [
-  {
-    name: 'should grade 3-5 survey with all first options',
-    answers: [
-      [0, 0, 0, 0, 0, 0],
-      [0, 0],
-      [0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0],
-      [0, 0]
-    ],
-    resultsMatch: /^v1_3-5_10:z:h_/,
-    x: [500, 500, 650, 300]
-  },
-  {
-    name: 'should grade 3-5 survey with all last options',
-    answers: [
-      [3, 3, 3, 3, 3, 3],
-      [3, 3],
-      [3, 3, 3, 3, 3, 3, 3],
-      [3, 3, 3],
-      [3, 3]
-    ],
-    resultsMatch: /^v1_3-5_g:e:a_/,
-    x: [40, 40, 40, 40]
-  }
-];
-
 function testWithValues(test) {
   const firsts = [ 1, 7, 9, 16, 19 ];
 
@@ -46,11 +19,12 @@ function testWithValues(test) {
     cy.get( submitSelector ).click();
   }
 
-  // p5 => /done/
-  // done sets cookie: resultUrl=...
-  // done removes cookie: wizard_survey_wizard.
-  // done => /results/
-
+  /**
+   * The survey POSTs the answers the final time to the last page URL: p5/.
+   * p5/ validates the answers and redirects to done/.
+   * done/ sets the cookie resultsUrl, and removes cookie wizard_survey_wizard.
+   * done/ then redirects to results/.
+   */
   cy.url().should( 'include', '/3-5/results/' );
   cy.getCookie( 'wizard_survey_wizard' ).should( 'not.exist' );
   cy.getCookie( 'resultUrl' )
@@ -59,7 +33,7 @@ function testWithValues(test) {
     } );
   cy.get( 'svg image' )
     .then( images => {
-      test.x.forEach( ( val, idx ) => {
+      test.carImageX.forEach( ( val, idx ) => {
         expect( images[idx].getAttribute( 'x' ) ).to.equal( String( val ) );
       } );
     } );
@@ -67,8 +41,50 @@ function testWithValues(test) {
 
 describe( 'Youth Financial Education Survey', () => {
   describe( 'Completion', () => {
-    it( tests[0].name, () => testWithValues(tests[0]) );
 
-    it( tests[1].name, () => testWithValues(tests[1]) );
+    it( 'should grade 3-5 survey with all first options', () => testWithValues( {
+      answers: [
+        [0, 0, 0, 0, 0, 0],
+        [0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0],
+        [0, 0]
+      ],
+      resultsMatch: /^v1_3-5_10:z:h_/,
+      carImageX: [500, 500, 650, 300]
+    } ) );
+
+    it( 'should grade 3-5 survey with all last options', () => testWithValues( {
+      answers: [
+        [3, 3, 3, 3, 3, 3],
+        [3, 3],
+        [3, 3, 3, 3, 3, 3, 3],
+        [3, 3, 3],
+        [3, 3]
+      ],
+      resultsMatch: /^v1_3-5_g:e:a_/,
+      carImageX: [40, 40, 40, 40]
+    } ) );
+
+    it( 'should reject direct requests for results', () => {
+      cy.clearCookies();
+
+      // Can't use visit() locally because 500
+      cy.request( {
+        url: '/consumer-tools/educator-tools/youth-financial-education/survey/3-5/results/',
+        followRedirect: false,
+      } ).then( resp => {
+        expect( resp.redirectedToUrl ).to.include( '/assess/survey/' );
+      } );
+    } );
+
+    it( 'should reject jumping later into survey', () => {
+      cy.window().then( win => {
+        win.sessionStorage.clear();
+      } );
+
+      cy.visit( '/consumer-tools/educator-tools/youth-financial-education/survey/3-5/p2/' );
+      cy.url().should( 'include', '/3-5/p1/' );
+    } );
   } );
 } );
