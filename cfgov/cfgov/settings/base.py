ec0c1defe4d9ac4d5cf0ec14aa758982a185a5ce
higs4281 <higs4281@gmail.com>
2021-09-16 16:47:28 -0400

    Remove code for retired feature flags
    These feature flags are no longer needed, but have references in code:
    - `CCDB5_RELEASE`
    - `CCDB5_TRENDS`
    - `YOUTH_EMPLOYMENT_SUCCESS`
    
    Once this change is merged, we can delete the flag references in Wagtail
    to complete the removal from code and databsae.

--- cfgov/cfgov/settings/base.py
+++ cfgov/cfgov/settings/base.py
@@ -711,10 +711,6 @@ FLAGS = {
     # Fix for margin-top when using the text inset
     # When enabled, the top margin of full-width text insets is increased
     "INSET_TEST": [],
-    # The next version of the public consumer complaint database
-    "CCDB5_RELEASE": [],
-    # The Trends feature inside Consumer Complaints
-    "CCDB5_TRENDS": [],
     # Google Optimize code snippets for A/B testing
     # When enabled this flag will add various Google Optimize code snippets.
     # Intended for use with path conditions.
@@ -731,8 +727,6 @@ FLAGS = {
     # Controls the /beta_external_testing endpoint, which Jenkins jobs
     # query to determine whether to refresh Beta database.
     "BETA_EXTERNAL_TESTING": [],
-    # Used to hide new youth employment success pages prior to public launch
-    "YOUTH_EMPLOYMENT_SUCCESS": [],
     # During a Salesforce system outage, the following flag should be enabled
     # to alert users that the Collect community is down.
     "COLLECT_OUTAGE": [
