96793f41f187ee52782f14a772bfd443eb507809
higs4281 <higs4281@gmail.com>
2021-09-20 16:51:27 -0400

    have test look for different class

--- cfgov/legacy/tests/views/test_complaint.py
+++ cfgov/legacy/tests/views/test_complaint.py
@@ -12,11 +12,11 @@ class ComplaintLandingViewTests(TestCase):
     def test_no_banner_when_flag_disabled(self):
         response = self.client.get(self.landing_url)
         self.assertEqual(response.status_code, 200)
-        self.assertNotContains(response, 'show-data-notification')
+        self.assertNotContains(response, 'm-notification_explanation')
 
     @override_settings(
         FLAGS={"CCDB_TECHNICAL_ISSUES": [("boolean", True)]})
     def test_banner_when_flag_enabled(self):
         response = self.client.get(self.landing_url)
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'show-data-notification')
+        self.assertContains(response, 'm-notification_explanation')
