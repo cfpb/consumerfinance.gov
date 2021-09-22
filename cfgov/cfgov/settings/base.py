5e6fb11e99ec8d861e7d2729ac224b51698d0cfb
higs4281 <higs4281@gmail.com>
2021-09-22 10:47:00 -0400

    remove sheer references and reorganize ES7 settings

--- cfgov/cfgov/settings/base.py
+++ cfgov/cfgov/settings/base.py
@@ -301,16 +301,6 @@ TAGGIT_CASE_INSENSITIVE = True
 WAGTAIL_USER_CREATION_FORM = "v1.auth_forms.UserCreationForm"
 WAGTAIL_USER_EDIT_FORM = "v1.auth_forms.UserEditForm"
 
-ES_PORT = os.getenv("ES_PORT", "9200")
-SHEER_ELASTICSEARCH_SERVER = (
-    os.environ.get("ES_HOST", "localhost")
-    + ":"
-    + ES_PORT
-)
-SHEER_ELASTICSEARCH_INDEX = os.environ.get(
-    "SHEER_ELASTICSEARCH_INDEX", "content"
-)
-ELASTICSEARCH_BIGINT = 50000
 
 # LEGACY APPS
 MAPBOX_ACCESS_TOKEN = os.environ.get("MAPBOX_ACCESS_TOKEN")
@@ -320,10 +310,11 @@ HOUSING_COUNSELOR_S3_PATH_TEMPLATE = (
     "/a/assets/hud/{file_format}s/{zipcode}.{file_format}"
 )
 
-ELASTICSEARCH_DEFAULT_ANALYZER = "snowball"
-
 # ElasticSearch 7 Configuration
 ES7_HOST = os.getenv('ES7_HOST', 'localhost')
+ES_PORT = os.getenv("ES_PORT", "9200")
+ELASTICSEARCH_BIGINT = 50000
+ELASTICSEARCH_DEFAULT_ANALYZER = "snowball"
 
 if os.environ.get('USE_AWS_ES', False):
     awsauth = AWS4Auth(
