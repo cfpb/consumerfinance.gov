0e6431ab5c8da93de74e2189fda7a1d3b338aebb
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2021-11-08 11:04:10 -0500

    Rename foia to privacy

--- cfgov/privacy/urls.py
+++ cfgov/privacy/urls.py
@@ -1,36 +1,36 @@
 from django.views.generic import TemplateView
 
 from flags.urls import flagged_re_path
-from foia.views import GetRecordsAccessForm, GetDisclosureConsentForm
+from privacy.views import GetRecordsAccessForm, GetDisclosureConsentForm
 
 
 urlpatterns = [
     flagged_re_path(
-        "FOIA_FORMS",
+        "PRIVACY_FORMS",
         r"^disclosure-consent/$",
         GetDisclosureConsentForm.as_view(),
         name="disclosure_consent",
     ),
     flagged_re_path(
-        "FOIA_FORMS",
+        "PRIVACY_FORMS",
         r"^records-access/$",
         GetRecordsAccessForm.as_view(),
         name="records_access",
     ),
     flagged_re_path(
-        "FOIA_FORMS",
+        "PRIVACY_FORMS",
         r"^test-form/form-submitted/$",
         TemplateView.as_view(
-            template_name="foia/form-submitted.html"
+            template_name="privacy/form-submitted.html"
         ),
         name="form_submitted",
     ),
     flagged_re_path(
-        "FOIA_FORMS",
+        "PRIVACY_FORMS",
         r"^privacy-act-statement/$",
         TemplateView.as_view(
-            template_name="foia/privacy.html"
+            template_name="privacy/privacy.html"
         ),
-        name="foia_privacy",
+        name="privacy_statement",
     ),
 ]
--- cfgov/privacy/views.py
+++ cfgov/privacy/views.py
@@ -1,13 +1,13 @@
 from django.urls import reverse_lazy
 from django.views.generic import FormView
 
-from foia.forms import DisclosureConsentForm, RecordsAccessForm
+from privacy.forms import DisclosureConsentForm, RecordsAccessForm
 
 
 class GetDisclosureConsentForm(FormView):
-    template_name = 'foia/disclosure-consent-form.html'
+    template_name = 'privacy/disclosure-consent-form.html'
     form_class = DisclosureConsentForm
-    success_url = reverse_lazy('foia:form_submitted')
+    success_url = reverse_lazy('privacy:form_submitted')
 
     def form_valid(self, form):
         # This method is called when valid form data has been POSTed.
@@ -16,9 +16,9 @@ class GetDisclosureConsentForm(FormView):
         return super(GetDisclosureConsentForm, self).form_valid(form)
 
 class GetRecordsAccessForm(FormView):
-    template_name = 'foia/records-access-form.html'
+    template_name = 'privacy/records-access-form.html'
     form_class = RecordsAccessForm
-    success_url = reverse_lazy('foia:form_submitted')
+    success_url = reverse_lazy('privacy:form_submitted')
 
     def form_valid(self, form):
         # This method is called when valid form data has been POSTed.
