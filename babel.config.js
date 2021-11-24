30e0ed276729f5718b182acdf16ccd78e305f3e9
Anselm Bradford <anselmbradford@users.noreply.github.com>
2021-11-24 15:34:55 -0500

    Add @babel/plugin-transform-runtime to handle regenerator runtime error

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,5 @@
+// This config is currently only consumed by babel-jest for the unit tests.
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          esmodules: true
-        }
-      }
-    ]
-  ]
+  presets: [ '@babel/preset-env' ],
+  plugins: [ '@babel/plugin-transform-runtime' ]
 };
