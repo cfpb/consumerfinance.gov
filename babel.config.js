ffa969aa5e208e2e5aca6f00309c552f593dd9e2
william higgins <higs4281@users.noreply.github.com>
2021-12-06 12:52:12 -0500

    add sourceType param to babel config (#6784)
    Co-authored-by: Ans <anselmbradford@users.noreply.github.com>

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,6 @@
 // This config is currently only consumed by babel-jest for the unit tests.
 module.exports = {
+  sourceType: 'unambiguous',
   presets: [ '@babel/preset-env' ],
   plugins: [ '@babel/plugin-transform-runtime' ]
 };
