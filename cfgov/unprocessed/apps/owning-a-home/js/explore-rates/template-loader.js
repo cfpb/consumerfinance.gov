39157e2759a75c39b0a65ee65ee07f4300d9ccce
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2022-01-07 10:34:45 -0500

    Switch oah handlebars templates to string constants and templates

--- cfgov/unprocessed/apps/owning-a-home/js/explore-rates/template-loader.js
+++ cfgov/unprocessed/apps/owning-a-home/js/explore-rates/template-loader.js
@@ -1,9 +1,21 @@
 /* eslint-disable camelcase, no-shadow */
 
 // Define string templates and constants
-const county = ( { complete_fips, gse_limit, fha_limit, va_limit, county } ) => `<option value="${ complete_fips }" data-gse="${ gse_limit }" data-fha="${ fha_limit }" data-va="${ va_limit }">${ county }</option>`;
+const county = ( { complete_fips, gse_limit, fha_limit, va_limit, county } ) => `<option
+  value="${ complete_fips }"
+  data-gse="${ gse_limit }"
+  data-fha="${ fha_limit }"
+  data-va="${ va_limit }">
+    ${ county }
+</option>`;
 
-const chartTooltip = ( { y, key } ) => `<div class="chart-tooltip"><strong class="lenders">${ y }</strong><span class="text">lender${ y === 1 ? ' is' : 's are' } offering <br> rates at <strong>${ key }</strong>.</span></div>`;
+const chartTooltip = ( { y, key } ) => `<div class="chart-tooltip">
+  <strong class="lenders">${ y }</strong>
+  <span class="text">
+    lender${ y === 1 ? ' is' : 's are' } offering <br> 
+    rates at <strong>${ key }</strong>.
+  </span>
+  </div>`;
 
 const countyConfWarning = 'Based on your loan amount, you may not be eligible for a regular (conforming) conventional loan. Please enter your county so we can find the right loan type for you and get you the most accurate rates.';
 const countyFHAWarning = 'Based on your loan amount, you may not be eligible for a regular FHA loan. Please enter your county so we can find the right loan type for you and get you the most accurate rates.';
