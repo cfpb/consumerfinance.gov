ff7c233fe684f9d928f8de4dbe39f7fce587d3f9
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2022-01-07 10:34:45 -0500

    Switch oah handlebars templates to string constants and templates

--- cfgov/unprocessed/apps/owning-a-home/js/explore-rates/template-loader.js
+++ cfgov/unprocessed/apps/owning-a-home/js/explore-rates/template-loader.js
@@ -1,11 +1,26 @@
-// Load our handlebar templates.
-const county = require( '../../templates/county-option.hbs' );
-const countyConfWarning = require( '../../templates/county-conf-warning.hbs' );
-const countyFHAWarning = require( '../../templates/county-fha-warning.hbs' );
-const countyVAWarning = require( '../../templates/county-va-warning.hbs' );
-const countyGenWarning = require( '../../templates/county-general-warning.hbs' );
-const chartTooltipSingle = require( '../../templates/chart-tooltip-single.hbs' );
-const chartTooltipMultiple = require( '../../templates/chart-tooltip-multiple.hbs' );
+/* eslint-disable camelcase, no-shadow */
+
+// Define string templates and constants
+const county = ( { complete_fips, gse_limit, fha_limit, va_limit, county } ) => `<option
+  value="${ complete_fips }"
+  data-gse="${ gse_limit }"
+  data-fha="${ fha_limit }"
+  data-va="${ va_limit }">
+    ${ county }
+</option>`;
+
+const chartTooltip = ( { y, key } ) => `<div class="chart-tooltip">
+  <strong class="lenders">${ y }</strong>
+  <span class="text">
+    lender${ y === 1 ? ' is' : 's are' } offering <br> 
+    rates at <strong>${ key }</strong>.
+  </span>
+  </div>`;
+
+const countyConfWarning = 'Based on your loan amount, you may not be eligible for a regular (conforming) conventional loan. Please enter your county so we can find the right loan type for you and get you the most accurate rates.';
+const countyFHAWarning = 'Based on your loan amount, you may not be eligible for a regular FHA loan. Please enter your county so we can find the right loan type for you and get you the most accurate rates.';
+const countyVAWarning = 'Based on your loan amount, you may not be eligible for a regular VA loan. Please enter your county so we can find the right loan type for you and get you the most accurate rates.';
+const countyGenWarning = 'Please enter your county so we can check what loan types are available at your loan amount and get you the most accurate rates.';
 
 export {
   county,
@@ -13,6 +28,5 @@ export {
   countyFHAWarning,
   countyVAWarning,
   countyGenWarning,
-  chartTooltipSingle,
-  chartTooltipMultiple
+  chartTooltip
 };
