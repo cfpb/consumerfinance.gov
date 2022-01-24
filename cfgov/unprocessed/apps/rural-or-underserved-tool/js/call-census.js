89468f7682e765076dab7a63d61c4c41f6e252d5
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2022-01-24 13:45:25 -0500

    Updates rural-or-underserved-tool data model. Now 6-7x more efficient on network/storage and simpler to update. Also makes 2021 rout update.

--- cfgov/unprocessed/apps/rural-or-underserved-tool/js/call-census.js
+++ cfgov/unprocessed/apps/rural-or-underserved-tool/js/call-census.js
@@ -6,10 +6,10 @@ import count from './count';
 /**
  * Call the census.gov API and display an error if warranted.
  * @param {string} address - An address.
- * @param {Object} rural - Rural data via json file.
- * @param {*} cb - Callback to call (unused).
+ * @param {object} ruralCounties - Rural counties for a chosen year
+ * @param {*} cb - Callback to call.
  */
-function callCensus( address, rural, cb ) {
+function callCensus( address, ruralCounties, cb ) {
   let url = 'https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?';
   url += 'address=' + address;
   url += '&benchmark=4';
@@ -17,7 +17,7 @@ function callCensus( address, rural, cb ) {
 
   jsonP( url ).promise
     .then( function( data ) {
-      window.callbacks.censusAPI( data, rural );
+      cb( data, ruralCounties );
     }
     )
     .catch( function( error ) {
--- cfgov/unprocessed/apps/rural-or-underserved-tool/js/get-rural-counties.js
+++ cfgov/unprocessed/apps/rural-or-underserved-tool/js/get-rural-counties.js
@@ -1,15 +1,26 @@
 require( 'es6-promise' ).polyfill();
 import { fetch } from 'cross-fetch';
 
+
+const cache = {};
+
 /**
  * @param {string} year - A year.
- * @returns {Object} Result of the call to a JSON file.
+ * @returns {array} An array of rural counties by fips.
  */
 function getRuralCounties( year ) {
+  if ( cache[year] ) return Promise.resolve( cache[year] );
+
   return fetch(
-    'https://files.consumerfinance.gov/rural-or-underserved-tool/data/' +
-    year + '.json'
-  ).then( v => v.json() );
+    'https://files.consumerfinance.gov/data/rural-or-underserved-tool/' +
+    year + '.txt'
+  ).then( v => v.text()
+  ).then( v => {
+    const val = v.split( '\n' );
+    cache[year] = val;
+    return val;
+  } );
 }
+
 
 export default getRuralCounties;
