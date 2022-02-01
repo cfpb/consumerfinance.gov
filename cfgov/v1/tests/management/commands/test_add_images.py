3302cc7480ee9c9d51c87e79c2692b9fd318011d
Andy Chosak <andy.chosak@cfpb.gov>
2022-02-01 14:31:44 -0500

    Fix missing coverage

--- cfgov/v1/tests/management/commands/test_add_images.py
+++ cfgov/v1/tests/management/commands/test_add_images.py
@@ -3,19 +3,13 @@ from io import BytesIO
 
 from django.conf import settings
 from django.contrib.auth.models import User
-from django.core.files.base import ContentFile
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import RequestFactory, TestCase
 
 from PIL import Image
 
 
-# from v1.management.commands.add_images import Command as cmd
-
-
-def create_image(
-    storage, filename, size=(100, 100), image_mode='RGB', image_format='PNG'
-):
+def create_image(size=(100, 100), image_mode='RGB', image_format='PNG'):
     """
     Generate a test image, returning the filename that it was saved as.
 
@@ -25,10 +19,7 @@ def create_image(
     data = BytesIO()
     Image.new(image_mode, size).save(data, image_format)
     data.seek(0)
-    if not storage:
-        return data
-    image_file = ContentFile(data.read())
-    return storage.save(filename, image_file)
+    return data
 
 
 class TestAddImages(TestCase):
@@ -42,8 +33,7 @@ class TestAddImages(TestCase):
 
     def test_add_image(self):
         filename_only = os.path.split(self.filename)[-1]
-        logo = create_image(None, filename_only)
-        # cmd.add_image(self, self.filename)
+        logo = create_image()
         image_file = SimpleUploadedFile(filename_only, logo.getvalue())
         post_data = {
             'title': filename_only,
