d76b11223103d38f495faaa031a7de1d9aaf73cb
Nicholas Johnson <nicholas.johnson@cfpb.gov>
2022-02-02 11:20:38 -0500

    Add document report unit tests

--- cfgov/v1/tests/views/test_metadata_report_view.py
+++ cfgov/v1/tests/views/test_metadata_report_view.py
@@ -3,18 +3,23 @@ from datetime import date
 from django.test import TestCase
 
 from wagtail.core.models import Site
+from wagtail.documents.models import Document
 
 from model_bakery import baker
 from taggit.models import Tag
 
 from v1.models import BlogPage, CFGOVPageCategory
 from v1.util.ref import categories
-from v1.views.reports import PageMetadataReportView, process_categories
+from v1.views.reports import (
+    DocumentsReportView, PageMetadataReportView, construct_absolute_url,
+    process_categories, process_tags
+)
 
 
 class ServeViewTestCase(TestCase):
     def setUp(self):
-        self.view = PageMetadataReportView()
+        self.page_metadata_report_view = PageMetadataReportView()
+        self.documents_report_view = DocumentsReportView()
         self.tag1 = baker.make(Tag, name="tag1")
         self.tag2 = baker.make(Tag, name="tag2")
         self.site = Site.objects.get(is_default_site=True)
@@ -25,6 +30,14 @@ class ServeViewTestCase(TestCase):
         self.category = CFGOVPageCategory(name=self.category_tuple[1])
         self.blog_page.categories.add(self.category)
         self.blog_page.tags.add(self.tag1, self.tag2)
+        self.document1 = Document(title="Test document 1")
+        self.document1.save()
+        self.document1.tags.add(self.tag1, self.tag2)
+
+    def test_construct_absolute_url(self):
+        self.assertEqual(
+            construct_absolute_url(self.blog_page.url),
+            "https://www.consumerfinance.gov" + self.blog_page.url)
 
     def test_process_categories(self):
         category_string = process_categories(self.blog_page.categories.all())
@@ -33,11 +46,31 @@ class ServeViewTestCase(TestCase):
         self.assertEqual(
             self.category.get_name_display(), self.category_tuple[1])
 
+    def test_process_tags(self):
+        tag_string = process_tags(self.document1.tags.all())
+        self.assertEqual(
+            tag_string,
+            'tag1, tag2'
+        )
+
     def test_metadata_report_get_filename(self):
         today = date.today()
         self.assertEqual(
-            self.view.get_filename(),
+            self.page_metadata_report_view.get_filename(),
             f"spreadsheet-export-{today}")
 
-    def test_get_queryset(self):
-        self.assertEqual(self.view.get_queryset().count(), 2)
+    def test_metadata_report_get_queryset(self):
+        self.assertEqual(
+            self.page_metadata_report_view.get_queryset().count(),
+            2)
+
+    def test_documents_report_get_filename(self):
+        today = date.today()
+        self.assertEqual(
+            self.documents_report_view.get_filename(),
+            f"all-cfgov-documents-{today}")
+
+    def test_documents_report_get_queryset(self):
+        self.assertEqual(
+            self.documents_report_view.get_queryset().count(),
+            1)
