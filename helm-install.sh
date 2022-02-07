8a1cce0993af9dd9a18e6aa54edd77b6de03583c
Justin Slay <justin.slay@cfpb.gov>
2022-02-07 04:33:20 -0700

    Added check for helm, create namespace if not exist

--- helm-install.sh
+++ helm-install.sh
@@ -4,6 +4,18 @@ realpath() {
     [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
 }
 
+# Ensure helm is installed and available via PATH
+if ! command -v helm &> /dev/null; then
+  echo "Helm not found in PATH. Please install helm (brew install helm) or add it to PATH."
+  exit 1
+fi
+
+# Build Dependency Charts
+if [ ! -d ./helm/cfgov/charts ]; then
+  helm dependency build ./helm/cfgov
+fi
+
+# Parse overrides list
 export PROJECT_DIR="$(dirname "$(realpath "$0")")"
 if [ $# -eq 0 ]; then
   ARGS="$PROJECT_DIR/helm/overrides/local.yaml"
@@ -11,6 +23,7 @@ else
   ARGS=$@
 fi
 
+# Substitute Environment Variables in override files
 tempFiles=()
 OVERRIDES=""
 for i in $ARGS; do
@@ -20,12 +33,12 @@ for i in $ARGS; do
   tempFiles+=($tempFile)
 done
 
-if [ ! -d ./helm/cfgov/charts ]; then
-  helm dependency build ./helm/cfgov
-fi
+# Install/Upgrade cfgov release to current context namespace
+# To install to different namespace, set context with namespace
+# kubectl config set-context --current --namespace=<insert-namespace-name-here>
+helm upgrade --install --create-namespace cfgov $OVERRIDES ./helm/cfgov
 
-helm upgrade --install cfgov $OVERRIDES ./helm/cfgov
-
+# Cleanup temp files
 for i in ${tempFiles[@]}; do
   rm "$i"
 done
