63facaeb0fa81c9976bae457dfc74110f17093b6
Colin Seifer <colin.seifer@cfpb.gov>
2022-02-10 17:00:20 -0500

    added mega menu creation to test data

--- cfgov/v1/tests/wagtail_pages/create_test_data.py
+++ cfgov/v1/tests/wagtail_pages/create_test_data.py
@@ -1,8 +1,13 @@
+import json
+
+from django.db import IntegrityError
+
 from v1.tests.wagtail_pages.helpers import (
     create_landing_page,
     create_sublanding_filterable_page,
     create_blog_page,
 )
+from mega_menu.models import Menu
 
 def create_blog():
     REQUIRED_BLOG_PAGES = 101
@@ -18,4 +23,35 @@ def create_blog():
             for i in range(REQUIRED_BLOG_PAGES):
                 create_blog_page(str(i), str(i), sublanding_page_url)
 
+def create_mega_menu_en():
+    # remove previous mega menu objects and create new test data
+    try:
+        Menu.objects.all().delete()
+        Menu.objects.create(language='en', submenus=json.dumps([
+            {'type': 'submenu', 'value': {
+                'title': 'Test Content',
+                'columns': [{
+                    'links': [
+                        {'text': 'Home', 'url': '/'},
+                        {'text': 'Still Home', 'url': '/'},
+                    ]
+                }]
+            }},
+            {'type': 'submenu', 'value': {
+                'title': 'Test Content 2',
+                'columns': [{
+                    'links': [
+                        {'text': 'Home', 'url': '/'},
+                        {'text': 'Still Home', 'url': '/'},
+                    ]
+                }]
+            }},
+        ]))
+    # if there was an error removing old data or new data can't be resolved.
+    except IntegrityError:
+        print("skipping en mega menu creation")
+        return
+
+
 create_blog()
+create_mega_menu_en()
