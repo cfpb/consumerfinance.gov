dd665afb0480ce7f5c4cb5e0d0f6667d4ed5c731
Justin Slay <justin.slay@cfpb.gov>
2022-02-11 19:32:51 -0700

    Flag migrations

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,17 +11,20 @@ until psql ${DATABASE_URL} -c '\q' &> /dev/null; do
 done
 
 # Do first-time set up of the database if necessary
-if ! psql ${DATABASE_URL} -c 'SELECT COUNT(*) FROM auth_user' &> /dev/null; then
-    echo "Doing first-time database and search index setup..."
-    if [ ! -z $CFGOV_PROD_DB_LOCATION ]; then
-        echo "Running refresh-data.sh..."
-        ./refresh-data.sh
-    else
-        echo "Running initial-data.sh..."
-        ./initial-data.sh
-    fi
-    echo "Create the cache table..."
-    ./cfgov/manage.py createcachetable
+RUN_MIGRATIONS=${RUN_MIGRATIONS:-"true"}
+if [ "$RUN_MIGRATIONS" = true ]; then
+  if ! psql ${DATABASE_URL} -c 'SELECT COUNT(*) FROM auth_user' &> /dev/null; then
+      echo "Doing first-time database and search index setup..."
+      if [ ! -z $CFGOV_PROD_DB_LOCATION ]; then
+          echo "Running refresh-data.sh..."
+          ./refresh-data.sh
+      else
+          echo "Running initial-data.sh..."
+          ./initial-data.sh
+      fi
+      echo "Create the cache table..."
+      ./cfgov/manage.py createcachetable
+  fi
 fi
 
 # Do first-time build of the front-end if necessary
