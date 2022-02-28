e2ddeec208398ccd934117cea95f72fec40b0c4d
Justin Slay <justin.slay@cfpb.gov>
2022-02-28 02:38:39 -0700

    Source .env in kubernetes if available, fix bashisms

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,10 +2,15 @@
 
 set -e
 
+ls -lah /var/run/secrets
+if [ -f '.env' ] && [ -d /var/run/secrets/kubernetes.io ]; then
+  . '.env'
+fi
+
 echo "Using $(python3 --version 2>&1) located at $(which python3)"
 
 # Wait for the database to be ready
-until psql ${DATABASE_URL} -c '\q' &> /dev/null; do
+until psql "${DATABASE_URL}" -c '\q' >/dev/null 2>&1; do
   >&2 echo "Postgres is unavailable - waiting"
   sleep 1
 done
@@ -13,9 +18,9 @@ done
 # Do first-time set up of the database if necessary
 RUN_MIGRATIONS=${RUN_MIGRATIONS:-"true"}
 if [ "$RUN_MIGRATIONS" = true ]; then
-  if ! psql ${DATABASE_URL} -c 'SELECT COUNT(*) FROM auth_user' &> /dev/null; then
+  if ! psql "${DATABASE_URL}" -c 'SELECT COUNT(*) FROM auth_user' >/dev/null 2>&1; then
       echo "Doing first-time database and search index setup..."
-      if [ ! -z $CFGOV_PROD_DB_LOCATION ]; then
+      if [ -n "$CFGOV_PROD_DB_LOCATION" ]; then
           echo "Running refresh-data.sh..."
           ./refresh-data.sh
       else
