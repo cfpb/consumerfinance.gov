157c5e9f68ccbab27e175da5dced0b83f03666e1
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2022-03-10 11:56:26 -0500

    trim copy task hack, put in place to solve former relative reference (#6943)

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -31,26 +31,10 @@ gulp.task( 'copy:root', () => {
   return stream;
 } );
 
-gulp.task( 'copy:icons:main', () => {
+gulp.task( 'copy:icons', () => {
   const stream = _genericCopy(
     iconSrc,
     `${ paths.processed }/icons/`
-  );
-  return stream;
-} );
-
-gulp.task( 'copy:icons:oah', () => {
-  const stream = _genericCopy(
-    iconSrc,
-    `${ paths.processed }/apps/owning-a-home/icons/`
-  );
-  return stream;
-} );
-
-gulp.task( 'copy:icons:r3k', () => {
-  const stream = _genericCopy(
-    iconSrc,
-    `${ paths.processed }/apps/regulations3k/icons/`
   );
   return stream;
 } );
@@ -62,14 +46,6 @@ gulp.task( 'copy:lightbox2', () => {
   );
   return stream;
 } );
-
-gulp.task( 'copy:icons',
-  gulp.parallel(
-    'copy:icons:main',
-    'copy:icons:oah',
-    'copy:icons:r3k'
-  )
-);
 
 gulp.task( 'copy',
   gulp.parallel(
