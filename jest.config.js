c47d310cf11451e6e658f26a72aad5c22336bc32
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2022-03-28 11:56:13 -0400

    suppress coverage from app script

--- jest.config.js
+++ jest.config.js
@@ -12,7 +12,8 @@ module.exports = {
     '<rootDir>/cfgov/unprocessed/apps/.+/index.js$',
     '<rootDir>/cfgov/unprocessed/apps/.+/common.js$',
     '<rootDir>/cfgov/unprocessed/apps/analytics-gtm/js/[a-zA-Z-]+.js$',
-    '<rootDir>/cfgov/unprocessed/js/routes/'
+    '<rootDir>/cfgov/unprocessed/js/routes/',
+    '<rootDir>/cfgov/unprocessed/js/apps/regulations3k/worker_and_manifest.js'
   ],
   coverageDirectory: '<rootDir>/test/unit_test_coverage',
   moduleNameMapper: {
