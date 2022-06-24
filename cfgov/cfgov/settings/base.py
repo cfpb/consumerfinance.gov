14ff829a0e112c232dc980db1ff7cbf1a3d9e929
Brenden Alexis-Robinson <brenden.alexis-robinson@cfpb.gov>
2022-06-24 11:20:58 -0400

    trying to take out the dj_databas_url and trying out using env vars.

--- cfgov/cfgov/settings/base.py
+++ cfgov/cfgov/settings/base.py
@@ -7,7 +7,6 @@ from django.conf import global_settings
 from django.core.exceptions import ImproperlyConfigured
 from django.utils.translation import gettext_lazy as _
 
-import dj_database_url
 from elasticsearch import RequestsHttpConnection
 from requests_aws4auth import AWS4Auth
 
@@ -237,9 +236,14 @@ if ALLOW_ADMIN_URL:
 # Override this by setting DATABASE_URL in the environment.
 # See https://github.com/jacobian/dj-database-url for URL formatting.
 DATABASES = {
-    "default": dj_database_url.config(
-        default="postgres://cfpb:cfpb@localhost/cfgov"
-    ),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("PGDATABASE", "cfpb"),
+        "USER": os.getenv("PGUSER", "user"),
+        "PASSWORD": os.getenv("PGPASSWORD", "password"),
+        "HOST": os.getenv("PGHOST", "localhost"),
+        "PORT": "5432",
+    }
 }
 
 # Internationalization
