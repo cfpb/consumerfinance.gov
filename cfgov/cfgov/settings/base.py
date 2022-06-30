19c9d617f8dc82258d1102ff91f66e550aaf9158
Brenden Alexis-Robinson <brenden.alexis-robinson@cfpb.gov>
2022-06-30 11:38:46 -0400

    typo fix and linting

--- cfgov/cfgov/settings/base.py
+++ cfgov/cfgov/settings/base.py
@@ -242,7 +242,7 @@ DATABASES = {
         "USER": os.getenv("PGUSER", "cfpb"),
         "PASSWORD": os.getenv("PGPASSWORD", "cfpb"),
         "HOST": os.getenv("PGHOST", "localhost"),
-        "PORT": os.getenv("PGHPORT", "5432"),
+        "PORT": os.getenv("PGPORT", "5432"),
     },
 }
 
