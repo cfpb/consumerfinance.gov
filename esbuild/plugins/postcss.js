ac4140cc8a1604d44b36b5b79d7a4f7427fd4725
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2022-07-07 15:11:18 -0400

    remove broken regs3k sourcemap

--- esbuild/plugins/postcss.js
+++ esbuild/plugins/postcss.js
@@ -1,4 +1,4 @@
-const { readFileSync } = require( 'fs' );
+const { readFile } = require( 'fs' ).promises;
 const { dirname } = require( 'path' );
 const postcss = require( 'postcss' );
 const less = require( 'less' );
@@ -10,7 +10,7 @@ const postCSSPlugin = ( {
   name: 'less-and-postcss',
   setup( build ) {
     build.onLoad( { filter: /.\.less$/ }, async args => {
-      const fileContent = readFileSync( args.path, { encoding: 'utf-8' } );
+      const fileContent = await readFile( args.path, { encoding: 'utf-8' } );
       const lessResult = await less.render( fileContent, {
         ...lessOptions,
         filename: args.path,
