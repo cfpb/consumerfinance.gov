d1c8578184234ca224258be2460431eeb6ed6ce1
Chris Contolini <contolini@users.noreply.github.com>
2022-07-12 18:27:03 -0400

    Improve FIG TOC mobile experience

--- cfgov/unprocessed/apps/filing-instruction-guide/js/fig-sidenav.js
+++ cfgov/unprocessed/apps/filing-instruction-guide/js/fig-sidenav.js
@@ -1,5 +1,5 @@
 import Expandable from '@cfpb/cfpb-expandables/src/Expandable';
-
+import { DESKTOP, viewportIsIn } from '../../../js/modules/util/breakpoint-state.js';
 
 const appRoot = () => document.querySelector( 'main.o-fig' );
 
@@ -8,6 +8,11 @@ const getNavItemContainer = target => getNavItem( target ).closest( '.o-secondar
 
 const highlightNavItem = target => getNavItem( target ).classList.add( 'm-nav-link__current' );
 const unHighlightNavItem = target => getNavItem( target ).classList.remove( 'm-nav-link__current' );
+const scrollNavItem = target => getNavItem( target ).scrollIntoView( {
+  behavior: 'auto',
+  block: 'nearest',
+  inline: 'start'
+} );
 
 const showElement = el => el && el.classList.remove( 'u-hide-on-desktop' );
 const hideElement = el => el && el.classList.add( 'u-hide-on-desktop' );
@@ -15,7 +20,22 @@ const hideElement = el => el && el.classList.add( 'u-hide-on-desktop' );
 let prevTarget;
 
 /**
- * Description
+ * The native scrollIntoView() function is great but when there's a sticky header,
+ * the target element gets hidden underneath. This lets us specify an offset for
+ * the target element to scroll to.
+ * @param {DOMNode} el - HTML element to scroll to
+ * @param {integer} offset - number of pixels away from element
+ */
+const scrollIntoViewWithOffset = ( el, offset ) => {
+  window.scrollTo( {
+    top: el.getBoundingClientRect().top - document.body.getBoundingClientRect().top - offset,
+    behavior: 'smooth'
+  } );
+};
+
+/**
+ * Update the sidenav by highlighting the current section and showing
+ * parent nav items as needed
  * @param {string} target - `href` value of the nav item to highlight
  */
 const updateNav = target => {
@@ -25,6 +45,7 @@ const updateNav = target => {
   const targetContainer = getNavItemContainer( target );
   showElement( targetContainer );
   highlightNavItem( target );
+  scrollNavItem( target );
 
   // Remove the highlighting from the previous menu item and close its container
   if ( prevTarget ) {
@@ -56,13 +77,15 @@ const handleIntersect = entries => {
  */
 const init = () => {
   // Only proceed if IntersectionObserver is supported (everything except IE)
+  // and we're on a larger screen
   // See https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
-  if ( 'IntersectionObserver' in window ) {
+  if ( 'IntersectionObserver' in window && viewportIsIn( DESKTOP ) ) {
     appRoot().querySelectorAll( '.o-secondary-navigation_list__children' ).forEach( ul => {
       hideElement( ul );
     } );
 
     const observer = new IntersectionObserver( handleIntersect, {
+
       // Sets an intersection area that spans 5% above the top of the viewport and
       // 95% above the bottom of the viewport, resulting in a box that is 10% of
       // the viewport height with 5% hanging over the top.
@@ -76,8 +99,20 @@ const init = () => {
 
     sections.forEach( section => observer.observe( section ) );
   }
+
+  if ( !viewportIsIn( DESKTOP ) ) {
+    appRoot().addEventListener( 'click', event => {
+      if ( event.target.classList.contains( 'm-nav-link' ) ) {
+        event.preventDefault();
+        document.querySelector( '.o-fig_sidebar .o-expandable_header' ).click();
+        setTimeout( () => {
+          scrollIntoViewWithOffset( document.getElementById( event.target.getAttribute( 'href' ).replace( '#', '' ) ), 60 );
+        }, 300);
+      }
+    } );
+  }
 };
 
 Expandable.init();
 
-window.addEventListener( 'load', init );
+init();
