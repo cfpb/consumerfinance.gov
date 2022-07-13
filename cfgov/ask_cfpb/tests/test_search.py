14be5e0da03cddec277b7c8a648a4c5c7aa399c4
cooldragontattoo <troy.rttr@gmail.com>
2022-07-13 10:25:05 -0400

    95% coverage

--- cfgov/ask_cfpb/tests/test_search.py
+++ cfgov/ask_cfpb/tests/test_search.py
@@ -1,25 +1,19 @@
-import json
-import pdb
+from io import StringIO
 import unittest
 import json
 from unittest import mock
+
 from ask_cfpb.models.answer_page import AnswerPage
 
-from django_elasticsearch_dsl.registries import DocumentRegistry, registry
-from django.apps import apps
-from django.http import Http404, HttpRequest, QueryDict
-from django.test import TestCase, override_settings
-from django.urls import reverse
-from ask_cfpb.forms import AutocompleteForm
+from django.test import TestCase
 from ask_cfpb.models.search import UNSAFE_CHARACTERS
 
 from elasticsearch.exceptions import RequestError
+from search.elasticsearch_helpers import ElasticsearchTestsMixin
 
-from ask_cfpb.documents import AnswerPageDocument
-from ask_cfpb.models import ENGLISH_PARENT_SLUG, SPANISH_PARENT_SLUG
+
+from ask_cfpb.models.answer_page import AnswerPage
 from ask_cfpb.models.search import AnswerPageSearch, make_safe
-from ask_cfpb.views import ask_search, redirect_ask_search
-from v1.util.migrations import get_or_create_page
 
 class TestSearchMakeSafe(unittest.TestCase):
 
@@ -28,55 +22,12 @@ class TestSearchMakeSafe(unittest.TestCase):
         unsafe_term = term + ''.join(UNSAFE_CHARACTERS)
         self.assertEqual(term, make_safe(unsafe_term))
 
-
-
-
-class TestAnswerPageSearch(unittest.TestCase):
+class TestAnswerPageSearch(ElasticsearchTestsMixin, TestCase):
 
     def setUp(self):
         from v1.models import HomePage
 
         self.ROOT_PAGE = HomePage.objects.get(slug="cfgov")
-        self.english_parent_page = get_or_create_page(
-            apps,
-            "ask_cfpb",
-            "AnswerLandingPage",
-            "Ask CFPB",
-            ENGLISH_PARENT_SLUG,
-            self.ROOT_PAGE,
-            language="en",
-            live=True,
-        )
-        self.spanish_parent_page = get_or_create_page(
-            apps,
-            "ask_cfpb",
-            "AnswerLandingPage",
-            "Obtener respuestas",
-            SPANISH_PARENT_SLUG,
-            self.ROOT_PAGE,
-            language="es",
-            live=True,
-        )
-        self.en_page = get_or_create_page(
-            apps,
-            "ask_cfpb",
-            "AnswerResultsPage",
-            "Mock English results page",
-            "ask-cfpb-search-results",
-            self.english_parent_page,
-            language="en",
-            live=True,
-        )
-        self.es_page = get_or_create_page(
-            apps,
-            "ask_cfpb",
-            "AnswerResultsPage",
-            "Mock Spanish results page",
-            "respuestas",
-            self.spanish_parent_page,
-            language="es",
-            live=True,
-        )
     
     def test_AnswerPageSearch_init_en(self):
         search_term = 'tax'
@@ -97,19 +48,40 @@ class TestAnswerPageSearch(unittest.TestCase):
         assert spanish_answerPage.suggestion == None
         assert spanish_answerPage.results == []
 
-
-    def test_AnswerPageSearch_autocomplete_en(self):
-        test_answer_page = AnswerPage(title="Hello", question="Hello?", slug="/test-answer-page")
-        test_answer_page.save()
-        test_answer_page.get_latest_revision().publish()
-        registry.update(test_answer_page)
-        # print('printttt')
-        # print(test_answer_page.search()
-        #         .filter("term", language=self.language)
-        #         .query(
-        #             "match",
-        #             autocomplete="hello",
-        #         ))
-        search_term = "hell"
+    def test_AnswerPageSearch_autocomplete(self):
+        test_answer_page = AnswerPage(title="Money 101", question="What is money?", slug="test-answer-page", live=True)
+        self.ROOT_PAGE.add_child(instance=test_answer_page)
+        self.rebuild_elasticsearch_index("ask_cfpb", stdout=StringIO())
+        search_term = "mone"
         test_answer_page_search = AnswerPageSearch(search_term=search_term)
-        self.assertEqual(test_answer_page_search.autocomplete(), "hello")
+        self.assertEqual(test_answer_page_search.autocomplete()[0]['question'], "What is money?")
+
+    def test_AnswerPageSearch_autocomplete_error(self):
+        with mock.patch("ask_cfpb.documents.AnswerPageDocument.search", side_effect=RequestError):
+            results = AnswerPageSearch.autocomplete('mone')
+            self.assertEqual(results, [])
+    
+    def test_AnswerPage_search(self):
+        test_answer_page = AnswerPage(title="Money 101", question="What is money?", answer_content="Money makes the world go round.", slug="test-answer-page", live=True)
+        self.ROOT_PAGE.add_child(instance=test_answer_page)
+        self.rebuild_elasticsearch_index("ask_cfpb", stdout=StringIO())
+        search_term = "What is money?"
+        test_answer_page_search = AnswerPageSearch(search_term=search_term)
+        test_answer_page_search_results = test_answer_page_search.search()['results'][0].text.rstrip()
+        self.assertEqual(test_answer_page_search_results, "What is money?")
+
+    def test_AnswerPage_suggest(self):
+        test_answer_page = AnswerPage(title="Money 101", question="What is money?", answer_content="Money makes the world go round.", slug="test-answer-page3", live=True)
+        self.ROOT_PAGE.add_child(instance=test_answer_page)
+        self.rebuild_elasticsearch_index("ask_cfpb", stdout=StringIO())
+        search_term = "monye"
+        test_answer_page_search = AnswerPageSearch(search_term=search_term)
+        test_answer_page_search_results = test_answer_page_search.suggest()
+        self.assertEqual(test_answer_page_search_results['search_term'], "money")
+
+    def test_AnswerPage_suggest_error(self):
+        with mock.patch("ask_cfpb.documents.AnswerPageDocument.search", side_effect=IndexError):
+            try:
+                AnswerPageSearch.suggest('money')
+            except IndexError:
+                assert True
