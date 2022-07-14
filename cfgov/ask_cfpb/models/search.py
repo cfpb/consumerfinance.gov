0f6f4ca9f76598102d35805c43f75751def2fd05
Will Barton <william.barton@cfpb.gov>
2022-07-14 11:37:51 -0400

    Remove commented-out pdb call

--- cfgov/ask_cfpb/models/search.py
+++ cfgov/ask_cfpb/models/search.py
@@ -82,7 +82,6 @@ class AnswerPageSearch:
             .suggest("suggestion", self.search_term, term={"field": "text"})
         )
         response = s.execute()
-        # pdb.set_trace()
         try:
             self.suggestion = response.suggest.suggestion[0].options[0].text
         except IndexError:
