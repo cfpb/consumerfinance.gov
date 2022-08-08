bc2b920ca7becef04c97cd58988dea10679366d2
Steve Clay <steve@mrclay.org>
2022-08-08 17:23:21 -0400

    [tdp] Add more related doc titles

--- cfgov/teachers_digital_platform/documents.py
+++ cfgov/teachers_digital_platform/documents.py
@@ -134,6 +134,10 @@ class ActivityPageDocument(Document):
             file = getattr(instance, field)
             if file:
                 content_bits.append(file.title)
+        for item in instance.activity_documents.all():
+            content_bits.append(item.title)
+        for item in instance.handout_documents.all():
+            content_bits.append(item.title)
         return " ".join(content_bits)
 
     def prepare_big_idea(self, instance):
