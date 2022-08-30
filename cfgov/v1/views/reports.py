7d355d514980b55a556b8e6403787ae5b4720ec1
Nicholas Johnson <nicholas.johnson@cfpb.gov>
2022-08-30 12:12:58 -0400

    Add Ask CFPB Wagtail report

--- cfgov/v1/views/reports.py
+++ cfgov/v1/views/reports.py
@@ -1,4 +1,7 @@
+import html
 from datetime import date
+
+from django.utils import html as html_util
 
 from wagtail.admin.views.reports import PageReportView, ReportView
 from wagtail.documents.models import Document
@@ -6,6 +9,7 @@ from wagtail.images import get_image_model
 
 from bs4 import BeautifulSoup
 
+from ask_cfpb.models.answer_page import AnswerPage
 from v1.models import CFGOVPage
 from v1.models.enforcement_action_page import EnforcementActionPage
 
@@ -18,6 +22,11 @@ def process_categories(queryset):
 def process_tags(queryset):
     """Prep the set of tags assocaited with a document or page."""
     return ", ".join([tag for tag in queryset])
+
+
+def strip_html(content):
+    unescaped = html.unescape(content)
+    return html_util.strip_tags(unescaped).strip()
 
 
 def process_enforcement_action_page_content(page_content):
@@ -213,3 +222,115 @@ class EnforcementActionsReportView(ReportView):
         return EnforcementActionPage.objects.all().prefetch_related(
             "categories", "statutes"
         )
+
+
+class AskReportView(ReportView):
+    header_icon = "help"
+    title = "Ask CFPB report"
+
+    list_export = [
+        "answer_base",
+        "id",
+        "question",
+        "short_answer",
+        "answer_content",
+        "url",
+        "live",
+        "last_edited",
+        "redirect_to_page",
+        "portal_topic",
+        "portal_category",
+        "related_questions",
+        "related_resource",
+        "language",
+    ]
+    export_headings = {
+        "answer_base": "Ask ID",
+        "id": "Page ID",
+        "question": "Question",
+        "short_answer": "Short answer",
+        "answer_content": "Answer",
+        "url": "URL",
+        "live": "Live",
+        "last_edited": "Last edited",
+        "redirect_to_page": "Redirect",
+        "portal_topic": "Portal topics",
+        "portal_category": "Portal categories",
+        "related_questions": "Related questions",
+        "related_resource": "Related resource",
+        "language": "Language",
+    }
+
+    def process_related_page_id(related_page):
+        if related_page:
+            return related_page.id
+        else:
+            return ""
+
+    def process_answer_content(answer_content):
+        answer_streamfield = answer_content.raw_data
+        answer_text = list(
+            filter(lambda item: item["type"] == "text", answer_streamfield)
+        )
+        if answer_text:
+            answer = answer_text[0].get("value").get("content")
+        else:
+            # If no text block is found,
+            # there is either a HowTo or FAQ schema block.
+            # Both define a description field, so we'll use that here.
+            answer_schema = list(
+                filter(
+                    lambda item: item["type"] == "how_to_schema"
+                    or item["type"] == "faq_schema",
+                    answer_streamfield,
+                )
+            )
+            if answer_schema:
+                answer = answer_schema[0].get("value").get("description")
+            else:
+                # This is a question with no answer, possibly a new draft.
+                answer = ""
+        return strip_html(answer)
+
+    def process_portal_topics(portal_data):
+        portal_topics = []
+        if portal_data:
+            for topic in portal_data.all():
+                portal_topics.append(topic.heading)
+        joined_topics = " | ".join(portal_topics)
+        return joined_topics
+
+    def process_related_questions(questions):
+        related_questions = []
+        if questions:
+            for question in questions.all():
+                related_questions.append(str(question.id))
+        joined_question_ids = " | ".join(related_questions)
+        return joined_question_ids
+
+    def process_related_resource(related_resource):
+        if related_resource:
+            return related_resource.title
+        else:
+            return ""
+
+    custom_field_preprocess = {
+        "answer_base": {"csv": process_related_page_id},
+        "short_answer": {"csv": strip_html},
+        "answer_content": {"csv": process_answer_content},
+        "redirect_to_page": {"csv": process_related_page_id},
+        "portal_topic": {"csv": process_portal_topics},
+        "portal_category": {"csv": process_portal_topics},
+        "related_questions": {"csv": process_related_questions},
+        "related_resource": {"csv": process_related_resource},
+    }
+
+    template_name = "v1/ask_report.html"
+
+    def get_filename(self):
+        return generate_filename("ask-cfpb")
+
+    def get_queryset(self):
+        return AnswerPage.objects.prefetch_related(
+            "portal_topic", "related_questions"
+        ).order_by("language", "-answer_base__id")
