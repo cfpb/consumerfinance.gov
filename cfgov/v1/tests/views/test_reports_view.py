c87451af65a37caeb8cb1e57cfafd82ddd1a13be
Nicholas Johnson <nicholas.johnson@cfpb.gov>
2022-09-06 11:23:34 -0400

    Reorganize reports view tests

--- cfgov/v1/tests/views/test_reports_view.py
+++ cfgov/v1/tests/views/test_reports_view.py
@@ -37,31 +37,13 @@ from v1.views.reports import (
 
 class ServeViewTestCase(TestCase):
     def setUp(self):
-        self.page_metadata_report_view = PageMetadataReportView()
-        self.documents_report_view = DocumentsReportView()
-        self.ask_report_view = AskReportView()
-        self.tag1 = baker.make(Tag, name="tag1")
-        self.tag2 = baker.make(Tag, name="tag2")
+        # Shared items
         self.site = Site.objects.get(is_default_site=True)
         self.root_page = self.site.root_page
-        self.blog_page = BlogPage(title="Blogojevich", live=True)
-        self.root_page.add_child(instance=self.blog_page)
         self.category_tuple = categories[0][1][0]
         self.category = CFGOVPageCategory(name=self.category_tuple[1])
-        self.blog_page.categories.add(self.category)
-        self.blog_page.tags.add(self.tag1, self.tag2)
-        self.document = Document(title="Test document 1")
-        self.document.save()
-        self.document.tags.add(self.tag1, self.tag2)
-        self.related_resource = baker.make(RelatedResource, title="Resource")
-
-        self.enforcement = EnforcementActionPage(
-            title="Great Test Page",
-            preview_description="This is a great test page.",
-            initial_filing_date=timezone.now(),
-        )
         self.html_content = "<p>Blog Text</p> <a href='www.test.com'>test</a>"
-        self.content = json.dumps(
+        self.full_width_text_content = json.dumps(
             [
                 {
                     "type": "full_width_text",
@@ -74,35 +56,41 @@ class ServeViewTestCase(TestCase):
                 },
             ]
         )
+        self.related_resource = baker.make(RelatedResource, title="Resource")
+        self.tag1 = baker.make(Tag, name="tag1")
+        self.tag2 = baker.make(Tag, name="tag2")
 
+        # Page metadata report
+        self.page_metadata_report_view = PageMetadataReportView()
+        self.blog_page = BlogPage(title="Blogojevich", live=True)
+        self.root_page.add_child(instance=self.blog_page)
+        self.blog_page.categories.add(self.category)
+        self.blog_page.tags.add(self.tag1, self.tag2)
+
+        # Enforcement actions report
+        self.enforcement_actions_report_view = EnforcementActionsReportView()
+        self.enforcement = EnforcementActionPage(
+            title="Great Test Page",
+            preview_description="This is a great test page.",
+            initial_filing_date=timezone.now(),
+        )
         status = EnforcementActionStatus(status="expired-terminated-dismissed")
         self.enforcement.statuses.add(status)
-        self.enforcement.content = self.content
+        self.enforcement.content = self.full_width_text_content
         product = EnforcementActionProduct(product="Fair Lending")
         self.enforcement.products.add(product)
         save_new_page(self.enforcement)
 
-        self.enforcement_actions_report_view = EnforcementActionsReportView()
+        # Documents report
+        self.documents_report_view = DocumentsReportView()
+        self.document = Document(title="Test document 1")
+        self.document.save()
+        self.document.tags.add(self.tag1, self.tag2)
 
-    def test_enforcement_report_get_queryset(self):
-        self.assertEqual(
-            self.enforcement_actions_report_view.get_queryset().count(), 1
-        )
+        # Ask report
+        self.ask_report_view = AskReportView()
 
-    def test_process_enforcement_content(self):
-        self.assertTrue(
-            process_enforcement_action_page_content(
-                self.enforcement.content
-            ).__contains__("Blog Text")
-        )
-
-    def test_enforcements_report_get_filename(self):
-        today = date.today()
-        self.assertEqual(
-            self.enforcement_actions_report_view.get_filename(),
-            f"enforcement-actions-report-{today}",
-        )
-
+    # Shared methods
     def test_construct_absolute_url(self):
         self.assertEqual(
             construct_absolute_url(self.blog_page.url),
@@ -138,6 +126,7 @@ class ServeViewTestCase(TestCase):
         stripped_content = strip_html(self.html_content)
         self.assertEqual(stripped_content, "Blog Text test")
 
+    # Page metadata report
     def test_metadata_report_get_filename(self):
         today = date.today()
         self.assertEqual(
@@ -150,6 +139,27 @@ class ServeViewTestCase(TestCase):
             self.page_metadata_report_view.get_queryset().count(), 3
         )
 
+    # Enforcement actions report
+    def test_enforcements_report_get_filename(self):
+        today = date.today()
+        self.assertEqual(
+            self.enforcement_actions_report_view.get_filename(),
+            f"enforcement-actions-report-{today}",
+        )
+
+    def test_enforcement_report_get_queryset(self):
+        self.assertEqual(
+            self.enforcement_actions_report_view.get_queryset().count(), 1
+        )
+
+    def test_process_enforcement_content(self):
+        self.assertTrue(
+            process_enforcement_action_page_content(
+                self.enforcement.content
+            ).__contains__("Blog Text")
+        )
+
+    # Documents report
     def test_documents_report_get_filename(self):
         today = date.today()
         self.assertEqual(
@@ -160,6 +170,7 @@ class ServeViewTestCase(TestCase):
     def test_documents_report_get_queryset(self):
         self.assertEqual(self.documents_report_view.get_queryset().count(), 1)
 
+    # Ask report
     def test_ask_report_get_filename(self):
         today = date.today()
         self.assertEqual(
