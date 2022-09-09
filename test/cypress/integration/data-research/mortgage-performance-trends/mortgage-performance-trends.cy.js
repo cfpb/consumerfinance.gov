196dc74487fa65f3331878acb1c5d9bcfd1f7de6
Colin-Seifer <colinmseifer@gmail.com>
2022-09-09 16:56:26 -0400

    added check for map to test file

--- test/cypress/integration/data-research/mortgage-performance-trends/mortgage-performance-trends.cy.js
+++ test/cypress/integration/data-research/mortgage-performance-trends/mortgage-performance-trends.cy.js
@@ -12,12 +12,14 @@ describe( 'Mortgage Performance Trends', () => {
 
   it( 'should display delinquency rates by month for a given state', () => {
     trends.open();
-    trends.selectStateForDelinquencyRatesPerMonth( 'Virginia' );
-    trends.selectMonth( 'January' );
-    trends.selectYear( '2017' );
-    trends.mapTitle().should( 'contain', 'Virginia' );
-    trends.mapTitle().should( 'contain', 'January' );
-    trends.mapTitle().should( 'contain', '2017' );
+    trends.mapTitle().then( () => {
+      trends.selectStateForDelinquencyRatesPerMonth( 'Virginia' );
+      trends.selectMonth( 'January' );
+      trends.selectYear( '2017' );
+      trends.mapTitle().should( 'contain', 'Virginia' );
+      trends.mapTitle().should( 'contain', 'January' );
+      trends.mapTitle().should( 'contain', '2017' );
+    } );
   } );
 
 } );
