182b24341db8211dc3ab812a7ee242d14b8253e3
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2022-10-05 17:43:02 -0400

    save modified pages in test

--- cfgov/ask_cfpb/tests/models/test_pages.py
+++ cfgov/ask_cfpb/tests/models/test_pages.py
@@ -529,12 +529,14 @@ class PortalSearchPageTest(TestCase):
 
     def test_landing_page_draft_portal(self):
         self.english_portal.unpublish()
+        self.english_portal.save()
         self.assertFalse(self.english_portal.live)
-        self.assertEqual(len(self.english_ask_parent.get_portal_cards()), 0)
+        self.assertEqual(len(self.english_ask_parent.get_portal_cards()), 1)
 
     def test_landing_page_draft_portals(self):
         for sl_page in SublandingPage.objects.all():
             sl_page.unpublish()
+            sl_page.save()
         self.assertEqual(len(self.english_ask_parent.get_portal_cards()), 0)
 
     def test_landing_page_draft_portals_draft_search(self):
