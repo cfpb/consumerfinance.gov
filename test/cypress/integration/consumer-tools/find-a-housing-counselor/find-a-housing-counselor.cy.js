ff74fe56776fc1efa8c0fcd25affaa9e6dd682ba
Nicholas Johnson <nicholas.johnson@cfpb.gov>
2022-10-12 16:25:48 -0400

    Stub Mapbox API in Cypress tests (#7279)

--- test/cypress/integration/consumer-tools/find-a-housing-counselor/find-a-housing-counselor.cy.js
+++ test/cypress/integration/consumer-tools/find-a-housing-counselor/find-a-housing-counselor.cy.js
@@ -4,9 +4,15 @@ const page = new FindAHousingCounselor();
 
 describe( 'Find a housing counselor', () => {
   describe( 'Search by ZIP code', () => {
+    beforeEach( () => {
+      // Return a fixture for the Mapbox APIs for the ZIP code 22204
+      page.interceptMapboxAPIRequests();
+    } );
+
     it( 'should return nearby counselors', () => {
       page.open();
       page.searchZipCode( '22204' );
+      cy.wait( [ '@mapboxStreets', '@mapboxText' ] );
       page.resultsSection().should( 'be.visible' );
     } );
   } );
