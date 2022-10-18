773dcb9d521ab73f4729f76109e07fce993749cc
Chris Contolini <contolini@users.noreply.github.com>
2022-10-17 23:53:25 -0400

    Dynamically toggle FIG mobile nav functionality
    Instead of only checking the user's viewport width on page load, listen
    for a media query change event and enable the FIG's mobile navigation
    functionality when appropriate.
    
    https://github.local/CFPB/1071-data-collection/issues/190

--- cfgov/unprocessed/apps/filing-instruction-guide/js/fig-sidenav.js
+++ cfgov/unprocessed/apps/filing-instruction-guide/js/fig-sidenav.js
@@ -1,6 +1,28 @@
+import varsBreakpoints from '@cfpb/cfpb-core/src/vars-breakpoints';
+
 import Expandable from '@cfpb/cfpb-expandables/src/Expandable';
 import { DESKTOP, viewportIsIn } from '../../../js/modules/util/breakpoint-state.js';
 import * as fig from './fig-sidenav-utils';
+
+/**
+ * The sidenav is nested inside an expandable on smaller screens.
+ * We have to close the expandable before sending the user to the appropriate section.
+ * @param {MouseEvent} event - Event from user clicking/tapping a nav item
+ */
+const handleMobileNav = event => {
+  if ( event.target.matches( '.m-nav-link' ) ) {
+    event.preventDefault();
+    document.querySelector( '.o-fig_sidebar .o-expandable_header' ).click();
+    // Scrolling before the expandable closes causes jitters on some devices
+    setTimeout( () => {
+      fig.scrollIntoViewWithOffset( document.getElementById( event.target.getAttribute( 'href' ).replace( '#', '' ) ), 60 );
+    }, 300 );
+  }
+  if ( event.target.matches( '.o-fig_heading > a' ) ) {
+    event.preventDefault();
+    fig.scrollIntoViewWithOffset( document.getElementById( event.target.getAttribute( 'href' ).replace( '#', '' ) ), 60 );
+  }
+};
 
 /**
  * init - Initialize everything on page load.
@@ -9,7 +31,7 @@ const init = () => {
   /* Only proceed if IntersectionObserver is supported (everything except IE)
      and we're on a larger screen
      See https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API */
-  if ( 'IntersectionObserver' in window && viewportIsIn( DESKTOP ) ) {
+  if ( 'IntersectionObserver' in window ) {
     fig.appRoot.querySelectorAll( '.o-secondary-navigation_list__children' ).forEach( ul => {
       fig.hideElement( ul );
     } );
@@ -32,22 +54,20 @@ const init = () => {
   }
 
   if ( !viewportIsIn( DESKTOP ) ) {
-    fig.appRoot.addEventListener( 'click', event => {
-      if ( event.target.matches( '.m-nav-link' ) ) {
-        event.preventDefault();
-        document.querySelector( '.o-fig_sidebar .o-expandable_header' ).click();
-        // Scrolling before the expandable closes causes jitters on some devices
-        setTimeout( () => {
-          fig.scrollIntoViewWithOffset( document.getElementById( event.target.getAttribute( 'href' ).replace( '#', '' ) ), 60 );
-        }, 300 );
-      }
-      if ( event.target.matches( '.o-fig_heading > a' ) ) {
-        event.preventDefault();
-        fig.scrollIntoViewWithOffset( document.getElementById( event.target.getAttribute( 'href' ).replace( '#', '' ) ), 60 );
-      }
-    } );
+    fig.appRoot.addEventListener( 'click', handleMobileNav );
   }
 };
+
+/* If the browser window is no greater than 900px, enable the mobile nav
+   functionality. Otherwise, disable it. This event is triggered whenever
+   the user resizes the viewport and its width passes the 900px threshold. */
+window.matchMedia( `(max-width: ${ varsBreakpoints.bpSM.max }px)` ).addEventListener( 'change', mediaQuery => {
+  if ( mediaQuery.matches ) {
+    fig.appRoot.addEventListener( 'click', handleMobileNav );
+  } else {
+    fig.appRoot.removeEventListener( 'click', handleMobileNav );
+  }
+} );
 
 Expandable.init();
 
