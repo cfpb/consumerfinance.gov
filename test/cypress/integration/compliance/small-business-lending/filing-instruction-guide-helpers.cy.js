0fea210e234d67f8abad937efb1a2fe84c5d06c4
Chris Contolini <contolini@users.noreply.github.com>
2022-10-18 00:51:23 -0400

    Update FIG browser tests
    Don't run FIG browser tests unless CYPRESS_FIG_TESTS is explicitly
    passed.

--- test/cypress/integration/compliance/small-business-lending/filing-instruction-guide-helpers.cy.js
+++ test/cypress/integration/compliance/small-business-lending/filing-instruction-guide-helpers.cy.js
@@ -1,7 +1,7 @@
 export class FilingInstructionGuide {
 
   url() {
-    return '/compliance/compliance-resources/small-business-lending/1071-filing-instruction-guide/';
+    return Cypress.env( 'FIG_URL' );
   }
 
   open() {
