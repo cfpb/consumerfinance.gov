fea23b412b69b5a937902b23fe9df26e4dc855e9
Chuck Werner <mistergone@gmail.com>
2022-10-31 10:31:21 -0400

    - Add additional validation for url parameters

--- cfgov/unprocessed/apps/paying-for-college/js/college-costs/util/url-parameter-utils.js
+++ cfgov/unprocessed/apps/paying-for-college/js/college-costs/util/url-parameter-utils.js
@@ -117,8 +117,10 @@ function getQueryVariables() {
     const pair = elem.split( '=' );
     const key = decodeURIComponent( pair[0] ).replace(/[^a-z0-9]/gi, '' );
     const value = decodeURIComponent( pair[1] ).replace(/[^a-z0-9.-]/gi, '' );
-    queryVariables[key] = value;
-  });
+    if ( urlParameters.hasOwnProperty( key ) ) {
+      queryVariables[key] = value;
+    }
+  } );
 
   return queryVariables;
 }
