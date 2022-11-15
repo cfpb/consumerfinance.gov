4acd191d6482fb7a88d220b8489b2eb5aee06050
Andy Chosak <andy.chosak@cfpb.gov>
2022-11-15 13:52:28 -0500

    Add all supported languages to Django settings
    This commit migrates the full list of supported website languages from
    a list in ref.py to Django `settings.LANGUAGES`'
    
    This allows for the creation of alternate mega menus in languages
    besides English and Spanish.

--- cfgov/v1/forms.py
+++ cfgov/v1/forms.py
@@ -3,6 +3,7 @@ from datetime import date
 from operator import itemgetter
 
 from django import forms
+from django.conf import settings
 from django.core.cache import cache
 from django.core.exceptions import ValidationError
 from django.forms import widgets
@@ -254,7 +255,7 @@ class FilterableListForm(forms.Form):
         # Grab the language names from the reference list.
         language_options = [
             (code, name)
-            for code, name in ref.supported_languages
+            for code, name in settings.LANGUAGES
             if code in language_codes
         ]
 
