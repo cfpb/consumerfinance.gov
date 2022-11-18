34e0f8e88cc90a75b64579da2b7e649a9f2a3c93
Ans <anselmbradford@users.noreply.github.com>
2022-11-18 15:30:09 -0500

    Regs3k/TDP: Delete non-functional unit tests (#7353)
    * Delete nonfunctional tests
    
    * Delete BASE_JS_PATH

--- test/unit_tests/apps/teachers-digital-platform/js/index-spec.js
+++
@@ -1,7 +0,0 @@
-const app = require('../../../../../cfgov/unprocessed/apps/teachers-digital-platform/js/index.js');
-
-describe('The app', () => {
-  it('should not throw any errors on init', () => {
-    expect(() => app).not.toThrow();
-  });
-});
