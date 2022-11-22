373bb373814dfe0190555b5c0f7378ce47fb95ee
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2022-11-22 10:26:57 -0500

    remove unneeded lint command

--- scripts/lint.sh
+++
@@ -1,8 +0,0 @@
-## Run prettier. See ignored path in .prettierignore.
-yarn prettier "./**/*.{js,jsx,ts,tsx,md}" --write
-
-## Run JS linting. See ignored path in .eslintignore.
-eslint --ignore-pattern node_modules './{cfgov/unprocessed,config,esbuild,scripts,test}/**/*.js' --fix
-
-## Run CSS linting. See ignored path in .stylelintignore.
-stylelint './cfgov/unprocessed/**/*.{css,less}' --fix
