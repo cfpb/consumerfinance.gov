cc64f97d360eb8d37ff4b84efd9b30d3ce4e734d
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2022-12-22 10:23:39 -0500

    Update tdp spy tests for esm (#7426)
    * update tdp spy tests for esm
    
    * assign to default at time of import
    
    * more specific mock names
    
    * more pr comments

--- test/unit_tests/apps/teachers-digital-platform/js/tdp-surveys-spec.js
+++ test/unit_tests/apps/teachers-digital-platform/js/tdp-surveys-spec.js
@@ -1,47 +1,58 @@
 import { jest } from '@jest/globals';
-import surveys from '../../../../../cfgov/unprocessed/apps/teachers-digital-platform/js/tdp-surveys.js';
-import * as gradeLevelModule from '../../../../../cfgov/unprocessed/apps/teachers-digital-platform/js/survey/grade-level-page.js';
-import * as resultsModule from '../../../../../cfgov/unprocessed/apps/teachers-digital-platform/js/survey/result-page.js';
-import * as surveyModule from '../../../../../cfgov/unprocessed/apps/teachers-digital-platform/js/survey/survey-page.js';
 
-let glp;
-let rp;
-let sp;
+const gl =
+  '../../../../../cfgov/unprocessed/apps/teachers-digital-platform/js/survey/grade-level-page.js';
+const res =
+  '../../../../../cfgov/unprocessed/apps/teachers-digital-platform/js/survey/result-page.js';
+const sur =
+  '../../../../../cfgov/unprocessed/apps/teachers-digital-platform/js/survey/survey-page.js';
 
-xdescribe('The TDP survey router', () => {
-  beforeEach(() => {
-    glp = jest
-      .spyOn(gradeLevelModule, 'gradeLevelPage')
-      .mockImplementation(() => 1);
-    rp = jest.spyOn(resultsModule, 'resultsPage').mockImplementation(() => 1);
-    sp = jest.spyOn(surveyModule, 'surveyPage').mockImplementation(() => 1);
-  });
+let surveys, gradeLevelPage, resultsPage, surveyPage;
 
-  afterEach(() => {
-    jest.restoreAllMocks();
+describe('The TDP survey router', () => {
+  beforeAll(async () => {
+    jest.unstable_mockModule(gl, () => ({
+      gradeLevelPage: jest.fn(),
+    }));
+
+    jest.unstable_mockModule(res, () => ({
+      resultsPage: jest.fn(),
+    }));
+
+    jest.unstable_mockModule(sur, () => ({
+      surveyPage: jest.fn(),
+    }));
+
+    surveys = await import(
+      '../../../../../cfgov/unprocessed/apps/teachers-digital-platform/js/tdp-surveys.js'
+    );
+    surveys = surveys.default;
+    gradeLevelPage = (await import(gl)).gradeLevelPage;
+    resultsPage = (await import(res)).resultsPage;
+    surveyPage = (await import(sur)).surveyPage;
   });
 
   it('recognizes grade level page', () => {
     document.body.innerHTML = '<div data-tdp-page="grade-level"></div>';
     surveys.init();
-    expect(glp).toHaveBeenCalled();
-    expect(sp).not.toHaveBeenCalled();
-    expect(rp).not.toHaveBeenCalled();
+    expect(gradeLevelPage).toHaveBeenCalled();
+    expect(resultsPage).not.toHaveBeenCalled();
+    expect(surveyPage).not.toHaveBeenCalled();
   });
 
   it('recognizes survey page', () => {
     document.body.innerHTML = '<div data-tdp-page="survey"></div>';
     surveys.init();
-    expect(glp).not.toHaveBeenCalled();
-    expect(sp).toHaveBeenCalled();
-    expect(rp).not.toHaveBeenCalled();
+    expect(gradeLevelPage.mock.calls.length).toBe(1);
+    expect(surveyPage.mock.calls.length).toBe(1);
+    expect(resultsPage).not.toHaveBeenCalled();
   });
 
   it('recognizes results page', () => {
     document.body.innerHTML = '<div data-tdp-page="results"></div>';
     surveys.init();
-    expect(glp).not.toHaveBeenCalled();
-    expect(sp).not.toHaveBeenCalled();
-    expect(rp).toHaveBeenCalled();
+    expect(gradeLevelPage.mock.calls.length).toBe(1);
+    expect(surveyPage.mock.calls.length).toBe(1);
+    expect(resultsPage.mock.calls.length).toBe(1);
   });
 });
