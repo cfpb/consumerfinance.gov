3535f68dea22c730eb9ee99646ff866b938660d7
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2022-12-27 13:50:03 -0500

    Remove table javascript (#7427)
    * remove table js
    
    * fix test
    
    Co-authored-by: Ans <anselmbradford@users.noreply.github.com>

--- cfgov/unprocessed/js/routes/on-demand/table.js
+++
@@ -1,7 +0,0 @@
-/* ==========================================================================
-   Scripts for Line Chart molecule.
-   ========================================================================== */
-
-// See https://github.com/cfpb/design-system/
-import Table from '@cfpb/cfpb-tables/src/Table';
-Table.init();
