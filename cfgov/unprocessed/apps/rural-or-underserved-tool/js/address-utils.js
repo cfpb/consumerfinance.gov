1ae9ed4d07916a7df519a918336287a33c8e594c
Ans <anselmbradford@users.noreply.github.com>
2023-03-17 14:35:43 -0400

    ROUT: Replace hardcoded icons with actual DS source icons (#7589)
    * Replace hardcoded icons with actual DS source icons
    
    * Update address-utils.js

--- cfgov/unprocessed/apps/rural-or-underserved-tool/js/address-utils.js
+++ cfgov/unprocessed/apps/rural-or-underserved-tool/js/address-utils.js
@@ -1,5 +1,7 @@
 import { addEl, getEl, getEls, removeClass } from './dom-tools.js';
 
+import PLUS_ROUND_ICON from '@cfpb/cfpb-icons/src/icons/plus-round.svg';
+
 /**
  * Checks whether an address is a duplicate to that in an array of addresses.
  *
@@ -94,41 +96,31 @@ function render(result) {
   if (hideRow === true) {
     rowHTML += ' u-hidden';
   }
-  rowHTML =
-    rowHTML +
-    '"><td>' +
-    result.input +
-    '</td>' +
-    '<td>' +
-    result.address +
-    '</td>' +
-    '<td>' +
-    result.countyName +
-    '</td>' +
-    '<td>' +
-    rural;
+  rowHTML = `${rowHTML}">
+    <td>${result.input}</td>
+    <td>${result.address}</td>
+    <td>${result.countyName}</td>
+    <td>${rural}`;
   // add the map link if needed
   if (rural !== '-') {
-    rowHTML =
-      rowHTML +
-      ' <a href="#" class="no-decoration hide-print' +
-      ' jsLoadMap right" data-map="false" data-lat="' +
-      result.x +
-      '" data-lon="' +
-      result.y +
-      '" data-id="loc-' +
-      result.id +
-      '">Show map <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1200" class="cf-icon-svg"><path d="M500 105.2c-276.1 0-500 223.9-500 500s223.9 500 500 500 500-223.9 500-500-223.9-500-500-500zm263.1 550.7H549.6v213.6c0 27.6-22.4 50-50 50s-50-22.4-50-50V655.9H236c-27.6 0-50-22.4-50-50s22.4-50 50-50h213.6V342.3c0-27.6 22.4-50 50-50s50 22.4 50 50v213.6h213.6c27.6 0 50 22.4 50 50s-22.5 50-50.1 50z"></path></svg></a>';
+    rowHTML = `${rowHTML}
+      <a href="#"
+         class="no-decoration hide-print jsLoadMap right"
+         data-map="false"
+         data-lat="${result.x}"
+         data-lon="${result.y}"
+         data-id="loc-${result.id}">
+          Show map ${PLUS_ROUND_ICON}`;
   }
   rowHTML += '</td></tr>';
   // add the map if needed
   if (rural !== '-') {
-    rowHTML =
-      rowHTML +
-      '<tr class="u-hidden"><td colspan="5">' +
-      '<div class="map" id="loc-' +
-      result.id +
-      '"></div></td></tr>';
+    rowHTML = `${rowHTML}
+      <tr class="u-hidden">
+        <td colspan="5">
+          <div class="map" id="loc-${result.id}"></div>
+        </td>
+      </tr>`;
   }
 
   removeClass('#' + result.type, 'u-hidden');
