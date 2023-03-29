86183116e5389b58e8b48085446cdbf6bafc32f4
Ans <git@anselmbradford.com>
2023-03-29 15:00:53 -0400

    Add rate-explorer tests

--- test/cypress/integration/consumer-tools/owning-a-home/explore-rates-helpers.cy.js
+++ test/cypress/integration/consumer-tools/owning-a-home/explore-rates-helpers.cy.js
@@ -1,13 +1,59 @@
 export class ExploreRates {
-  open() {
-    cy.visit('/owning-a-home/explore-rates/');
-  }
-
   selectState(state) {
     cy.get('#location').select(state);
   }
 
-  graph() {
+  selectRateType(val) {
+    this.getRateTypeSelector().select(val);
+  }
+
+  selectLoanType(val) {
+    this.getLoanTypeSelector().select(val);
+  }
+
+  selectCounty(val) {
+    this.getCountySelector().select(val);
+  }
+
+  getHousePriceInput() {
+    return cy.get('#house-price');
+  }
+
+  getCountySelector() {
+    return cy.get('#county');
+  }
+
+  getCountyAlert() {
+    return cy.get('#county-warning');
+  }
+
+  getHighBalanceAlert() {
+    return cy.get('#hb-warning');
+  }
+
+  // Loan type and term drop-down menus.
+  getRateTypeSelector() {
+    return cy.get('#rate-structure');
+  }
+
+  getLoanTermSelector() {
+    return cy.get('#loan-term');
+  }
+
+  getLoanTypeSelector() {
+    return cy.get('#loan-type');
+  }
+
+  getArmTypeSelector() {
+    return cy.get('#arm-type');
+  }
+
+  // Main graph area.
+  getGraph() {
     return cy.get('#chart-section').within(() => cy.get('figure:first'));
+  }
+
+  getChartResultAlert() {
+    return cy.get('#chart-result-alert');
   }
 }
