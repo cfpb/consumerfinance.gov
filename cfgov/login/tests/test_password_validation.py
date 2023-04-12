016526f4925c8a811c886d991115f8187fe79205
Andy Chosak <andy.chosak@cfpb.gov>
2023-04-12 15:40:00 -0400

    Fixup tests

--- cfgov/login/tests/test_password_validation.py
+++ cfgov/login/tests/test_password_validation.py
@@ -1,11 +1,10 @@
-from datetime import timedelta
-
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
 from django.core.exceptions import ValidationError
 from django.test import TestCase
-from django.utils import timezone
+
+from freezegun import freeze_time
 
 from login.models import PasswordHistoryItem
 from login.password_validation import (
@@ -13,6 +12,9 @@ from login.password_validation import (
     ComplexityValidator,
     HistoryValidator,
 )
+
+
+User = get_user_model()
 
 
 class TestComplexityValidator(TestCase):
@@ -62,39 +64,61 @@ class TestComplexityValidator(TestCase):
         )
 
 
-class TestAgeValidator(TestCase):
-    def test_age_validation_error(self):
-        UserModel = get_user_model()
-        user = UserModel.objects.create_user(
+class ValidatorTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
             "test_user", email="test@example.com"
         )
-        last_password = make_password("testing")
-        PasswordHistoryItem(
-            user=user,
-            encrypted_password=last_password,
-            locked_until=timezone.now() + timedelta(hours=1),
-            expires_at=timezone.now() + timedelta(days=90),
-        ).save()
 
-        validator = AgeValidator()
-        with self.assertRaises(ValidationError):
-            validator.validate("testing", user=user)
+    def test_age_validation_new_user(self):
+        # New users can always reset their password.
+        AgeValidator(hours=1).validate("testing", user=self.user)
 
+    def test_age_validation(self):
+        with freeze_time("9999-01-01") as frozen_time:
+            PasswordHistoryItem.objects.create(
+                user=self.user,
+                encrypted_password=make_password("testing"),
+            )
 
-class TestHistoryValidator(TestCase):
-    def test_history_validation_error(self):
-        UserModel = get_user_model()
-        user = UserModel.objects.create_user(
-            "test_user", email="test@example.com"
+            # Fast forward time by 90 minutes.
+            frozen_time.tick(60 * 90)
+
+            # AgeValidator with a 1 hour check will pass validation,
+            # because password was last set over an hour ago.
+            AgeValidator(hours=1).validate("testing", user=self.user)
+
+            # AgeValidator with a 2 hour check will fail validation,
+            # because password was last set less than 2 hours ago.
+            with self.assertRaises(ValidationError) as e:
+                AgeValidator(hours=2).validate("testing", user=self.user)
+
+            self.assertIn("once in 2 hours", e.exception.message)
+
+    def test_age_validation_superuser(self):
+        PasswordHistoryItem.objects.create(
+            user=self.user, encrypted_password=make_password("testing")
         )
-        last_password = make_password("testing")
-        PasswordHistoryItem(
-            user=user,
-            encrypted_password=last_password,
-            locked_until=timezone.now() + timedelta(hours=1),
-            expires_at=timezone.now() + timedelta(days=90),
-        ).save()
 
-        validator = HistoryValidator()
-        with self.assertRaises(ValidationError):
-            validator.validate("testing", user=user)
+        self.user.is_superuser = True
+
+        # AgeValidator with a 1 hour check will pass validation,
+        # even though password was last set less than an hour ago,
+        # because the user is a superuser.
+        AgeValidator(hours=1).validate("testing", user=self.user)
+
+    def test_history_validation(self):
+        # Make a history of 10 passwords, "testing0" ... "testing9".
+        for i in range(10):
+            PasswordHistoryItem.objects.create(
+                user=self.user, encrypted_password=make_password(f"testing{i}")
+            )
+
+        # A validator that checks the last 5 passwords will pass "testing0".
+        HistoryValidator(count=5).validate("testing0", user=self.user)
+
+        # A validator that checks the last 10 passwords will fail "testing0".
+        with self.assertRaises(ValidationError) as e:
+            HistoryValidator(count=10).validate("testing0", user=self.user)
+
+        self.assertIn("last 10 passwords", e.exception.message)
