00941342e97f540c852af8f09da1ee314ffc9b20
Andy Chosak <andy.chosak@cfpb.gov>
2023-04-12 13:41:16 -0400

    Cleanup v1.signals and make sure it is imported
    The various login-related signals are already covered in login.signals,
    implemented in commit 7258a1301d42cc78c59ccba38041f160ba3c7dde. So
    those can be deleted.
    
    At the same time, though, we still do want the v1.signals file to be
    imported, which that commit mistakenly removed. So this commit restores
    that import.

--- cfgov/v1/signals.py
+++ cfgov/v1/signals.py
@@ -1,14 +1,10 @@
-from datetime import timedelta
 from itertools import chain
 
 from django.conf import settings
 from django.core.cache import cache, caches
 from django.dispatch import receiver
-from django.utils import timezone
 
 from wagtail.signals import page_published, page_unpublished
-
-from axes.utils import reset as axes_reset
 
 from teachers_digital_platform.models.activity_index_page import (
     ActivityPage,
@@ -21,41 +17,6 @@ from v1.models.filterable_list_mixins import (
     FilterableListMixin,
 )
 from v1.util.ref import get_category_children
-
-
-def new_phi(user, expiration_days=90, locked_days=1):
-    now = timezone.now()
-    locked_until = now + timedelta(days=locked_days)
-    expires_at = now + timedelta(days=expiration_days)
-
-    from v1.models import PasswordHistoryItem
-
-    password_history = PasswordHistoryItem(
-        user=user,
-        encrypted_password=user.password,
-        locked_until=locked_until,
-        expires_at=expires_at,
-    )
-
-    password_history.save()
-    axes_reset(username=user.username)
-
-
-def user_save_callback(sender, **kwargs):
-    user = kwargs["instance"]
-
-    if kwargs["created"]:
-        if user.is_superuser:
-            # If a superuser was created, don't expire its password.
-            new_phi(user, locked_days=0)
-        else:
-            # If a regular user was just created, force a new password to be
-            # set right away by expiring the password and unlocking it.
-            new_phi(user, locked_days=0, expiration_days=0)
-    else:
-        current_password_history = user.passwordhistoryitem_set.latest()
-        if user.password != current_password_history.encrypted_password:
-            new_phi(user)
 
 
 def invalidate_post_preview(sender, **kwargs):
