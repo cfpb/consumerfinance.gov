cfpb/consumerfinance.gov

commit 11c6b8dfe86032bb4eea9c9e7f54cce0de3da509

Allow disabling StreamField block unescaping
By default we always unescape the rendering of all StreamField blocks;
this allows potentially unsafe HTML characters to be added to just
about any block, which is not great. See internal platform 3839.

This adds the ability to disable unescaping for particular blocks that
we know should never contain any raw HTML characters, and for which we
want to explicitly prohibit this from happening.

This can be done by setting an "unescape = False" property to the
block's Meta class, for example:

```
class MyBlock(blocks.StructBlock):
   ...

   class Meta:
      ...
      unescape = False
```

If this is not set, by default the current behavior will continue,
meaning that the block's rendered HTML will be unescaped before being
added to the page.

cfgov/v1/jinja2/v1/includes/templates/render_block.html
@@ -18,6 +18,9 @@
 
    <div class="{{ block_classes | unique | join(' ') }}"
       {{- accessible_languages.render() -}}>
-      {{ render_stream_child(block) | safe }}
+      {{ render_stream_child(
+         block,
+         unescape=block.block.meta.unescape | default( true )
+      ) }}
    </div>
 {% endmacro %}

cfgov/v1/jinja2tags/__init__.py
@@ -67,7 +67,7 @@ def is_filter_selected(context, fieldname, value):
     return value in query_string_values
 
 
-def render_stream_child(context, stream_child):
+def render_stream_child(context, stream_child, unescape=True):
     # Use the django_jinja to get the template content based on its name
     try:
         template = context.environment.get_template(
@@ -88,9 +88,11 @@ def render_stream_child(context, stream_child):
 
     # Render the template with the context
     html_result = template.render(new_context)
-    unescaped = html.unescape(html_result)
-    # Return the rendered template as safe html
-    return Markup(unescaped)
+
+    if unescape:
+        html_result = html.unescape(html_result)
+
+    return Markup(html_result)
 
 
 def unique_id_in_context(context):

cfgov/v1/atomic_elements/tables.py
@@ -126,3 +126,4 @@ class CaseDocketTable(blocks.StructBlock):
         icon = "table"
         template = "v1/includes/organisms/tables/case-docket.html"
         label = "Table (Case Docket)"
+        unescape = False

cfgov/v1/tests/test_render_block.py
@@ -1,0 +1,44 @@
+from django.template import loader
+from django.test import SimpleTestCase
+
+from wagtail import blocks
+
+
+class RenderBlockTests(SimpleTestCase):
+    def render_block(self, block_class, text):
+        stream_block = blocks.StreamBlock([("myblock", block_class())])
+        stream_value = stream_block.to_python(
+            [{"type": "myblock", "value": {"text": text}}]
+        )
+
+        tmpl = loader.get_template("v1/includes/templates/render_block.html")
+        return tmpl.template.module.render(block=stream_value[0], index=0)
+
+    def test_default_unescape(self):
+        class MyBlock(blocks.StructBlock):
+            text = blocks.CharBlock()
+
+            class Meta:
+                template = "v1/includes/atoms/hyperlink.html"
+
+        self.assertIn("<test>", self.render_block(MyBlock, "<test>"))
+
+    def test_unescape_true(self):
+        class MyBlock(blocks.StructBlock):
+            text = blocks.CharBlock()
+
+            class Meta:
+                template = "v1/includes/atoms/hyperlink.html"
+                unescape = True
+
+        self.assertIn("<test>", self.render_block(MyBlock, "<test>"))
+
+    def test_unescape_false(self):
+        class MyBlock(blocks.StructBlock):
+            text = blocks.CharBlock()
+
+            class Meta:
+                template = "v1/includes/atoms/hyperlink.html"
+                unescape = False
+
+        self.assertIn("&lt;test&gt", self.render_block(MyBlock, "<test>"))

commit ea4594efe011c27b9f80c0715ab028804611a573

Simplify Wagtail block unescaping logic
Commit 11c6b8dfe86032bb4eea9c9e7f54cce0de3da509 introduced a bug where
legacy page content wasn't being rendered properly (tracked internally
at D&CT#241).

This commit tries to fix that and simplifies our unnecessarily custom
render_stream_child logic. We have this custom logic because we have
historically supported raw HTML tags in any Wagtail text or rich text
field. Ideally we could remove this in future.

cfgov/v1/jinja2/v1/includes/templates/render_block.html
@@ -19,9 +19,6 @@
 
    <div class="{{ block_classes | unique | join(' ') }}"
       {{- accessible_languages.render() -}}>
-      {{ render_stream_child(
-         block,
-         unescape=block.block.meta.unescape | default( true )
-      ) }}
+      {{ render_stream_child( block ) }}
    </div>
 {% endmacro %}

cfgov/v1/jinja2tags/__init__.py
@@ -67,32 +67,23 @@ def is_filter_selected(context, fieldname, value):
     return value in query_string_values
 
 
-def render_stream_child(context, stream_child, unescape=True):
-    # Use the django_jinja to get the template content based on its name
-    try:
-        template = context.environment.get_template(
-            stream_child.block.meta.template
-        )
-    except Exception:
-        return stream_child
+def render_stream_child(context, stream_child):
+    rendered = stream_child.render(context=context)
 
-    # Create a new context based on the current one as we can't edit it
-    # directly
-    new_context = context.get_all()
-    # Add the value on the context (value is the keyword chosen by
-    # wagtail for the blocks context)
-    try:
-        new_context["value"] = stream_child.value
-    except AttributeError:
-        new_context["value"] = stream_child
+    # This logic is needed because historically we have supported the
+    # inclusion of raw HTML tags in any Wagtail text or rich text block.
+    # Ideally we could remove this logic, but before we do so we need to
+    # eliminate all such tags from our field content.
+    #
+    # By default all blocks are unescaped, but individual blocks can disable
+    # this behavior by setting unescape=False in their Meta class. Once all
+    # blocks have been audited for raw HTML tags, and all have unescape set
+    # to False, this logic can be removed, and we can simplify our templates
+    # to use {% include_block %} instead of {{ render_stream_child }}.
+    if getattr(stream_child.block.meta, "unescape", True):
+        rendered = html.unescape(rendered)
 
-    # Render the template with the context
-    html_result = template.render(new_context)
-
-    if unescape:
-        html_result = html.unescape(html_result)
-
-    return Markup(html_result)
+    return Markup(rendered)
 
 
 def unique_id_in_context(context):