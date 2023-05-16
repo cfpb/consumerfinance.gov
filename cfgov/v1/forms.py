cfpb/consumerfinance.gov

commit 2d9839bfb1460c630a0d1de21bbe3e2f7b72b984

Allow filterable search by date or alphabetically
This change fixes an existing bug in how filterable list search
doesn't always properly order search results by date (internal
D&CP#234). It also adds the ability to order search results
alphabetically by the title of the filtered pages (which addresses
internal D&CP#238).

With this change every page on the site will be using order by date.
I've chosen not to add a data migration to migrate existing fields in
the interest of simplicity. We will need to run manual search index
reindexing to activate this once it gets deployed.

This logic works by adding two new fields to the search index:
`start_date` and `end_date` which are used for all filtering and
ordering. Previously, we indexed fields from our different page types
(like EventPage's `start_dt` or EnforcementActionPage's
`initial_filing_date`) and then had to configure the search to use the
right field for filtering/ordering. This way, we always use consistent
fields for those operations and only have to make sure that the correct
page model fields get indexed at index time.

This change does remove the option to sort results by search "score" -
this isn't used publicly on the site anywhere and hasn't been tested.
We retain the ability to re-add this at some point in the future.

To test, either run a full refresh-data.sh or re-index your local search
index with:

```
./cfgov/manage.py opensearch index --force rebuild
./cfgov/manage.py opensearch document --force --refresh --parallel index
```

Then try editing a page with a FilterableList and try changing the sort
order to alphabetical.

cfgov/v1/forms.py
@@ -190,12 +190,6 @@ class FilterableListForm(forms.Form):
                 return ref.get_category_children(self.filterable_categories)
         return categories
 
-    def get_order_by(self):
-        if self.wagtail_block is not None:
-            return self.wagtail_block.value.get("order_by", "-date_published")
-        else:
-            return "-date_published"
-
     def get_page_set(self):
         categories = self.get_categories()
 
@@ -207,16 +201,14 @@ class FilterableListForm(forms.Form):
             from_date=self.cleaned_data.get("from_date"),
         )
 
-        results = self.filterable_search.search(
-            title=self.cleaned_data.get("title"), order_by=self.get_order_by()
-        )
+        results = self.filterable_search.search(self.cleaned_data.get("title"))
 
         return results
 
     def first_page_date(self):
         if len(self.all_filterable_results) > 0:
             first_post = self.all_filterable_results[0]
-            return first_post.date_published.date()
+            return first_post.start_date.date()
         return date(2010, 1, 1)
 
     def prepare_options(self, arr):
@@ -394,7 +386,7 @@ class EventArchiveFilterForm(FilterableListForm):
             from_date=self.cleaned_data.get("from_date"),
         )
         results = self.filterable_search.search(
-            title=self.cleaned_data.get("title"), order_by=self.get_order_by()
+            title=self.cleaned_data.get("title")
         )
         return results
 

cfgov/v1/atomic_elements/organisms.py
@@ -752,14 +752,15 @@ class FilterableList(BaseExpandable):
         required=True,
         help_text='Whether to include a "Topics" filter in the filter controls',
     )
-    order_by = blocks.ChoiceBlock(
+    DEFAULT_ORDERING = "-start_date"
+    ordering = blocks.ChoiceBlock(
         choices=[
-            ("-date_published", "Date Published"),
-            ("_score", "Relevance"),
+            ("-start_date", "Date"),
+            ("title.raw", "Alphabetical"),
         ],
         required=True,
         help_text="How to order results",
-        default="-date_published",
+        default=DEFAULT_ORDERING,
     )
     statuses = blocks.BooleanBlock(
         default=False,

cfgov/v1/documents.py
@@ -22,39 +22,42 @@ from v1.models.newsroom_page import LegacyNewsroomPage, NewsroomPage
 
 @registry.register_document
 class FilterablePagesDocument(Document):
+    model_class = fields.KeywordField()
+
+    path = fields.KeywordField()
+    depth = fields.IntegerField()
+    title = fields.TextField(fields={"raw": fields.KeywordField()})
+
+    start_date = fields.DateField()
+    end_date = fields.DateField()
+    language = fields.KeywordField()
+
     tags = fields.ObjectField(
         properties={"slug": fields.KeywordField(), "name": fields.TextField()}
     )
     categories = fields.ObjectField(properties={"name": fields.KeywordField()})
-    language = fields.KeywordField()
 
-    title = fields.TextField(attr="title")
-    date_published = fields.DateField(attr="date_published")
-    url = fields.KeywordField()
-    start_dt = fields.DateField()
-    end_dt = fields.DateField()
     statuses = fields.KeywordField()
     products = fields.KeywordField()
-    initial_filing_date = fields.DateField()
-    model_class = fields.KeywordField()
     content = fields.TextField()
     preview_title = fields.TextField()
     preview_subheading = fields.TextField()
     preview_description = fields.TextField()
-    path = fields.KeywordField()
-    depth = fields.IntegerField()
 
     def get_queryset(self, *args, **kwargs):
         return AbstractFilterPage.objects.live().public().specific()
 
-    def prepare_url(self, instance):
-        return instance.url
+    def prepare_model_class(self, instance):
+        return instance.__class__.__name__
 
-    def prepare_start_dt(self, instance):
-        return getattr(instance, "start_dt", None)
+    def prepare_start_date(self, instance):
+        return getattr(instance, instance.__class__.start_date_field)
 
-    def prepare_end_dt(self, instance):
-        return getattr(instance, "end_dt", None)
+    def prepare_end_date(self, instance):
+        if hasattr(instance.__class__, "end_date_field"):
+            return getattr(instance, instance.__class__.end_date_field)
+        else:
+            return self.prepare_start_date(instance)
 
     def prepare_statuses(self, instance):
         statuses = getattr(instance, "statuses", None)
@@ -69,12 +72,6 @@ class FilterablePagesDocument(Document):
             return [p.product for p in products.all()]
         else:
             return None
-
-    def prepare_initial_filing_date(self, instance):
-        return getattr(instance, "initial_filing_date", None)
-
-    def prepare_model_class(self, instance):
-        return instance.__class__.__name__
 
     def prepare_content(self, instance):
         try:
@@ -116,7 +113,7 @@ class FilterablePagesDocument(Document):
 
 
 class FilterablePagesDocumentSearch:
-    def __init__(self, root_page, children_only=True):
+    def __init__(self, root_page, children_only=True, ordering=None):
         search = FilterablePagesDocument.search()
         search = search.filter("prefix", path=root_page.path)
 
@@ -124,6 +121,9 @@ class FilterablePagesDocumentSearch:
             search = search.filter("term", depth=root_page.depth + 1)
         else:
             search = search.filter("range", depth={"gt": root_page.depth})
+
+        if ordering:
+            search = search.sort(ordering)
 
         self.search_obj = search
 
@@ -152,11 +152,20 @@ class FilterablePagesDocumentSearch:
             )
 
     def filter_date(self, from_date=None, to_date=None):
-        if to_date is not None and from_date is not None:
-            self.search_obj = self.search_obj.filter(
-                "range",
-                **{"date_published": {"gte": from_date, "lte": to_date}},
-            )
+        if from_date and to_date:
+            ranges = [
+                {"end_date": {"gte": from_date}},
+                {"start_date": {"lte": to_date}},
+            ]
+        elif from_date:
+            ranges = [{"end_date": {"gte": from_date}}]
+        elif to_date:
+            ranges = [{"start_date": {"lte": to_date}}]
+        else:
+            ranges = []
+
+        for range in ranges:
+            self.search_obj = self.search_obj.filter("range", **range)
 
     def search_title(self, title=""):
         if title not in ([], "", None):
@@ -174,10 +183,6 @@ class FilterablePagesDocumentSearch:
                 slop=2,
             )
             self.search_obj = self.search_obj.query(query)
-
-    def order(self, order_by="-date_published"):
-        """Sort results by the given field"""
-        self.search_obj = self.search_obj.sort(order_by)
 
     def filter(
         self,
@@ -200,18 +205,17 @@ class FilterablePagesDocumentSearch:
         self.filter_language(language=language)
         self.filter_date(from_date=from_date, to_date=to_date)
 
-    def search(self, title="", order_by="date_published"):
+    def search(self, title=""):
         """Perform a search for the given title"""
         self.search_title(title=title)
-        self.order(order_by=order_by)
         return self.search_obj[0 : self.count()].to_queryset(keep_order=True)
 
     def count(self):
         """Return the search object's current result count"""
         return self.search_obj.count()
 
-    def get_raw_results(self, order_by="date_published"):
-        """Get the Elasticsearch DSL Resposne object for current results.
+    def get_raw_results(self):
+        """Get the Elasticsearch DSL Response object for current results.
 
         This can be called any time, before, between, or after calls to
         filter() or search().
@@ -220,7 +224,6 @@ class FilterablePagesDocumentSearch:
         object:
         https://elasticsearch-dsl.readthedocs.io/en/latest/search_dsl.html#response
         """
-        self.order(order_by=order_by)
         search = self.search_obj[0 : self.count()]
 
         # Also aggregate unique languages in the result.
@@ -230,12 +233,6 @@ class FilterablePagesDocumentSearch:
 
 
 class EventFilterablePagesDocumentSearch(FilterablePagesDocumentSearch):
-    def filter_date(self, from_date=None, to_date=None):
-        if to_date is not None and from_date is not None:
-            self.search_obj = self.search_obj.filter(
-                "range", **{"start_dt": {"gte": from_date}}
-            ).filter("range", **{"end_dt": {"lte": to_date}})
-
     def filter(self, **kwargs):
         self.search_obj = self.search_obj.filter(
             "term", model_class="EventPage"
@@ -246,13 +243,6 @@ class EventFilterablePagesDocumentSearch(FilterablePagesDocumentSearch):
 class EnforcementActionFilterablePagesDocumentSearch(
     FilterablePagesDocumentSearch
 ):
-    def filter_date(self, from_date=None, to_date=None):
-        if to_date is not None and from_date is not None:
-            self.search_obj = self.search_obj.filter(
-                "range",
-                **{"initial_filing_date": {"gte": from_date, "lte": to_date}},
-            )
-
     def filter(self, statuses=None, products=None, **kwargs):
         if statuses is None:
             statuses = []
@@ -274,6 +264,3 @@ class EnforcementActionFilterablePagesDocumentSearch(
             )
 
         super().filter(**kwargs)
-
-    def order(self, order_by="-initial_filing_date"):
-        self.search_obj = self.search_obj.sort("-initial_filing_date")

commit 597b7c5f3abb9207ed3d936e6d8d19faa010829b

Fix "first page date" logic
Commit 2d9839bfb1460c630a0d1de21bbe3e2f7b72b984 introduced a bug in how
we determine the default "first page date" when rendering filterable
list pages. Previously, we could assume that Elasticsearch results were
always sorted from earliest to latest, and so the code used
`all_filterable_results[0]` to get the earliest post. Now, we can't
assume that, so we have to find the earliest post ourselves.

To test, visit a URL like this:

http://localhost:8000/about-us/blog/?title=&from_date=&to_date=2021-01-15

If you don't specify a `from_date`, the code fills it in. On production,
this is currently wrong:

https://www.consumerfinance.gov/about-us/blog/?title=&from_date=&to_date=2021-01-15

but this change fixes that.

cfgov/v1/forms.py
@@ -1,6 +1,6 @@
 from collections import Counter
 from datetime import date
-from operator import itemgetter
+from operator import attrgetter, itemgetter
 
 from django import forms
 from django.conf import settings
@@ -206,10 +206,12 @@ class FilterableListForm(forms.Form):
         return results
 
     def first_page_date(self):
-        if len(self.all_filterable_results) > 0:
-            first_post = self.all_filterable_results[0]
-            return first_post.start_date.date()
-        return date(2010, 1, 1)
+        if not self.all_filterable_results:
+            return date(2010, 1, 1)
+
+        return min(
+            map(attrgetter("start_date"), self.all_filterable_results)
+        ).date()
 
     def prepare_options(self, arr):
         """