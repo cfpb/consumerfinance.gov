6cbd481a182854a22ebc9ae4c2e81a046568cc26
Ans <git@anselmbradford.com>
2023-06-08 10:47:02 -0400

    Strip `-US` from language, as we use `en`

--- cfgov/unprocessed/apps/owning-a-home/js/common.js
+++ cfgov/unprocessed/apps/owning-a-home/js/common.js
@@ -9,7 +9,7 @@ const BASE_CLASS = 'o-email-signup';
 const emailSignup = document.body.querySelector('.' + BASE_CLASS);
 
 if (emailSignup) {
-  const language = document.documentElement.lang || 'en-US';
+  const language = document.documentElement.lang || 'en';
   const formSubmit = new FormSubmit(emailSignup, BASE_CLASS, {
     validator: (fields) =>
       validateEmail(fields.email, '', { language: language }).msg,
