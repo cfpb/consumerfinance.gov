a83061e763706fe565c44e726229af9f83809968
Chris Contolini <contolini@users.noreply.github.com>
2023-06-14 11:20:28 -0400

    Trivial JS syntax change to please the linter

--- cfgov/unprocessed/apps/filing-instruction-guide/js/fig-init.js
+++ cfgov/unprocessed/apps/filing-instruction-guide/js/fig-init.js
@@ -2,7 +2,7 @@ import { SecondaryNav } from '../../../js/organisms/SecondaryNav.js';
 import { init as figSidenavInit } from './fig-sidenav.js';
 import { init as figSearchInit } from './fig-search.js';
 
-let secondaryNav = SecondaryNav.init()[0];
+const secondaryNav = SecondaryNav.init()[0];
 
 figSidenavInit(secondaryNav);
 figSearchInit(secondaryNav);
