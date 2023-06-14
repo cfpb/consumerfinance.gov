0bcb90f597d066acc39262e9ba4bf5f554aa8a30
Ans <git@anselmbradford.com>
2023-06-14 18:49:32 -0400

    Change order of plugin

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -9,10 +9,6 @@ module.exports = defineConfig({
   defaultCommandTimeout: 25000,
   blockHosts: ['*google-analytics.com', '*googletagmanager.com'],
   e2e: {
-    baseUrl: 'http://localhost:8000',
-    specPattern: 'test/cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: 'test/cypress/support/e2e.js',
-    excludeSpecPattern: 'test/cypress/integration/**/*-helpers.cy.js',
     // setupNodeEvents can be defined in either
     // the e2e or component configuration.
     setupNodeEvents(on, config) {
@@ -36,6 +32,10 @@ module.exports = defineConfig({
       // IMPORTANT return the updated config object.
       return config;
     },
+    baseUrl: 'http://localhost:8000',
+    specPattern: 'test/cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'test/cypress/support/e2e.js',
+    excludeSpecPattern: 'test/cypress/integration/**/*-helpers.cy.js',
   },
   component: {
     specPattern: 'test/cypress/component/**/*.cy.{js,jsx,ts,tsx}',
