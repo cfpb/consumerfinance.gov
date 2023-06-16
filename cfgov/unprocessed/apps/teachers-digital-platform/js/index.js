38e5eb1cb663d29c87736f719b28eba6ec5ec382
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2023-06-16 14:08:21 -0400

    Sunset TDP journey code

--- cfgov/unprocessed/apps/teachers-digital-platform/js/index.js
+++ cfgov/unprocessed/apps/teachers-digital-platform/js/index.js
@@ -1,19 +1,15 @@
 // External modules
-const AOS = require('aos/dist/aos');
 import cfExpandables from './Expandable.js';
 import expandableFacets from './expandable-facets.js';
 import beforeExpandableTransitionInit from './expandable-mobile.js';
 
 // Internal modules
-import scroll from './scroll.js';
 import { init as searchInit } from './search.js';
-import sticky from './sticky.js';
 import { bindAnalytics } from './tdp-analytics.js';
 import surveys from './tdp-surveys.js';
 
 const app = {
   init: () => {
-    AOS.init();
 
     /**
      * This must come before searchInit() because it will also initialize
@@ -25,8 +21,6 @@ const app = {
     cfExpandables.init();
     expandableFacets.init();
 
-    scroll.init();
-    sticky.init();
     surveys.init();
     bindAnalytics();
   },
