d1bd1a77ddd6fbde435979f7f97faeddbdbea7b9
Ans <git@anselmbradford.com>
2023-06-28 08:56:23 -0400

    Revert saving videos on failure

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   experimentalMemoryManagement: true,
-  video: true,
+  video: false,
   screenshotOnRunFailure: true,
   fixturesFolder: 'test/cypress/fixtures',
   videosFolder: 'test/cypress/videos',
