878a5b5373c94e0538ea20094c21e44ebc8688c8
troy <troy.ritter@cfpb.gov>
2023-07-13 15:31:14 -0400

    add test-deploy2

---
+++ test_deploy2.groovy
@@ -0,0 +1,28 @@
+@Library('Dev Platforms Shared Libraries') _
+
+pipeline{
+    agent{
+        kubernetes{
+            defaultContainer 'python'
+            yaml '''
+                kind: Pod
+                spec:
+                serviceAccountName: cfpb-ci-sa
+                containers:
+                - name: python
+                    image: python:3-alpine
+                    imagePullPolicy: Always
+                    command:
+                    - cat
+                    tty: true
+    '''
+        }
+    }
+
+    stages{
+        stage('get docker version'){
+            configureDocker()
+            docker --version 
+        }
+    }
+}
