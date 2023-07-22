3c7b7463e1dae8fd9c97724a57bbbf7b53a6c664
Scott Cranfill <scott@scottcranfill.com>
2023-07-21 22:59:16 -0400

    Add blank lines for Black

--- cfgov/paying_for_college/handlers.py
+++ cfgov/paying_for_college/handlers.py
@@ -1,5 +1,6 @@
 from corsheaders.signals import check_request_enabled
 
+
 def cors_allow_particular_urls(sender, request, **kwargs):
     """Allow all requests starting with given path to bypass CORS
 
@@ -12,5 +13,6 @@ def cors_allow_particular_urls(sender, request, **kwargs):
     return request.path.startswith(
         "/paying-for-college2/understanding-your-financial-aid-offer/api/"
     )
+
 
 check_request_enabled.connect(cors_allow_particular_urls)
