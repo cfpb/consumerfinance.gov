39bc7a2c246f871b37c76938cd4b443a238bb505
Ans <git@anselmbradford.com>
2023-08-04 17:26:33 -0400

    Replace stylelint-config-prettier with stylelint-config-standard

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -7,13 +7,24 @@ rule-empty-line-before -
 no-descending-specificity -
   Turned off, but probably shouldn't be.
   TODO: Turn on this rule and see if issues can be fixed.
+selector-id-pattern -
+  Turned off.
+  TODO: Turn on this rule and work out regex for BEM syntax.
+selector-class-pattern -
+  Turned off.
+  TODO: Turn on this rule and work out regex for BEM syntax.
+declaration-property-value-no-unknown -
+  Turned off for Less per documentation guidance.
+function-no-unknown -
+  Ignore the 'unit' helper function that comes from Less.
 less/color-no-invalid-hex
 less/no-duplicate-variables
   Both of the above settings are turned off till
   https://github.com/ssivanatarajan/stylelint-less/issues/6 is addressed.
 */
 module.exports = {
-  extends: ['stylelint-config-recommended-less', 'stylelint-config-standard'],
+  extends: ['stylelint-config-standard'],
+  plugins: ['stylelint-less'],
   ignoreFiles: ['packages/**/node_modules/**/*.less'],
   customSyntax: 'postcss-less',
   rules: {
@@ -29,6 +40,11 @@ module.exports = {
       },
     ],
     'no-descending-specificity': null,
+    'selector-id-pattern': null,
+    'selector-class-pattern': null,
+    'declaration-property-value-no-unknown': null,
+    'function-no-unknown': { ignoreFunctions: ['unit'] },
+    'number-max-precision': 10,
     'less/color-no-invalid-hex': null,
     'less/no-duplicate-variables': null,
   },
