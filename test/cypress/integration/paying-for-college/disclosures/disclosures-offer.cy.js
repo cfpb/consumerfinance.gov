fa156ad69c35d86321a7e6c5839e9f618c223c04
Chuck Werner <mistergone@gmail.com>
2023-08-21 10:34:03 -0400

    -Fix reference to fixtures

--- test/cypress/integration/paying-for-college/disclosures/disclosures-offer.cy.js
+++ test/cypress/integration/paying-for-college/disclosures/disclosures-offer.cy.js
@@ -1,12 +1,17 @@
 import { DynamicDisclosures } from './disclosures-helpers.cy.js';
 
 const page = new DynamicDisclosures();
+const apiConstants = '/paying-for-college2/understanding-your-financial-aid-offer/api/constants/';
+const apiSchoolOne = '/paying-for-college2/understanding-your-financial-aid-offer/api/school/133465/';
+const apiProgramOne = '/paying-for-college2/understanding-your-financial-aid-offer/api/program/133465_5287/'
+const apiSchoolTwo = '/paying-for-college2/understanding-your-financial-aid-offer/api/school/224776/';
+const apiProgramTwo = '/paying-for-college2/understanding-your-financial-aid-offer/api/program/224776_444/';
 
 describe('Dynamic Disclosures offer page', () => {
-  const url = 'http://localhost:8000/paying-for-college2/understanding-your-financial-aid-offer/offer/?iped=133465&pid=5287&oid=ABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDE&totl=45000&tuit=38976&hous=3000&book=650&tran=500&othr=500&pelg=1500&schg=2000&stag=2000&othg=100&ta=3000&mta=3000&gib=3000&wkst=3000&parl=14000&perl=3000&subl=15000&unsl=2000&ppl=1000&gpl=1000&prvl=3000&prvi=4.55&prvf=1.01&insl=3000&insi=4.55&inst=8';
+  const urlOne = 'http://localhost:8000/paying-for-college2/understanding-your-financial-aid-offer/offer/?iped=133465&pid=5287&oid=ABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDE&totl=45000&tuit=38976&hous=3000&book=650&tran=500&othr=500&pelg=1500&schg=2000&stag=2000&othg=100&ta=3000&mta=3000&gib=3000&wkst=3000&parl=14000&perl=3000&subl=15000&unsl=2000&ppl=1000&gpl=1000&prvl=3000&prvi=4.55&prvf=1.01&insl=3000&insi=4.55&inst=8&leng=30';
+  const urlTwo = 'http://localhost:8000/paying-for-college2/understanding-your-financial-aid-offer/offer/?iped=224776&pid=444&oid=ABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDE&totl=45000&tuit=38976&hous=3000&book=650&tran=500&othr=500&pelg=1500&schg=2000&stag=2000&othg=100&ta=3000&mta=3000&gib=3000&wkst=3000&parl=14000&perl=3000&subl=15000&unsl=2000&ppl=1000&gpl=1000&prvl=3000&prvi=4.55&prvf=1.01&insl=3000&insi=4.55&inst=8&leng=30';
 
   beforeEach(() => {
-    cy.visit( url );
     cy.intercept( 'GET', apiConstants, {host: 'localhost', fixture: 'paying-for-college/constants.json'});
     cy.intercept( 'GET', apiSchoolOne, {host: 'localhost', fixture: 'paying-for-college/school-133465.json'});
     cy.intercept( 'GET', apiSchoolTwo, {host: 'localhost', fixture: 'paying-for-college/school-224776.json'});
