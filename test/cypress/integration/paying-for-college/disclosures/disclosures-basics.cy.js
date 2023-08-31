575936d6011d66facd040f220453f391697b5b96
Chuck Werner <mistergone@gmail.com>
2023-08-31 09:32:52 -0400

    fix

--- test/cypress/integration/paying-for-college/disclosures/disclosures-basics.cy.js
+++ test/cypress/integration/paying-for-college/disclosures/disclosures-basics.cy.js
@@ -12,7 +12,6 @@ describe('Dynamic Disclosures', () => {
   const urlOne = '/paying-for-college2/understanding-your-financial-aid-offer/offer/?iped=133465&pid=5287&oid=ABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDE&totl=45000&tuit=38976&hous=3000&book=650&tran=500&othr=500&pelg=1500&schg=2000&stag=2000&othg=100&ta=3000&mta=3000&gib=3000&wkst=3000&parl=14000&perl=3000&subl=15000&unsl=2000&ppl=1000&gpl=1000&prvl=3000&prvi=4.55&prvf=1.01&insl=3000&insi=4.55&inst=8&leng=30';
   
   beforeEach(() => {
-    cy.visit( urlOne );
     cy.intercept( 'GET', apiConstants, (request) => {
       request.reply( dataConstants );
     }).as('intConstants');
@@ -22,18 +21,21 @@ describe('Dynamic Disclosures', () => {
     cy.intercept( 'GET', apiProgramOne, (request) => {
       request.reply( dataProgramOne );
     }).as('intProgramOne');
-    cy.wait(['@intConstants', '@intSchoolOne', '@intProgramOne']);
+    cy.visit( urlOne );
   });
 
 
   it( 'should properly update when the tuition and fees are modified', () => {
-    page.confirmVerification();
-    page.stepTwo();
-    page.setText('costs__tuition','40000');
-    cy.get('#summary_total-cost').should(
-      'contain',
-      '34,550'
-    );
+    cy.wait(['@intConstants', '@intSchoolOne', '@intProgramOne']).then( resp => {
+      page.confirmVerification();
+      page.stepTwo();
+      page.setText('costs__tuition','40000');
+      cy.get('#summary_total-cost').should(
+        'contain',
+        '34,550'
+      );      
+    })
+
   });
 
   it( 'should properly update when any costs fields are modified', () => {
