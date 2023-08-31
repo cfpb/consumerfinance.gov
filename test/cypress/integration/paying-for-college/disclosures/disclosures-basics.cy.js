08f4eb73d4f9699998c98e977aa8a60418aae6db
Chuck Werner <mistergone@gmail.com>
2023-08-31 14:51:07 -0400

    maybe different intercepts

--- test/cypress/integration/paying-for-college/disclosures/disclosures-basics.cy.js
+++ test/cypress/integration/paying-for-college/disclosures/disclosures-basics.cy.js
@@ -4,8 +4,8 @@ import dataProgramOne from '../../../fixtures/paying-for-college/program-133465_
 import { DynamicDisclosures } from './disclosures-helpers.cy.js';
 
 const page = new DynamicDisclosures();
-const apiConstants = '/paying-for-college2/understanding-your-financial-aid-offer/api/constants/';
-const apiSchoolOne = '/paying-for-college2/understanding-your-financial-aid-offer/api/school/133465/';
+const apiConstants = '/paying-for-college2/understanding-your-financial-aid-offer/api/constants';
+const apiSchoolOne = '/paying-for-college2/understanding-your-financial-aid-offer/api/school/133465';
 const apiProgramOne = '/paying-for-college2/understanding-your-financial-aid-offer/api/program/133465_5287/'
 const urlOne = '/paying-for-college2/understanding-your-financial-aid-offer/offer/?iped=133465&pid=5287&oid=ABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDE&totl=45000&tuit=38976&hous=3000&book=650&tran=500&othr=500&pelg=1500&schg=2000&stag=2000&othg=100&ta=3000&mta=3000&gib=3000&wkst=3000&parl=14000&perl=3000&subl=15000&unsl=2000&ppl=1000&gpl=1000&prvl=3000&prvi=4.55&prvf=1.01&insl=3000&insi=4.55&inst=8&leng=30';
 
@@ -26,11 +26,21 @@ describe('Dynamic Disclosures', () => {
 
 
   it( 'should properly update when the tuition and fees are modified', () => {
+    cy.intercept( '/paying-for-college2/understanding-your-financial-aid-offer/api/constants/' )
+      .as( 'getConstants' );
+    cy.intercept( '/paying-for-college2/understanding-your-financial-aid-offer/api/school/133465/' )
+      .as( 'getSchoolOne' );
+    cy.intercept( '/paying-for-college2/understanding-your-financial-aid-offer/api/program/133465_5287/' )
+      .as( 'getProgramOne' );
     cy.visit( urlOne );
 
     // cy.intercept( 'GET', apiConstants, dataConstants ).as( 'intConstants' );
     // cy.intercept( 'GET', apiSchoolOne, dataSchoolOne ).as( 'intSchoolOne' );
     // cy.intercept( 'GET', apiProgramOne, dataProgramOne ).as( 'intProgramOne' );
+
+    cy.wait( '@getConstants' );
+    cy.wait( '@getSchoolOne' );
+    cy.wait( '@getProgramOne' );
 
     page.confirmVerification();
     page.stepTwo();
