060e5796241c63309bd56dca6741eea0bbd6ede5
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2023-09-06 18:45:49 -0400

    more logs

--- test/cypress/integration/paying-for-college/disclosures/disclosures-basics.cy.js
+++ test/cypress/integration/paying-for-college/disclosures/disclosures-basics.cy.js
@@ -23,8 +23,9 @@ describe('Dynamic Disclosures', () => {
   //   }).as('intProgramOne');
   //   cy.visit( urlOne );
   // });
-
+  cy.log('Logging works');
   it('should properly update when the tuition and fees are modified', () => {
+    cy.log('pre intercept');
     cy.intercept('GET', apiConstants, {
       fixture: 'paying-for-college/constants.json',
     }).as('intConstants');
