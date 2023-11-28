d498232c9aae5ae50cfe06b452973998980d1e08
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2023-11-28 11:45:09 -0500

    Update cfgov/paying_for_college/views.py
    Co-authored-by: william higgins <higs4281@users.noreply.github.com>

--- cfgov/paying_for_college/views.py
+++ cfgov/paying_for_college/views.py
@@ -301,7 +301,7 @@ def school_autocomplete(request):
 
 class VerifyView(View):
     def post(self, request):
-        data = json.loads(request.body)
+        data = json.loads(request.body.decode("utf-8"))
         timestamp = timezone.now()
         if data.get("oid") and validate_oid(data["oid"]):
             OID = data["oid"]
