df63e52d78ed9f33e5515ae6c3edac0753486aa1
Andy Chosak <andy.chosak@cfpb.gov>
2023-12-07 10:09:32 -0500

    Migrate mortage-related models to snippets

--- cfgov/data_research/wagtail_hooks.py
+++ cfgov/data_research/wagtail_hooks.py
@@ -1,28 +1,6 @@
-from wagtail.contrib.modeladmin.options import (
-    ModelAdmin,
-    ModelAdminGroup,
-    modeladmin_register,
-)
+from wagtail.snippets.models import register_snippet
 
-from data_research.models import MortgageDataConstant, MortgageMetaData
+from data_research.views import DataResearchViewSetGroup
 
 
-class MortgageMetaDataModelAdmin(ModelAdmin):
-    model = MortgageMetaData
-    menu_label = "Mortgage metadata"
-    menu_icon = "list-ul"
-    list_display = ("name", "json_value", "note", "updated")
-
-
-class MortgageDataConstantModelAdmin(ModelAdmin):
-    model = MortgageDataConstant
-    menu_label = "Mortgage performance constants"
-    menu_icon = "list-ul"
-    list_display = ("name", "value", "date_value", "updated")
-
-
-@modeladmin_register
-class ResearchModelAdminGroup(ModelAdminGroup):
-    menu_label = "Data Research"
-    menu_icon = "list-ul"
-    items = (MortgageDataConstantModelAdmin, MortgageMetaDataModelAdmin)
+register_snippet(DataResearchViewSetGroup)
