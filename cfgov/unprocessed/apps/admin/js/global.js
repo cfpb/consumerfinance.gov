b26230059f22f55c3866589af4f0cbf2aa79f408
Andy Chosak <andy.chosak@cfpb.gov>
2023-12-14 16:22:52 -0500

    Remove deprecated use of autosize
    As of Wagtail 5.0, we no longer need to do the workaround for the bug
    described here: https://github.com/wagtail/wagtail/issues/10549

--- cfgov/unprocessed/apps/admin/js/global.js
+++ cfgov/unprocessed/apps/admin/js/global.js
@@ -1,8 +1,3 @@
-// This is a workaround for an issue in the jQuery autosize plugin in Wagtail 4.
-// TODO: remove this after upgrading to Wagtail 5.
-import autosize from 'autosize';
-window.autosize = autosize;
-
 // This is a workaround to an issue in fbjs.
 // See https://github.com/facebook/fbjs/issues/290
 // eslint-disable-next-line no-global-assign
