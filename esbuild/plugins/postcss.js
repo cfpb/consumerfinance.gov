ee4b0b145706284b8e4d87c76fd4e239a49512f9
Ans <git@anselmbradford.com>
2024-03-26 14:19:54 -0400

    Update to latest design-system@0.42.0, cfpb-chart-builder@6.5.0

--- esbuild/plugins/postcss.js
+++ esbuild/plugins/postcss.js
@@ -9,12 +9,15 @@ const postCSSPlugin = ({ plugins = [], lessOptions = {} }) => ({
   setup(build) {
     build.onLoad({ filter: /.\.less$/ }, async (args) => {
       const fileContent = await readFile(args.path, { encoding: 'utf-8' });
+      if (fileContent.indexOf('pacific') !== -1) {
+        console.log(args.path);
+      }
+
       const lessResult = await less.render(fileContent, {
         ...lessOptions,
         filename: args.path,
         rootpath: dirname(args.path),
       });
-
       const result = await postcss(plugins).process(lessResult.css, {
         from: args.path,
       });
