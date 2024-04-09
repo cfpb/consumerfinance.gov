984c4cb3f33959fc795ef6fa6e14450173cb407f
Chris Contolini <contolini@users.noreply.github.com>
2024-04-09 13:53:59 -0400

    Make our filterable list controls way cooler with htmx
    See https://htmx.org/

--- cfgov/unprocessed/js/routes/on-demand/filterable-list-controls.js
+++ cfgov/unprocessed/js/routes/on-demand/filterable-list-controls.js
@@ -2,5 +2,9 @@
    Scripts for Filterable List Controls organism
    ========================================================================== */
 
+import htmx from 'htmx.org';
+
 import FilterableListControls from '../../organisms/FilterableListControls.js';
 FilterableListControls.init();
+
+htmx.process(document.body);
