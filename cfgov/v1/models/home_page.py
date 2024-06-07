4521a212e18af7c6da5cbd0df558be1916d8a9c1
Chuck Sebian-Lander <Charles.Sebian-Lander@cfpb.gov>
2024-06-07 13:31:24 -0400

    no one has ever had less of an idea what they're doing

--- cfgov/v1/models/home_page.py
+++ cfgov/v1/models/home_page.py
@@ -1,93 +1,109 @@
 from django.db import models
 
+from wagtail import blocks
 from wagtail.admin.panels import (
     FieldPanel,
     MultiFieldPanel,
     ObjectList,
     TabbedInterface,
 )
-from wagtail.blocks import ListBlock, StructBlock, TextBlock
+from wagtail.fields import StreamField
 
 from v1.models.base import CFGOVPage
 
 
-class home_card(StructBlock):
-    icon = TextBlock()
-    text = TextBlock()
-    url = TextBlock()
-
-
-class home_highlight(StructBlock):
-    card_type = "highlight"
-    img_src = TextBlock()
-    heading = TextBlock()
-    link_text = TextBlock()
-    link_url = TextBlock()
-
-
-class home_breakout(StructBlock):
-    card_type = "breakout"
-    link_text = TextBlock()
-    link_url = TextBlock()
-    img_src = TextBlock()
-
-
-class HomeTopics(StructBlock):
-    topics = ListBlock("home_card", default=list())
-
-
-class HomeHighlights(StructBlock):
-    highlights = ListBlock("home_highlight", default=list())
-
-
-class HomeBreakouts(StructBlock):
-    breakouts = ListBlock("home_breakout", default=list())
-
-
 class HomePage(CFGOVPage):
     hero_heading = models.TextField(
-        default="On your side",
-        help_text=("Will be formatted in bold"),
+        blank=True,
+        null=True,
     )
 
     hero_heading_continued = models.TextField(
-        default="through life's financial moments."
+        blank=True,
+        null=True,
     )
 
-    hero_image = models.TextField()
+    hero_image = models.TextField(
+        blank=True,
+        null=True,
+    )
 
-    answers_heading = models.TextField(
-        default="Find answers to your money questions",
+    topics_heading = models.TextField(
+        blank=True,
+        null=True,
     )
 
     breakout_cards_heading = models.TextField(
-        default="Get help planning for future goals"
+        blank=True,
+        null=True,
+    )
+
+    class HighlightItemBlock(blocks.StructBlock):
+        img_src = blocks.CharBlock(label="Image URL")
+        heading = blocks.CharBlock()
+        link_text = blocks.CharBlock()
+        link_url = blocks.CharBlock()
+
+    highlights = StreamField(
+        [("highlight", HighlightItemBlock())],
+        blank=True,
+    )
+
+    class TopicItemBlock(blocks.StructBlock):
+        icon = blocks.CharBlock()
+        text = blocks.CharBlock()
+        url = blocks.CharBlock()
+
+    topics = StreamField(
+        [("topic", TopicItemBlock())],
+        blank=True,
+    )
+
+    class BreakoutCardBlock(blocks.StructBlock):
+        link_text = blocks.CharBlock()
+        link_url = blocks.CharBlock()
+        img_src = blocks.CharBlock(label="Image URL")
+
+    breakout_cards = StreamField(
+        [("card", BreakoutCardBlock())],
+        blank=True,
     )
 
     content_panels = CFGOVPage.content_panels + [
         MultiFieldPanel(
             [
-                FieldPanel("hero_heading"),
-                FieldPanel("hero_heading_continued"),
-                FieldPanel("hero_image"),
+                FieldPanel("hero_heading", heading="Hero H1 (bold)"),
+                FieldPanel(
+                    "hero_heading_continued", heading="Hero H1 continued"
+                ),
+                FieldPanel("hero_image", heading="Image URL"),
             ],
             heading="Hero",
+            classname="collapsible",
         ),
-        FieldPanel("highlights"),
         MultiFieldPanel(
             [
-                FieldPanel("answers_heading"),
-                FieldPanel("home_topics", HomeTopics()),
+                FieldPanel("highlights"),
+            ],
+            heading="Highlights",
+            classname="collapsible",
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel("topics_heading"),
+                FieldPanel("topics"),
             ],
             heading="Topics",
+            classname="collapsible",
         ),
-        # 50/50 goes here when we un-hardcode it
+        # 5050 goes here
         MultiFieldPanel(
             [
                 FieldPanel("breakout_cards_heading"),
                 FieldPanel("breakout_cards"),
             ],
             heading="Breakout cards",
+            classname="collapsible",
         ),
     ]
 
