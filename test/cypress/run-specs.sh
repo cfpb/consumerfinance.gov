58267570d2de5c484d007220bf1855545c881f62
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2024-06-13 12:37:39 -0400

    global sed

--- test/cypress/run-specs.sh
+++ test/cypress/run-specs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo -n "${CHANGED_FILES:-$(git --no-pager diff --name-only origin/main)}" \
-  | sed 's/___/\n/' \
+  | sed 's/___/\n/g' \
   | node "$(git rev-parse --show-toplevel)/test/cypress/spec-map.js"
