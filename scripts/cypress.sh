23f96f264ef7907eea8f22f0b994f9ff44448cd0
Ans <anselmbradford@users.noreply.github.com>
2024-09-04 12:47:36 -0400

    Update scripts/cypress.sh
    Co-authored-by: Wyatt Pearsall <wyatt.pearsall@cfpb.gov>

--- scripts/cypress.sh
+++ scripts/cypress.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = "fast" ]; then
     ./scripts/cypress-fast-specs.sh && ./node_modules/.bin/cypress run
-elif [ ${1:-run} = "open" ]; then
+elif [ "$1" = "open" ]; then
     ./node_modules/.bin/cypress open --config-file cypress.template.mjs
 else
     ./node_modules/.bin/cypress run --config-file cypress.template.mjs
