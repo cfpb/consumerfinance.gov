1ca69b404078408d6676dcb1782cca9f2fec4e42
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2024-10-10 23:07:34 -0700

    Add wagtail hook to create menu items for each other translation of the given page

--- cfgov/v1/wagtail_hooks.py
+++ cfgov/v1/wagtail_hooks.py
@@ -8,6 +8,7 @@ from django.utils.html import format_html_join
 
 from wagtail import hooks
 from wagtail.admin import messages
+from wagtail.admin.action_menu import ActionMenuItem
 from wagtail.admin.menu import MenuItem
 from wagtail.snippets.models import register_snippet
 
@@ -43,6 +44,34 @@ from v1.views.snippets import BannerViewSet
 
 
 logger = logging.getLogger(__name__)
+
+languages = dict(settings.LANGUAGES)
+
+
+class LanguageMenuItem(ActionMenuItem):
+    icon_name = "globe"
+
+    def __init__(self, label, url):
+        self.label = label
+        self.url = url
+
+    def get_url(self, context):
+        return self.url
+
+
+@hooks.register("construct_page_action_menu")
+def add_language_links(menu_items, request, context):
+    page = context["page"]
+    return menu_items.extend(
+        [
+            LanguageMenuItem(
+                f"Edit {languages[translation.language]} page",
+                f"/admin/pages/{translation.pk}/edit/",
+            )
+            for translation in page.get_translations()
+            if translation.language != page.language
+        ]
+    )
 
 
 @hooks.register("after_delete_page")
