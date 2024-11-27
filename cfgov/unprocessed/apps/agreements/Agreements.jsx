1d8fed36ac1e15b8e884c5c8a61a8edf5f4ce4a2
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2024-11-27 06:22:59 -0800

    lint

--- cfgov/unprocessed/apps/agreements/Agreements.jsx
+++ cfgov/unprocessed/apps/agreements/Agreements.jsx
@@ -1,29 +1,33 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import Select from 'react-select'
+import Select from 'react-select';
 
-function onChange(val){
-  window.location = `/credit-cards/agreements/issuer/${val.value}/`
+function onChange(val) {
+  window.location = `/credit-cards/agreements/issuer/${val.value}/`;
 }
 
-function getOption(options, val){
-  for(let i=0; i<options.length; i++){
-    if(options[i].value === val) return options[i];
+function getOption(options, val) {
+  for (let i = 0; i < options.length; i++) {
+    if (options[i].value === val) return options[i];
   }
-  return null
+  return null;
 }
 
 function Wrapper() {
-  const options = JSON.parse(document.getElementById('issuer-json').textContent);
+  const options = JSON.parse(
+    document.getElementById('issuer-json').textContent,
+  );
   const defaultValue = document.getElementById('issuer-slug')?.textContent;
 
-  return <Select
-           options={options}
-           defaultValue={defaultValue ? getOption(options, defaultValue) : null}
-           placeholder="Search for an issuer"
-           onChange={onChange}
-           isSearchable={true}
-         />
+  return (
+    <Select
+      options={options}
+      defaultValue={defaultValue ? getOption(options, defaultValue) : null}
+      placeholder="Search for an issuer"
+      onChange={onChange}
+      isSearchable={true}
+    />
+  );
 }
 
 const root = createRoot(document.getElementById('select-root'));
