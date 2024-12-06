aa437f3e8fa25277b707ddab742e54286f8543c7
Andy Chosak <andy.chosak@cfpb.gov>
2024-12-06 11:53:05 -0500

    Revert refresh-data noindex changes

--- refresh-data.sh
+++ refresh-data.sh
@@ -8,8 +8,6 @@
 
 set -e
 
-NO_INDEX=0
-
 usage() {
     cat << EOF
 Please download a recent database dump before running this script:
@@ -24,7 +22,7 @@ download it for you:
 
 Additional options:
 
-      --no-index  Do not update search indexes after refreshing
+      --noindex  Do not update search indexes after refreshing
 
 EOF
     exit 1;
@@ -87,11 +85,12 @@ get_data() {
     fi
 }
 
+noindex=false
 for arg in "$@"; do
     shift
     case "$arg" in
-        "--no-index")
-            NO_INDEX=1
+        "--noindex")
+            noindex=1
             ;;
         *)
             refresh_dump_name=$arg
@@ -104,6 +103,6 @@ check_data
 refresh_data
 initial_data
 
-if [[ $NO_INDEX -ne 1 ]]; then
+if [[ $noindex -ne 1 ]]; then
     update_index
 fi
