2fddf73c5201474dd5db251f18fb4d7754274a76
cooldragontattoo <troy.ritter@cfpb.gov>
2024-12-10 17:19:12 -0700

    add docs, helm-uninstall and clean up values.yaml

--- helm-init.sh
+++ helm-init.sh
@@ -1,86 +1,88 @@
 #!/bin/bash
 
-# Function to display usage
-usage() {
-    echo "Usage: $0 [--release <release-name>]"
-    exit 1
-}
+# Define color codes for pretty output
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
 
-# Parse command-line arguments
-while [[ "$#" -gt 0 ]]; do
-    case $1 in
-        --release)
-            RELEASE_NAME="$2"
-            shift
-            ;;
-        *)
-            usage
-            ;;
-    esac
-    shift
-done
+OUTPUT_FILE=$(mktemp)
 
+# Check if helm is installed
 if ! command -v helm &> /dev/null; then
-    echo "helm could not be found. Please install it before running this script."
+    echo -e "${RED}Helm could not be found. Please install it before running this script.${NC}"
     exit 1
 fi
 
-
+# Check if kubectl is installed
 if ! command -v kubectl &> /dev/null; then
-    echo "kubectl could not be found. Please install it before running this script."
+    echo -e "${RED}Kubectl could not be found. Please install it before running this script.${NC}"
     exit 1
 fi
 
+if ! docker images | grep -q "cfgov"; then
+    echo -e "${RED}Docker images 'cfgov' could not be found." 
+    echo -e "Please run:{$NC} docker build . -t cfgov:latest"
+    exit 1
+fi
+
+# Get the current Kubernetes cluster context
 CLUSTER=$(kubectl config current-context)
 
+# Check if a Kubernetes cluster is configured
 if [ -z "$CLUSTER" ]; then
-    echo "No Kubernetes cluster is currently configured. Please configure a cluster before running this script."
+    echo -e "${RED}No Kubernetes cluster is currently configured. Please configure a cluster before running this script.${NC}"
     exit 1
 fi
 
-echo $CLUSTER
+echo -e "${GREEN}Current Kubernetes cluster: $CLUSTER${NC}"
 
+# Check if the current cluster is a local cluster (docker-desktop or colima)
 if [[ "$CLUSTER" != "docker-desktop" &&  "$CLUSTER" != "colima" ]]; then
-    echo ""
-    echo -e "This script is intended to be run on a local Kubernetes cluster (i.e. docker-desktop or colima). The current cluster is $CLUSTER."
-    echo -e "Your current cluster is $CLUSTER." 
+    echo -e "${YELLOW}"
+    echo -e "This script is intended to be run on a local Kubernetes cluster (i.e. docker-desktop or colima)."
+    echo -e "Your current cluster is ${RED}$CLUSTER${YELLOW}."
     echo -e "Please switch to docker-desktop or colima before running this script."
-    echo ""
-    echo ""
-    echo "Exiting helm-init"
+    echo -e "${NC}"
+    echo -e "${RED}Exiting helm-init${NC}"
     exit 1
 fi
 
+# Define the Helm directory
 HELM_DIR="./helm"
 
-if [ -n "$RELEASE_NAME" ]; then
-    echo "Release name: $RELEASE_NAME"
-else
-    echo "No release name provided."
-    echo "Using default release name: 'cfgov'"
-    RELEASE_NAME="cfgov"
-fi
 
+# Check if the ./cfgov/apache directory exists
 if [ -d "./cfgov/apache" ]; then
-    echo "Moving ./cfgov/apache to $HELM_DIR"
+    echo -e "${GREEN}Moving ./cfgov/apache to $HELM_DIR${NC}"
     cp -r ./cfgov/apache $HELM_DIR
 else
-    echo "Directory ./cfgov/apache does not exist."
+    echo -e "${RED}Directory ./cfgov/apache does not exist.${NC}"
     exit 1
 fi
 
-if [! -d ./helm/charts]; then
-    echo "Building Helm charts..."
-    helm repo update
+# Check if the ./helm/charts directory exists
+if [ ! -d "./helm/charts" ]; then
+    echo -e "${GREEN}Building Helm charts...${NC}"
+    helm dependency update $HELM_DIR
     helm dependency build $HELM_DIR
 else
-    if [ -z $SKIP_DEP_UPDATE ]; then
-        echo "Updating Helm dependencies..."
+    # Update Helm dependencies if SKIP_DEP_UPDATE is not set
+    if [ -z "$SKIP_DEP_UPDATE" ]; then
+        echo -e "${GREEN}Updating Helm dependencies...${NC}"
         helm dependency update $HELM_DIR
     fi
 fi 
 
-helm upgrade --install $RELEASE_NAME $HELM_DIR -f $HELM_DIR/values.yaml
+# Upgrade or install the Helm release
+echo -e "${GREEN}Upgrading/Installing Helm release...${NC}"
+helm upgrade --install cfgov $HELM_DIR -f $HELM_DIR/values.local.yaml >> $OUTPUT_FILE 2>&1
 
+# Remove the copied apache directory
+echo -e "${GREEN}Cleaning up...${NC}"
 rm -r $HELM_DIR/apache
 
+echo -e "${GREEN}Helm initialization script completed successfully.${NC}"
+
+cat $OUTPUT_FILE
+rm $OUTPUT_FILE
