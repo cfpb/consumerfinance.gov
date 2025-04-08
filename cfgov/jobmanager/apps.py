5d76d07358c25f183a72c7124f1ba6915b53576c
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2025-04-08 15:56:14 -0700

    restore apps.py

---
+++ cfgov/jobmanager/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class JobManagerAppConfig(AppConfig):
+    name = "jobmanager"
+    label = "jobmanager"
+    verbose_name = "Job Manager"
+    default_auto_field = "django.db.models.BigAutoField"
