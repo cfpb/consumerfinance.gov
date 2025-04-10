b41a6feb595c7bbe2801a9aa797dc7811c22905d
Ans <git@anselmbradford.com>
2025-04-10 15:52:53 -0400

    Add notification to browse filterable page content area

--- cfgov/v1/models/browse_filterable_page.py
+++ cfgov/v1/models/browse_filterable_page.py
@@ -33,6 +33,7 @@ class BrowseFilterablePage(AbstractFilterablePage, AbstractBrowsePage):
         [
             ("text_introduction", molecules.TextIntroduction()),
             ("featured_content", organisms.FeaturedContent()),
+            ("notification", molecules.Notification()),
         ],
     )
     content = StreamField(
