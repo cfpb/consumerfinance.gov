c9ce737f990ee9b0672cb72dd3d49b80188edc01
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2025-06-16 22:56:20 -0700

    Remove unneeded lastmod method and latest_lastmod setting logic

--- cfgov/v1/sitemap.py
+++ cfgov/v1/sitemap.py
@@ -12,17 +12,10 @@ class Sitemap(WagtailSitemap):
     # This can be updated to trigger sitemap updates that are not reflected
     # in Wagtail page publish events.
     MIN_SITEMAP_DATE = datetime(2025, 6, 4, tzinfo=timezone.utc)
-
-    def lastmod(self, obj):
-        return _max_none(super().lastmod(obj), self.MIN_SITEMAP_DATE)
+    latest_lastmod = MIN_SITEMAP_DATE
 
     def get_urls(self, page=1, site=None, protocol=None):
         urls = super().get_urls(page=page, site=site, protocol=protocol)
-
-        if self.latest_lastmod:
-            self.latest_lastmod = max(
-                self.latest_lastmod, self.MIN_SITEMAP_DATE
-            )
 
         for url in urls:
             url["lastmod"] = _max_none(url["lastmod"], self.MIN_SITEMAP_DATE)
