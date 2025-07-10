431c531e6986a767ba7e45021ae3a727a418703c
Ans <anselmbradford@users.noreply.github.com>
2025-07-10 10:04:37 -0400

    Update scripts/fonts.sh
    Co-authored-by: Richard Dinh <richard.dinh@cfpb.gov>

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -4,6 +4,6 @@ FONT_VARIABLE="source-sans-3-latin-wght-normal.woff2"
 BASE_DIR="./static.in/cfgov-fonts/fonts"
 
 # Add required webfonts locally to docs/fonts/ directory.
-mkdir $BASE_DIR
+mkdir -p $BASE_DIR
 echo "Copying ./node_modules/@fontsource-variable/source-sans-3/files/$FONT_VARIABLE to $BASE_DIR/$FONT_VARIABLE"
 cp ./node_modules/@fontsource-variable/source-sans-3/files/$FONT_VARIABLE $BASE_DIR/$FONT_VARIABLE
